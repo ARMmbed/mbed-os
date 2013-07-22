b172cd866c4ca7bca10d096b4d4b377a1da83ddf
Toyomasa Watarai <toyomasa.watarai@arm.com>
2013-07-22 15:12:42 +0900

    More fix for uARM build to avoid link error

--- libraries/mbed/targets/cmsis/TARGET_NXP/TARGET_LPC11XX/TOOLCHAIN_ARM_MICRO/startup_LPC11xx.s
+++ libraries/mbed/targets/cmsis/TARGET_NXP/TARGET_LPC11XX/TOOLCHAIN_ARM_MICRO/startup_LPC11xx.s
@@ -205,33 +205,10 @@ PIOINT0_IRQHandler
 
                 ENDP
 
-
                 ALIGN
 
-
-; User Initial Stack & Heap
-
-                IF      :DEF:__MICROLIB
-                
-                EXPORT  __initial_sp
-                EXPORT  __heap_base
-                EXPORT  __heap_limit
-                
-                ELSE
-                
-                IMPORT  __use_two_region_memory
-                EXPORT  __user_initial_stackheap
-__user_initial_stackheap
-
-                LDR     R0, =  Heap_Mem
-                LDR     R1, =(Stack_Mem + Stack_Size)
-                LDR     R2, = (Heap_Mem +  Heap_Size)
-                LDR     R3, = Stack_Mem
-                BX      LR
-
-                ALIGN
-
-                ENDIF
-
+; @toyowata removed "User Initial Stack & Heap" block here,
+; since arm.py script doesn't pass -D__MICROLIB definision to armasm.
+; Now, required symbols (__initial_sp etc) were exported in this code.
 
                 END
