4d26e2ef4abf130fda3fb78a4de17736661dc8e8
Emilio Monti <emilmont@gmail.com>
2013-08-15 16:09:12 +0100

    Tidy up default settings

--- workspace_tools/settings.py
+++ workspace_tools/settings.py
@@ -18,38 +18,15 @@ from os.path import join, abspath, dirname
 import logging
 
 ROOT = abspath(join(dirname(__file__), ".."))
-BUILD_DIR = abspath(join(ROOT, "..", "build"))
 
 # These default settings have two purposes:
 #    1) Give a template for writing local "private_settings.py"
 #    2) Give default initialization fields for the "toolchains.py" constructors
 
-SERVER_PORT = 59432
-SERVER_ADDRESS = "10.2.200.94"
-LOCALHOST = "10.2.200.94"
-
-MUTs = {
-    "1" : {"mcu": "LPC1768",
-        "port":"COM41", "disk":'E:\\',
-        "peripherals": ["TMP102", "digital_loop", "port_loop", "analog_loop", "SD"]
-    },
-    "2": {"mcu": "LPC11U24",
-        "port":"COM42", "disk":'F:\\',
-        "peripherals":  ["TMP102", "digital_loop", "port_loop", "SD"]
-    },
-    "3" : {"mcu": "LPC2368",
-        "port":"COM43", "disk":'G:\\',
-        "peripherals":  ["TMP102", "digital_loop", "port_loop", "analog_loop", "SD"]
-    },
-    "4" : {"mcu": "KL25Z",
-        "port":"COM44", "disk":'H:\\',
-        "peripherals":  ["TMP102", "digital_loop", "port_loop", "analog_loop", "SD"]
-    },
-	"5" : {"mcu": "LPC1114",
-		"port":"COM45", "disk":"I:\\",
-		"peripherals":	["TMP102", "digital_loop", "port_loop", "analog_loop", "SD"]
-	}
-}
+##############################################################################
+# Build System Settings
+##############################################################################
+BUILD_DIR = abspath(join(ROOT, "build"))
 
 # ARM
 armcc = "standalone" # "keil", or "standalone", or "ds-5"
@@ -76,8 +53,7 @@ ARM_CPPLIB = join(ARM_LIB, "cpplib")
 MY_ARM_CLIB = join(ARM_PATH, "lib", "microlib")
 
 # GCC ARM
-# GCC_ARM_PATH = "C:/arm-none-eabi-gcc-4_7/bin"
-GCC_ARM_PATH = ""
+GCC_ARM_PATH = "C:/arm-none-eabi-gcc-4_7/bin"
 
 # GCC CodeSourcery
 GCC_CS_PATH = "C:/Program Files (x86)/CodeSourcery/Sourcery_CodeBench_Lite_for_ARM_EABI/bin"
@@ -94,10 +70,34 @@ CW_EWL_PATH = "C:/Freescale/CW MCU v10.3/MCU/ARM_GCC_Support/ewl/lib"
 
 BUILD_OPTIONS = []
 
+##############################################################################
+# Test System Settings
+##############################################################################
+SERVER_PORT = 59432
+SERVER_ADDRESS = "10.2.200.94"
+LOCALHOST = "10.2.200.94"
+
+MUTs = {
+    "1" : {"mcu": "LPC1768",
+        "port":"COM41", "disk":'E:\\',
+        "peripherals": ["TMP102", "digital_loop", "port_loop", "analog_loop", "SD"]
+    },
+    "2": {"mcu": "LPC11U24",
+        "port":"COM42", "disk":'F:\\',
+        "peripherals":  ["TMP102", "digital_loop", "port_loop", "SD"]
+    },
+    "3" : {"mcu": "KL25Z",
+        "port":"COM43", "disk":'G:\\',
+        "peripherals":  ["TMP102", "digital_loop", "port_loop", "analog_loop", "SD"]
+    },
+}
+
+##############################################################################
+# Private Settings
+##############################################################################
 try:
     # Allow to overwrite the default settings without the need to edit the
     # settings file stored in the repository
     from workspace_tools.private_settings import *
-    logging.debug("Loaded user settings")
 except ImportError:
-    logging.debug("[WARNING] Using default settings. Define you settings in the file private_settings.py")
+    print '[WARNING] Using default settings. Define you settings in the file "workspace_tools/private_settings.py"'
