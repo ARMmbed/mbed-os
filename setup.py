e0034f204defa83a3f19d049369e5aab3cbb1bf6
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-10-18 11:10:22 +0100

    Added first version of setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='Mbed',
+      version='0.0.1',
+      description='Package description',
+      long_description='Package long description',
+      author='Author name',
+      author_email='Author email',
+      maintainer='Maintainer name',
+      maintainer_email='Maintainer email',
+      url='Homepage url',
+      download_url='Pacjage download url',
+      license='Apache License string')
