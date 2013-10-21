a4ac8a734add877bfc18f89132dd3a3bd945a94d
Paris Kasidiaris <pariskasidiaris@gmail.com>
2013-10-21 10:27:53 +0100

    Added long description

---
+++ description.rst
@@ -0,0 +1,3 @@
+A set of Python scripts that can be used to compile programs written on top of the `mbed framework`_. It can also be used to export mbed projects to other build systems and IDEs (uVision, IAR, makefiles).
+
+.. _mbed framework: http://mbed.org
--- setup.py
+++ setup.py
@@ -6,15 +6,15 @@ PyPI package for the Mbed SDK
 from distutils.core import setup
 
 LICENSE = open('LICENSE').read()
+DESCRIPTION = open('description.rst')
 
 setup(name='Mbed',
       version='0.0.1',
-      description='Package description',
-      long_description='Package long description',
+      description='Build and test system for mbed',
+      long_description=DESCRIPTION,
       author='Author name',
       author_email='Author email',
       maintainer='Maintainer name',
       maintainer_email='Maintainer email',
       url='https://github.com/mbedmicro/mbed',
-      download_url='Package download url',
       license=LICENSE)
