908c0da4714906e0ac4a98b40c899b1170ead4c2
Matthew Else <matthewelse1997@gmail.com>
2013-10-29 12:29:57 +0000

    (Hopefully) got rid of all of the setup output

--- travis/install_dependencies.sh
+++ travis/install_dependencies.sh
@@ -3,22 +3,22 @@ echo "Installing compilation dependencies."
 # Install GCC-ARM Compiler.
 
 echo "Adding apt repositories."
-sudo add-apt-repository -y ppa:terry.guo/gcc-arm-embedded > /dev/null
-sudo add-apt-repository -y ppa:ubuntu-wine/ppa > /dev/null
+sudo add-apt-repository -y ppa:terry.guo/gcc-arm-embedded
+sudo add-apt-repository -y ppa:ubuntu-wine/ppa
 
 echo "Installing gcc_arm software"
-sudo apt-get update > /dev/null
-sudo apt-get install -y gcc-arm-none-eabi > /dev/null
+sudo apt-get update
+sudo apt-get install -y gcc-arm-none-eabi
 
 echo "Setting up Wine."
-sudo apt-get install -y wine1.5 > /dev/null
+sudo apt-get install -y wine1.5
 
 
 # Download ARMCC (Testing Purposes only at the moment)
 
 echo "Installing ARMCC"
-wget https://dl.dropboxusercontent.com/u/15449666/ARMCC.tar.gz > /dev/null
-tar xvfz ARMCC.tar.gz > /dev/null
+wget https://dl.dropboxusercontent.com/u/15449666/ARMCC.tar.gz
+tar xvfz ARMCC.tar.gz
 
 # Setup ARMCC environment variables
 
