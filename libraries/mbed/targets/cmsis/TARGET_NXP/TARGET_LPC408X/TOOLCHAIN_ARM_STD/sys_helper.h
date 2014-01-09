1448b871c12852c351713e2e62e36d1c18023a6a
Andreas Rebert <andreas.rebert@embeddedartists.com>
2014-01-09 08:52:33 +0100

    [LPC4088] Making it possible to reserve stack space by weak function.

---
+++ libraries/mbed/targets/cmsis/TARGET_NXP/TARGET_LPC408X/TOOLCHAIN_ARM_STD/sys_helper.h
@@ -0,0 +1,17 @@
+#ifndef SYS_HELPER_H
+#define SYS_HELPER_H
+
+#include <stdint.h>
+#include "toolchain.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+uint32_t __reserved_stack_size();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
