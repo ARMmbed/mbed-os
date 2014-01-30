ca173a1867f40bb54b5bc93c9f81065cc9b90936
bcostm <bco.stm@gmail.com>
2014-01-30 15:29:07 +0100

    [NUCLEO_F030R8] Add SPI api, corrections in I2C

--- libraries/mbed/targets/hal/TARGET_STM/TARGET_NUCLEO_F030R8/device.h
+++ libraries/mbed/targets/hal/TARGET_STM/TARGET_NUCLEO_F030R8/device.h
@@ -44,7 +44,7 @@
 #define DEVICE_I2C              1
 #define DEVICE_I2CSLAVE         0
 
-#define DEVICE_SPI              0
+#define DEVICE_SPI              1
 #define DEVICE_SPISLAVE         0
 
 #define DEVICE_RTC              1
