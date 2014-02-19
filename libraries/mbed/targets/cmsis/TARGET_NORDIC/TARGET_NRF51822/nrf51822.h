befaefcdb8af8a5365b47af5a793ce9157687043
tkuyucu <tuze.kuyucu@nordicsemi.no>
2014-02-19 12:43:56 +0100

    Updated copyright headers
    commented out unused variable in cmsis_nvic.c

--- libraries/mbed/targets/cmsis/TARGET_NORDIC/TARGET_NRF51822/nrf51822.h
+++ libraries/mbed/targets/cmsis/TARGET_NORDIC/TARGET_NRF51822/nrf51822.h
@@ -1,6 +1,6 @@
 /* mbed Microcontroller Library
 
- * Copyright (c) 2006-2013 ARM Limited
+ * Copyright (c) 2013 Nordic Semiconductor.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
