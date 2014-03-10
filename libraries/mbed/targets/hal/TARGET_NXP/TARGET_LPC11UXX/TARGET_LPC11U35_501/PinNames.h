f34be808744ffa690544bdc2c4354133a5565ffc
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-03-10 13:15:40 +0000

    Fix compilation errors from GPIO pull request

--- libraries/mbed/targets/hal/TARGET_NXP/TARGET_LPC11UXX/TARGET_LPC11U35_501/PinNames.h
+++ libraries/mbed/targets/hal/TARGET_NXP/TARGET_LPC11UXX/TARGET_LPC11U35_501/PinNames.h
@@ -165,7 +165,8 @@ typedef enum {
     PullDown = 1,
     PullNone = 0,
     Repeater = 3,
-    OpenDrain = 4
+    OpenDrain = 4,
+    PullDefault = PullDown
 } PinMode;
 
 #ifdef __cplusplus
--- libraries/mbed/targets/hal/TARGET_NXP/TARGET_LPC176X/TARGET_UBLOX_C027/platform_init.c
+++ libraries/mbed/targets/hal/TARGET_NXP/TARGET_LPC176X/TARGET_UBLOX_C027/platform_init.c
@@ -9,18 +9,18 @@ void mbed_sdk_init()
     gpio_t gpsEn, gpsRst, led, modemRts;
     
     // start with modem disabled 
-    gpio_init_out_ex(&modemEn,     MDMEN,     PullNone, 0);
-    gpio_init_out_ex(&modemRst,    MDMRST,    PullNone, 1);
-    gpio_init_out_ex(&modemPwrOn,  MDMPWRON,  PullNone, 1);
-    gpio_init_out_ex(&modemLvlOe,  MDMLVLOE,  PullNone, 1);
-    gpio_init_out_ex(&modemILvlOe, MDMILVLOE, PullNone, 0);
-    gpio_init_out_ex(&modemUsbDet, MDMUSBDET, PullNone, 0);
-    gpio_init_out_ex(&modemRts,    MDMRTS,    PullNone, 0);
+    gpio_init_out_ex(&modemEn,     MDMEN,     0);
+    gpio_init_out_ex(&modemRst,    MDMRST,    1);
+    gpio_init_out_ex(&modemPwrOn,  MDMPWRON,  1);
+    gpio_init_out_ex(&modemLvlOe,  MDMLVLOE,  1);
+    gpio_init_out_ex(&modemILvlOe, MDMILVLOE, 0);
+    gpio_init_out_ex(&modemUsbDet, MDMUSBDET, 0);
+    gpio_init_out_ex(&modemRts,    MDMRTS,    0);
     // start with gps disabled 
-    gpio_init_out_ex(&gpsEn,       GPSEN,     PullNone, 0);
-    gpio_init_out_ex(&gpsRst,      GPSRST,    PullNone, 1);
+    gpio_init_out_ex(&gpsEn,       GPSEN,     0);
+    gpio_init_out_ex(&gpsRst,      GPSRST,    1);
     // led should be off
-    gpio_init_out_ex(&led,         LED,       PullNone, 0);
+    gpio_init_out_ex(&led,         LED,       0);
     
     wait_ms(50); // when USB cable is inserted the interface chip issues 
     // multiple resets to the target CPU We wait here for a short period to 
