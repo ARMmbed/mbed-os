c2cd398d6a8e8dfe27daddc5642f9370f95f8355
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-03-25 17:09:17 +0000

    Added MBED_15 RPC test to automated test suite

--- workspace_tools/tests.py
+++ workspace_tools/tests.py
@@ -346,7 +346,7 @@ TESTS = [
     {
         "id": "MBED_15", "description": "RPC",
         "source_dir": join(TEST_DIR, "mbed", "rpc"),
-        "dependencies": [MBED_LIBRARIES, join(LIB_DIR, "rpc")],
+        "dependencies": [MBED_LIBRARIES, join(LIB_DIR, "rpc"), TEST_MBED_LIB],
         "host_test": "rpc",
     },
     {
