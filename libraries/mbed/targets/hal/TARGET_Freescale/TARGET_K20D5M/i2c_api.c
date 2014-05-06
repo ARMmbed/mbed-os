04fa77f90972e7194ac71c4faba9d8a378a4cdef
mazgch <mazgch@gmail.com>
2014-05-06 09:13:58 +0200

    increase timeout for slow slaves that use clock extensive stretching

--- libraries/mbed/targets/hal/TARGET_Freescale/TARGET_K20D5M/i2c_api.c
+++ libraries/mbed/targets/hal/TARGET_Freescale/TARGET_K20D5M/i2c_api.c
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.00
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -103,7 +103,7 @@ int i2c_stop(i2c_t *obj) {
 }
 
 static int timeout_status_poll(i2c_t *obj, uint32_t mask) {
-    uint32_t i, timeout = 1000;
+    uint32_t i, timeout = 100000;
 
     for (i = 0; i < timeout; i++) {
         if (obj->i2c->S & mask)
