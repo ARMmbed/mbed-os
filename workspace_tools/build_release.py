9b366ee8010e2eb71daa976d9c03449eb616ceeb
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-05-07 22:12:23 +0100

    Added verbose option to build_release.py

--- workspace_tools/build_release.py
+++ workspace_tools/build_release.py
@@ -59,6 +59,8 @@ if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option('-o', '--official', dest="official_only", default=False, action="store_true",
                       help="Build using only the official toolchain for each target")
+    parser.add_option("-v", "--verbose", action="store_true", dest="verbose",
+                      default=False, help="Verbose diagnostic output")
     options, args = parser.parse_args()
     start = time()
     failures = []
@@ -71,7 +73,7 @@ if __name__ == '__main__':
         for toolchain in toolchains:
             id = "%s::%s" % (target_name, toolchain)
             try:
-                build_mbed_libs(TARGET_MAP[target_name], toolchain)
+                build_mbed_libs(TARGET_MAP[target_name], toolchain, verbose=options.verbose)
                 successes.append(id)
             except Exception, e:
                 failures.append(id)
