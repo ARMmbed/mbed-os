61a72711eba38ebe59552dc29e5f21943e612726
Rohit Grover <rohit.grover@arm.com>
2014-05-20 14:41:03 +0100

    Removed BLE related code to github
    As part of transitioning to mbed.org, the BLE code is now only available
    on mbed.org:
    
    http://mbed.org/teams/Bluetooth-Low-Energy/code/BLE_API/
    http://mbed.org/teams/Nordic-Semiconductor/code/nRF51822/

--- workspace_tools/synch.py
+++ workspace_tools/synch.py
@@ -67,9 +67,6 @@ OFFICIAL_CODE = (
     ("UbloxUSBModem", "net/cellular/UbloxUSBModem"),
     ("UbloxModemHTTPClientTest", ["tests/net/cellular/http/common", "tests/net/cellular/http/ubloxusb"]),
     ("UbloxModemSMSTest", ["tests/net/cellular/sms/common", "tests/net/cellular/sms/ubloxusb"]),
-
-    ("BLE_API", "ble/ble-api", "Bluetooth-Low-Energy"),
-    ("nRF51822", "ble/nRF51822", "Nordic-Semiconductor"),
 )
 
 
