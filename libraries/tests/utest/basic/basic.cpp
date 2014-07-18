f7ab12865e0237324c698e653c363ccf828f6bd9
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-07-18 10:20:34 +0100

    Added simple checks to make sure assertions outside test runs don't crash

--- libraries/tests/utest/basic/basic.cpp
+++ libraries/tests/utest/basic/basic.cpp
@@ -6,5 +6,7 @@ TEST_GROUP(FirstTestGroup)
 
 TEST(FirstTestGroup, FirstTest)
 {
+    /* These checks are here to make sure assertions outside test runs don't crash */
     CHECK(true);
+    LONGS_EQUAL(1, 1);
 }
