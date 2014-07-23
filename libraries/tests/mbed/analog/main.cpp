1543ff177b78c793d819ac7b0c9a57f1ac1511d3
Andreas Løve Selvik <andreas.selvik@silabs.com>
2014-07-23 11:27:34 +0200

    [EFM32] Make analog test compatible with GG, TG, G and WG

--- libraries/tests/mbed/analog/main.cpp
+++ libraries/tests/mbed/analog/main.cpp
@@ -16,7 +16,11 @@ AnalogOut out(PTB1); // D1
 AnalogIn in(PTB0);
 AnalogOut out(PTE30);
 
-#elif defined(TARGET_EFM32LG_STK3600)
+#elif defined(TARGET_EFM32LG_STK3600) \
+    ||defined(TARGET_EFM32GG_STK3700) \
+    ||defined(TARGET_EFM32TG_STK3300) \
+    ||defined(TARGET_EFM32_G8XX_STK) \
+    ||defined(TARGET_EFM32WG_STK3800)
 AnalogIn in(PD5);
 AnalogOut out(PB12);
 
