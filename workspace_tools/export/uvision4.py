9ad78393783d3c753366996e4b992ef8fb91e1fe
Aksel Skauge Mellbye <Aksel.Mellbye@silabs.com>
2014-07-24 11:48:25 +0200

    [EFM32WG] Add uvision export.

--- workspace_tools/export/uvision4.py
+++ workspace_tools/export/uvision4.py
@@ -28,6 +28,7 @@ class Uvision4(Exporter):
         'EFM32GG_STK3700',
         'EFM32LG_STK3600',
         'EFM32TG_STK3300',
+        'EFM32WG_STK3800',
         'EFM32ZG_STK3200',
         'KL05Z',
         'KL25Z',
