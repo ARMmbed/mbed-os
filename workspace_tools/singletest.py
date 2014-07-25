4c10fbb660aace75d5edcb69032bf9659c6d31cf
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-07-25 17:37:16 +0100

    Changed copyright date to 2014

--- workspace_tools/singletest.py
+++ workspace_tools/singletest.py
@@ -2,7 +2,7 @@
 
 """
 mbed SDK
-Copyright (c) 2011-2013 ARM Limited
+Copyright (c) 2011-2014 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
