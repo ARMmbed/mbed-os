755d0174d628e0225982408f399243e53376f329
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-08-07 10:36:38 +0100

    Removed repeated lines from Copyright disclaimer/preamble

--- workspace_tools/build_api.py
+++ workspace_tools/build_api.py
@@ -5,7 +5,6 @@ Copyright (c) 2011-2013 ARM Limited
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
