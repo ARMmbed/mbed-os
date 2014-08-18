0cd6ca6600a49f51948350ebe0b259def68fba1e
Yihui Xiong <xiongyihui3@gmail.com>
2014-08-18 11:12:18 +0800

    add missed port

--- libraries/mbed/targets/hal/TARGET_STM/TARGET_DISCO_F407VG/pinmap.c
+++ libraries/mbed/targets/hal/TARGET_STM/TARGET_DISCO_F407VG/pinmap.c
@@ -68,9 +68,25 @@ uint32_t Set_GPIO_Clock(uint32_t port_idx) {
             gpio_add = GPIOD_BASE;
             __GPIOD_CLK_ENABLE();
             break;
+        case PortE:
+            gpio_add = GPIOE_BASE;
+            __GPIOE_CLK_ENABLE();
+            break;
+        case PortF:
+            gpio_add = GPIOF_BASE;
+            __GPIOF_CLK_ENABLE();
+            break;
+        case PortG:
+            gpio_add = GPIOG_BASE;
+            __GPIOG_CLK_ENABLE();
+            break;
         case PortH:
             gpio_add = GPIOH_BASE;
             __GPIOH_CLK_ENABLE();
+            break;
+        case PortI:
+            gpio_add = GPIOI_BASE;
+            __GPIOI_CLK_ENABLE();
             break;
         default:
             error("Pinmap error: wrong port number.");
