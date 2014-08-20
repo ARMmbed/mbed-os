13c3a64e3f3f65a6a074a92bdbb4d2e9342ba8dc
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-08-20 11:45:31 +0100

    Fixed CPPUTEST test runner for other compilers. Now we will force to pass main(argc, argv) ourselves to CPPUTEST test suite - as should be done in embedded project

--- libraries/tests/utest/basic/basic.cpp
+++ libraries/tests/utest/basic/basic.cpp
@@ -9,4 +9,5 @@ TEST(FirstTestGroup, FirstTest)
     /* These checks are here to make sure assertions outside test runs don't crash */
     CHECK(true);
     LONGS_EQUAL(1, 1);
+    STRCMP_EQUAL("Mbed SDK!", "Mbed SDK!");
 }
