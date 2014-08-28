fee50a80ffdc8f9d9ee97be545ff36b5a986f041
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-08-28 13:14:38 +0100

    Fixed case for FATFileSystem

--- workspace_tools/synch.py
+++ workspace_tools/synch.py
@@ -67,7 +67,7 @@ OFFICIAL_CODE = (
     ("UbloxUSBModem", "net/cellular/UbloxUSBModem"),
     ("UbloxModemHTTPClientTest", ["tests/net/cellular/http/common", "tests/net/cellular/http/ubloxusb"]),
     ("UbloxModemSMSTest", ["tests/net/cellular/sms/common", "tests/net/cellular/sms/ubloxusb"]),
-    ("FatFileSystem", "fs/fat", "mbed-official"),
+    ("FATFileSystem", "fs/fat", "mbed-official"),
 )
 
 
