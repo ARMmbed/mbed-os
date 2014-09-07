654929803b399ffb58c9d8cc334da3958566cf8a
Sissors <erik_olieman@hotmail.com>
2014-09-07 21:39:53 +0200

    And unbreaking MMA test code

--- libraries/tests/mbed/i2c_mma7660/main.cpp
+++ libraries/tests/mbed/i2c_mma7660/main.cpp
@@ -2,13 +2,9 @@
 #include "test_env.h"
 #include "MMA7660.h"
 
-//MMA7660 MMA(p28, p27);
-I2C i2c(PTB3, PTB2);
+MMA7660 MMA(p28, p27);
 
 int main() {
-for (int i = 0; i<128; i++)
-    printf("Write to %d = %d\r\n", i, i2c.write(i * 2, NULL, 0));
-/*
     if (!MMA.testConnection())
         notify_completion(false);
 
@@ -16,6 +12,6 @@ for (int i = 0; i<128; i++)
         printf("x: %f, y: %f, z: %f\r\n", MMA.x(), MMA.y(), MMA.z());
         wait(0.2);
     }
-*/
+
     notify_completion(true);
 }
