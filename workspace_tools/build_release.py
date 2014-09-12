305b62863944f9df403e9c01916631dc772c532b
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-09-12 10:50:46 +0100

    Fixed syntax error in build_release.py

--- workspace_tools/build_release.py
+++ workspace_tools/build_release.py
@@ -59,7 +59,7 @@ OFFICIAL_MBED_LIBRARY_BUILD = (
     ('NUCLEO_L053R8', ('ARM', 'uARM')),
     ('NUCLEO_L152RE', ('ARM', 'uARM')),
 
-    {'ARCH_MAX', ('ARM', 'GCC_ARM')),
+    ('ARCH_MAX', ('ARM', 'GCC_ARM')),
 
     ('NRF51822',     ('ARM', 'GCC_ARM')),
     ('HRM1017',      ('ARM', 'GCC_ARM')),
