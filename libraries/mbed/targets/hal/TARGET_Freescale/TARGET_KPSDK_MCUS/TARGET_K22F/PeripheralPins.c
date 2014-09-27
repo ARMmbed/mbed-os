cc3456f254d24887540e7acc8bb90195b3a2a52a
Sissors <erik_olieman@hotmail.com>
2014-09-27 10:57:36 +0200

    [HAL][K22F] Added final UART pinnames
    See title

--- libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_K22F/PeripheralPins.c
+++ libraries/mbed/targets/hal/TARGET_Freescale/TARGET_KPSDK_MCUS/TARGET_K22F/PeripheralPins.c
@@ -76,17 +76,25 @@ const PinMap PinMap_I2C_SCL[] = {
 
 /************UART***************/
 const PinMap PinMap_UART_TX[] = {
-    {PTA2, UART_0, 2},
-    {PTE0, UART_1, 3},
-    {PTD3, UART_2, 3},
-    {NC  ,  NC    , 0}
+    {PTA2 , UART_0, 2},
+    {PTA14, UART_0, 3},
+    {PTB17, UART_0, 3},
+    {PTD7 , UART_0, 3},
+    {PTC4 , UART_1, 3},
+    {PTE0 , UART_1, 3},
+    {PTD3 , UART_2, 3},
+    {NC   ,  NC   , 0}
 };
 
 const PinMap PinMap_UART_RX[] = {
-    {PTA1, UART_0, 2},
-    {PTE1, UART_1, 3},
-    {PTD2, UART_2, 3},
-    {NC  ,  NC    , 0}
+    {PTA1 , UART_0, 2},
+    {PTA15, UART_0, 3},
+    {PTB16, UART_0, 3},
+    {PTD6 , UART_0, 3},
+    {PTC3 , UART_1, 3},
+    {PTE1 , UART_1, 3},
+    {PTD2 , UART_2, 3},
+    {NC   ,  NC    , 0}
 };
 
 /************SPI***************/
