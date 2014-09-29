eab929a023c1b49ef2dbe1081aa0c752504c5236
bcostm <bco.stm@gmail.com>
2014-09-29 10:56:06 +0200

    Remove duplicated entry

--- workspace_tools/tests.py
+++ workspace_tools/tests.py
@@ -130,7 +130,7 @@ TESTS = [
         "peripherals": ["analog_loop"],
         "mcu": ["LPC1768", "LPC2368", "KL25Z", "K64F", "K22F", "LPC4088", "LPC1549",
                 "NUCLEO_F072RB", "NUCLEO_F091RC", "NUCLEO_F302R8", "NUCLEO_F303RE",
-                "NUCLEO_F334R8", "NUCLEO_L053R8", "NUCLEO_L152RE", "NUCLEO_F334R8"]
+                "NUCLEO_F334R8", "NUCLEO_L053R8", "NUCLEO_L152RE"]
     },
     {
         "id": "MBED_A9", "description": "Serial Echo at 115200",
