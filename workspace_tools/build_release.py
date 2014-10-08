cc18319a4bbb5d6082d702af61263441ad32ca0a
Paul Cox <pauldanielcox@gmail.com>
2014-10-08 16:24:37 +0200

    Adding GCC_ARM build for NUCLEO_F401RE
    The GCC_ARM build for NUCLEO_F401RE works fine but was not being
    included in the release. This commit adds that build combination.

--- workspace_tools/build_release.py
+++ workspace_tools/build_release.py
@@ -56,7 +56,7 @@ OFFICIAL_MBED_LIBRARY_BUILD = (
     ('NUCLEO_F103RB', ('ARM', 'uARM')),
     ('NUCLEO_F302R8', ('ARM', 'uARM')),
     ('NUCLEO_F334R8', ('ARM', 'uARM')),
-    ('NUCLEO_F401RE', ('ARM', 'uARM')),
+    ('NUCLEO_F401RE', ('ARM', 'uARM', 'GCC_ARM')),
     ('NUCLEO_F411RE', ('ARM', 'uARM')),
     ('NUCLEO_L053R8', ('ARM', 'uARM')),
     ('NUCLEO_L152RE', ('ARM', 'uARM')),
