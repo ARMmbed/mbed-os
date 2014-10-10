3b87e28eb6dd4178d5ab29ee4b481cfa91428955
0xc0170 <c0170@rocketmail.com>
2014-10-10 10:40:23 +0200

    Export: IAR - sources in the project file, 3 targets ewp templates

--- workspace_tools/export/iar.py
+++ workspace_tools/export/iar.py
@@ -28,13 +28,20 @@ class IAREmbeddedWorkbench(Exporter):
     ]
 
     def generate(self):
+
+        sources = []
+        sources += self.resources.c_sources
+        sources += self.resources.cpp_sources
+        sources += self.resources.s_sources
+
         ctx = {
             'name': self.program_name,
             'include_paths': self.resources.inc_dirs,
             'linker_script': self.resources.linker_script,
             'object_files': self.resources.objects,
             'libraries': self.resources.libraries,
-            'symbols': self.toolchain.get_symbols()
+            'symbols': self.toolchain.get_symbols(),
+            'source_files': sources,
         }
-        self.gen_file('iar.ewp.tmpl', ctx, '%s.ewp' % self.program_name)
+        self.gen_file('iar_%s.ewp.tmpl' % self.target.lower(), ctx, '%s.ewp' % self.program_name)
         self.gen_file('iar.eww.tmpl', ctx, '%s.eww' % self.program_name)
