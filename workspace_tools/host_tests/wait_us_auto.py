755565d8543b527c258ccf0e85a8d5620df8485d
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-10-17 15:14:53 +0100

    Host test refactoring
    Host test refactoring: replaced magic values of test results returned by host test
    
    Prints with stdout.flush() replaced with notify function
    
    Host test refactoring: created separate enum like class for test result strings

--- workspace_tools/host_tests/wait_us_auto.py
+++ workspace_tools/host_tests/wait_us_auto.py
@@ -24,28 +24,28 @@ class WaitusTest(DefaultTest):
         test_result = True
         # First character to start test (to know after reset when test starts)
         if self.mbed.serial_timeout(None) is None:
-            self.print_result("ioerr_serial")
+            self.print_result(self.RESULT_IO_SERIAL)
             return
         c = self.mbed.serial_read(1)
         if c is None:
-            self.print_result("ioerr_serial")
+            self.print_result(self.RESULT_IO_SERIAL)
             return
         if c == '$': # target will printout TargetID e.g.: $$$$1040e649d5c09a09a3f6bc568adef61375c6
             #Read additional 39 bytes of TargetID
             if self.mbed.serial_read(39) is None:
-                self.print_result("ioerr_serial")
+                self.print_result(self.RESULT_IO_SERIAL)
                 return
             c = self.mbed.serial_read(1) # Re-read first 'tick'
             if c is None:
-                self.print_result("ioerr_serial")
+                self.print_result(self.RESULT_IO_SERIAL)
                 return
-        print "Test started"
+        self.notify("Test started")
         start_serial_pool = time()
         start = time()
         for i in range(0, 10):
             c = self.mbed.serial_read(1)
             if c is None:
-                self.print_result("ioerr_serial")
+                self.print_result(self.RESULT_IO_SERIAL)
                 return
             if i > 2: # we will ignore first few measurements
                 delta = time() - start
@@ -57,18 +57,15 @@ class WaitusTest(DefaultTest):
                 deviation_ok = True if delta > 0 and deviation <= 0.10 else False # +/-10%
                 test_result = test_result and deviation_ok
                 msg = "OK" if deviation_ok else "FAIL"
-                print ". in %.2f sec (%.2f) [%s]" % (delta, deviation, msg)
+                self.notify(". in %.2f sec (%.2f) [%s]" % (delta, deviation, msg))
             else:
-                print ". skipped"
+                self.notify(". skipped")
             stdout.flush()
             start = time()
         measurement_time = time() - start_serial_pool
-        print "Completed in %.2f sec" % (measurement_time)
+        self.notify("Completed in %.2f sec" % (measurement_time))
+        self.print_result(self.RESULT_SUCCESS if test_result else self.RESULT_FAILURE)
 
-        if test_result: # All numbers are the same
-            self.print_result('success')
-        else:
-            self.print_result('failure')
-
+        
 if __name__ == '__main__':
     WaitusTest().run()
