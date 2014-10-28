7c2bab54d59d1828d5c810b5010192c80d3c996c
Sissors <erik_olieman@hotmail.com>
2014-10-28 21:04:48 +0100

    [USBDevice] Revert changes which add count argument to USBMSD
    See: https://github.com/mbedmicro/mbed/issues/506

--- libraries/USBDevice/USBMSD/USBMSD.cpp
+++ libraries/USBDevice/USBMSD/USBMSD.cpp
@@ -232,7 +232,7 @@ void USBMSD::memoryWrite (uint8_t * buf, uint16_t size) {
     // if the array is filled, write it in memory
     if (!((addr + size)%BlockSize)) {
         if (!(disk_status() & WRITE_PROTECT)) {
-            disk_write(page, addr/BlockSize, 1);
+            disk_write(page, addr/BlockSize);
         }
     }
 
@@ -257,7 +257,7 @@ void USBMSD::memoryVerify (uint8_t * buf, uint16_t size) {
 
     // beginning of a new block -> load a whole block in RAM
     if (!(addr%BlockSize))
-        disk_read(page, addr/BlockSize, 1);
+        disk_read(page, addr/BlockSize);
 
     // info are in RAM -> no need to re-read memory
     for (n = 0; n < size; n++) {
@@ -505,7 +505,7 @@ void USBMSD::memoryRead (void) {
 
     // we read an entire block
     if (!(addr%BlockSize))
-        disk_read(page, addr/BlockSize, 1);
+        disk_read(page, addr/BlockSize);
 
     // write data which are in RAM
     writeNB(EPBULK_IN, &page[addr%BlockSize], n, MAX_PACKET_SIZE_EPBULK);
