f560a877b0098eee0d209a82a450f7075035338d
Neil Thiessen <neilt66@icloud.com>
2014-11-08 22:18:31 -0700

    Update RTX_Conf_CM.c
    RTOS: RTX - Idle task weak symbol
    
    Changed os_idle_demon() to a weak symbol so that it can be overridden in the main program.

--- libraries/rtos/rtx/TARGET_CORTEX_M/RTX_Conf_CM.c
+++ libraries/rtos/rtx/TARGET_CORTEX_M/RTX_Conf_CM.c
@@ -33,7 +33,7 @@
  *---------------------------------------------------------------------------*/
 
 #include "cmsis_os.h"
-
+#include "toolchain.h"
 
 /*----------------------------------------------------------------------------
  *      RTX User configuration part BEGIN
@@ -247,7 +247,7 @@
 /*----------------------------------------------------------------------------
  *      OS Idle daemon
  *---------------------------------------------------------------------------*/
-void os_idle_demon (void) {
+WEAK void os_idle_demon (void) {
   /* The idle demon is a system thread, running when no other thread is      */
   /* ready to run.                                                           */
 
