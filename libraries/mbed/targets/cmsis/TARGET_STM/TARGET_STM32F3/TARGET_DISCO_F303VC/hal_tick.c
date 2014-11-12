691f34097a5237959329c66355946a6ac2340ff8
Olaf Hagendorf <olaf.hagendorf@hs-wismar.de>
2014-11-12 14:31:57 +0100

    [TARGET_DISCO_F303] reorder structure part4
    Remove change of wrong target and add it to the wright one. Additionally
    changing the CoIDE template file to the correct mcu.

--- libraries/mbed/targets/cmsis/TARGET_STM/TARGET_STM32F3/TARGET_DISCO_F303VC/hal_tick.c
+++ libraries/mbed/targets/cmsis/TARGET_STM/TARGET_STM32F3/TARGET_DISCO_F303VC/hal_tick.c
@@ -71,6 +71,9 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority) {
     // Reset timer
     TIM_MST_RESET_ON;
     TIM_MST_RESET_OFF;
+
+    // Update the SystemCoreClock variable
+    SystemCoreClockUpdate();
   
     // Configure time base
     TimMasterHandle.Instance = TIM_MST;
