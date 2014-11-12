857d59a6c0ad80140fa1a56fd4e527270902ea2c
GustavWi <Gustav.Wiklander@iar.com>
2014-11-12 13:31:30 +0100

    Added IAR export support for NUCLEO F103RB

--- workspace_tools/export/iar.py
+++ workspace_tools/export/iar.py
@@ -35,6 +35,7 @@ class IAREmbeddedWorkbench(Exporter):
         'NUCLEO_F030R8',
         'NUCLEO_F072RB',
         'NUCLEO_F091RC',
+        'NUCLEO_F103RB',        
         'NUCLEO_F302R8',
         'NUCLEO_F334R8',
         'NUCLEO_F401RE',
