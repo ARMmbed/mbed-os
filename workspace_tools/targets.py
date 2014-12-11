c08208fe4fa934ffc317afb47a09b7844326936a
Masao Hamanaka <masao.hamanaka.zn@renesas.com>
2014-12-11 19:26:03 +0900

    Update upstream pushed files

--- workspace_tools/targets.py
+++ workspace_tools/targets.py
@@ -136,6 +136,14 @@ class LPC11U35_501(LPCTarget):
         self.supported_toolchains = ["ARM", "uARM", "GCC_ARM", "GCC_CR" , "IAR"]
         self.default_toolchain = "uARM"
 
+class LPC11U35_Y5_MBUG(LPCTarget):
+    def __init__(self):
+        LPCTarget.__init__(self)
+        self.core = "Cortex-M0"
+        self.extra_labels = ['NXP', 'LPC11UXX', 'MCU_LPC11U35_501']
+        self.supported_toolchains = ["ARM", "uARM", "GCC_ARM", "GCC_CR" , "IAR"]
+        self.default_toolchain = "uARM"
+
 class LPC11U37_501(LPCTarget):
     def __init__(self):
         LPCTarget.__init__(self)
@@ -742,6 +750,12 @@ class RBLAB_BLENANO(NRF51822):
         self.extra_labels = ['NORDIC', 'MCU_NRF51822', 'MCU_NORDIC_16K']
         self.macros = ['TARGET_NRF51822']
 
+class NRF51822_Y5_MBUG(NRF51822):
+    def __init__(self):
+        NRF51822.__init__(self)
+        self.extra_labels = ['NORDIC', 'MCU_NRF51822', 'MCU_NORDIC_16K']
+        self.macros = ['TARGET_NRF51822']
+
 class XADOW_M0(LPCTarget):
     def __init__(self):
         LPCTarget.__init__(self)
@@ -794,6 +808,7 @@ TARGETS = [
     LPC11U24_301(),
     LPC11U35_401(),
     LPC11U35_501(),
+    LPC11U35_Y5_MBUG(),
     LPC11U37_501(),
     LPCCAPPUCCINO(),# LPC11U37_501
     ARCH_GPRS(),    # LPC11U37_501
@@ -859,6 +874,7 @@ TARGETS = [
     HRM1017(),      # nRF51822
     RBLAB_NRF51822(),# nRF51822
     RBLAB_BLENANO(),# nRF51822
+	NRF51822_Y5_MBUG(),#nRF51822
     XADOW_M0(),     # nRF51822
     WALLBOT_BLE(),  # nRF51822
 
