743bf2b255f68a76e4b39f80e1fafeba1e9a0318
Mike Fiore <mfiore@multitech.com>
2014-12-11 13:48:06 -0600

    [mbed][MTS_DRAGONFLY_F411RE MTS_MDOT_F411RE NUCLEO_F411RE] remove unnecessary Dragonfly hal files & move PeripheralNames.h to mDot and Nucleo dirs to perserve differences from Dragonfly

--- libraries/mbed/targets/hal/TARGET_STM/TARGET_STM32F411RE/TARGET_NUCLEO_F411RE/PeripheralNames.h
+++ libraries/mbed/targets/hal/TARGET_STM/TARGET_STM32F411RE/TARGET_NUCLEO_F411RE/PeripheralNames.h
@@ -27,22 +27,56 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************
  */
-#ifndef MBED_PORTNAMES_H
-#define MBED_PORTNAMES_H
+#ifndef MBED_PERIPHERALNAMES_H
+#define MBED_PERIPHERALNAMES_H
+
+#include "cmsis.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
-    PortA = 0,
-    PortB = 1,
-    PortC = 2,
-    PortD = 3,
-    PortH = 7
-} PortName;
+    ADC_1 = (int)ADC1_BASE
+} ADCName;
+
+typedef enum {
+    UART_1 = (int)USART1_BASE,
+    UART_2 = (int)USART2_BASE,
+    UART_6 = (int)USART6_BASE
+} UARTName;
+
+#define STDIO_UART_TX  PA_2
+#define STDIO_UART_RX  PA_3
+#define STDIO_UART     UART_2
+
+typedef enum {
+    SPI_1 = (int)SPI1_BASE,
+    SPI_2 = (int)SPI2_BASE,
+    SPI_3 = (int)SPI3_BASE,
+    SPI_4 = (int)SPI4_BASE,
+    SPI_5 = (int)SPI5_BASE
+} SPIName;
+
+typedef enum {
+    I2C_1 = (int)I2C1_BASE,
+    I2C_2 = (int)I2C2_BASE,
+    I2C_3 = (int)I2C3_BASE
+} I2CName;
+
+typedef enum {
+    PWM_1  = (int)TIM1_BASE,
+    PWM_2  = (int)TIM2_BASE,
+    PWM_3  = (int)TIM3_BASE,
+    PWM_4  = (int)TIM4_BASE,
+    PWM_5  = (int)TIM5_BASE,
+    PWM_9  = (int)TIM9_BASE,
+    PWM_10 = (int)TIM10_BASE,
+    PWM_11 = (int)TIM11_BASE
+} PWMName;
 
 #ifdef __cplusplus
 }
 #endif
+
 #endif
