d9eb8b71d5def1a889ee685e24fccced83d4075f
bcostm <bco.stm@gmail.com>
2014-12-18 09:30:19 +0100

    [NUCLEO_F070RB]Add new target - part 3 workspace_tools

--- workspace_tools/targets.py
+++ workspace_tools/targets.py
@@ -431,6 +431,16 @@ class NUCLEO_F030R8(Target):
         self.supported_form_factors = ["ARDUINO", "MORPHO"]
         self.detect_code = ["0725"]
 
+class NUCLEO_F070RB(Target):
+    def __init__(self):
+        Target.__init__(self)
+        self.core = "Cortex-M0"
+        self.extra_labels = ['STM', 'STM32F0', 'STM32F070RB']
+        self.supported_toolchains = ["ARM", "uARM", "IAR", "GCC_ARM"]
+        self.default_toolchain = "uARM"
+        self.supported_form_factors = ["ARDUINO", "MORPHO"]
+        self.detect_code = ["0755"]
+
 class NUCLEO_F072RB(Target):
     def __init__(self):
         Target.__init__(self)
@@ -841,6 +851,7 @@ TARGETS = [
 
     ### STMicro ###
     NUCLEO_F030R8(),
+    NUCLEO_F070RB(),
     NUCLEO_F072RB(),
     NUCLEO_F091RC(),
     NUCLEO_F103RB(),
