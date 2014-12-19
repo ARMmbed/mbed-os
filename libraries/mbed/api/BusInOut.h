7720989e2096a9d70476c0fcc0ff710b13637e5e
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2014-12-19 14:45:32 +0000

    Added operator[] and mask() function to BusInOut
    This change follows changes in BUsIn and BusOUt API

--- libraries/mbed/api/BusInOut.h
+++ libraries/mbed/api/BusInOut.h
@@ -73,6 +73,18 @@ public:
      */
     void mode(PinMode pull);
 
+    /** Binary mask of bus pins connected to actual pins (not NC pins)
+     *  If bus pin is in NC state make corresponding bit will be cleared (set to 0), else bit will be set to 1
+     *
+     *  @returns
+     *    Binary mask of connected pins
+     */
+    int mask() {
+        return _nc_mask;
+    }
+
+    static DigitalInOut dinout_dummy;
+
 #ifdef MBED_OPERATORS
      /** A shorthand for write()
      */
@@ -87,10 +99,15 @@ public:
 protected:
     DigitalInOut* _pin[16];
 
+    /** Mask of NC pins, if bit [n] bit is set to 1, [n] pin in bus is in NC state
+     */
+    int _nc_mask;
+
     /* disallow copy constructor and assignment operators */
 private:
     BusInOut(const BusInOut&);
     BusInOut & operator = (const BusInOut&);
+    DigitalInOut& operator[] (unsigned int index);
 };
 
 } // namespace mbed
--- libraries/mbed/common/BusInOut.cpp
+++ libraries/mbed/common/BusInOut.cpp
@@ -17,17 +17,27 @@
 
 namespace mbed {
 
+DigitalInOut BusInOut::dinout_dummy(NC);
+
 BusInOut::BusInOut(PinName p0, PinName p1, PinName p2, PinName p3, PinName p4, PinName p5, PinName p6, PinName p7, PinName p8, PinName p9, PinName p10, PinName p11, PinName p12, PinName p13, PinName p14, PinName p15) {
     PinName pins[16] = {p0, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15};
 
+    _nc_mask = 0;
     for (int i=0; i<16; i++) {
         _pin[i] = (pins[i] != NC) ? new DigitalInOut(pins[i]) : 0;
+        if (pins[i] != NC) {
+            _nc_mask |= (1 << i);
+        }
     }
 }
 
 BusInOut::BusInOut(PinName pins[16]) {
+    _nc_mask = 0;
     for (int i=0; i<16; i++) {
         _pin[i] = (pins[i] != NC) ? new DigitalInOut(pins[i]) : 0;
+        if (pins[i] != NC) {
+            _nc_mask |= (1 << i);
+        }
     }
 }
 
@@ -91,6 +101,16 @@ BusInOut& BusInOut::operator= (BusInOut& rhs) {
     write(rhs.read());
     return *this;
 }
+
+DigitalInOut& BusInOut::operator[] (unsigned int index) {
+    //MBED_ASSERT(index >= MBED_BUS_SIZE);
+    //MBED_ASSERT(_pin[index]);
+    if (index >= 16 || _pin[index] == NULL) {
+        return dinout_dummy;
+    }
+    return *_pin[index];
+}
+
 
 BusInOut::operator int() {
     return read();
