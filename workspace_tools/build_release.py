b0184e636988cf5bb1227ae22672bac3542a677a
jesusalvarez <jalvarez@micromint.com>
2015-01-20 23:49:10 -0400

    Update tools

--- workspace_tools/build_release.py
+++ workspace_tools/build_release.py
@@ -42,7 +42,7 @@ OFFICIAL_MBED_LIBRARY_BUILD = (
     ('LPC11U35_501', ('ARM', 'uARM','GCC_ARM','GCC_CR', 'IAR')),
     ('LPC1549',      ('uARM','GCC_ARM','GCC_CR', 'IAR')),
     ('XADOW_M0',     ('ARM', 'uARM','GCC_ARM','GCC_CR')),
-    ('ARCH_GPRS',    ('ARM', 'uARM', 'GCC_ARM', 'GCC_CR')),
+    ('ARCH_GPRS',    ('ARM', 'uARM', 'GCC_ARM', 'GCC_CR', 'IAR')),
     ('LPC4330_M4',   ('ARM', 'GCC_ARM', 'GCC_CR')),
     ('LPC4337',      ('ARM',)),
     ('LPC11U37H_401', ('ARM', 'uARM','GCC_ARM','GCC_CR', 'IAR')),
