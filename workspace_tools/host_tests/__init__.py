34b55c81eb194f807c91035e0ab0a102bebc114f
Przemek Wirkus <przemyslaw.wirkus@arm.com>
2015-01-30 10:35:58 +0000

    Simple indent inside Python module init

--- workspace_tools/host_tests/__init__.py
+++ workspace_tools/host_tests/__init__.py
@@ -16,29 +16,32 @@ limitations under the License.
 """
 
 from host_registry import HostRegistry
-from default_auto import DefaultAuto
-from hello_auto import HelloTest
-from wait_us_auto import WaitusTest
-from stdio_auto import StdioTest
-from dev_null_auto import DevNullTest
-from rtc_auto import RTCTest
+
+# Host test supervisors
 from echo import EchoTest
+from rtc_auto import RTCTest
+from stdio_auto import StdioTest
+from hello_auto import HelloTest
 from detect_auto import DetectPlatformTest
+from default_auto import DefaultAuto
+from dev_null_auto import DevNullTest
+from wait_us_auto import WaitusTest
 from tcpecho_server_auto import TCPEchoServerTest
 from udpecho_server_auto import UDPEchoServerTest
 from tcpecho_client_auto import TCPEchoClientTest
 from udpecho_client_auto import UDPEchoClientTest
 
+# Populate registry with supervising objects
 HOSTREGISTRY = HostRegistry()
-HOSTREGISTRY.register_host_test("default", DefaultAuto())
-HOSTREGISTRY.register_host_test("default_auto", DefaultAuto())
-HOSTREGISTRY.register_host_test("hello_auto", HelloTest())
-HOSTREGISTRY.register_host_test("wait_us_auto", WaitusTest())
-HOSTREGISTRY.register_host_test("stdio_auto", StdioTest())
-HOSTREGISTRY.register_host_test("dev_null_auto", DevNullTest())
-HOSTREGISTRY.register_host_test("rtc_auto", RTCTest())
 HOSTREGISTRY.register_host_test("echo", EchoTest())
+HOSTREGISTRY.register_host_test("default", DefaultAuto())
+HOSTREGISTRY.register_host_test("rtc_auto", RTCTest())
+HOSTREGISTRY.register_host_test("hello_auto", HelloTest())
+HOSTREGISTRY.register_host_test("stdio_auto", StdioTest())
 HOSTREGISTRY.register_host_test("detect_auto", DetectPlatformTest())
+HOSTREGISTRY.register_host_test("default_auto", DefaultAuto())
+HOSTREGISTRY.register_host_test("wait_us_auto", WaitusTest())
+HOSTREGISTRY.register_host_test("dev_null_auto", DevNullTest())
 HOSTREGISTRY.register_host_test("tcpecho_server_auto", TCPEchoServerTest())
 HOSTREGISTRY.register_host_test("udpecho_server_auto", UDPEchoServerTest())
 HOSTREGISTRY.register_host_test("tcpecho_client_auto", TCPEchoClientTest())
