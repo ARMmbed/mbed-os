be043144193cb9a3dda5066fea1a21999fffff58
Mihail Stoyanov <wm@smode.org>
2015-02-10 18:32:44 +0200

    Add Cortex-M7 and Cortex-M1 symbols

--- workspace_tools/toolchains/__init__.py
+++ workspace_tools/toolchains/__init__.py
@@ -199,11 +199,14 @@ class mbedToolchain:
     VERBOSE = True
 
     CORTEX_SYMBOLS = {
-        "Cortex-M3" : ["__CORTEX_M3", "ARM_MATH_CM3"],
         "Cortex-M0" : ["__CORTEX_M0", "ARM_MATH_CM0"],
         "Cortex-M0+": ["__CORTEX_M0PLUS", "ARM_MATH_CM0PLUS"],
+        "Cortex-M1" : ["__CORTEX_M3", "ARM_MATH_CM1"],
+        "Cortex-M3" : ["__CORTEX_M3", "ARM_MATH_CM3"],
         "Cortex-M4" : ["__CORTEX_M4", "ARM_MATH_CM4"],
         "Cortex-M4F" : ["__CORTEX_M4", "ARM_MATH_CM4", "__FPU_PRESENT=1"],
+        "Cortex-M7" : ["__CORTEX_M7", "ARM_MATH_CM7"],
+        "Cortex-M7F" : ["__CORTEX_M7", "ARM_MATH_CM7", "__FPU_PRESENT=1"],
         "Cortex-A9" : ["__CORTEX_A9", "ARM_MATH_CA9", "__FPU_PRESENT", "__CMSIS_RTOS", "__EVAL", "__MBED_CMSIS_RTOS_CA9"],
     }
 
