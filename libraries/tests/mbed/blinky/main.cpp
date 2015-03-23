2e0aff95a9172563d903e3dd87ca77c42ca92714
Marcomissyou <marco.missyou@gmail.com>
2015-03-23 10:22:04 +0800

    modify blinky main

--- libraries/tests/mbed/blinky/main.cpp
+++ libraries/tests/mbed/blinky/main.cpp
@@ -1,13 +1,12 @@
-#include "mbed.h"		 #include "mbed.h"
-+#include "spi_flash.h"
- 		 
--DigitalOut myled(LED1);		+DigitalOut LED(LED1);
--		+ 
- int main() {		 int main() {
--    while(1) {		+
--        myled = 1;		+	spi_flash_init();
--        wait(0.2);		+    spi_flash_powerDown(); 
--        myled = 0;		+	
--        wait(0.2);		+    LED = 1;
--    }		+    wait(2);
--}
+#include "mbed.h"
+
+DigitalOut myled(LED1);
+
+int main() {
+    while(1) {
+        myled = 1;
+        wait(0.2);
+        myled = 0;
+        wait(0.2);
+    }
+}
