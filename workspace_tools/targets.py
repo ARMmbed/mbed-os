e75efe346742fa22e124861fddf1b51c291f4219
Mihail Stoyanov <wm@smode.org>
2015-05-06 13:49:05 +0300

    Tidy up targets.py

--- workspace_tools/targets.py
+++ workspace_tools/targets.py
@@ -820,7 +820,6 @@ class UBLOX_C029(Target):
         self.supported_form_factors = ["ARDUINO"]
 
 
-
 ### Nordic ###
 
 class NRF51822(Target):
@@ -1054,58 +1053,74 @@ class DELTA_DFCM_NNN40_OTA(NRF51822):
         self.extra_labels = ['NORDIC', 'MCU_NRF51822', 'MCU_NORDIC_16K', 'DELTA_DFCM_NNN40']
         self.MERGE_SOFT_DEVICE = False
         self.macros += self.common_macros
+
+
 ### ARM ###
 
-class ARM_MPS2_M0(Target):
+class ARM_MPS2_Target(Target):
     def __init__(self):
         Target.__init__(self)
+        self.OUTPUT_EXT = 'axf'
+
+    def init_hooks(self, hook, toolchain_name):
+        hook.hook_add_binary("replace", self.output_axf)
+
+    @staticmethod
+    def output_axf(t_self, resources, elf, bin):
+        shutil.copy(elf, bin)
+        t_self.debug("Passing ELF file %s" % bin)
+
+
+class ARM_MPS2_M0(ARM_MPS2_Target):
+    def __init__(self):
+        ARM_MPS2_Target.__init__(self)
         self.core = "Cortex-M0"
-        self.extra_labels = ['ARM_SSG', 'MPS2_M0']
+        self.extra_labels = ['ARM_SSG', 'MPS2', 'MPS2_M0']
         self.macros = ['CMSDK_CM0']
         self.supported_toolchains = ["ARM", "GCC_ARM"]
         self.default_toolchain = "ARM"
 
-class ARM_MPS2_M0P(Target):
+class ARM_MPS2_M0P(ARM_MPS2_Target):
     def __init__(self):
-        Target.__init__(self)
+        ARM_MPS2_Target.__init__(self)
         self.core = "Cortex-M0+"
-        self.extra_labels = ['ARM_SSG', 'MPS2_M0P']
+        self.extra_labels = ['ARM_SSG', 'MPS2', 'MPS2_M0P']
         self.macros = ['CMSDK_CM0plus']
         self.supported_toolchains = ["ARM", "GCC_ARM"]
         self.default_toolchain = "ARM"
 
-class ARM_MPS2_M1(Target):
+class ARM_MPS2_M1(ARM_MPS2_Target):
     def __init__(self):
-        Target.__init__(self)
+        ARM_MPS2_Target.__init__(self)
         self.core = "Cortex-M1"
-        self.extra_labels = ['ARM_SSG', 'MPS2_M1']
+        self.extra_labels = ['ARM_SSG', 'MPS2', 'MPS2_M1']
         self.macros = ['CMSDK_CM1']
         self.supported_toolchains = ["ARM", "GCC_ARM"]
         self.default_toolchain = "ARM"
 
-class ARM_MPS2_M3(Target):
+class ARM_MPS2_M3(ARM_MPS2_Target):
     def __init__(self):
-        Target.__init__(self)
+        ARM_MPS2_Target.__init__(self)
         self.core = "Cortex-M3"
-        self.extra_labels = ['ARM_SSG', 'MPS2_M3']
+        self.extra_labels = ['ARM_SSG', 'MPS2', 'MPS2_M3']
         self.macros = ['CMSDK_CM3']
         self.supported_toolchains = ["ARM", "GCC_ARM"]
         self.default_toolchain = "ARM"
 
-class ARM_MPS2_M4(Target):
+class ARM_MPS2_M4(ARM_MPS2_Target):
     def __init__(self):
-        Target.__init__(self)
+        ARM_MPS2_Target.__init__(self)
         self.core = "Cortex-M4F"
-        self.extra_labels = ['ARM_SSG', 'MPS2_M4']
+        self.extra_labels = ['ARM_SSG', 'MPS2', 'MPS2_M4']
         self.macros = ['CMSDK_CM4']
         self.supported_toolchains = ["ARM", "GCC_ARM"]
         self.default_toolchain = "ARM"
 
-class ARM_MPS2_M7(Target):
+class ARM_MPS2_M7(ARM_MPS2_Target):
     def __init__(self):
-        Target.__init__(self)
+        ARM_MPS2_Target.__init__(self)
         self.core = "Cortex-M7F"
-        self.extra_labels = ['ARM_SSG', 'MPS2_M7']
+        self.extra_labels = ['ARM_SSG', 'MPS2', 'MPS2_M7']
         self.macros = ['CMSDK_CM7']
         self.supported_toolchains = ["ARM", "GCC_ARM"]
         self.default_toolchain = "ARM"
@@ -1149,7 +1164,9 @@ class MAX32600MBED(Target):
         self.supported_toolchains = ["GCC_ARM", "IAR", "ARM"]
         self.default_toolchain = "ARM"
 
+
 ### Silicon Labs ###
+
 class EFM32GG_STK3700(Target):
     def __init__(self):
         Target.__init__(self)
