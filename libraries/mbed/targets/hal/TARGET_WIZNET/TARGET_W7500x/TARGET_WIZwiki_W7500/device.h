7d82bcacaa3e2480f6a728f147a57cc19a8eb0ff
hjjeon0608 <hjjeon0608@wiznet.co.kr>
2015-05-18 14:31:19 +0900

    Remove rtc_api.c and pwmout_api.c And change define in device.h about RTC and pwmout.

--- libraries/mbed/targets/hal/TARGET_WIZNET/TARGET_W7500x/TARGET_WIZwiki_W7500/device.h
+++ libraries/mbed/targets/hal/TARGET_WIZNET/TARGET_W7500x/TARGET_WIZwiki_W7500/device.h
@@ -48,9 +48,9 @@
 #define DEVICE_SPI              1
 #define DEVICE_SPISLAVE         1
 
-#define DEVICE_RTC              1
+#define DEVICE_RTC              0
 
-#define DEVICE_PWMOUT           1
+#define DEVICE_PWMOUT           0
 
 #define DEVICE_SLEEP            1
 
