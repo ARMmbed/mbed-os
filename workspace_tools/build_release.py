4b93618af68071c92bbdc1e239d9735b0c9aeb8a
0xc0170 <c0170@rocketmail.com>
2015-06-03 08:07:07 +0100

    Squashed commit of the following (#1149 PR on github):
    commit 4810979575d8feccff1d6dc75ab24e3753c4e0f0
    Merge: 83c82b0 0e23067
    Author: 0xc0170 <c0170@rocketmail.com>
    Date:   Wed Jun 3 08:06:14 2015 +0100
    
        Merge branch 'master' of https://github.com/screamerbg/mbed into screamerbg-master
    
    commit 0e23067718c61808401311bb594ed25fb4ed62c6
    Author: Mihail Stoyanov <wm@smode.org>
    Date:   Tue Jun 2 16:08:08 2015 +0300
    
        Add the Delta NNN40 platform to releases
    
    commit 62a585dcda553b6e3b5c84e90a55b465f0894863
    Merge: e1cd545 7a1d25e
    Author: Mihail Stoyanov <wm@smode.org>
    Date:   Tue Jun 2 16:06:44 2015 +0300
    
        Merge branch 'master' of https://github.com/mbedmicro/mbed
    
        Conflicts:
        	workspace_tools/tests.py
    
    commit e1cd545a9c92c4177f72da964d50d163dae5990f
    Merge: 5c5e61f ffd5586
    Author: Mihail Stoyanov <wm@smode.org>
    Date:   Thu May 7 13:07:48 2015 +0300
    
        Merge branch 'master' of https://github.com/mbedmicro/mbed
    
    commit 5c5e61f0ac38d91ad04738fd33af36f91452d984
    Author: Mihail Stoyanov <wm@smode.org>
    Date:   Thu May 7 13:07:35 2015 +0300
    
        Remove the Disco L053 from RTOS tests as it's not ready yet
    
    commit e75efe346742fa22e124861fddf1b51c291f4219
    Author: Mihail Stoyanov <wm@smode.org>
    Date:   Wed May 6 13:49:05 2015 +0300
    
        Tidy up targets.py

--- workspace_tools/build_release.py
+++ workspace_tools/build_release.py
@@ -87,6 +87,7 @@ OFFICIAL_MBED_LIBRARY_BUILD = (
     ('RBLAB_NRF51822', ('ARM', 'GCC_ARM')),
     ('RBLAB_BLENANO', ('ARM', 'GCC_ARM')),
     ('WALLBOT_BLE',  ('ARM', 'GCC_ARM')),
+    ('DELTA_DFCM_NNN40',  ('ARM', 'GCC_ARM')),
 
     ('LPC11U68',     ('ARM', 'uARM','GCC_ARM','GCC_CR', 'IAR')),
     ('OC_MBUINO',     ('ARM', 'uARM', 'GCC_ARM', 'IAR')),
