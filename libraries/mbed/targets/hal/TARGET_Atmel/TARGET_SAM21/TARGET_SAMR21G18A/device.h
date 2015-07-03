28484f79425635111c0c8244be422f6601d790c5
akhilpanayam <akhil.p@atmel.com>
2015-07-03 18:49:50 +0530

    * Base commit for DAC.

--- libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM21/TARGET_SAMR21G18A/device.h
+++ libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM21/TARGET_SAMR21G18A/device.h
@@ -23,7 +23,7 @@
 #define DEVICE_INTERRUPTIN      1
 
 #define DEVICE_ANALOGIN         1
-#define DEVICE_ANALOGOUT        0
+#define DEVICE_ANALOGOUT        1
 
 #define DEVICE_SERIAL           1
 #define DEVICE_SERIAL_FC        1
