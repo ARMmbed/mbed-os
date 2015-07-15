da743d2aa7d5599674adabe4ffcaa75b589a34f2
akhilpanayam <akhil.p@atmel.com>
2015-07-15 11:09:40 +0530

    * corrections in External Interrupt GPIO Peripheral Pins.

--- libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM21/TARGET_SAMR21G18A/PeripheralPins.c
+++ libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM21/TARGET_SAMR21G18A/PeripheralPins.c
@@ -102,23 +102,20 @@ const PinMap PinMap_PWM[] = {
 
 /**********EXTINT*************/
 const PinMap PinMap_EXTINT[] = {
-    {PA16,  EXTINT_0, 0},
     {PB00,  EXTINT_0, 0},
     {PB16,  EXTINT_0, 0},
-    {PA00,  EXTINT_0, 0},
 
     {PA17,  EXTINT_1, 0},
     {PB17,  EXTINT_1, 0},
     {PA01,  EXTINT_1, 0},
 
     {PA18,  EXTINT_2, 0},
-    {PA02,  EXTINT_2, 0},
+    {PB02,  EXTINT_2, 0},
 
     {PA19,  EXTINT_3, 0},
     {PB03,  EXTINT_3, 0},
 
     {PA04,  EXTINT_4, 0},
-    {PA20,  EXTINT_4, 0},
 
     {PA05,  EXTINT_5, 0},
 
@@ -128,18 +125,14 @@ const PinMap PinMap_EXTINT[] = {
 
     {PA07,  EXTINT_7, 0},
     {PA23,  EXTINT_7, 0},
-    {PB28,  EXTINT_7, 0},
+    {PB23,  EXTINT_7, 0},
 
     {PA28,  EXTINT_8, 0},
-    {PB08,  EXTINT_8, 0},
 
     {PA09,  EXTINT_9, 0},
-    {PB09,  EXTINT_9, 0},
 
-    {PA10,  EXTINT_10, 0},
     {PA30,  EXTINT_10, 0},
 
-    {PA11,  EXTINT_11, 0},
     {PA31,  EXTINT_11, 0},
 
     {PA12,  EXTINT_12, 0},
@@ -148,14 +141,10 @@ const PinMap PinMap_EXTINT[] = {
     {PA13,  EXTINT_13, 0},
     {PA25,  EXTINT_13, 0},
 
-    {PB14,  EXTINT_14, 0},
-    {PB30,  EXTINT_14, 0},
     {PA14,  EXTINT_14, 0},
 
     {PA15,  EXTINT_15, 0},
     {PA27,  EXTINT_15, 0},
-    {PB15,  EXTINT_15, 0},
-    {PB31,  EXTINT_15, 0},
 
     /* Not connected */
     {NC  , NC   , NC}
