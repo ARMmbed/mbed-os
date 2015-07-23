750b73e10e87c698c23d12fd00138e71a29a076b
Jonathan Austin <jonathan.austin@arm.com>
2015-07-23 18:31:37 +0100

    Delta: move the delta OTA target to use inheritance
    Like the HRM1017 platform and the micro:bit, the delta platform
    should use inheritance to define the OTA target. This allows the
    TARGET_NRF_LFCLK_RC macro to be set once in the parent target.

--- workspace_tools/targets.py
+++ workspace_tools/targets.py
@@ -1143,16 +1143,14 @@ class DELTA_DFCM_NNN40(NRF51822):
         NRF51822.__init__(self)
         self.core = "Cortex-M0"
         self.extra_labels = ['NORDIC', 'MCU_NRF51822', 'MCU_NORDIC_16K', "MCU_NORDIC_16K_S130"]
-        self.macros = ['TARGET_NRF51822']
+        self.macros = ['TARGET_NRF51822', 'TARGET_NRF_LFCLK_RC']
         self.macros += self.common_macros
 
-class DELTA_DFCM_NNN40_OTA(NRF51822):
+class DELTA_DFCM_NNN40_OTA(DELTA_DFCM_NNN40):
     def __init__(self):
-        NRF51822.__init__(self)
-        self.core = "Cortex-M0"
-        self.extra_labels = ['NORDIC', 'MCU_NRF51822', 'MCU_NORDIC_16K', 'DELTA_DFCM_NNN40', "MCU_NORDIC_16K_S130"]
+        DELTA_DFCM_NNN40.__init__(self)
+        self.macros += ['TARGET_DELTA_DFCM_NNN40', 'TARGET_OTA_ENABLED']
         self.MERGE_SOFT_DEVICE = False
-        self.macros += self.common_macros
 
 class NRF51_MICROBIT(NRF51822):
     def __init__(self):
