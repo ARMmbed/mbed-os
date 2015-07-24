3eaef2e09535d2cd006896bf2c416a619a43ceb8
Mikko Polojarvi <mikko.polojarvi@pollentech.io>
2015-07-24 11:42:07 +0300

    SiL USB: Update copyrights and licence boilerplate
    Everything should be copyrighted to SiLabs, and licensed
    under Apache 2.0.

--- libraries/USBDevice/USBDevice/TARGET_Silicon_Labs/inc/em_usbtypes.h
+++ libraries/USBDevice/USBDevice/TARGET_Silicon_Labs/inc/em_usbtypes.h
@@ -7,9 +7,17 @@
  * <b>(C) Copyright 2014 Silicon Labs, http://www.silabs.com</b>
  *******************************************************************************
  *
- * This file is licensed under the Silabs License Agreement. See the file
- * "Silabs_License_Agreement.txt" for details. Before using this software for
- * any purpose, you must agree to the terms of that agreement.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  ******************************************************************************/
 
