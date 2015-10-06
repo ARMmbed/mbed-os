3e9b838a7a668f4d2359075ed66493c5a4e33deb
Brian Daniels <brianddaniels@gmail.com>
2015-10-06 10:28:16 -0500

    Removed uneeded dependencies and added license header

--- workspace_tools/upload_results.py
+++ workspace_tools/upload_results.py
@@ -1,21 +1,24 @@
-#!/usr/bin/env python
+"""
+mbed SDK
+Copyright (c) 2011-2013 ARM Limited
 
-import os
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 import sys
 import argparse
-import ast
-import urllib2
-import urllib
-import time
-import datetime
 import xml.etree.ElementTree as ET
-
-import pprint
-
 import requests
 import urlparse
-
-pp = pprint.PrettyPrinter(indent=4)
 
 def create_headers(args):
     return { 'X-Api-Key':  args.api_key }
