bf4cf1a5ec8263ce5be6edbb1bcfb4d50ac3854b
Mikko Polojarvi <mikko.polojarvi@pollentech.io>
2015-10-12 12:02:27 +0300

    SiLabs Pearl: RTCC support

--- libraries/mbed/targets/hal/TARGET_Silicon_Labs/TARGET_EFM32/rtc_api.c
+++ libraries/mbed/targets/hal/TARGET_Silicon_Labs/TARGET_EFM32/rtc_api.c
@@ -45,6 +45,10 @@ static uint32_t     time_extend = 0;
 
 static void (*comp0_handler)(void) = NULL;
 
+#ifndef RTCC_COUNT
+
+/* Using RTC API */
+
 #define RTC_LEAST_ACTIVE_SLEEPMODE  EM2
 #define RTC_NUM_BITS                (24)
 
@@ -65,12 +69,12 @@ void RTC_IRQHandler(void)
     }
 }
 
-uint32_t rtc_get_32bit(void) 
+uint32_t rtc_get_32bit(void)
 {
     return (RTC_CounterGet() + (time_extend << RTC_NUM_BITS));
 }
 
-uint64_t rtc_get_full(void) 
+uint64_t rtc_get_full(void)
 {
     uint64_t ticks = 0;
     ticks += time_extend;
@@ -78,18 +82,6 @@ uint64_t rtc_get_full(void)
     ticks += RTC_CounterGet();
     return ticks;
 }
-
-void rtc_set_comp0_handler(uint32_t handler)
-{
-    comp0_handler = (void (*)(void)) handler;
-}
-
-void rtc_init(void)
-{
-    /* Register that the RTC is used for timekeeping. */
-    rtc_init_real(RTC_INIT_RTC);
-}
-
 
 void rtc_init_real(uint32_t flags)
 {
@@ -143,6 +135,111 @@ void rtc_free_real(uint32_t flags)
     }
 }
 
+#else
+
+/* Using RTCC API */
+
+#define RTCC_LEAST_ACTIVE_SLEEPMODE  EM2
+#define RTCC_NUM_BITS                (32)
+
+void RTCC_IRQHandler(void)
+{
+    uint32_t flags;
+    flags = RTCC_IntGet();
+
+    if (flags & RTCC_IF_OF) {
+        RTCC_IntClear(RTCC_IF_OF);
+        /* RTC has overflowed (24 bits). Use time_extend as software counter for 32 more bits. */
+        time_extend += 1;
+    }
+
+    if (flags & RTCC_IF_COMP0) {
+        RTCC_IntClear(RTCC_IF_COMP0);
+        if (comp0_handler != NULL) {
+            comp0_handler();
+        }
+    }
+}
+
+uint32_t rtc_get_32bit(void)
+{
+    return RTCC_CounterGet();
+}
+
+uint64_t rtc_get_full(void)
+{
+    uint64_t ticks = 0;
+    ticks += time_extend;
+    ticks = ticks << RTCC_NUM_BITS;
+    ticks += RTCC_CounterGet();
+    return ticks;
+}
+
+void rtc_init_real(uint32_t flags)
+{
+    useflags |= flags;
+
+    if (!rtc_inited) {
+        CMU_ClockEnable(cmuClock_RTCC, true);
+
+        /* Enable clock to the interface of the low energy modules */
+        CMU_ClockEnable(cmuClock_CORELE, true);
+
+        /* Scale clock to save power */
+        CMU_ClockDivSet(cmuClock_RTCC, RTC_CLOCKDIV);
+
+        /* Initialize RTC */
+        RTCC_Init_TypeDef init = RTCC_INIT_DEFAULT;
+        init.enable = 1;
+        init.precntWrapOnCCV0 = false;
+        init.cntWrapOnCCV1 = false;
+
+        /* Enable Interrupt from RTC */
+        RTCC_IntEnable(RTCC_IEN_OF);
+        NVIC_EnableIRQ(RTCC_IRQn);
+        NVIC_SetVector(RTCC_IRQn, (uint32_t)RTCC_IRQHandler);
+
+        /* Initialize */
+        RTCC_Init(&init);
+
+        blockSleepMode(RTCC_LEAST_ACTIVE_SLEEPMODE);
+        rtc_inited = true;
+    }
+}
+
+void rtc_free(void)
+{
+    rtc_free_real(RTC_INIT_RTC);
+}
+
+void rtc_free_real(uint32_t flags)
+{
+    /* Clear use flag */
+    useflags &= ~flags;
+
+    /* Disable the RTC if it was inited and is no longer in use by anyone. */
+    if (rtc_inited && (useflags == 0)) {
+        NVIC_DisableIRQ(RTCC_IRQn);
+        RTCC_Reset();
+        CMU_ClockEnable(cmuClock_RTCC, false);
+        unblockSleepMode(RTCC_LEAST_ACTIVE_SLEEPMODE);
+        rtc_inited = false;
+    }
+}
+
+#endif /* RTCC_COUNT */
+
+void rtc_set_comp0_handler(uint32_t handler)
+{
+    comp0_handler = (void (*)(void)) handler;
+}
+
+void rtc_init(void)
+{
+    /* Register that the RTC is used for timekeeping. */
+    rtc_init_real(RTC_INIT_RTC);
+}
+
 int rtc_isenabled(void)
 {
     return rtc_inited;
@@ -153,7 +250,7 @@ time_t rtc_read(void)
     return (time_t) (rtc_get_full() >> RTC_FREQ_SHIFT) + time_base;
 }
 
-time_t rtc_read_uncompensated(void) 
+time_t rtc_read_uncompensated(void)
 {
     return (time_t) (rtc_get_full() >> RTC_FREQ_SHIFT);
 }
