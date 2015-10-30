927e72a394fc6cc2b00c106a8c339de53e43acc8
Rafal Fabich <rafal.fabich@espotel.com>
2015-10-30 09:14:36 +0100

    Fixed FMPI2C1 defines in i2c_api.c

--- libraries/mbed/targets/hal/TARGET_STM/TARGET_STM32F4/i2c_api.c
+++ libraries/mbed/targets/hal/TARGET_STM/TARGET_STM32F4/i2c_api.c
@@ -90,7 +90,7 @@ void i2c_init(i2c_t *obj, PinName sda, PinName scl)
         pin_mode(scl, OpenDrain);
     }
 #endif
-#if defined FMPI2C1_BASE
+#if (defined(FMPI2C1_BASE) && defined(FMPI2C_1))
     // Enable I2C3 clock and pinout if not done
     if ((obj->i2c == FMPI2C_1) && !fmpi2c1_inited) {
         fmpi2c1_inited = 1;
@@ -337,7 +337,7 @@ void i2c_reset(i2c_t *obj)
     }
 #endif
 
-#if defined FMPI2C1_BASE
+#if (defined(FMPI2C1_BASE) && defined(FMPI2C_1))
     if (obj->i2c == FMPI2C_1) {
         __FMPI2C1_FORCE_RESET();
         __FMPI2C1_RELEASE_RESET();
