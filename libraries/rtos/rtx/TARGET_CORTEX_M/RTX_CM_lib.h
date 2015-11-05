743c3194a4b09679859b6c0d63b972af1eb03da4
Wojciech Gorniak <Wojciech.Gorniak@espotel.com>
2015-11-05 16:20:04 +0100

    added target definition to RTOS-related files

--- libraries/rtos/rtx/TARGET_CORTEX_M/RTX_CM_lib.h
+++ libraries/rtos/rtx/TARGET_CORTEX_M/RTX_CM_lib.h
@@ -271,6 +271,9 @@ osThreadDef_t os_thread_def_main = {(os_pthread)main, osPriorityNormal, 0, NULL}
 #elif defined(TARGET_STM32F411RE)
 #define INITIAL_SP            (0x20020000UL)
 
+#elif defined(TARGET_STM32F410RB)
+#define INITIAL_SP            (0x20008000UL)
+
 #elif defined(TARGET_STM32F103RB)
 #define INITIAL_SP            (0x20005000UL)
 
