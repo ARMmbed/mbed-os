cdd074b7fe45984080208592b0fac5cb91bad131
0xc0170 <c0170@rocketmail.com>
2015-12-07 18:33:40 +0000

    Exporters - uvision and iar are using progen, others regular mbed exporters

--- workspace_tools/export/__init__.py
+++ workspace_tools/export/__init__.py
@@ -23,6 +23,8 @@ from workspace_tools.export import uvision4, codesourcery, codered, gccarm, ds5_
 from workspace_tools.export.exporters import zip_working_directory_and_clean_up, OldLibrariesException
 from workspace_tools.targets import TARGET_NAMES, EXPORT_MAP
 
+from project_generator_definitions.definitions import ProGenDef
+
 EXPORTERS = {
     'uvision': uvision4.Uvision4,
     'lpcxpresso': codered.CodeRed,
@@ -59,7 +61,7 @@ def export(project_path, project_name, ide, target, destination='/tmp/',
     if tempdir is None:
         tempdir = tempfile.mkdtemp()
 
-    report = {'success': False}
+    report = {'success': False, 'errormsg':''}
     if ide is None or ide == "zip":
         # Simple ZIP exporter
         try:
@@ -72,13 +74,29 @@ def export(project_path, project_name, ide, target, destination='/tmp/',
             report['errormsg'] = ERROR_MESSAGE_NOT_EXPORT_LIBS
     else:
         if ide not in EXPORTERS:
-            report['errormsg'] = "Unsupported toolchain"
+            report['errormsg'] = ERROR_MESSAGE_UNSUPPORTED_TOOLCHAIN % (target, ide)
         else:
             Exporter = EXPORTERS[ide]
             target = EXPORT_MAP.get(target, target)
-            if target not in Exporter.TARGETS:
-                report['errormsg'] = ERROR_MESSAGE_UNSUPPORTED_TOOLCHAIN % (target, ide)
+            # use progen targets or mbed exporters targets, check progen attribute
+            use_progen = False
+            supported = True
+            try:
+                if Exporter.PROGEN_ACTIVE:
+                    use_progen = True
+            except AttributeError:
+                pass
+            if use_progen:
+                if target in Exporter.PROGEN_TARGETS.keys():
+                    progen_target = Exporter.PROGEN_TARGETS[target]
+                    if not ProGenDef(ide).is_supported(progen_target):
+                        supported = False
             else:
+                if target not in Exporter.TARGETS:
+                    supported = False
+
+            if supported:
+                # target checked, export
                 try:
                     exporter = Exporter(target, tempdir, project_name, build_url_resolver, extra_symbols=extra_symbols)
                     exporter.scan_and_copy_resources(project_path, tempdir)
@@ -86,6 +104,8 @@ def export(project_path, project_name, ide, target, destination='/tmp/',
                     report['success'] = True
                 except OldLibrariesException, e:
                     report['errormsg'] = ERROR_MESSAGE_NOT_EXPORT_LIBS
+            else:
+                report['errormsg'] = ERROR_MESSAGE_UNSUPPORTED_TOOLCHAIN % (target, ide)
 
     zip_path = None
     if report['success']:
--- workspace_tools/export/iar.py
+++ workspace_tools/export/iar.py
@@ -17,77 +17,14 @@ limitations under the License.
 from workspace_tools.export.exporters import Exporter
 import re
 import os
+
 class IAREmbeddedWorkbench(Exporter):
     """
     Exporter class for IAR Systems.
     """
     NAME = 'IAR'
     TOOLCHAIN = 'IAR'
-
-    PROGEN_TARGETS = {
-        'LPC1768': 'mbed-lpc1768',
-        # 'LPC1347',
-        # 'LPC11U24',
-        # 'LPC11U35_401',
-        # 'LPC11U35_501',
-        # #Removed LPCCAPPUCCINO linker file and startup file missing
-        # #'LPCCAPPUCCINO',
-        # 'LPC1114',
-        # 'LPC1549',
-        # 'LPC812',
-        # 'LPC4088',
-        # 'LPC4088_DM',
-        # 'LPC824',
-        # 'UBLOX_C027',
-        # 'ARCH_PRO',
-        # 'K20D50M',
-        # 'KL05Z',
-        # 'KL25Z',
-        # 'KL46Z',
-        # 'K22F',
-        'K64F': 'frdm-k64f',
-        # 'NUCLEO_F030R8',
-        # 'NUCLEO_F031K6',
-        # 'NUCLEO_F042K6',
-        # 'NUCLEO_F070RB',
-        # 'NUCLEO_F072RB',
-        # 'NUCLEO_F091RC',
-        # 'NUCLEO_F103RB',
-        # 'NUCLEO_F302R8',
-        # 'NUCLEO_F303K8',
-        # 'NUCLEO_F303RE',
-        # 'NUCLEO_F334R8',
-        # 'NUCLEO_F401RE',
-        # 'NUCLEO_F410RB',
-        # 'NUCLEO_F411RE',
-        # 'NUCLEO_F446RE',
-        # 'NUCLEO_L053R8',
-        # 'NUCLEO_L073RZ',
-        # 'NUCLEO_L152RE',
-        # 'NUCLEO_L476RG',
-        # 'DISCO_F429ZI',
-        # 'DISCO_L053C8',
-        # 'DISCO_F334C8',
-        # 'DISCO_F469NI',
-        # 'DISCO_F746NG',
-        # 'DISCO_L476VG',
-        # #'STM32F407', Fails to build same for GCC
-        # 'MAXWSNENV',
-        # 'MAX32600MBED',
-        # 'MTS_MDOT_F405RG',
-        # 'MTS_MDOT_F411RE',
-        # 'MTS_DRAGONFLY_F411RE',
-        # 'NRF51822',
-        # 'NRF51_DK',
-        # 'NRF51_DONGLE',
-        # 'DELTA_DFCM_NNN40',
-        # 'SEEED_TINY_BLE',
-        # 'HRM1017',
-        # 'ARCH_BLE',
-        # 'MOTE_L152RC',
-    }
-
-    TARGETS = PROGEN_TARGETS.keys()
+    PROGEN_ACTIVE = True
 
     def generate(self):
         """ Generates the project files """
