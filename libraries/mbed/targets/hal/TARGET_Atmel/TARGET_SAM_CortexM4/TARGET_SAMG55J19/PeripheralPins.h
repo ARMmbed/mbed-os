02d8c99768456214c6cb5aa63bbd85617e4da22b
Parthasarathy.S <parthasarathy.s@atmel.com>
2015-12-18 11:50:23 +0530

    Removed unused SPI pin map

--- libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM_CortexM4/TARGET_SAMG55J19/PeripheralPins.h
+++ libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM_CortexM4/TARGET_SAMG55J19/PeripheralPins.h
@@ -34,7 +34,6 @@ extern const PinMap PinMap_SPI_SCLK[];
 extern const PinMap PinMap_SPI_MOSI[];
 extern const PinMap PinMap_SPI_MISO[];
 extern const PinMap PinMap_SPI_SSEL[];
-extern const PinMap PinMap_SPI[];
 /************I2C***************/
 extern const PinMap PinMap_I2C_SDA[];
 extern const PinMap PinMap_I2C_SCL[];
