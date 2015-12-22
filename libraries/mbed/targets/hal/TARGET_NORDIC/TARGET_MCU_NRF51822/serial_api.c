ARMmbed/mbed-os

commit 0a6e345400e5e73eeb8df189bc3ce112f0dbd793

Add support for 56000 baud on nrf51
Add support for the baudrate 56000.  This is not documented in the
datasheet, but Nordic has indicated in can be used by setting the
BAUD register to 56000.

libraries/mbed/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/serial_api.c
@@ -39,6 +39,7 @@ static const int acceptedSpeeds[17][2] = {
     {28800, UART_BAUDRATE_BAUDRATE_Baud28800},
     {31250, (0x00800000UL) /* 31250 baud */},
     {38400, UART_BAUDRATE_BAUDRATE_Baud38400},
+    {56000, (0x00E51000UL) /* 56000 baud */},
     {57600, UART_BAUDRATE_BAUDRATE_Baud57600},
     {76800, UART_BAUDRATE_BAUDRATE_Baud76800},
     {115200, UART_BAUDRATE_BAUDRATE_Baud115200},

commit e825431c1aa035c5e182e35fc9195b6eb0f540da

Fix nRF51 baud table size
Increase the number of entries in the baud rate table from 17 to 18.
This problem was introduced in the patch:
0a6e345400e5e73eeb8df189bc3ce112f0dbd793 -
Add support for 56000 baud on nrf51

libraries/mbed/targets/hal/TARGET_NORDIC/TARGET_MCU_NRF51822/serial_api.c
@@ -29,7 +29,7 @@
 
 static uint32_t serial_irq_ids[UART_NUM] = {0};
 static uart_irq_handler irq_handler;
-static const int acceptedSpeeds[17][2] = {
+static const int acceptedSpeeds[18][2] = {
     {1200, UART_BAUDRATE_BAUDRATE_Baud1200},
     {2400, UART_BAUDRATE_BAUDRATE_Baud2400},
     {4800, UART_BAUDRATE_BAUDRATE_Baud4800},