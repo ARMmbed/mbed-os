428a138e92ece59327b497ddbf99c88b5070e929
Mikko Polojarvi <mikko.polojarvi@pollentech.io>
2015-12-22 16:01:01 +0200

    SiLabs Pearl: Support for clocking via RC oscillators
    Allows clocking the device from RC oscillators (HFRCO, LFRCO).
    Note that we can not use the em_cmu.h enums directly as the
    preprocessor can not do comparisons on them.
    
    Related changes in serial_api, so that LEUART clock is within
    acceptable limits on Pearl.
    
    Contains quite a bit of indentation changes to make the preprocessor
    logic more readable so recommend viewing the diff in ignore whitespace
    mode.

--- libraries/mbed/targets/hal/TARGET_Silicon_Labs/TARGET_EFM32/mbed_overrides.c
+++ libraries/mbed/targets/hal/TARGET_Silicon_Labs/TARGET_EFM32/mbed_overrides.c
@@ -53,51 +53,85 @@ void mbed_sdk_init()
     SystemHFXOClockSet(HFXO_FREQUENCY);
 #elif( CORE_CLOCK_SOURCE == HFRCO)
     CMU_ClockSelectSet(cmuClock_HF, cmuSelect_HFRCO);
+# if defined _CMU_HFRCOCTRL_BAND_MASK
     CMU_HFRCOBandSet(HFRCO_FREQUENCY);
+# elif defined _CMU_HFRCOCTRL_FREQRANGE_MASK
+#  ifndef PER_CMU_HFRCO_FREQ_1MHZ
+#   error "Need PER_CMU_ definitions"
+#  endif
+#  if HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_1MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_1M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_2MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_2M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_4MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_4M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_7MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_7M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_13MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_13M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_16MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_16M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_19MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_19M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_26MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_26M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_32MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_32M0Hz);
+#  elif HFRCO_FREQUENCY == PER_CMU_HFRCO_FREQ_38MHZ
+    CMU_HFRCOFreqSet(cmuHFRCOFreq_38M0Hz);
+#  else
+#   error "Invalid HFRCO clock selection"
+#  endif
+# else
+#  error "Can't set HFRCO frequency"
+# endif
 #else
-#error "Core clock selection not valid (mbed_overrides.c)"
+# error "Core clock selection not valid (mbed_overrides.c)"
 #endif
 
     CMU_ClockEnable(cmuClock_CORELE, true);
 
 #if( LOW_ENERGY_CLOCK_SOURCE == LFXO )
-#ifdef CMU_LFACLKSEL_REG
+# ifdef CMU_LFACLKSEL_REG
     CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFXO);
-#endif
-#ifdef CMU_LFBCLKSEL_REG
+# endif
+# ifdef CMU_LFBCLKSEL_REG
     /* cmuClock_LFB (to date) only has LEUART peripherals.
     *  This gets set automatically whenever you create serial objects using LEUART
     */
-#endif
-#ifdef _CMU_LFECLKEN0_MASK
+# endif
+# ifdef _CMU_LFECLKEN0_MASK
     CMU_ClockSelectSet(cmuClock_LFE, cmuSelect_LFXO);
-#endif
+# endif
     SystemLFXOClockSet(LFXO_FREQUENCY);
 
 #elif( LOW_ENERGY_CLOCK_SOURCE == LFRCO )
-#ifdef CMU_LFACLKSEL_REG
+# ifdef CMU_LFACLKSEL_REG
     CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_LFRCO);
-#endif
-#ifdef CMU_LFBCLKSEL_REG
+# endif
+# ifdef CMU_LFBCLKSEL_REG
     //CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_LFRCO);
-#endif
-#ifdef CMU_LFECLKSEL_REG
+# endif
+# ifdef _CMU_LFECLKEN0_MASK
     CMU_ClockSelectSet(cmuClock_LFE, cmuSelect_LFRCO);
-#endif
+# endif
+
+# if defined _CMU_HFRCOCTRL_BAND_MASK
     CMU_HFRCOBandSet(HFRCO_FREQUENCY);
+# endif
 
 #elif( LOW_ENERGY_CLOCK_SOURCE == ULFRCO)
-#ifdef CMU_LFACLKSEL_REG
+# ifdef CMU_LFACLKSEL_REG
     CMU_ClockSelectSet(cmuClock_LFA, cmuSelect_ULFRCO);
-#endif
-#ifdef CMU_LFBCLKSEL_REG
+# endif
+# ifdef CMU_LFBCLKSEL_REG
     CMU_ClockSelectSet(cmuClock_LFB, cmuSelect_ULFRCO);
-#endif
-#ifdef CMU_LFECLKSEL_REG
+# endif
+# ifdef CMU_LFECLKSEL_REG
     CMU_ClockSelectSet(cmuClock_LFE, cmuSelect_ULFRCO);
-#endif
+# endif
 #else
-#error "Low energy clock selection not valid"
+# error "Low energy clock selection not valid"
 #endif
 
     /* Enable BC line driver to avoid garbage on CDC port */
