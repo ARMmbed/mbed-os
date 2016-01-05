438f84536b66bd27ba2abbb5acd8ea158c97488a
akhilpanayam <akhil.p@atmel.com>
2016-01-05 17:11:28 +0530

    * Added implementation for rtc apis.

--- libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM_CortexM4/rtc_api.c
+++ libraries/mbed/targets/hal/TARGET_Atmel/TARGET_SAM_CortexM4/rtc_api.c
@@ -23,11 +23,6 @@ static int rtc_inited = 0;
 
 extern uint8_t g_sys_init;
 
-/** Initialize the RTC
- *
- * Initialize the RTC with default time
- * @param[void] void
- */
 void rtc_init(void)
 {
     if (g_sys_init == 0) {
@@ -35,53 +30,69 @@ void rtc_init(void)
         system_board_init();
         g_sys_init = 1;
     }
-
+    /* Default RTC configuration, 24-hour mode */
+    rtc_set_hour_mode(RTC, 0);
     rtc_inited = 1;
 }
 
-/** Frees the RTC
- *
- * @param[void] void
- */
 void rtc_free(void)
 {
+    /*This is a free running peripheral and cannot be disabled*/
+    rtc_inited = 0;
 }
 
-/** Checks whether RTC is enabled or not
- *
- * To check whether RTC module is enabled or not
- * @param[void] void
- * @return      Non zero if RTC is already enabled, else zero
- */
+
 int rtc_isenabled(void)
 {
     return rtc_inited;
 }
 
-/** Reads the RTC value
- *
- * Reads and return the current time in RTC
- * @param[void] void
- * @return      the current value in RTC
- */
+
 time_t rtc_read(void)
 {
     if (!rtc_inited) {
         /* Return invalid time for now! */
         return 0;
     }
+    struct tm timeinfo;
+    uint32_t ul_hour, ul_minute, ul_second;
+    uint32_t ul_year, ul_month, ul_day, ul_week;
+
+    rtc_get_time(RTC, &ul_hour, &ul_minute, &ul_second);
+    rtc_get_date(RTC, &ul_year, &ul_month, &ul_day, &ul_week);
+
+    timeinfo.tm_sec = ul_second;
+    timeinfo.tm_min = ul_minute;
+    timeinfo.tm_hour = ul_hour;
+    timeinfo.tm_mday = ul_day;
+    timeinfo.tm_wday = ul_week;
+    timeinfo.tm_mon = ul_month;
+    timeinfo.tm_year = (ul_year - 1900);
+
+    /* Convert to timestamp */
+    time_t t = mktime(&timeinfo);
+    return t;
 }
 
-/** Write the RTC value
- *
- * Update the time value in RTC
- * @param[in] t The time value to be written
- * @return      void
- */
 void rtc_write(time_t t)
 {
     if (!rtc_inited) {
         /* Initialize the RTC is not yet initialized */
         rtc_init();
     }
+    struct tm *timeinfo = localtime(&t);
+    uint32_t ul_hour, ul_minute, ul_second;
+    uint32_t ul_year, ul_month, ul_day, ul_week;
+
+    ul_second = timeinfo->tm_sec;
+    ul_minute = timeinfo->tm_min;
+    ul_hour = timeinfo->tm_hour;
+    ul_day = timeinfo->tm_mday;
+    ul_week = timeinfo->tm_wday;
+    ul_month = timeinfo->tm_mon;
+    ul_year = timeinfo->tm_year;
+
+    /* Set the RTC  */
+    rtc_set_time(RTC, ul_hour, ul_minute, ul_second);
+    rtc_set_date(RTC, ul_year, ul_month, ul_day, ul_week);
 }
