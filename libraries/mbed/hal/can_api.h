c11325e11bf00fd0a8de3fc535baa3218ef7627d
adustm <adustm13@gmail.com>
2016-02-01 10:13:29 +0100

    [B96B_F446VE] add IRQ_RX1 interrupt
    Can contains 2 fifos. Both generates its own interrupt.
    IRQ_RX occurs when a message has arrived in FIFO-0 , FIFO-0 is full or
    overrun.
    IRQ_RX1 occurs when a message has arrived in FIFO-1 , FIFO-1 is full or
    overrun.

--- libraries/mbed/hal/can_api.h
+++ libraries/mbed/hal/can_api.h
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
+ * Copyright (c) 2006-2016 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
