3f612c5b486c18f57a51fede9f1ddc91f43548ba
0xc0170 <c0170@rocketmail.com>
2016-04-27 14:03:49 -0500

    i2c - fix return value description i2c_byte_write()
    Fixes #1670.

--- libraries/mbed/hal/i2c_api.h
+++ libraries/mbed/hal/i2c_api.h
@@ -124,7 +124,7 @@ int  i2c_byte_read(i2c_t *obj, int last);
 /** Write one byte.
  *  @param obj The i2c object
  *  @param data Byte to be written
- *  @return 1 if NAK was received, 0 if ACK was received, 2 for timeout.
+ *  @return 0 if NAK was received, 1 if ACK was received, 2 for timeout.
  */
 int  i2c_byte_write(i2c_t *obj, int data);
 
