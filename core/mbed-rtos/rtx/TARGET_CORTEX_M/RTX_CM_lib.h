fcea510163db9e4f82d45116715377d492c8d1cc
Russ Butler <russ.butler@arm.com>
2016-04-27 17:35:51 -0500

    Remove dead CodeSourcery init code
    The CodeSourcery compiler is not longer supported.  This patch removes
    the CodeSourcery init code in RTX_CM_lib.h.

--- core/mbed-rtos/rtx/TARGET_CORTEX_M/RTX_CM_lib.h
+++ core/mbed-rtos/rtx/TARGET_CORTEX_M/RTX_CM_lib.h
@@ -486,62 +486,6 @@ __asm void __rt_entry (void) {
 
 #elif defined (__GNUC__)
 
-#ifdef __CS3__
-
-/* CS3 start_c routine.
- *
- * Copyright (c) 2006, 2007 CodeSourcery Inc
- *
- * The authors hereby grant permission to use, copy, modify, distribute,
- * and license this software and its documentation for any purpose, provided
- * that existing copyright notices are retained in all copies and that this
- * notice is included verbatim in any distributions. No written agreement,
- * license, or royalty fee is required for any of the authorized uses.
- * Modifications to this software may be copyrighted by their authors
- * and need not follow the licensing terms described here, provided that
- * the new terms are clearly indicated on the first page of each file where
- * they apply.
- */
-
-#include "cs3.h"
-
-extern void __libc_init_array (void);
-
-__attribute ((noreturn)) void __cs3_start_c (void){
-  unsigned regions = __cs3_region_num;
-  const struct __cs3_region *rptr = __cs3_regions;
-
-  /* Initialize memory */
-  for (regions = __cs3_region_num, rptr = __cs3_regions; regions--; rptr++) {
-    long long *src = (long long *)rptr->init;
-    long long *dst = (long long *)rptr->data;
-    unsigned limit = rptr->init_size;
-    unsigned count;
-
-    if (src != dst)
-      for (count = 0; count != limit; count += sizeof (long long))
-        *dst++ = *src++;
-    else
-      dst = (long long *)((char *)dst + limit);
-    limit = rptr->zero_size;
-    for (count = 0; count != limit; count += sizeof (long long))
-      *dst++ = 0;
-  }
-
-  /* Run initializers.  */
-  __libc_init_array ();
-
-  osKernelInitialize();
-#ifdef __MBED_CMSIS_RTOS_CM
-  set_main_stack();
-#endif
-  osThreadCreate(&os_thread_def_main, NULL);
-  osKernelStart();
-  for (;;);
-}
-
-#else
-
 __attribute__((naked)) void software_init_hook (void) {
   __asm (
     ".syntax unified\n"
@@ -566,8 +510,6 @@ __attribute__((naked)) void software_init_hook (void) {
     "bl   exit\n"
   );
 }
-
-#endif
 
 #elif defined (__ICCARM__)
 
