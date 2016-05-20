77d8232f900f0ca16406eb8a00e046e004f45579
Antti Kauppila <antti.kauppila@arm.com>
2016-05-20 16:28:35 +0300

    randlib unit tested

---
+++ test/mbed-client-randlib/unittest/stubs/random_stub.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2016, ARM Limited, All Rights Reserved
+ */
+#include "inttypes.h"
+
+
+void arm_random_module_init(void)
+{
+
+}
+
+uint32_t arm_random_seed_get(void)
+{
+    return 4;
+}
