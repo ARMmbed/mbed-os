f66fdb81c4c3912a3cd156bdb7eec167617c4fbc
Sam Grove <sam.w.grove@gmail.com>
2016-05-26 12:09:34 +0800

    Add D14 and D15 mappings to NC for arduino FF

--- hal/targets/hal/TARGET_STM/TARGET_STM32F3/TARGET_NUCLEO_F303K8/PinNames.h
+++ hal/targets/hal/TARGET_STM/TARGET_STM32F3/TARGET_NUCLEO_F303K8/PinNames.h
@@ -148,7 +148,9 @@ typedef enum {
     PWM_OUT     = PA_8,
 
     // Not connected
-    NC = (int)0xFFFFFFFF
+    NC = (int)0xFFFFFFFF,
+    D14         = NC,
+    D15         = NC,
 } PinName;
 
 typedef enum {
