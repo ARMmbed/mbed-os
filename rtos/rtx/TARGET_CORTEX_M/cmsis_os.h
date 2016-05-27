0fd3721d2eac25c6f8540be874f94f41e104d66a
Neil Thiessen <neilt66@icloud.com>
2016-05-27 10:10:52 -0600

    [RTOS] Fixed online compiler macro warnings
    Added an #ifndef directive to the __MBED_CMSIS_RTOS_CM and __CMSIS_RTOS macro definitions in order to prevent "Incompatible redefinition of macro" warnings from the online compiler.

--- rtos/rtx/TARGET_CORTEX_M/cmsis_os.h
+++ rtos/rtx/TARGET_CORTEX_M/cmsis_os.h
@@ -57,9 +57,13 @@
 #define CMSIS_OS_RTX
 
 // __MBED_CMSIS_RTOS_CM captures our changes to the RTX kernel
+#ifndef __MBED_CMSIS_RTOS_CM
 #define __MBED_CMSIS_RTOS_CM
+#endif
 // we use __CMSIS_RTOS version, which changes some API in the kernel
+#ifndef __CMSIS_RTOS
 #define __CMSIS_RTOS
+#endif
 
 // The stack space occupied is mainly dependent on the underling C standard library
 #if defined(TOOLCHAIN_GCC) || defined(TOOLCHAIN_ARM_STD) || defined(TOOLCHAIN_IAR)
