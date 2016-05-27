b16f971274ef3f95ed94456bd800fe00e388e385
Bartosz Szczepanski <bartosz.szczepanski@tieto.com>
2016-05-27 15:34:01 +0200

    [NUCLEO_F746ZG] Enable LowPowerTimer
    We need to enable the LSI clock to be able to start the RTC periodic unit.
    
    Change-Id: Ic7518dfcf5153f925c7c030f8de0259e19aab963

--- hal/targets/hal/TARGET_STM/TARGET_STM32F7/TARGET_NUCLEO_F746ZG/device.h
+++ hal/targets/hal/TARGET_STM/TARGET_STM32F7/TARGET_NUCLEO_F746ZG/device.h
@@ -49,11 +49,13 @@
 #define DEVICE_SPISLAVE         1
 
 #define DEVICE_RTC              1
-#define DEVICE_RTC_LSI          0
+#define DEVICE_RTC_LSI          1
 
 #define DEVICE_PWMOUT           1
 
 #define DEVICE_SLEEP            1
+
+#define DEVICE_LOWPOWERTIMER    1
 
 //=======================================
 
