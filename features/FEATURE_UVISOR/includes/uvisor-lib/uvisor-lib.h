55f464da270d7a7e7c770f3b01d3fb46a5f2c3f6
Jaeden Amero <jaeden.amero@arm.com>
2016-06-08 11:14:52 +0100

    Copy memory management code over from uVisor
    Add the memory management code from uVisor to a shared location that is
    suitable for both supported and unsupported use. To do this, we copy all
    the RTX-specific files from the RTX folders in uVisor.
    
    Note that the page allocator code is so identical between uVisor supported
    and unsupported mode that we can copy the file as-is from uVisor for use on
    unsupported targets. This commit performs that copying, to a file called
    page_allocator.c_inc, which is included from unsupported_page_allocator.c
    only for targets where uVisor is not present.

--- features/FEATURE_UVISOR/includes/uvisor-lib/uvisor-lib.h
+++ features/FEATURE_UVISOR/includes/uvisor-lib/uvisor-lib.h
@@ -35,5 +35,8 @@
 
 /* The uVisor API main header file will use the above definitions. */
 #include "uvisor/api/inc/uvisor-lib.h"
+#include "uvisor-lib/rtx/process_malloc.h"
+#include "uvisor-lib/rtx/rtx_box_index.h"
+#include "uvisor-lib/rtx/secure_allocator.h"
 
 #endif /* __UVISOR_LIB_UVISOR_LIB_H__ */
