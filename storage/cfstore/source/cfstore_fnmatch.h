837f0baa37f8e0d4dabefc8a108f1e20fec83ea9
Simon D Hughes <simon.hughes@arm.com>
2016-06-09 11:13:56 +0100

    Fix for GCC 5.0 which appears not to define _GNU_SOURCE, define _ANSI_SOURCE and define _POSIX_SOURCE

--- storage/cfstore/source/cfstore_fnmatch.h
+++ storage/cfstore/source/cfstore_fnmatch.h
@@ -39,18 +39,11 @@
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
 
-#if defined(_GNU_SOURCE) || !defined(_ANSI_SOURCE) && !defined(_POSIX_SOURCE)
 #define	FNM_LEADING_DIR	0x08	/* Ignore /<tail> after Imatch. */
 #define	FNM_CASEFOLD	0x10	/* Case insensitive search. */
 #define	FNM_IGNORECASE	FNM_CASEFOLD
 #define	FNM_FILE_NAME	FNM_PATHNAME
-#endif
 
-//todo: #include <sys/cdefs.h>
-//#include <cdefs.h>
-
-//__BEGIN_DECLS
 int	 fnmatch(const char *, const char *, int);
-//__END_DECLS
 
 #endif /* !_FNMATCH_H_ */
