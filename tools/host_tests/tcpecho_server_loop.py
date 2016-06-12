c182b46fcd1b8c695bb1d3332275b9c34e26e556
Mihail Stoyanov <wm@smode.org>
2016-06-12 19:06:17 +0100

    Ported changes from mbedmicro/mbed

--- tools/host_tests/tcpecho_server_loop.py
+++ tools/host_tests/tcpecho_server_loop.py
@@ -20,7 +20,7 @@ from os.path import join, abspath, dirname
 ROOT = abspath(join(dirname(__file__), "..", ".."))
 sys.path.insert(0, ROOT)
 
-from tools.private_settings import LOCALHOST
+from mbed_settings import LOCALHOST
 from SocketServer import BaseRequestHandler, TCPServer
 
 
