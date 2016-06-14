b03fbe48341a34a83f0ef7fd3e6ad0a55be1e472
Mihail Stoyanov <wm@smode.org>
2016-06-14 02:13:14 +0100

    Updated core (mbedmicro) and removed tools (defork)

--- tools/test/config_test/test18/test_data.py
+++
@@ -1,8 +0,0 @@
-# Like test17, but this time the invalid re-definition is in lib2, not in the app
-
-expected_results = {
-    "K64F": {
-        "desc": "test invalid macro re-definition in a library",
-        "exception_msg": "defined in both"
-    }
-}
