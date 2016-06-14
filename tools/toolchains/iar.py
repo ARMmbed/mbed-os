d0db5c94e5ce899412280d8c71d65850e0c8b30b
Mihail Stoyanov <wm@smode.org>
2016-06-15 00:06:43 +0100

    Removed IAR --vla flag which breaks compatibility with IAR Workbench

--- tools/toolchains/iar.py
+++ tools/toolchains/iar.py
@@ -40,7 +40,7 @@ class IAR(mbedToolchain):
             "-e", # Enable IAR language extension
             "--diag_suppress=Pa050,Pa084,Pa093,Pa082"],
         'asm': [],
-        'c': ["--vla"],
+        'c': [],
         'cxx': ["--c++",  "--no_rtti", "--no_exceptions", "--guard_calls"],
         'ld': ["--skip_dynamic_initialization", "--threaded_lib"],
     }
