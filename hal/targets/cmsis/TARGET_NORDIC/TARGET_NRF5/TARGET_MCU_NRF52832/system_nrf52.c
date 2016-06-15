082fd409cec3071f4d508dca6774a7e1a7e319cd
Głąbek, Andrzej <andrzej.glabek@nordicsemi.no>
2016-06-15 21:43:38 +0100

    Added LF clock starting code to SysInit.

--- hal/targets/cmsis/TARGET_NORDIC/TARGET_NRF5/TARGET_MCU_NRF52832/system_nrf52.c
+++ hal/targets/cmsis/TARGET_NORDIC/TARGET_NRF5/TARGET_MCU_NRF52832/system_nrf52.c
@@ -187,6 +187,16 @@ void SystemInit(void)
     #endif
 
     SystemCoreClockUpdate();
+
+    // Start the external 32khz crystal oscillator.
+    NRF_CLOCK->LFCLKSRC             = (CLOCK_LFCLKSRC_SRC_Xtal << CLOCK_LFCLKSRC_SRC_Pos);
+    NRF_CLOCK->EVENTS_LFCLKSTARTED  = 0;
+    NRF_CLOCK->TASKS_LFCLKSTART     = 1;
+
+    // Wait for the external oscillator to start up.
+    while (NRF_CLOCK->EVENTS_LFCLKSTARTED == 0) {
+        // Do nothing.
+    }
 }
 
 
