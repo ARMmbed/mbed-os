bb27b849d5500228244efa1d2412dfe9e9ead481
Rob Meades <rob.meades@u-blox.com>
2016-06-16 08:02:28 +0100

    Initial commit of ublox target files, containing HI2110 (AKA Boudica, the NBIOT chip), SARA_NBIOT (the NBIOT module that contains the HI2110 chip) and SARA_NBIOT_EVK (the board that carries SARA_NBIOT).  The code compiles and runs but there are know bugs in that us_ticker stops working correctly after about 10 seconds and it doesn't yet receive characters correctly.

---
+++ hal/targets/hal/TARGET_ublox/TARGET_HI2110/HI2110_sdk/drivers/boudica/platform/shared/platform.h
@@ -0,0 +1,22 @@
+/******************************************************************************
+ * @brief    BOUDICA Platform Header
+ * Copyright (c) 2015 NEUL LIMITED
+ ******************************************************************************/
+
+#ifndef PLATFORM_H_
+#define PLATFORM_H_
+
+#include "platform_core.h"
+#include "pcb_config.h"
+
+/** @defgroup PLATFORM OS Agnostic BOUDICA Platform Definitions.
+ * OS Agnostic BOUDICA Platform Definitions.
+ * @{
+ * */
+
+/**  Reference clock in Hz */
+#define CLOCKS_REFERENCE_CLOCK_FREQ   32768
+
+/** @} end of group PLATFORM */
+
+#endif /* PLATFORM_H_ */
