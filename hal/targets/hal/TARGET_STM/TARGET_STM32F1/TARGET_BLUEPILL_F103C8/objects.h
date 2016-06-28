f6164f34cd6f9a31ed1b35d6ca90d56d25d73a28
Laurent MEUNIER <laurent.meuniert.com>
2016-06-28 18:56:32 +0200

    [STM32F1] place holder for common object definition
    Some of the objects in object.h are the same for all targets.
    Create a place where to define those common definitions, and
    start using it for pwm object.

--- hal/targets/hal/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/objects.h
+++ hal/targets/hal/TARGET_STM/TARGET_STM32F1/TARGET_BLUEPILL_F103C8/objects.h
@@ -89,19 +89,13 @@ struct i2c_s {
     uint32_t slave;
 };
 
-struct pwmout_s {
-    PWMName pwm;
-    PinName pin;
-    uint32_t period;
-    uint32_t pulse;
-};
-
 struct can_s {
     CANName can;
     int index;
 };
 
 #include "gpio_object.h"
+#include "common_objects.h"
 
 #ifdef __cplusplus
 }
