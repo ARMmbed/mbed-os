0249ee12e7d910e9002bf0b90eaebbfe26a1d20e
Andrzej Puzdrowski <andrzej.puzdrowski@nordicsemi.no>
2016-07-01 17:01:37 +0200

    add missing dates in some nordic license disclamer

--- hal/targets/hal/TARGET_NORDIC/TARGET_NRF5/sdk/drivers_nrf/pstorage/pstorage.h
+++ hal/targets/hal/TARGET_NORDIC/TARGET_NRF5/sdk/drivers_nrf/pstorage/pstorage.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Nordic Semiconductor ASA
+ * Copyright (c) 2013 Nordic Semiconductor ASA
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
