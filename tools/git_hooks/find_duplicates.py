9b187dcceb6fda3f6e079fdaaddfdecc3b7cda0a
Jimmy Brisson <theotherjimmy@gmail.com>
2016-07-08 15:10:30 -0500

    Add SUPERTARGET!  Use scan_resources to find duplicates

--- tools/git_hooks/find_duplicates.py
+++ tools/git_hooks/find_duplicates.py
@@ -1,5 +1,12 @@
 from os import walk
-from os.path import join
+from os.path import join, abspath, dirname, basename
+import sys
+
+ROOT = abspath(join(dirname(__file__), "..", ".."))
+sys.path.insert(0, ROOT)
+
+from tools.toolchains.gcc import GCC_ARM
+from tools.targets import TARGET_MAP
 from argparse import ArgumentParser
 
 if __name__ == "__main__":
@@ -9,13 +16,16 @@ if __name__ == "__main__":
     parser.add_argument("--silent", help="Supress printing of filenames, just return number of duplicates", action="store_true")
     args = parser.parse_args()
 
+    toolchain = GCC_ARM(TARGET_MAP[".Super-Target"])
+
+    resources = sum([toolchain.scan_resources(d) for d in args.dirs], None)
+
     scanned_files = {}
 
-    for dir in args.dirs:
-        for root, dirs, files in walk(dir):
-            for file in files:
-                scanned_files.setdefault(file, [])
-                scanned_files[file].append(join(root, file))
+    for r in [resources] + resources.features.values():
+        for file in r.c_sources + r.s_sources + r.cpp_sources + r.objects + r.libraries + r.hex_files + r.bin_files:
+            scanned_files.setdefault(basename(file), [])
+            scanned_files[basename(file)].append(file)
 
     count_dupe = 0
     for key, value in scanned_files.iteritems():
