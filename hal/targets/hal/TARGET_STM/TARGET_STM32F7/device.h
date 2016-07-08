2b4ca5003f02a8f26eb8badeea8b565f5c82a619
Laurent MEUNIER <laurent.meuniert.com>
2016-07-08 11:43:08 +0200

    Make device.h a common file

--- hal/targets/hal/TARGET_STM/TARGET_STM32F7/device.h
+++ hal/targets/hal/TARGET_STM/TARGET_STM32F7/device.h
@@ -1,6 +1,8 @@
+// The 'features' section in 'target.json' is now used to create the device's hardware preprocessor switches.
+// Check the 'features' section of the target description in 'targets.json' for more details.
 /* mbed Microcontroller Library
  *******************************************************************************
- * Copyright (c) 2016, STMicroelectronics
+ * Copyright (c) 2014, STMicroelectronics
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -31,7 +33,6 @@
 #define MBED_DEVICE_H
 
 //=======================================
-
 #define DEVICE_ID_LENGTH       24
 
 #include "objects.h"
