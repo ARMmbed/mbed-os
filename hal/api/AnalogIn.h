c1728p9/mbed

Create singleton class and update code to use it
Create the wrapper class SingletonPtr.  This provides a safe way to
declare and use singletons.  This class allows both the lazy
initialization of a singleton, and allows the singleton to be
garbage collected by the linker if it is never referenced.

This patch also updates the HAL to use SingletonPtr when declaring
singleton mutexes.

hal/api/AnalogIn.h
@@ -21,6 +21,7 @@
 #if DEVICE_ANALOGIN
 
 #include "analogin_api.h"
+#include "mbed_singleton.h"
 
 namespace mbed {
 
@@ -110,15 +111,15 @@ public:
 protected:
 
     virtual void lock() {
-        _mutex.lock();
+        _mutex->lock();
     }
 
     virtual void unlock() {
-        _mutex.unlock();
+        _mutex->unlock();
     }
 
     analogin_t _adc;
-    static PlatformMutex _mutex;
+    static SingletonPtr<PlatformMutex> _mutex;
 };
 
 } // namespace mbed

hal/api/FileBase.h
@@ -41,6 +41,7 @@ typedef long off_t;
 #endif
 
 #include "platform.h"
+#include "mbed_singleton.h"
 
 namespace mbed {
 
@@ -65,7 +66,7 @@ public:
     /* disallow copy constructor and assignment operators */
 private:
     static FileBase *_head;
-    static PlatformMutex _mutex;
+    static SingletonPtr<PlatformMutex> _mutex;
 
     FileBase   *_next;
     const char * const _name;

hal/api/I2C.h
@@ -21,6 +21,7 @@
 #if DEVICE_I2C
 
 #include "i2c_api.h"
+#include "mbed_singleton.h"
 
 #if DEVICE_I2C_ASYNCH
 #include "CThunk.h"
@@ -181,7 +182,7 @@ protected:
     i2c_t _i2c;
     static I2C  *_owner;
     int         _hz;
-    static PlatformMutex _mutex;
+    static SingletonPtr<PlatformMutex> _mutex;
 };
 
 } // namespace mbed

hal/common/retarget.cpp
@@ -21,6 +21,7 @@
 #include "toolchain.h"
 #include "semihost_api.h"
 #include "mbed_interface.h"
+#include "mbed_singleton.h"
 #if DEVICE_STDIO_MESSAGES
 #include <stdio.h>
 #endif
@@ -72,17 +73,17 @@ extern const char __stderr_name[] = "/stderr";
  * (or rather index+3, as filehandles 0-2 are stdin/out/err).
  */
 static FileHandle *filehandles[OPEN_MAX];
-static PlatformMutex filehandle_mutex;
+static SingletonPtr<PlatformMutex> filehandle_mutex;
 
 FileHandle::~FileHandle() {
-    filehandle_mutex.lock();
+    filehandle_mutex->lock();
     /* Remove all open filehandles for this */
     for (unsigned int fh_i = 0; fh_i < sizeof(filehandles)/sizeof(*filehandles); fh_i++) {
         if (filehandles[fh_i] == this) {
             filehandles[fh_i] = NULL;
         }
     }
-    filehandle_mutex.unlock();
+    filehandle_mutex->unlock();
 }
 
 #if DEVICE_SERIAL
@@ -162,17 +163,17 @@ extern "C" FILEHANDLE PREFIX(_open)(const char* name, int openmode) {
     #endif
 
     // find the first empty slot in filehandles
-    filehandle_mutex.lock();
+    filehandle_mutex->lock();
     unsigned int fh_i;
     for (fh_i = 0; fh_i < sizeof(filehandles)/sizeof(*filehandles); fh_i++) {
         if (filehandles[fh_i] == NULL) break;
     }
     if (fh_i >= sizeof(filehandles)/sizeof(*filehandles)) {
-        filehandle_mutex.unlock();
+        filehandle_mutex->unlock();
         return -1;
     }
     filehandles[fh_i] = (FileHandle*)FILE_HANDLE_RESERVED;
-    filehandle_mutex.unlock();
+    filehandle_mutex->unlock();
 
     FileHandle *res;
 

ARMmbed/mbed-os

hal/api/AnalogIn.h
@@ -21,6 +21,7 @@
 #if DEVICE_ANALOGIN
 
 #include "analogin_api.h"
+#include "SingletonPtr.h"
 
 namespace mbed {
 
@@ -110,15 +111,15 @@ public:
 protected:
 
     virtual void lock() {
-        _mutex.lock();
+        _mutex->lock();
     }
 
     virtual void unlock() {
-        _mutex.unlock();
+        _mutex->unlock();
     }
 
     analogin_t _adc;
-    static PlatformMutex _mutex;
+    static SingletonPtr<PlatformMutex> _mutex;
 };
 
 } // namespace mbed

hal/api/FileBase.h
@@ -41,6 +41,7 @@ typedef long off_t;
 #endif
 
 #include "platform.h"
+#include "SingletonPtr.h"
 
 namespace mbed {
 
@@ -65,7 +66,7 @@ public:
     /* disallow copy constructor and assignment operators */
 private:
     static FileBase *_head;
-    static PlatformMutex _mutex;
+    static SingletonPtr<PlatformMutex> _mutex;
 
     FileBase   *_next;
     const char * const _name;

hal/api/I2C.h
@@ -21,6 +21,7 @@
 #if DEVICE_I2C
 
 #include "i2c_api.h"
+#include "SingletonPtr.h"
 
 #if DEVICE_I2C_ASYNCH
 #include "CThunk.h"
@@ -181,7 +182,7 @@ protected:
     i2c_t _i2c;
     static I2C  *_owner;
     int         _hz;
-    static PlatformMutex _mutex;
+    static SingletonPtr<PlatformMutex> _mutex;
 };
 
 } // namespace mbed

hal/common/retarget.cpp
@@ -21,6 +21,7 @@
 #include "toolchain.h"
 #include "semihost_api.h"
 #include "mbed_interface.h"
+#include "SingletonPtr.h"
 #if DEVICE_STDIO_MESSAGES
 #include <stdio.h>
 #endif
@@ -72,17 +73,17 @@ extern const char __stderr_name[] = "/stderr";
  * (or rather index+3, as filehandles 0-2 are stdin/out/err).
  */
 static FileHandle *filehandles[OPEN_MAX];
-static PlatformMutex filehandle_mutex;
+static SingletonPtr<PlatformMutex> filehandle_mutex;
 
 FileHandle::~FileHandle() {
-    filehandle_mutex.lock();
+    filehandle_mutex->lock();
     /* Remove all open filehandles for this */
     for (unsigned int fh_i = 0; fh_i < sizeof(filehandles)/sizeof(*filehandles); fh_i++) {
         if (filehandles[fh_i] == this) {
             filehandles[fh_i] = NULL;
         }
     }
-    filehandle_mutex.unlock();
+    filehandle_mutex->unlock();
 }
 
 #if DEVICE_SERIAL
@@ -162,17 +163,17 @@ extern "C" FILEHANDLE PREFIX(_open)(const char* name, int openmode) {
     #endif
 
     // find the first empty slot in filehandles
-    filehandle_mutex.lock();
+    filehandle_mutex->lock();
     unsigned int fh_i;
     for (fh_i = 0; fh_i < sizeof(filehandles)/sizeof(*filehandles); fh_i++) {
         if (filehandles[fh_i] == NULL) break;
     }
     if (fh_i >= sizeof(filehandles)/sizeof(*filehandles)) {
-        filehandle_mutex.unlock();
+        filehandle_mutex->unlock();
         return -1;
     }
     filehandles[fh_i] = (FileHandle*)FILE_HANDLE_RESERVED;
-    filehandle_mutex.unlock();
+    filehandle_mutex->unlock();
 
     FileHandle *res;
 