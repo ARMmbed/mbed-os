727ac9fb6d105c67c24fa6665befc72e5da521f0
Jimmy Brisson <theotherjimmy@gmail.com>
2016-07-26 15:30:59 -0500

    Update copyright

--- tools/test/export/build_test.py
+++ tools/test/export/build_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 """
 mbed SDK
-Copyright (c) 2011-2013 ARM Limited
+Copyright (c) 2011-2016 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
