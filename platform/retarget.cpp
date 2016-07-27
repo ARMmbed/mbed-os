58dae8999cb0e03581115588b8d24c8e53de6b7d
Christopher Haster <chaster@utexas.edu>
2016-07-27 03:30:05 -0500

    [restructure] Moved stdio-config into platform namespace
    core.stdio-convert-newlines -> platform.stdio-convert-newlines
    core.stdio-baud-rate        -> platform.stdio-baud-rate

--- platform/retarget.cpp
+++ platform/retarget.cpp
@@ -96,7 +96,7 @@ FileHandle::~FileHandle() {
 #if DEVICE_SERIAL
 extern int stdio_uart_inited;
 extern serial_t stdio_uart;
-#if MBED_CONF_CORE_STDIO_CONVERT_NEWLINES
+#if MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES
 static char stdio_in_prev;
 static char stdio_out_prev;
 #endif
@@ -106,8 +106,8 @@ static void init_serial() {
 #if DEVICE_SERIAL
     if (stdio_uart_inited) return;
     serial_init(&stdio_uart, STDIO_UART_TX, STDIO_UART_RX);
-#if MBED_CONF_CORE_STDIO_BAUD_RATE
-    serial_baud(&stdio_uart, MBED_CONF_CORE_STDIO_BAUD_RATE);
+#if MBED_CONF_PLATFORM_STDIO_BAUD_RATE
+    serial_baud(&stdio_uart, MBED_CONF_PLATFORM_STDIO_BAUD_RATE);
 #endif
 #endif
 }
@@ -241,7 +241,7 @@ extern "C" int PREFIX(_write)(FILEHANDLE fh, const unsigned char *buffer, unsign
     if (fh < 3) {
 #if DEVICE_SERIAL
         if (!stdio_uart_inited) init_serial();
-#if MBED_CONF_CORE_STDIO_CONVERT_NEWLINES
+#if MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES
         for (unsigned int i = 0; i < length; i++) {
             if (buffer[i] == '\n' && stdio_out_prev != '\r') {
                  serial_putc(&stdio_uart, '\r');
@@ -279,7 +279,7 @@ extern "C" int PREFIX(_read)(FILEHANDLE fh, unsigned char *buffer, unsigned int 
         // only read a character at a time from stdin
 #if DEVICE_SERIAL
         if (!stdio_uart_inited) init_serial();
-#if MBED_CONF_CORE_STDIO_CONVERT_NEWLINES
+#if MBED_CONF_PLATFORM_STDIO_CONVERT_NEWLINES
         while (true) {
             char c = serial_getc(&stdio_uart);
             if ((c == '\r' && stdio_in_prev != '\n') ||
