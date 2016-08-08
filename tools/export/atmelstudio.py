d4bfca79341e5ed2b22360206f771ffe32556ef4
Jimmy Brisson <theotherjimmy@gmail.com>
2016-08-08 15:01:24 -0500

    Fix renaming issues in non-CI ides
    Affects these ides:
     - Atmel Studio
     - Code Red (I don't think we support this)
     - Coide
     - DS-5
     - E2Studio
     - EMblocks
     - KDS
     - Simplicity v3
     - SW 4 STM32
    
    also corrects flags usage in EMBlocks

--- tools/export/atmelstudio.py
+++ tools/export/atmelstudio.py
@@ -69,7 +69,7 @@ class AtmelStudio(Exporter):
             'library_paths': self.resources.lib_dirs,
             'linker_script': self.resources.linker_script,
             'libraries': libraries,
-            'symbols': self.get_symbols(),
+            'symbols': self.toolchain.get_symbols(),
             'solution_uuid': solution_uuid.upper(),
             'project_uuid': project_uuid.upper()
         }
