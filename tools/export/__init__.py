17e096b3693bb927942f595763c732d92f375130
Jimmy Brisson <theotherjimmy@gmail.com>
2016-08-22 11:23:58 -0500

    Move make stuff to it's own folder; add a make_gcc_arm exporter

--- tools/export/__init__.py
+++ tools/export/__init__.py
@@ -19,7 +19,7 @@ from os.path import join, exists, basename
 from shutil import copytree, rmtree, copy
 import yaml
 
-from tools.export import uvision4, uvision5, codered, gccarm, ds5_5, iar
+from tools.export import uvision4, uvision5, codered, makefile, ds5_5, iar
 from tools.export import emblocks, coide, kds, simplicityv3, atmelstudio
 from tools.export import sw4stm32, e2studio, zip
 from tools.export.exporters import OldLibrariesException, FailedBuildException
@@ -32,7 +32,8 @@ EXPORTERS = {
     'uvision4': uvision4.Uvision4,
     'uvision5': uvision5.Uvision5,
     'lpcxpresso': codered.CodeRed,
-    'gcc_arm': gccarm.GccArm,
+    'gcc_arm': makefile.GccArm,
+    'make_gcc_arm': makefile.GccArm,
     'ds5_5': ds5_5.DS5_5,
     'iar': iar.IAREmbeddedWorkbench,
     'emblocks' : emblocks.IntermediateFile,
