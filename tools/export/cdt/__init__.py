32b1203b6ccd088de7d2cae18619f79bc0a786a8
Sarah Marsh <Sarah.Marsh@arm.com>
2016-08-24 12:54:34 -0500

    Generic makefile in eclipse

---
+++ tools/export/cdt/__init__.py
@@ -0,0 +1,47 @@
+from os.path import join, exists, realpath, relpath, basename
+from os import makedirs
+
+from tools.export.makefile import Makefile, GccArm, Armc5, IAR
+
+class Eclipse(Makefile):
+    """Generic Eclipse project. Intended to be subclassed by classes that
+    specify a type of Makefile.
+    """
+    def generate(self):
+        """Generate Makefile, .cproject & .project Eclipse project file,
+        py_ocd_settings launch file, and software link .p2f file
+        """
+        super(Eclipse, self).generate()
+        ctx = {
+            'name': self.project_name,
+            'elf_location': join('.build',self.project_name)+'.elf',
+            'c_symbols': self.toolchain.get_symbols(),
+            'asm_symbols': self.toolchain.get_symbols(True),
+            'target': self.target,
+            'include_paths': self.resources.inc_dirs
+        }
+
+        if not exists(join(self.export_dir,'eclipse-extras')):
+            makedirs(join(self.export_dir,'eclipse-extras'))
+
+
+        self.gen_file('cdt/py_ocd_settings.tmpl', ctx,
+                      join('eclipse-extras',self.target+'_py_ocd_settings.launch'))
+        self.gen_file('cdt/necessary_software.tmpl', ctx,
+                      join('eclipse-extras','necessary_software.p2f'))
+
+        cproj = relpath('.cproject',self.export_dir)
+        self.gen_file('cdt/.cproject.tmpl', ctx,
+                      cproj)
+        proj = relpath('.project',self.export_dir)
+        self.gen_file('cdt/.project.tmpl', ctx,
+                      proj)
+
+
+class EclipseGcc(Eclipse, GccArm): pass
+
+class EclipseArmc5(Eclipse, Armc5): pass
+
+class EclipseIAR(Eclipse, IAR): pass
+
+
