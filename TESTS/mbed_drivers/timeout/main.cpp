5e7c9332788dcf44272f2fbd630636fa42ff35a3
Brian Daniels <brianddaniels@gmail.com>
2016-08-30 14:38:55 -0500

    Modifying timing tests to use timing drift host test

--- TESTS/mbed_drivers/timeout/main.cpp
+++ TESTS/mbed_drivers/timeout/main.cpp
@@ -22,41 +22,51 @@ using namespace utest::v1;
 
 Timeout timer;
 DigitalOut led(LED1);
+volatile int ticker_count = 0;
+volatile bool print_tick = false;
+static const int total_ticks = 10;
 
 namespace {
     const int MS_INTERVALS = 1000;
 }
 
 void send_kv_tick() {
-    static int count = 0;
-    if (count < 10) {
-        greentea_send_kv("tick", count);
-    } else if (count == 10) {
-        Harness::validate_callback();
+    if (ticker_count <= total_ticks) {
+        print_tick = true;
     }
-    count++;
 }
 
 void toggleOff(void);
 
 void toggleOn(void) {
     static int toggle_counter = 0;
+    timer.attach_us(toggleOff, 500);
+    
     if (toggle_counter == MS_INTERVALS) {
-        led = !led;
         send_kv_tick();
         toggle_counter = 0;
+    } else {
+        toggle_counter++;
     }
-    toggle_counter++;
-    timer.attach_us(toggleOff, 500);
 }
 
 void toggleOff(void) {
     timer.attach_us(toggleOn, 500);
 }
 
-control_t test_case_ticker() {
+void wait_and_print() {
+    while(ticker_count <= total_ticks) {
+        if (print_tick) {
+            print_tick = false;
+            greentea_send_kv("tick", ticker_count++);
+            led = !led;
+        }
+    }
+}
+
+void test_case_ticker() {
     toggleOn();
-    return CaseTimeout(15 * 1000);
+    wait_and_print();
 }
 
 // Test cases
@@ -65,7 +75,7 @@ Case cases[] = {
 };
 
 utest::v1::status_t greentea_test_setup(const size_t number_of_cases) {
-    GREENTEA_SETUP(20, "wait_us_auto");
+    GREENTEA_SETUP(total_ticks + 5, "timing_drift_auto");
     return greentea_test_setup_handler(number_of_cases);
 }
 
