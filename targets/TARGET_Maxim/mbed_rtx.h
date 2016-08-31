7f4986f4cd2fb97a7b6c5dead6681a7fd90dfbd8
Jeremy Brodt <jeremy.brodt@maximintegrated.com>
2016-08-31 10:09:21 -0500

    Adding new Maxim Integrated target.

--- targets/TARGET_Maxim/mbed_rtx.h
+++ targets/TARGET_Maxim/mbed_rtx.h
@@ -62,6 +62,21 @@
 #define OS_CLOCK                48000000
 #endif
 
+#elif defined(TARGET_MAX32625)
+
+#ifndef INITIAL_SP
+#define INITIAL_SP              (0x20028000UL)
+#endif
+#ifndef OS_TASKCNT
+#define OS_TASKCNT              14
+#endif
+#ifndef OS_MAINSTKSIZE
+#define OS_MAINSTKSIZE          256
+#endif
+#ifndef OS_CLOCK
+#define OS_CLOCK                96000000
+#endif
+
 #endif
 
 #endif  // MBED_MBED_RTX_H
