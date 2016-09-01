1707b3c582e4a58c22a2891ed53d0e85549f745a
Erwan GOURIOU <frq07517@st.com>
2016-09-01 17:38:34 +0200

    [dev_asynch_i2c] Move i2c_s to common_objects.h

--- hal/targets/hal/TARGET_STM/TARGET_STM32F4/TARGET_NUCLEO_F411RE/objects.h
+++ hal/targets/hal/TARGET_STM/TARGET_STM32F4/TARGET_NUCLEO_F411RE/objects.h
@@ -60,21 +60,6 @@ struct analogin_s {
     uint8_t channel;
 };
 
-struct i2c_s {
-    I2CName  i2c;
-    I2C_HandleTypeDef handle;
-    IRQn_Type event_i2cIRQ;
-    IRQn_Type error_i2cIRQ;
-    uint32_t slave;
-#if DEVICE_I2C_ASYNCH
-    uint32_t event;
-    uint32_t address;
-    uint32_t stop;
-    uint32_t available_events;
-    uint32_t XferOperation;
-#endif
-};
-
 #include "common_objects.h"
 #include "gpio_object.h"
 
