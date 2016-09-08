448205ac877f92817bb3fb5ea09b1554693cf203
Sarah Marsh <Sarah.Marsh@arm.com>
2016-09-08 14:34:07 -0500

    device_name in targets.json. Removed progen target

--- tools/project_api.py
+++ tools/project_api.py
@@ -176,6 +176,19 @@ def export_project(src_paths, export_path, target, ide,
         paths = [src_paths]
 
     # Extend src_paths wit libraries_paths
+
+
+
+
+
+
+
+
+
+
+
+
+
     if libraries_paths is not None:
         paths.extend(libraries_paths)
 
