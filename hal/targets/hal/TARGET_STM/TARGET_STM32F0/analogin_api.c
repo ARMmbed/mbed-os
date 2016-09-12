bf42846e818d4d2a36f88f9c85c9342c28bd071b
bcostm <bco.stm@gmail.com>
2016-09-12 15:28:04 +0200

    STM32F0xx - Add support of ADC internal channels

--- hal/targets/hal/TARGET_STM/TARGET_STM32F0/analogin_api.c
+++ hal/targets/hal/TARGET_STM/TARGET_STM32F0/analogin_api.c
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2015, STMicroelectronics
+ * Copyright (c) 2016, STMicroelectronics
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -50,8 +50,10 @@ void analogin_init(analogin_t *obj, PinName pin) {
     MBED_ASSERT(function != (uint32_t)NC);
     obj->channel = STM_PIN_CHANNEL(function);
 
-    // Configure GPIO
-    pinmap_pinout(pin, PinMap_ADC);
+    // Configure GPIO excepted for internal channels (Temperature, Vref, Vbat)
+    if ((obj->channel != 16) && (obj->channel != 17) && (obj->channel != 18)) {
+        pinmap_pinout(pin, PinMap_ADC);
+    }
 
     // Save pin number for the read function
     obj->pin = pin;
@@ -149,6 +151,15 @@ static inline uint16_t adc_read(analogin_t *obj) {
             break;
         case 15:
             sConfig.Channel = ADC_CHANNEL_15;
+            break;
+        case 16:
+            sConfig.Channel = ADC_CHANNEL_16;
+            break;
+        case 17:
+            sConfig.Channel = ADC_CHANNEL_17;
+            break;
+        case 18:
+            sConfig.Channel = ADC_CHANNEL_18;
             break;
         default:
             return 0;
