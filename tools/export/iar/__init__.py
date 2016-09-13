4557916afb2ca2cc9a75f71e9d98890da5d0e6a7
Sarah Marsh <Sarah.Marsh@arm.com>
2016-09-13 16:31:33 -0500

    Iar nucleo template referenced correctly

--- tools/export/iar/__init__.py
+++ tools/export/iar/__init__.py
@@ -25,7 +25,7 @@ class IAR(Exporter):
 
     SPECIAL_TEMPLATES = {
         'rz_a1h'  : 'iar/iar_rz_a1h.ewp.tmpl',
-        'nucleo_f746zg' : 'iar/nucleo_f746zg.ewp.tmpl'
+        'nucleo_f746zg' : 'iar/iar_nucleo_f746zg.ewp.tmpl'
     }
 
     def iar_groups(self, grouped_src):
