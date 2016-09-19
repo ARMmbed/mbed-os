b0c52c50569f63b290fc0d95165cfd34c022aace
Mahadevan Mahesh <Mahesh.Mahadevan@nxp.com>
2016-09-19 09:43:43 -0500

    Update tests to add support for FRDM KL43Z and FRDM KL82Z
    Signed-off-by: Mahadevan Mahesh <Mahesh.Mahadevan@nxp.com>

--- features/unsupported/tests/rtos/mbed/file/main.cpp
+++ features/unsupported/tests/rtos/mbed/file/main.cpp
@@ -24,7 +24,7 @@ void sd_thread(void const *argument)
 #if defined(TARGET_KL25Z)
     SDFileSystem sd(PTD2, PTD3, PTD1, PTD0, "sd");
 
-#elif defined(TARGET_KL46Z)
+#elif defined(TARGET_KL46Z) || defined(TARGET_KL43Z) || defined(TARGET_KL82Z)
     SDFileSystem sd(PTD6, PTD7, PTD5, PTD4, "sd");
 
 #elif defined(TARGET_K64F) || defined(TARGET_K66F)
