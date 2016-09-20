a30aa8f21f37adabbe695688ffb3f302e3b625ac
Bartek Szatkowski <bulislaw@linux.com>
2016-09-20 16:17:00 +0100

    EMAC: Add LWIP adaptation layer

--- features/net/FEATURE_IPV4/lwip-interface/eth_arch.h
+++ features/net/FEATURE_IPV4/lwip-interface/eth_arch.h
@@ -37,6 +37,10 @@ void emac_lwip_disable_interrupts(void);
 #else /* DEVICE_EMAC */
 void eth_arch_enable_interrupts(void);
 void eth_arch_disable_interrupts(void);
+
+#if DEVICE_EMAC
+err_t emac_lwip_if_init(struct netif *netif)
+#else /* DEVICE_EMAC */
 err_t eth_arch_enetif_init(struct netif *netif);
 #endif /* DEVICE_MAC */
 
