1ac09d3a19447d8175d88dfcb49a54e03af33da8
Mahadevan Mahesh <Mahesh.Mahadevan@nxp.com>
2016-09-20 12:49:47 -0500

    Add support for FRDM-K82F
    Signed-off-by: Mahadevan Mahesh <Mahesh.Mahadevan@nxp.com>

--- targets/TARGET_Freescale/mbed_rtx.h
+++ targets/TARGET_Freescale/mbed_rtx.h
@@ -217,6 +217,21 @@
 #define OS_CLOCK                48000000
 #endif
 
+#elif defined(TARGET_K82F)
+
+#ifndef INITIAL_SP
+#define INITIAL_SP              (0x20030000UL)
+#endif
+#ifndef OS_TASKCNT
+#define OS_TASKCNT              14
+#endif
+#ifndef OS_MAINSTKSIZE
+#define OS_MAINSTKSIZE          256
+#endif
+#ifndef OS_CLOCK
+#define OS_CLOCK                120000000
+#endif
+
 #endif
 
 #endif  // MBED_MBED_RTX_H
