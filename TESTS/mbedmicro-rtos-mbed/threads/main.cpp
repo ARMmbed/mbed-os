52f70b06d037f6aeda3bf46808afc1d519d6933b
jeromecoutant <jerome.coutant@st.com>
2016-09-27 10:58:06 +0200

    [STM32F091RC] patch for tests-mbedmicro-rtos-mbed-threads

--- TESTS/mbedmicro-rtos-mbed/threads/main.cpp
+++ TESTS/mbedmicro-rtos-mbed/threads/main.cpp
@@ -17,7 +17,7 @@
  */
 #if defined(TARGET_MCU_NRF51822) || defined(TARGET_MCU_NRF52832)
     #define STACK_SIZE 512
-#elif (defined(TARGET_STM32F070RB) || defined(TARGET_STM32F072RB) || defined(TARGET_STM32F103RB))
+#elif defined(TARGET_STM32F070RB) || defined(TARGET_STM32F072RB) || defined(TARGET_STM32F103RB) || defined(TARGET_STM32F091RC)
     #define STACK_SIZE 512
 #else
     #define STACK_SIZE DEFAULT_STACK_SIZE
