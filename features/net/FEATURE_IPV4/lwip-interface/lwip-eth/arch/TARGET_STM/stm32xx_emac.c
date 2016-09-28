7ec5de8c780a4615e2fbfe0fc848519a44bd4487
jeromecoutant <jerome.coutant@st.com>
2016-09-28 09:10:25 +0200

    space correction

--- features/net/FEATURE_IPV4/lwip-interface/lwip-eth/arch/TARGET_STM/stm32xx_emac.c
+++ features/net/FEATURE_IPV4/lwip-interface/lwip-eth/arch/TARGET_STM/stm32xx_emac.c
@@ -440,12 +440,12 @@ void eth_arch_disable_interrupts(void)
 */
 
 void mbed_mac_address(char *mac) {
-	if (mbed_otp_mac_address(mac)) {
-		return;
-	} else {
-		mbed_default_mac_address(mac);
-	}
-	return;
+    if (mbed_otp_mac_address(mac)) {
+        return;
+    } else {
+        mbed_default_mac_address(mac);
+    }
+    return;
 }
 
 __weak uint8_t mbed_otp_mac_address(char *mac) {
