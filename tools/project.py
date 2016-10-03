0c76ba54ffd4cc79fe0167d15f33d679babde80d
Brian Daniels <brianddaniels@gmail.com>
2016-10-03 14:36:17 -0500

    Fixing python import errors

--- tools/project.py
+++ tools/project.py
@@ -10,8 +10,7 @@ from shutil import move, rmtree
 from argparse import ArgumentParser
 from os.path import normpath, realpath
 
-from tools.paths import EXPORT_DIR, MBED_DRIVERS, MBED_PLATFORM, \
-    MBED_HAL, MBED_TARGETS_PATH, MBED_LIBRARIES
+from tools.paths import EXPORT_DIR, MBED_HAL, MBED_LIBRARIES
 from tools.export import EXPORTERS, mcu_ide_matrix
 from tools.tests import TESTS, TEST_MAP
 from tools.tests import test_known, test_name_known, Test
@@ -19,7 +18,7 @@ from tools.targets import TARGET_NAMES
 from tools.utils import argparse_filestring_type, argparse_many, args_error
 from tools.utils import argparse_force_lowercase_type
 from tools.utils import argparse_force_uppercase_type
-from tools.project_api import export_project
+from tools.project_api import export_project, get_exporter_toolchain
 from tools.options import extract_profile
 
 
@@ -55,9 +54,6 @@ def setup_project(ide, target, program=None, source_dir=None, build=None, export
             if MBED_LIBRARIES in test.dependencies:
                 test.dependencies.remove(MBED_LIBRARIES)
                 test.dependencies.append(MBED_HAL)
-                test.dependencies.append(MBED_DRIVERS)
-                test.dependencies.append(MBED_PLATFORM)
-                test.dependencies.append(MBED_TARGETS_PATH)
 
 
         src_paths = [test.source_dir]
