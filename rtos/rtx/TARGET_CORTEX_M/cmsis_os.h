15f0b8d12c6faf8a9c0d22e8c736ac975b5a0aae
Russ Butler <russ.butler@arm.com>
2016-10-10 15:27:51 -0500

    Revert "Turn on main thread stack checking"
    This reverts commit 81382ebdb2332b1032cbe6b11b3a89bbbbf93214.

--- rtos/rtx/TARGET_CORTEX_M/cmsis_os.h
+++ rtos/rtx/TARGET_CORTEX_M/cmsis_os.h
@@ -83,6 +83,14 @@
 #define OS_TIMERS   0
 #endif
 
+/* If os timers macro is set to 0, there's no timer thread created, therefore
+ * main thread has tid 0x01  
+ */
+#if defined(OS_TIMERS) && (OS_TIMERS == 0)
+#define MAIN_THREAD_ID 0x01
+#else
+#define MAIN_THREAD_ID 0x02
+#endif
 #endif
 
 #if defined(TARGET_XDOT_L151CC)
