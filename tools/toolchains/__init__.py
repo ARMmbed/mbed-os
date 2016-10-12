376ab4079e9bb370444715716bba5a3159625676
Kevin Gillespie <kgills@gmail.com>
2016-10-12 10:14:28 -0500

    [Tools] Adding Target and Toolchain definitions to assembly commands.

--- tools/toolchains/__init__.py
+++ tools/toolchains/__init__.py
@@ -413,7 +413,10 @@ class mbedToolchain:
     def get_symbols(self, for_asm=False):
         if for_asm:
             if self.asm_symbols is None:
-                self.asm_symbols = []
+                # Target and Toolchain symbols
+                labels = self.get_labels()
+                self.asm_symbols = ["TARGET_%s" % t for t in labels['TARGET']]
+                self.asm_symbols.extend(["TOOLCHAIN_%s" % t for t in labels['TOOLCHAIN']])
 
                 # Cortex CPU symbols
                 if self.target.core in mbedToolchain.CORTEX_SYMBOLS:
