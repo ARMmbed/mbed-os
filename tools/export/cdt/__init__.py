5c35230657b5f6344bcb44fcb4f829af643a937c
Jimmy Brisson <theotherjimmy@gmail.com>
2016-10-17 14:51:28 -0500

    Remove unneeded relpath calculations

--- tools/export/cdt/__init__.py
+++ tools/export/cdt/__init__.py
@@ -31,12 +31,8 @@ class Eclipse(Makefile):
         self.gen_file('cdt/necessary_software.tmpl', ctx,
                       join('eclipse-extras','necessary_software.p2f'))
 
-        cproj = relpath('.cproject',self.export_dir)
-        self.gen_file('cdt/.cproject.tmpl', ctx,
-                      cproj)
-        proj = relpath('.project',self.export_dir)
-        self.gen_file('cdt/.project.tmpl', ctx,
-                      proj)
+        self.gen_file('cdt/.cproject.tmpl', ctx, '.cproject')
+        self.gen_file('cdt/.project.tmpl', ctx, '.project')
 
 
 class EclipseGcc(Eclipse, GccArm):
