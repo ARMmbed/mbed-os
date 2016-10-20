abc3c8d7937e3a56027199e20125101481c0b94b
Laurent MEUNIER <laurent.meunier@st.com>
2016-10-20 17:57:48 +0200

    STM32: I2C: Change the slave API implementation to use ITs
    With this new implementation, the slave use the Interrupt
    to be notified of a request from master, instead of
    accessing to registers continuously.
    
    This has 2 main advantages:
    - this shall improve performances overall and allows for sleep
    time in the future
    - this also removes some direct registers access from this
    layer of code and makes it more generic among families

--- targets/TARGET_STM/TARGET_STM32F4/common_objects.h
+++ targets/TARGET_STM/TARGET_STM32F4/common_objects.h
@@ -93,10 +93,14 @@ struct i2c_s {
     uint8_t index;
     IRQn_Type event_i2cIRQ;
     IRQn_Type error_i2cIRQ;
+    volatile uint8_t event;
+#if DEVICE_I2CSLAVE
     uint8_t slave;
+    volatile uint8_t pending_slave_tx_master_rx;
+    volatile uint8_t pending_slave_rx_maxter_tx;
+#endif
 #if DEVICE_I2C_ASYNCH
     uint32_t address;
-    uint8_t event;
     uint8_t stop;
     uint8_t available_events;
     uint8_t XferOperation;
