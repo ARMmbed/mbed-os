fa29e60f6062a61a0d15db70547fd8516750d30e
Steven Cooreman <steven.cooreman@silabs.com>
2016-10-22 16:40:38 +0200

    [EFM32] Condense common header files
    First wave of squashing header files in order to get to a more sensible folder tree.
    * Moved header files with 100% similarity to common folder
    * Created a CommonPinNames.h header containing the base pin names P[A-F][0-15] and NC

--- targets/TARGET_Silicon_Labs/TARGET_EFM32/TARGET_EFM32PG_STK3401/PinNames.h
+++ targets/TARGET_Silicon_Labs/TARGET_EFM32/TARGET_EFM32PG_STK3401/PinNames.h
@@ -23,29 +23,14 @@
 #ifndef MBED_PINNAMES_H
 #define MBED_PINNAMES_H
 
-#include "cmsis.h"
-#include "em_gpio.h"
+#include "CommonPinNames.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
-    PIN_INPUT = 0,
-    PIN_OUTPUT = 1
-} PinDirection;
-
-typedef enum {
-    /* EFM32 Pin Names
-     * First 4 bits represent pin number, the remaining
-     * bits represent port number (A = 0, B = 1, ...)
-     */
-    PA0 = 0 << 4, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA11, PA12, PA13, PA14, PA15,
-    PB0 = 1 << 4, PB1, PB2, PB3, PB4, PB5, PB6, PB7, PB8, PB9, PB10, PB11, PB12, PB13, PB14, PB15,
-    PC0 = 2 << 4, PC1, PC2, PC3, PC4, PC5, PC6, PC7, PC8, PC9, PC10, PC11, PC12, PC13, PC14, PC15,
-    PD0 = 3 << 4, PD1, PD2, PD3, PD4, PD5, PD6, PD7, PD8, PD9, PD10, PD11, PD12, PD13, PD14, PD15,
-    PE0 = 4 << 4, PE1, PE2, PE3, PE4, PE5, PE6, PE7, PE8, PE9, PE10, PE11, PE12, PE13, PE14, PE15,
-    PF0 = 5 << 4, PF1, PF2, PF3, PF4, PF5, PF6, PF7, PF8, PF9, PF10, PF11, PF12, PF13, PF14, PF15,
+    EFM32_STANDARD_PIN_DEFINITIONS,
 
     /* Starter Kit says LED0 and LED1, but mbed expects 1 and 2. This way using 1 and 2 or 0 and 1 will work. */
     LED0 = PF4,
@@ -67,47 +52,8 @@ typedef enum {
     /* Board Controller UART (USB) + enable pin */
     USBTX       = PA0,
     USBRX       = PA1,
-    EFM_BC_EN   = PA5,
-
-    /* Not connected */
-    NC = (int) 0xFFFFFFFF
+    EFM_BC_EN   = PA5
 } PinName;
-
-
-/* 0x10 represents setting the DOUT register (see efm32 modes in ref. manual) */
-typedef enum {
-    /* EFM32 pin modes */
-    Disabled             = gpioModeDisabled,
-    DisabledPullUp       = gpioModeDisabled | 0x10,
-    Input                = gpioModeInput,
-    InputFilter          = gpioModeInput | 0x10,
-    InputPullDown        = gpioModeInputPull,
-    InputPullUp          = gpioModeInputPull | 0x10,
-    InputPullFilterDown  = gpioModeInputPullFilter,
-    InputPullFilterUp    = gpioModeInputPullFilter | 0x10,
-    PushPull             = gpioModePushPull,
-    WiredOr              = gpioModeWiredOr,
-    WiredOrPullDown      = gpioModeWiredOrPullDown,
-    WiredAnd             = gpioModeWiredAnd,
-    WiredAndFilter       = gpioModeWiredAndFilter,
-    WiredAndPullUp       = gpioModeWiredAndPullUp,
-    WiredAndPullUpFilter = gpioModeWiredAndPullUpFilter,
-
-    /* mbed modes:
-     * PullUp, PullDown, PullNone, OpenDrain
-     *
-     * mbed default digital input mode:
-     * PullDefault
-     *
-     * mbed default digital output mode:
-     * PullNone
-     */
-    PullUp = InputPullUp,
-    PullDown = InputPullDown,
-    OpenDrain = WiredAnd,
-    PullNone = PushPull,
-    PullDefault = PushPull
-} PinMode;
 
 #ifdef __cplusplus
 }
