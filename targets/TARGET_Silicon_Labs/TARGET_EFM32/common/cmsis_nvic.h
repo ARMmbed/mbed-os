f7ca57c61ba6ae4482d3382ddfda5e896ff4e813
Steven Cooreman <steven.cooreman@silabs.com>
2016-10-22 21:06:26 +0200

    [EFM32] Collapse NVIC relocation handling
    Gecko SDK 5.0.0 provides a convenient define for the amount of vectors wired on the chip, so we can use that to collapse the cmsis_nvic.h header

--- targets/TARGET_Silicon_Labs/TARGET_EFM32/common/cmsis_nvic.h
+++ targets/TARGET_Silicon_Labs/TARGET_EFM32/common/cmsis_nvic.h
@@ -2,22 +2,22 @@
  * Copyright (c) 2009-2011 ARM Limited. All rights reserved.
  *
  * CMSIS-style functionality to support dynamic vectors
- */
+ */ 
 
 #ifndef MBED_CMSIS_NVIC_H
 #define MBED_CMSIS_NVIC_H
 
 #include "cmsis.h"
 
-#define NVIC_NUM_VECTORS      (16 + 19)   // CORE + MCU Peripherals
+#define NVIC_NUM_VECTORS      (16 + EXT_IRQ_COUNT)   // CORE + MCU Peripherals
 #define NVIC_USER_IRQ_OFFSET  16
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void NVIC_SetVector(IRQn_Type IRQn, uint32_t vector);
-uint32_t NVIC_GetVector(IRQn_Type IRQn);
+void __NVIC_SetVector(IRQn_Type IRQn, uint32_t vector);
+uint32_t __NVIC_GetVector(IRQn_Type IRQn);
 
 #ifdef __cplusplus
 }
