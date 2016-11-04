2922de8dffa4e81a19bac0af4c9efa2e5736740b
ccli8 <ccli8@nuvoton.com>
2016-11-04 15:41:34 +0800

    [NUC472/M453] Remove dead code in device.h

--- targets/TARGET_NUVOTON/TARGET_M451/TARGET_NUMAKER_PFM_M453/device.h
+++ targets/TARGET_NUVOTON/TARGET_M451/TARGET_NUMAKER_PFM_M453/device.h
@@ -17,46 +17,7 @@
 #ifndef MBED_DEVICE_H
 #define MBED_DEVICE_H
 
-#define DEVICE_PORTIN           1
-#define DEVICE_PORTOUT          1
-#define DEVICE_PORTINOUT        1
-
-#define DEVICE_INTERRUPTIN      1
-
-#define DEVICE_ANALOGIN         1
-#define DEVICE_ANALOGOUT        0
-
-#define DEVICE_SERIAL           1
-#define DEVICE_SERIAL_FC        1
-#define DEVICE_SERIAL_ASYNCH    1
-
-#define DEVICE_I2C              1
-#define DEVICE_I2CSLAVE         1
-#define DEVICE_I2C_ASYNCH       1
-
-#define DEVICE_SPI              1
-#define DEVICE_SPI_ASYNCH       1
-#define DEVICE_SPISLAVE         1
-
-#define DEVICE_RTC              1
-
-#define DEVICE_ETHERNET         0
-
-#define DEVICE_PWMOUT           1
-
-#define DEVICE_SEMIHOST         0
-#define DEVICE_LOCALFILESYSTEM  0
 #define DEVICE_ID_LENGTH       24
-
-#define DEVICE_SLEEP            1
-
-#define DEVICE_DEBUG_AWARENESS  0
-
-#define DEVICE_STDIO_MESSAGES   1
-
-#define DEVICE_ERROR_RED        0
-
-#define DEVICE_LOWPOWERTIMER    1
 
 #include "objects.h"
 
