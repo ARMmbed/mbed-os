e293e077492265884730594c002ba716d779b34d
Laurent MEUNIER <laurent.meunier@st.com>
2016-11-10 17:30:04 +0100

    stm32f4 make comment more explicit
    As reported during review, this was not understandable as it is.
    the get_i2c_obj allows to get a pointer to i2c_s struct from the
    handle pointer. This therefore makes a hard-coded assumption
    about the struct itself

--- targets/TARGET_STM/TARGET_STM32F4/common_objects.h
+++ targets/TARGET_STM/TARGET_STM32F4/common_objects.h
@@ -87,6 +87,7 @@ struct i2c_s {
     /*  The 1st 2 members I2CName i2c
      *  and I2C_HandleTypeDef handle should
      *  be kept as the first members of this struct
+     *  to have get_i2c_obj() function work as expected
      */
     I2CName  i2c;
     I2C_HandleTypeDef handle;
