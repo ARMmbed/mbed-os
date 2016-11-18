7633015e0efa6048689efe62f5e0dfee6b8dc09d
Christopher Haster <chaster@utexas.edu>
2016-11-18 17:10:44 -0600

    Added the FATFileSystem from mbed 2
    Relatively unmodified, except for the addition of support for
    interchangeable block devices

---
+++ features/TESTS/filesystem/fat_file_system/main.cpp
@@ -0,0 +1,80 @@
+#include "mbed.h"
+#include "greentea-client/test_env.h"
+#include "unity.h"
+#include "utest.h"
+
+#include "MemBlockDevice.h"
+#include "FATFileSystem.h"
+#include <stdlib.h>
+
+using namespace utest::v1;
+
+// Test block device
+#define BLOCK_SIZE 512
+MemBlockDevice bd(BLOCK_SIZE, 128);
+
+
+void test_format() {
+    int err = FATFileSystem::format(&bd);
+    TEST_ASSERT_EQUAL(0, err);
+}
+
+template <ssize_t TEST_SIZE>
+void test_read_write() {
+    FATFileSystem fs("fat");
+
+    int err = fs.mount(&bd);
+    TEST_ASSERT_EQUAL(0, err);
+
+    uint8_t *buffer = (uint8_t *)malloc(TEST_SIZE);
+    TEST_ASSERT(buffer);
+    
+    // Fill with random sequence
+    srand(1);
+    for (int i = 0; i < TEST_SIZE; i++) {
+        buffer[i] = 0xff & rand();
+    }
+
+    // write and read file
+    FileHandle *file = fs.open("test_read_write.dat", O_WRONLY | O_CREAT);
+    TEST_ASSERT(file);
+    ssize_t size = file->write(buffer, TEST_SIZE);
+    TEST_ASSERT_EQUAL(TEST_SIZE, size);
+    err = file->close();
+    TEST_ASSERT_EQUAL(0, err);
+
+    file = fs.open("test_read_write.dat", O_RDONLY);
+    TEST_ASSERT(file);
+    size = file->read(buffer, TEST_SIZE);
+    TEST_ASSERT_EQUAL(TEST_SIZE, size);
+    err = file->close();
+    TEST_ASSERT_EQUAL(0, err);
+
+    // Check that the data was unmodified
+    srand(1);
+    for (int i = 0; i < TEST_SIZE; i++) {
+        TEST_ASSERT_EQUAL(0xff & rand(), buffer[i]);
+    }
+
+    err = fs.unmount();
+    TEST_ASSERT_EQUAL(0, err);
+}
+
+
+// Test setup
+utest::v1::status_t test_setup(const size_t number_of_cases) {
+    GREENTEA_SETUP(10, "default_auto");
+    return verbose_test_setup_handler(number_of_cases);
+}
+
+Case cases[] = {
+    Case("Testing formating", test_format),
+    Case("Testing read write < block", test_read_write<BLOCK_SIZE/2>),
+    Case("Testing read write > block", test_read_write<2*BLOCK_SIZE>),
+};
+
+Specification specification(test_setup, cases);
+
+int main() {
+    return !Harness::run(specification);
+}
--- features/filesystem/fat/FATFileSystem.cpp
+++ features/filesystem/fat/FATFileSystem.cpp
@@ -21,6 +21,7 @@
  */
 #include "mbed.h"
 
+#include "diskio.h"
 #include "ffconf.h"
 #include "mbed_debug.h"
 
@@ -29,6 +30,13 @@
 #include "FATDirHandle.h"
 #include "critical.h"
 
+
+// Global access to block device from FAT driver
+static BlockDevice *_ffs[_VOLUMES] = {0};
+static SingletonPtr<PlatformMutex> _ffs_mutex;
+
+
+// FAT driver functions
 DWORD get_fattime(void) {
     time_t rawtime;
     time(&rawtime);
@@ -41,51 +49,163 @@ DWORD get_fattime(void) {
          | (DWORD)(ptm->tm_sec/2    );
 }
 
-FATFileSystem *FATFileSystem::_ffs[_VOLUMES] = {0};
-static PlatformMutex * mutex = NULL;
-
-PlatformMutex * get_fat_mutex() {
-    PlatformMutex * new_mutex = new PlatformMutex;
-
-    core_util_critical_section_enter();
-    if (NULL == mutex) {
-        mutex = new_mutex;
-    }
-    core_util_critical_section_exit();
-
-    if (mutex != new_mutex) {
-        delete new_mutex;
-    }
-    return mutex;
+// Implementation of diskio functions (see ChaN/diskio.h)
+DSTATUS disk_status(BYTE pdrv) {
+    debug_if(FFS_DBG, "disk_status on pdrv [%d]\n", pdrv);
+    return (DSTATUS)_ffs[pdrv]->status();
 }
 
-FATFileSystem::FATFileSystem(const char* n) : FileSystemLike(n), _mutex(get_fat_mutex()) {
-    lock();
-    debug_if(FFS_DBG, "FATFileSystem(%s)\n", n);
-    for(int i=0; i<_VOLUMES; i++) {
-        if(_ffs[i] == 0) {
-            _ffs[i] = this;
-            _fsid[0] = '0' + i;
-            _fsid[1] = '\0';
-            debug_if(FFS_DBG, "Mounting [%s] on ffs drive [%s]\n", getName(), _fsid);
-            f_mount(&_fs, _fsid, 0);
-            unlock();
-            return;
-        }
+DSTATUS disk_initialize(BYTE pdrv) {
+    debug_if(FFS_DBG, "disk_initialize on pdrv [%d]\n", pdrv);
+    return (DSTATUS)_ffs[pdrv]->init();
+}
+
+DRESULT disk_read(BYTE pdrv, BYTE *buff, DWORD sector, UINT count) {
+    debug_if(FFS_DBG, "disk_read(sector %d, count %d) on pdrv [%d]\n", sector, count, pdrv);
+    int res = _ffs[pdrv]->read(buff, sector, count);
+    return (res != (int)count) ? RES_PARERR : RES_OK;
+}
+
+DRESULT disk_write(BYTE pdrv, const BYTE *buff, DWORD sector, UINT count) {
+    debug_if(FFS_DBG, "disk_write(sector %d, count %d) on pdrv [%d]\n", sector, count, pdrv);
+    int res = _ffs[pdrv]->write(buff, sector, count);
+    return (res != (int)count) ? RES_PARERR : RES_OK;
+}
+
+DRESULT disk_ioctl(BYTE pdrv, BYTE cmd, void *buff) {
+    debug_if(FFS_DBG, "disk_ioctl(%d)\n", cmd);
+    switch (cmd) {
+        case CTRL_SYNC:
+            if (_ffs[pdrv] == NULL) {
+                return RES_NOTRDY;
+            } else {
+                int err = _ffs[pdrv]->sync();
+                return err ? RES_ERROR : RES_OK;
+            }
+        case GET_SECTOR_COUNT:
+            if (_ffs[pdrv] == NULL) {
+                return RES_NOTRDY;
+            } else {
+                DWORD res = _ffs[pdrv]->get_block_count();
+                if (res > 0) {
+                    *((DWORD*)buff) = res; // minimum allowed
+                    return RES_OK;
+                } else {
+                    return RES_ERROR;
+                }
+            }
+        case GET_SECTOR_SIZE:
+            if (_ffs[pdrv] == NULL) {
+                return RES_NOTRDY;
+            } else {
+                DWORD res = _ffs[pdrv]->get_block_size();
+                if (res > 0) {
+                    *((DWORD*)buff) = res; // minimum allowed
+                    return RES_OK;
+                } else {
+                    return RES_ERROR;
+                }
+            }
+        case GET_BLOCK_SIZE:
+            *((DWORD*)buff) = 1; // default when not known
+            return RES_OK;
     }
-    error("Couldn't create %s in FATFileSystem::FATFileSystem\n", n);
-    unlock();
+
+    return RES_PARERR;
+}
+
+
+// Filesystem implementation (See FATFilySystem.h)
+FATFileSystem::FATFileSystem(const char *n, BlockDevice *bd)
+        : FileSystemLike(n), _id(-1) {
+    if (bd) {
+        mount(bd);
+    }
 }
 
 FATFileSystem::~FATFileSystem() {
+    // nop if unmounted
+    unmount();
+}
+
+int FATFileSystem::mount(BlockDevice *bd) {
+    return mount(bd, true);
+}
+
+int FATFileSystem::mount(BlockDevice *bd, bool force) {
     lock();
-    for (int i=0; i<_VOLUMES; i++) {
-        if (_ffs[i] == this) {
-            _ffs[i] = 0;
-            f_mount(NULL, _fsid, 0);
+    if (_id != -1) {
+        unlock();
+        return -1;
+    }
+
+    for (int i = 0; i < _VOLUMES; i++) {
+        if (!_ffs[i]) {
+            _id = i;
+            _ffs[_id] = bd;
+            _fsid[0] = '0' + _id;
+            _fsid[1] = '\0';
+            debug_if(FFS_DBG, "Mounting [%s] on ffs drive [%s]\n", getName(), _fsid);
+            FRESULT res = f_mount(&_fs, _fsid, force);
+            unlock();
+            return res == 0 ? 0 : -1;
         }
     }
+
     unlock();
+    return -1;
+}
+
+int FATFileSystem::unmount() {
+    lock();
+    if (_id == -1) {
+        unlock();
+        return -1;
+    }
+
+    int err = _ffs[_id]->sync();
+    if (err) {
+        unlock();
+        return -1;
+    }
+
+    FRESULT res = f_mount(NULL, _fsid, 0);
+    _ffs[_id] = NULL;
+    _id = -1;
+    unlock();
+    return res == 0 ? 0 : -1;
+}
+
+int FATFileSystem::sync() {
+    lock();
+    if (_id == -1) {
+        unlock();
+        return -1;
+    }
+
+    int err = _ffs[_id]->sync();
+    unlock();
+    return err ? -1 : 0;
+}
+
+int FATFileSystem::format(BlockDevice *bd) {
+    FATFileSystem fs("");
+    int err = fs.mount(bd, false);
+    if (err) {
+        return -1;
+    }
+
+    // Logical drive number, Partitioning rule, Allocation unit size (bytes per cluster)
+    fs.lock();
+    FRESULT res = f_mkfs(fs._fsid, 0, 512); 
+    fs.unlock();
+
+    err = fs.unmount();
+    if (err) {
+        return -1;
+    }
+
+    return res == 0 ? 0 : -1;
 }
 
 FileHandle *FATFileSystem::open(const char* name, int flags) {
@@ -121,7 +241,7 @@ FileHandle *FATFileSystem::open(const char* name, int flags) {
     if (flags & O_APPEND) {
         f_lseek(&fh, fh.fsize);
     }
-    FATFileHandle * handle = new FATFileHandle(fh, _mutex);
+    FATFileHandle *handle = new FATFileHandle(fh, _ffs_mutex.get());
     unlock();
     return handle;
 }
@@ -150,18 +270,6 @@ int FATFileSystem::rename(const char *oldname, const char *newname) {
     return 0;
 }
 
-int FATFileSystem::format() {
-    lock();
-    FRESULT res = f_mkfs(_fsid, 0, 512); // Logical drive number, Partitioning rule, Allocation unit size (bytes per cluster)
-    if (res) {
-        debug_if(FFS_DBG, "f_mkfs() failed: %d\n", res);
-        unlock();
-        return -1;
-    }
-    unlock();
-    return 0;
-}
-
 DirHandle *FATFileSystem::opendir(const char *name) {
     lock();
     FATFS_DIR dir;
@@ -170,7 +278,7 @@ DirHandle *FATFileSystem::opendir(const char *name) {
         unlock();
         return NULL;
     }
-    FATDirHandle *handle = new FATDirHandle(dir, _mutex);
+    FATDirHandle *handle = new FATDirHandle(dir, _ffs_mutex.get());
     unlock();
     return handle;
 }
@@ -182,28 +290,10 @@ int FATFileSystem::mkdir(const char *name, mode_t mode) {
     return res == 0 ? 0 : -1;
 }
 
-int FATFileSystem::mount() {
-    lock();
-    FRESULT res = f_mount(&_fs, _fsid, 1);
-    unlock();
-    return res == 0 ? 0 : -1;
-}
-
-int FATFileSystem::unmount() {
-    lock();
-    if (disk_sync()) {
-        unlock();
-        return -1;
-    }
-    FRESULT res = f_mount(NULL, _fsid, 0);
-    unlock();
-    return res == 0 ? 0 : -1;
-}
-
 void FATFileSystem::lock() {
-    _mutex->lock();
+    _ffs_mutex->lock();
 }
 
 void FATFileSystem::unlock() {
-    _mutex->unlock();
+    _ffs_mutex->unlock();
 }
