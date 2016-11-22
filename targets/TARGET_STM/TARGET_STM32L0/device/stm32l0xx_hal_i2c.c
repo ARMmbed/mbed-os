77576190c6dc399082288c23468e079574f6f32a
Laurent MEUNIER <laurent.meunier@st.com>
2016-11-22 14:43:26 +0100

    HAL I2C (V2) sequential transmit / receive
    In case of sequential transmit / receive, there is a need to:
    - not use the reload option
    - generate a new START on each new transaction
    
    This applies to all HAL supporting the IP version V2.

--- targets/TARGET_STM/TARGET_STM32L0/device/stm32l0xx_hal_i2c.c
+++ targets/TARGET_STM/TARGET_STM32L0/device/stm32l0xx_hal_i2c.c
@@ -2591,7 +2591,7 @@ HAL_StatusTypeDef HAL_I2C_Master_Sequential_Transmit_IT(I2C_HandleTypeDef *hi2c,
     /* Prepare transfer parameters */
     hi2c->pBuffPtr    = pData;
     hi2c->XferCount   = Size;
-    hi2c->XferOptions = XferOptions;
+    hi2c->XferOptions = (XferOptions & (~I2C_RELOAD_MODE));
     hi2c->XferISR     = I2C_Master_ISR_IT;
 
     /* If size > MAX_NBYTE_SIZE, use reload mode */
@@ -2604,13 +2604,6 @@ HAL_StatusTypeDef HAL_I2C_Master_Sequential_Transmit_IT(I2C_HandleTypeDef *hi2c,
     {
       hi2c->XferSize = hi2c->XferCount;
       xfermode = hi2c->XferOptions;
-    }
-
-    /* If transfer direction not change, do not generate Restart Condition */
-    /* Mean Previous state is same as current state */
-    if(hi2c->PreviousState == I2C_STATE_MASTER_BUSY_TX)
-    {
-      xferrequest = I2C_NO_STARTSTOP;
     }
 
     /* Send Slave Address and set NBYTES to write */
@@ -2664,7 +2657,7 @@ HAL_StatusTypeDef HAL_I2C_Master_Sequential_Receive_IT(I2C_HandleTypeDef *hi2c, 
     /* Prepare transfer parameters */
     hi2c->pBuffPtr    = pData;
     hi2c->XferCount   = Size;
-    hi2c->XferOptions = XferOptions;
+    hi2c->XferOptions = (XferOptions & (~I2C_RELOAD_MODE));
     hi2c->XferISR     = I2C_Master_ISR_IT;
 
     /* If hi2c->XferCount > MAX_NBYTE_SIZE, use reload mode */
@@ -2677,13 +2670,6 @@ HAL_StatusTypeDef HAL_I2C_Master_Sequential_Receive_IT(I2C_HandleTypeDef *hi2c, 
     {
       hi2c->XferSize = hi2c->XferCount;
       xfermode = hi2c->XferOptions;
-    }
-
-    /* If transfer direction not change, do not generate Restart Condition */
-    /* Mean Previous state is same as current state */
-    if(hi2c->PreviousState == I2C_STATE_MASTER_BUSY_RX)
-    {
-      xferrequest = I2C_NO_STARTSTOP;
     }
 
     /* Send Slave Address and set NBYTES to read */
