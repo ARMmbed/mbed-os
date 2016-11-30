3a53a5adc8668b4ee1592ec74b135afba9ad8f45
adustm <adustm13@gmail.com>
2016-11-30 15:53:21 +0000

    DISCO_F429ZI: Add support for uVisor

--- targets/TARGET_STM/TARGET_STM32F4/device/stm32f4xx_hal.c
+++ targets/TARGET_STM/TARGET_STM32F4/device/stm32f4xx_hal.c
@@ -179,9 +179,6 @@ HAL_StatusTypeDef HAL_Init(void)
   __HAL_FLASH_PREFETCH_BUFFER_ENABLE();
 #endif /* PREFETCH_ENABLE */
 
-  /* Set Interrupt Group Priority */
-  HAL_NVIC_SetPriorityGrouping(NVIC_PRIORITYGROUP_4);
-
   /* Use systick as time base source and configure 1ms tick (default clock after Reset is HSI) */
   HAL_InitTick(TICK_INT_PRIORITY);
   
