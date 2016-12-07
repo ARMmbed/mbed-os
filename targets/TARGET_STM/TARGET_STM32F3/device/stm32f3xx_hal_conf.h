218d9c61a71aab7f71169f1620d9b7ebfb7b2fe6
jeromecoutant <jerome.coutant@st.com>
2016-12-07 15:09:55 +0100

    STM32F3 : map ST HAL assert into MBED assert

--- targets/TARGET_STM/TARGET_STM32F3/device/stm32f3xx_hal_conf.h
+++ targets/TARGET_STM/TARGET_STM32F3/device/stm32f3xx_hal_conf.h
@@ -321,9 +321,8 @@
   *         If expr is true, it returns no value.
   * @retval None
   */
-  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
-/* Exported functions ------------------------------------------------------- */
-  void assert_failed(uint8_t* file, uint32_t line);
+  #include "mbed_assert.h"
+  #define assert_param(expr) MBED_ASSERT(expr)
 #else
   #define assert_param(expr) ((void)0U)
 #endif /* USE_FULL_ASSERT */    
