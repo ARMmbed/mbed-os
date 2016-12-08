3ff2df18755c2577072929d81ce67f7268223e39
ccli8 <ccli8@nuvoton.com>
2016-12-08 16:25:35 +0800

    [NUC472] Fix compile error for SHA-256 alternative on some condition
    Also include non-issue refinement for SHA-1/SHA-256 alternatives.

--- targets/TARGET_NUVOTON/TARGET_NUC472/crypto/sha/sha1_alt_sw.c
+++ targets/TARGET_NUVOTON/TARGET_NUC472/crypto/sha/sha1_alt_sw.c
@@ -36,6 +36,14 @@
 #include "mbedtls/sha1.h"
 
 #include <string.h>
+#if defined(MBEDTLS_SELF_TEST)
+#if defined(MBEDTLS_PLATFORM_C)
+#include "mbedtls/platform.h"
+#else
+#include <stdio.h>
+#define mbedtls_printf printf
+#endif /* MBEDTLS_PLATFORM_C */
+#endif /* MBEDTLS_SELF_TEST */
 
 /* Implementation that should never be optimized out by the compiler */
 static void mbedtls_zeroize( void *v, size_t n ) {
