3402b59ff1d93547eb4543d768f7ec29706185f8
Christopher Haster <chaster@utexas.edu>
2016-12-09 12:22:29 -0600

    lwip - Marked *_packet_pressure tests as "extended tests"
    *_packet_pressure_parallel tests are useful for checking for synchronization
    errors, but push the practical limitations of the network stack. Failing
    these tests is not unreasonable.
    
    *_packet_pressure tests are a little bit less unreasonable, but also
    push the practical limitations of the network stack. Hopefully these
    will become stable in the near future.

--- features/FEATURE_LWIP/TESTS/mbedmicro-net/tcp_packet_pressure_parallel/main.cpp
+++ features/FEATURE_LWIP/TESTS/mbedmicro-net/tcp_packet_pressure_parallel/main.cpp
@@ -1,6 +1,9 @@
 #if !FEATURE_LWIP
     #error [NOT_SUPPORTED] LWIP not supported for this target
 #endif
+#ifndef MBED_EXTENDED_TESTS
+    #error [NOT_SUPPORTED] Parallel pressure tests are not supported by default
+#endif
 
 #include "mbed.h"
 #include "EthernetInterface.h"
