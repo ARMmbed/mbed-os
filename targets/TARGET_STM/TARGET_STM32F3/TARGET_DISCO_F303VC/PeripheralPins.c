f5f3dc81437da7d3703fc7a0b205756ab3e73f08
Jared Szechy <jared.szechy@gmail.com>
2016-12-11 22:03:29 -0500

    Enable CAN on DISCO_F303VC

--- targets/TARGET_STM/TARGET_STM32F3/TARGET_DISCO_F303VC/PeripheralPins.c
+++ targets/TARGET_STM/TARGET_STM32F3/TARGET_DISCO_F303VC/PeripheralPins.c
@@ -342,3 +342,13 @@ const PinMap PinMap_SPI_SSEL[] = {
     {PD_15, SPI_2, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_PULLUP, GPIO_AF6_SPI2)},
     {NC,    NC,    0}
 };
+
+const PinMap PinMap_CAN_RD[] = {
+    {PA_11, CAN_1, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_NOPULL, GPIO_AF9_CAN)},
+    {NC,    NC,    0}
+};
+
+const PinMap PinMap_CAN_TD[] = {
+    {PA_12, CAN_1, STM_PIN_DATA(STM_MODE_AF_PP, GPIO_NOPULL, GPIO_AF9_CAN)},
+    {NC,    NC,    0}
+};
