c82415cc4a65c5e279f4042bfb77ed9b62221549
Christopher Haster <chaster@utexas.edu>
2017-01-05 11:16:24 -0600

    lwip - Filtered network tests based on DEVICE_EMAC
    Currently DEVICE_EMAC is incompatible with the EthernetInterface

--- features/FEATURE_LWIP/TESTS/mbedmicro-net/tcp_packet_pressure/main.cpp
+++ features/FEATURE_LWIP/TESTS/mbedmicro-net/tcp_packet_pressure/main.cpp
@@ -1,6 +1,9 @@
 #if !FEATURE_LWIP
     #error [NOT_SUPPORTED] LWIP not supported for this target
 #endif
+#if DEVICE_EMAC
+    #error [NOT_SUPPORTED] Not supported for WiFi targets
+#endif
 #ifndef MBED_EXTENDED_TESTS
     #error [NOT_SUPPORTED] Pressure tests are not supported by default
 #endif
