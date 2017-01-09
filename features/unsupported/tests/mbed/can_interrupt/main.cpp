04359aa9f41f3ba138ffc3e2ac5cd0958cf30cde
bcostm <bco.stm@gmail.com>
2017-01-09 14:53:41 +0100

    Add NUCLEO_F412ZG in main.cpp files used for the tests

--- features/unsupported/tests/mbed/can_interrupt/main.cpp
+++ features/unsupported/tests/mbed/can_interrupt/main.cpp
@@ -20,6 +20,7 @@ CAN can1(PD_0, PD_1);
       defined(TARGET_NUCLEO_F302R8) || defined(TARGET_NUCLEO_F446RE) || \
       defined(TARGET_DISCO_F429ZI)  || defined(TARGET_NUCLEO_F103RB) || \
       defined(TARGET_NUCLEO_F746ZG) || defined(TARGET_NUCLEO_L476RG) || \
+      defined(TARGET_NUCLEO_F412ZG) || \
       defined(TARGET_NUCLEO_L432KC) || defined(TARGET_DISCO_F303VC)
 CAN can1(PA_11, PA_12);
 #elif defined(TARGET_DISCO_F469NI) || defined(TARGET_DISCO_F746NG)
@@ -34,6 +35,7 @@ CAN can2(p34, p33);
 CAN can2(p30, p29);
 #elif defined(TARGET_NUCLEO_F446RE) || defined(TARGET_DISCO_F469NI) || \
       defined(TARGET_DISCO_F429ZI)  || defined(TARGET_NUCLEO_F746ZG) || \
+      defined(TARGET_NUCLEO_F412ZG) || \
       defined(TARGET_DISCO_F746NG)
 CAN can2(PB_5, PB_6);
 #endif
