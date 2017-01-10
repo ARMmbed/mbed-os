4b87dc3c32109676fd1be94404bbc1e6f60227d6
Jimmy Brisson <theotherjimmy@gmail.com>
2017-01-10 15:24:20 -0600

    Change sorting to ignore case

--- tools/export/uvision/__init__.py
+++ tools/export/uvision/__init__.py
@@ -181,8 +181,9 @@ class Uvision(Exporter):
     def format_src(self, srcs):
         """Make sources into the named tuple for use in the template"""
         grouped = self.group_project_files(srcs)
-        for group, files in sorted(grouped.items()):
-            grouped[group] = sorted(list(self.uv_files(files)))
+        for group, files in grouped.items():
+            grouped[group] = sorted(list(self.uv_files(files)),
+                                    key=lambda (_, __, name): name.lower())
         return grouped
 
     def generate(self):
@@ -198,7 +199,8 @@ class Uvision(Exporter):
             'name': self.project_name,
             # project_files => dict of generators - file group to generator of
             # UVFile tuples defined above
-            'project_files': sorted(list(self.format_src(srcs).iteritems())),
+            'project_files': sorted(list(self.format_src(srcs).iteritems()),
+                                    key=lambda (group, _): group.lower()),
             'linker_script':self.resources.linker_script,
             'include_paths': '; '.join(self.resources.inc_dirs).encode('utf-8'),
             'device': DeviceUvision(self.target),
