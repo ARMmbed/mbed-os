ARMmbed/mbed-os

commit 329be06ad02b82e045c1aadc799f88005276400a

exporters - group by directories in prj root
Update exporter grouping code to group by directories in the root
of the project rather than by the parent directory of each file. This
reduces the number of groups and allows all mbed-os code to reside
in its own folder.

tools/export/exporters.py
@@ -2,7 +2,7 @@
 import os
 from abc import abstractmethod, ABCMeta
 import logging
-from os.path import join, dirname, relpath, basename, realpath
+from os.path import join, dirname, relpath, basename, realpath, normpath
 from itertools import groupby
 from jinja2 import FileSystemLoader
 from jinja2.environment import Environment
@@ -130,9 +130,13 @@ class Exporter(object):
         Positional Arguments:
         src - the src's location
         """
-        key = basename(dirname(src))
-        if key == ".":
-            key = basename(realpath(self.export_dir))
+        rel_path = relpath(src, self.resources.file_basepath[src])
+        path_list = os.path.normpath(rel_path).split(os.sep)
+        assert path_list >= 1
+        if len(path_list) == 1:
+            key = self.project_name
+        else:
+            key = path_list[0]
         return key
 
     def group_project_files(self, sources):

commit 06ca004bfa80edf63ecf29862a97143b3a45d9d9

Fix invalid assert in exporters
Assert that the length is greater than one rather than the value
itself. This bug was introduced in the commit:
329be06ad02b82e045c1aadc799f88005276400a -
"exporters - group by directories in prj root"

tools/export/exporters.py
@@ -132,7 +132,7 @@ class Exporter(object):
         """
         rel_path = relpath(src, self.resources.file_basepath[src])
         path_list = os.path.normpath(rel_path).split(os.sep)
-        assert path_list >= 1
+        assert len(path_list) >= 1
         if len(path_list) == 1:
             key = self.project_name
         else: