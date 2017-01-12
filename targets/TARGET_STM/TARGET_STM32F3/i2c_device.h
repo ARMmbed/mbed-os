dadf996091bf5e1a9319ad9dc87ee25aa81e7628
Brian Daniels <brianddaniels@gmail.com>
2017-01-12 15:09:28 -0600

    Fixes linking errors when building with debug profile
    When building with the debug profile, certain ST plaforms error with
    'get_i2c_timing' not being defined. This is because the function is not
    defined as 'static inline', but just 'inline'.

--- targets/TARGET_STM/TARGET_STM32F3/i2c_device.h
+++ targets/TARGET_STM/TARGET_STM32F3/i2c_device.h
@@ -48,7 +48,7 @@ extern "C" {
 #define I2CAPI_I2C3_CLKSRC RCC_I2C3CLKSOURCE_SYSCLK
 
 /*  Provide the suitable timing depending on requested frequencie */
-inline uint32_t get_i2c_timing(int hz)
+static inline uint32_t get_i2c_timing(int hz)
 {
     uint32_t tim = 0;
     /*
--- targets/TARGET_STM/TARGET_STM32L4/i2c_device.h
+++ targets/TARGET_STM/TARGET_STM32L4/i2c_device.h
@@ -49,7 +49,7 @@ extern "C" {
 #define I2CAPI_I2C4_CLKSRC RCC_I2C4CLKSOURCE_SYSCLK
 
 /*  Provide the suitable timing depending on requested frequencie */
-inline uint32_t get_i2c_timing(int hz)
+static inline uint32_t get_i2c_timing(int hz)
 {
     uint32_t tim = 0;
     if (SystemCoreClock == 80000000) {
