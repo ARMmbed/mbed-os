9b732a32c0114b6e4ce03608520ef9e7310cce22
Sam Grove <sam.w.grove@gmail.com>
2017-02-01 13:09:56 -0600

    [exporters] update import name to match directory simplicityv3 -> simplicity

--- tools/export/__init__.py
+++ tools/export/__init__.py
@@ -16,7 +16,7 @@
 # limitations under the License.
 
 from tools.export import codered, ds5_5, iar, makefile
-from tools.export import embitz, coide, kds, simplicityv3, atmelstudio
+from tools.export import embitz, coide, kds, simplicity, atmelstudio
 from tools.export import sw4stm32, e2studio, zip, cmsis, uvision, cdt, vscode
 from tools.targets import TARGET_NAMES
 
