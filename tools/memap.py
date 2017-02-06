369ca612013db5e33a7693c2eb8049ff5c17311e
JojoS <sn-mail@onlinehome.de>
2017-02-06 19:01:03 +0100

    Create memory map for GCC_CR
    add GCC_CR to memmap tool

--- tools/memap.py
+++ tools/memap.py
@@ -593,7 +593,7 @@ class MemapParser(object):
                    toolchain == "ARM_MICRO":
                     self.search_objects(os.path.abspath(mapfile))
                     self.parse_map_file_armcc(file_input)
-                elif toolchain == "GCC_ARM":
+                elif toolchain == "GCC_ARM" or toolchain == "GCC_CR":
                     self.parse_map_file_gcc(file_input)
                 elif toolchain == "IAR":
                     self.search_objects(os.path.abspath(mapfile))
