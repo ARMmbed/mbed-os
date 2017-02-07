70a2c48b31cfe21c90cbb599fd234719e681e013
JojoS <sn-mail@onlinehome.de>
2017-02-07 12:39:49 +0100

    adding LPC812

--- tools/export/codered/__init__.py
+++ tools/export/codered/__init__.py
@@ -34,8 +34,6 @@ class CodeRed(Exporter):
         'LPC11U35_501',
         'UBLOX_C027',
         'ARCH_PRO',
-        'LPC812',
-        'LPC824',
         'LPC1549',
         'LPC11U68',
         'LPCCAPPUCCINO',
