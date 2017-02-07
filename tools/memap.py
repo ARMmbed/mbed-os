16db3d9154d53e595833301b9772946ca4a1c78d
JojoS <sn-mail@onlinehome.de>
2017-02-07 12:34:18 +0100

    enable GCC_CR in memmap.py
    GCC_CR uses the same toolchain as GCC_ARM, so the memmap tool is working
    for GCC_CR too.

--- tools/memap.py
+++ tools/memap.py
@@ -538,7 +538,7 @@ class MemapParser(object):
 
         return output
 
-    toolchains = ["ARM", "ARM_STD", "ARM_MICRO", "GCC_ARM", "IAR"]
+    toolchains = ["ARM", "ARM_STD", "ARM_MICRO", "GCC_ARM", "GCC_CR", "IAR"]
 
     def compute_report(self):
         for k in self.sections:
@@ -593,7 +593,7 @@ class MemapParser(object):
                    toolchain == "ARM_MICRO":
                     self.search_objects(os.path.abspath(mapfile))
                     self.parse_map_file_armcc(file_input)
-                elif toolchain == "GCC_ARM":
+                elif toolchain == "GCC_ARM" or toolchain == "GCC_CR":
                     self.parse_map_file_gcc(file_input)
                 elif toolchain == "IAR":
                     self.search_objects(os.path.abspath(mapfile))
