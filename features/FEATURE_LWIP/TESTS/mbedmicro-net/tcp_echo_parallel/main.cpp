ff3fc8b532b956200f21d2578d8aec526bfd14f6
Christopher Haster <chaster@utexas.edu>
2017-02-23 12:18:43 -0600

    lwip: Increase timeout on tests
    Sometimes when under heavy load, the CI machines can take a significant
    amount of time to bring up a python process (~10s). The timeouts for
    the network tests were chosen without much thought, and didn't leave
    much room for this sort of delay.
    
    This patch brings up timeouts for ntetwork tests 20s -> 60s

--- features/FEATURE_LWIP/TESTS/mbedmicro-net/tcp_echo_parallel/main.cpp
+++ features/FEATURE_LWIP/TESTS/mbedmicro-net/tcp_echo_parallel/main.cpp
@@ -88,7 +88,7 @@ Echo echoers[MBED_CFG_TCP_CLIENT_ECHO_THREADS];
 
 
 int main() {
-    GREENTEA_SETUP(20, "tcp_echo");
+    GREENTEA_SETUP(60, "tcp_echo");
 
     int err = net.connect();
     TEST_ASSERT_EQUAL(0, err);
