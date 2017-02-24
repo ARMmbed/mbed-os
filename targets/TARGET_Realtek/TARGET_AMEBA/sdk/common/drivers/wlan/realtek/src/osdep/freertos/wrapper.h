5e47ad075df4bd28f52c87c80d28a393649303be
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 17:46:40 +0800

    Fix headers issue in TARGET_AMEBA\sdk\common folder

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/drivers/wlan/realtek/src/osdep/freertos/wrapper.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/drivers/wlan/realtek/src/osdep/freertos/wrapper.h
@@ -1,10 +1,27 @@
+/******************************************************************************
+ *
+ * Copyright(c) 2007 - 2012 Realtek Corporation. All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ *
+ ******************************************************************************
+ *  Wrapper provide a linux-like interface 
+ ************************************************************************/
 #ifndef __WRAPPER_H__
 #define __WRAPPER_H__
-/**************************************************************************
-  *  Wrapper provide a linux-like interface 
-  *
-  *  Copyright (c) 2013 Realtek Semiconductor Corp.
-  ************************************************************************/
+
 
 //----- ------------------------------------------------------------------
 // Include Files
