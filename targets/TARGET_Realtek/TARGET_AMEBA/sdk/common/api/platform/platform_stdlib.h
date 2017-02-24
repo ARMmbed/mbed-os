21b1d084f0f60eb0ee086edc1ae4f12366e8f804
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 17:46:40 +0800

    Fix headers issue in TARGET_AMEBA\sdk\common folder

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/platform/platform_stdlib.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/platform/platform_stdlib.h
@@ -12,7 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  ******************************************************************************/
 #ifndef __PLATFORM_STDLIB_H__
 #define __PLATFORM_STDLIB_H__
