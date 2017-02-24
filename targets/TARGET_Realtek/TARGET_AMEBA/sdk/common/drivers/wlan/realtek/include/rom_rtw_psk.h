d4b264cb4f8b5223d5589b60c8b6ba4715652162
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 17:46:40 +0800

    Fix headers issue in TARGET_AMEBA\sdk\common folder

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/drivers/wlan/realtek/include/rom_rtw_psk.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/drivers/wlan/realtek/include/rom_rtw_psk.h
@@ -2,8 +2,23 @@
  *
  * Copyright(c) 2007 - 2011 Realtek Corporation. All rights reserved.
  *                                        
- * This is ROM code section. 
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
  *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110, USA
+ *
+ *
+ ******************************************************************************
+ *                                        
+ * This is ROM code section. 
  *
  ******************************************************************************/
 #ifndef __ROM_RTW_PSK_H_
--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/drivers/wlan/realtek/wlan_ram_map/rom/rom_wlan_ram_map.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/drivers/wlan/realtek/wlan_ram_map/rom/rom_wlan_ram_map.h
@@ -1,4 +1,4 @@
-/*
+/******************************************************************************
  * Copyright (c) 2013-2016 Realtek Semiconductor Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ ******************************************************************************/
 #ifndef ROM_WLAN_RAM_MAP_H
 #define ROM_WLAN_RAM_MAP_H
 
@@ -22,3 +22,4 @@ struct _rom_wlan_ram_map {
 };
 
 #endif	/* ROM_WLAN_RAM_MAP_H */
+
