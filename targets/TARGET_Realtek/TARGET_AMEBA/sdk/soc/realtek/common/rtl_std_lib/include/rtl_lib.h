c72257cbf8f2bc3adc9e3b043eadb3795479e203
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 19:34:28 +0800

    update other file headers

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/soc/realtek/common/rtl_std_lib/include/rtl_lib.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/soc/realtek/common/rtl_std_lib/include/rtl_lib.h
@@ -1,8 +1,19 @@
-/*
- * rtl_lib.h
+/******************************************************************************
+ * Copyright (c) 2013-2016 Realtek Semiconductor Corp.
  *
- * Definitions for RTL library functions
- */
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
  
 #ifndef _RTL_LIB_H_
 #define	_RTL_LIB_H_
@@ -141,3 +152,4 @@ extern int rtl_string_test(IN u16 argc, IN u8 *argv[]);
 
 
 #endif /* _RTL_LIB_H_ */
+
