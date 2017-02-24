4d52f985ebf0acfa2f60765a88e93e31b0b53108
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 15:23:46 +0800

    change proprietary headers (APACHE2.0)
    correct proprietary headers for some of our codes

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/platform/platform_stdlib.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/platform/platform_stdlib.h
@@ -1,7 +1,17 @@
 /******************************************************************************
+ * Copyright (c) 2013-2016 Realtek Semiconductor Corp.
  *
- * Copyright(c) 2007 - 2014 Realtek Corporation. All rights reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *
  ******************************************************************************/
 #ifndef __PLATFORM_STDLIB_H__
@@ -267,3 +277,4 @@ extern void vPortFree( void *pv );
 
 
 #endif //__PLATFORM_STDLIB_H__
+
--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/wifi/wifi_ind.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/wifi/wifi_ind.h
@@ -1,18 +1,25 @@
-/**
-  ******************************************************************************
-  * @file    wifi_ind.h
-  * @author
-  * @version
-  * @brief   This file provides the functions related to event handler mechanism.
-  ******************************************************************************
-  * @attention
-  *
-  * This module is a confidential and proprietary property of RealTek and
-  * possession or use of this module requires written permission of RealTek.
-  *
-  * Copyright(c) 2016, Realtek Semiconductor Corporation. All rights reserved.
-  ****************************************************************************** 
-  */
+/******************************************************************************
+ * Copyright (c) 2013-2016 Realtek Semiconductor Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************
+ * @file    wifi_ind.h
+ * @author
+ * @version
+ * @brief   This file provides the functions related to event handler mechanism.
+ ******************************************************************************
+ */
   
 #ifndef _WIFI_INDICATE_H
 #define _WIFI_INDICATE_H
