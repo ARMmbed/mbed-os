65899acc2d735106857f9a58214832c55b17f6f0
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 15:23:46 +0800

    change proprietary headers (APACHE2.0)
    correct proprietary headers for some of our codes

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/network/dhcp/dhcps.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/network/dhcp/dhcps.h
@@ -1,4 +1,19 @@
-
+/******************************************************************************
+ * Copyright (c) 2013-2016 Realtek Semiconductor Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 #ifndef __DHCPS_H__
 #define __DHCPS_H__
 
@@ -140,4 +155,5 @@ void dhcps_deinit(void);
 
 extern struct netif *netif_default;
 
-#endif
+#endif /*__DHCPS_H__*/
+
