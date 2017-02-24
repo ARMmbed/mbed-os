609c465fe0a5f120f48bc7dc008c0a81f1c18cd4
Yuguo Zou <zouyuguo@realtek-sg.com>
2017-02-24 15:23:46 +0800

    change proprietary headers (APACHE2.0)
    correct proprietary headers for some of our codes

--- targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/platform/dlist.h
+++ targets/TARGET_Realtek/TARGET_AMEBA/sdk/common/api/platform/dlist.h
@@ -1,3 +1,19 @@
+/******************************************************************************
+ * Copyright (c) 2013-2016 Realtek Semiconductor Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ ******************************************************************************/
 #ifndef __LIST_H
 #define __LIST_H
 
@@ -260,3 +276,4 @@ static inline void list_splice_init(struct list_head *list,
 	     pos = n, n = list_entry(n->member.next, type, member))
 
 #endif
+
