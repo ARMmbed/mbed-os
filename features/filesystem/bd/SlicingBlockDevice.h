31e0875cd1a0b1b8f21bc7b860db0d6ac49a20ff
Christopher Haster <chaster@utexas.edu>
2017-03-01 16:11:40 -0600

    bd: Fix missing const attributes on functions

--- features/filesystem/bd/SlicingBlockDevice.h
+++ features/filesystem/bd/SlicingBlockDevice.h
@@ -138,7 +138,7 @@ public:
      *
      *  @return         Size of the underlying device in bytes
      */
-    virtual bd_size_t size();
+    virtual bd_size_t size() const;
 
 protected:
     BlockDevice *_bd;
