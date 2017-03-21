88606f2d44cdde6f02f44ca8735e8325650af040
Kevin Bracey <kevin.bracey@arm.com>
2017-03-21 10:29:52 +0200

    NSAPI/lwIP: Free held netbuf on close
    mbed_lwip_socket_recv() takes one netbuf at a time from the netconn API,
    and it holds a partially-read netbuf if necessary in order to present as
    a stream for TCP.
    
    This held netbuf was not being freed when the socket was closed.

--- features/FEATURE_LWIP/lwip-interface/lwip_stack.c
+++ features/FEATURE_LWIP/lwip-interface/lwip_stack.c
@@ -691,6 +691,7 @@ static nsapi_error_t mbed_lwip_socket_close(nsapi_stack_t *stack, nsapi_socket_t
 {
     struct lwip_socket *s = (struct lwip_socket *)handle;
 
+    netbuf_delete(s->buf);
     err_t err = netconn_delete(s->conn);
     mbed_lwip_arena_dealloc(s);
     return mbed_lwip_err_remap(err);
