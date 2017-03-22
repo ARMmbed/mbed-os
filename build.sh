d437e7bbb06819c4139851e120f933411a6ad16c
George Hampton <undividedwholness@gmail.com>
2017-03-22 13:27:08 -0400

    add build for KL27

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@ dir=$(pwd)
 rm -rf BUILD
 
 rm -rf build_rel
-python tools/build.py -r -d -m KL26Z -t GCC_ARM --profile tools/profiles/release.json
+python tools/build.py -r -d -m KL26Z,KL27Z -t GCC_ARM --profile tools/profiles/release.json
 mv BUILD build_rel
 
 rm -rf build_debug
-python tools/build.py -r -d -m KL26Z -t GCC_ARM --profile tools/profiles/debug.json
+python tools/build.py -r -d -m KL26Z,KL27Z -t GCC_ARM --profile tools/profiles/debug.json
 mv BUILD build_debug
 
 ln -s $dir/build_rel $dir/BUILD
