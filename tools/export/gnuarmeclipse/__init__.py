6a3c71c0028d3720b96cfeb0c0e47d0cb6d70768
Jimmy Brisson <theotherjimmy@gmail.com>
2017-03-29 14:48:46 -0500

    Update gnuarmeclipse to preprocess linker scripts

--- tools/export/gnuarmeclipse/__init__.py
+++ tools/export/gnuarmeclipse/__init__.py
@@ -243,7 +243,9 @@ class GNUARMEclipse(Exporter):
             opts['ld']['object_files'] = objects
             opts['ld']['user_libraries'] = libraries
             opts['ld']['system_libraries'] = self.system_libraries
-            opts['ld']['script'] = self.ld_script
+            opts['ld']['script'] = join(id.capitalize(),
+                                        "linker-script-%s.ld" % id)
+            opts['cpp_cmd'] = " ".join(toolchain.preproc)
 
             # Unique IDs used in multiple places.
             # Those used only once are implemented with {{u.id}}.
@@ -260,6 +262,7 @@ class GNUARMEclipse(Exporter):
 
         jinja_ctx = {
             'name': self.project_name,
+            'ld_script': self.ld_script,
 
             # Compiler & linker command line options
             'options': self.options,
