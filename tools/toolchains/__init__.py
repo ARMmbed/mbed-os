d5ecd7adf8158156bd38fb489af48f76cbb03e57
Jimmy Brisson <theotherjimmy@gmail.com>
2017-04-03 15:43:22 -0500

    Fix a scoping typo in toolchains

--- tools/toolchains/__init__.py
+++ tools/toolchains/__init__.py
@@ -474,7 +474,7 @@ class mbedToolchain:
             # This is a policy decision and it should /really/ be in the config system
             # ATM it's here for backward compatibility
             if ((("-g" in self.flags['common'] or "-g3" in self.flags['common']) and
-                 "-O0") in self.flags['common'] or
+                 "-O0" in self.flags['common']) or
                 ("-r" in self.flags['common'] and
                  "-On" in self.flags['common'])):
                 self.labels['TARGET'].append("DEBUG")
