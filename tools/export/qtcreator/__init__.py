9934b91c1ea10e8925ea17a79541ea5bca3a3d75
Matteo Italia <matteo@mitalia.net>
2017-04-06 23:19:52 +0200

    Updated year in license header

--- tools/export/qtcreator/__init__.py
+++ tools/export/qtcreator/__init__.py
@@ -1,6 +1,6 @@
 """
 mbed SDK
-Copyright (c) 2014-2016 ARM Limited
+Copyright (c) 2014-2017 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
