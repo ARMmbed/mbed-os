theotherjimmy/mbed

Add type to build_data.json

tools/make.py
@@ -339,4 +339,4 @@ if __name__ == '__main__':
             
             sys.exit(1)
     if options.metadata:
-        merge_metadata(options.metadata, metadata_blob)
+        merge_build_data(options.metadata, metadata_blob, "application")

ARMmbed/mbed-os

tools/make.py
@@ -352,4 +352,4 @@ if __name__ == '__main__':
             
             sys.exit(1)
     if options.build_data:
-        merge_build_data(options.build_data, build_data_blob)
+        merge_build_data(options.build_data, build_data_blob, "application")