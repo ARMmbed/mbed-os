1fe20b281ac94d9f897a85108ceb64c922368c48
adustm <adustm13@gmail.com>
2017-04-24 18:22:56 +0200

    Add can_init_frequency for NXP platforms

--- targets/TARGET_NXP/TARGET_LPC176X/can_api.c
+++ targets/TARGET_NXP/TARGET_LPC176X/can_api.c
@@ -292,7 +292,7 @@ static unsigned int can_speed(unsigned int sclk, unsigned int pclk, unsigned int
 
 }
 
-void can_init(can_t *obj, PinName rd, PinName td) {
+void can_init_freq(can_t *obj, PinName rd, PinName td, int hz) {
     CANName can_rd = (CANName)pinmap_peripheral(rd, PinMap_CAN_RD);
     CANName can_td = (CANName)pinmap_peripheral(td, PinMap_CAN_TD);
     obj->dev = (LPC_CAN_TypeDef *)pinmap_merge(can_rd, can_td);
@@ -313,9 +313,13 @@ void can_init(can_t *obj, PinName rd, PinName td) {
     
     can_reset(obj);
     obj->dev->IER = 0;             // Disable Interrupts
-    can_frequency(obj, 100000);
+    can_frequency(obj, hz);
 
     LPC_CANAF->AFMR = ACCF_BYPASS; // Bypass Filter
+}
+
+void can_init(can_t *obj, PinName rd, PinName td) {
+    can_init_freq(obj, rd, td, 100000);
 }
 
 void can_free(can_t *obj) {
