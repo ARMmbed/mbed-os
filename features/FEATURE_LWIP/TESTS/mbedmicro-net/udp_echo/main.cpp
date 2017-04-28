50912c004e5a5a76b22aa939f000d3dd0d39b180
Brian Daniels <brianddaniels@gmail.com>
2017-04-28 11:43:40 -0500

    Arranging logic in test to prevent left-over behavior

--- features/FEATURE_LWIP/TESTS/mbedmicro-net/udp_echo/main.cpp
+++ features/FEATURE_LWIP/TESTS/mbedmicro-net/udp_echo/main.cpp
@@ -47,53 +47,61 @@ int main() {
     printf("Got a uuid of %s\r\n", uuid);
     size_t uuid_len = strlen(uuid);
     EthernetInterface eth;
-    eth.connect();
-    printf("UDP client IP Address is %s\n", eth.get_ip_address());
 
-    greentea_send_kv("target_ip", eth.get_ip_address());
+    int err = eth.connect();
+    TEST_ASSERT_EQUAL(0, err);
 
-    char recv_key[] = "host_port";
-    char ipbuf[60] = {0};
-    char portbuf[16] = {0};
-    unsigned int port = 0;
+    if (err) {
+        printf("MBED: failed to connect with an error of %d\r\n", err);
+        GREENTEA_TESTSUITE_RESULT(false);
+    } else {
+        printf("UDP client IP Address is %s\n", eth.get_ip_address());
 
-    UDPSocket sock;
-    sock.open(&eth);
-    sock.set_timeout(MBED_CFG_UDP_CLIENT_ECHO_TIMEOUT);
+        greentea_send_kv("target_ip", eth.get_ip_address());
 
-    greentea_send_kv("host_ip", " ");
-    greentea_parse_kv(recv_key, ipbuf, sizeof(recv_key), sizeof(ipbuf));
+        char recv_key[] = "host_port";
+        char ipbuf[60] = {0};
+        char portbuf[16] = {0};
+        unsigned int port = 0;
 
-    greentea_send_kv("host_port", " ");
-    greentea_parse_kv(recv_key, portbuf, sizeof(recv_key), sizeof(ipbuf));
-    sscanf(portbuf, "%u", &port);
+        UDPSocket sock;
+        sock.open(&eth);
+        sock.set_timeout(MBED_CFG_UDP_CLIENT_ECHO_TIMEOUT);
 
-    printf("MBED: UDP Server IP address received: %s:%d \n", ipbuf, port);
-    SocketAddress udp_addr(ipbuf, port);
+        greentea_send_kv("host_ip", " ");
+        greentea_parse_kv(recv_key, ipbuf, sizeof(recv_key), sizeof(ipbuf));
 
-    int success = 0;
-    int i = 0;
-    while (success < ECHO_LOOPS) {
-        prep_buffer(uuid, uuid_len, tx_buffer, sizeof(tx_buffer));
-        const int ret = sock.sendto(udp_addr, tx_buffer, sizeof(tx_buffer));
-        printf("[%02d] sent %d Bytes - %.*s  \n", i, ret, MBED_CFG_UDP_CLIENT_ECHO_BUFFER_SIZE, tx_buffer);
-        SocketAddress temp_addr;
-        const int n = sock.recvfrom(&temp_addr, rx_buffer, sizeof(rx_buffer));
-        printf("[%02d] recv %d Bytes - %.*s  \n", i, ret, MBED_CFG_UDP_CLIENT_ECHO_BUFFER_SIZE, rx_buffer);
+        greentea_send_kv("host_port", " ");
+        greentea_parse_kv(recv_key, portbuf, sizeof(recv_key), sizeof(ipbuf));
+        sscanf(portbuf, "%u", &port);
 
-        if ((temp_addr == udp_addr &&
-             n == sizeof(tx_buffer) &&
-             memcmp(rx_buffer, tx_buffer, sizeof(rx_buffer)) == 0)) {
-            success += 1;
+        printf("MBED: UDP Server IP address received: %s:%d \n", ipbuf, port);
+        SocketAddress udp_addr(ipbuf, port);
 
-            printf("[%02d] success #%d\n", i, success);
+        int success = 0;
+        int i = 0;
+        while (success < ECHO_LOOPS) {
+            prep_buffer(uuid, uuid_len, tx_buffer, sizeof(tx_buffer));
+            const int ret = sock.sendto(udp_addr, tx_buffer, sizeof(tx_buffer));
+            printf("[%02d] sent %d Bytes - %.*s  \n", i, ret, MBED_CFG_UDP_CLIENT_ECHO_BUFFER_SIZE, tx_buffer);
+            SocketAddress temp_addr;
+            const int n = sock.recvfrom(&temp_addr, rx_buffer, sizeof(rx_buffer));
+            printf("[%02d] recv %d Bytes - %.*s  \n", i, ret, MBED_CFG_UDP_CLIENT_ECHO_BUFFER_SIZE, rx_buffer);
+
+            if ((temp_addr == udp_addr &&
+                 n == sizeof(tx_buffer) &&
+                 memcmp(rx_buffer, tx_buffer, sizeof(rx_buffer)) == 0)) {
+                success += 1;
+
+                printf("[%02d] success #%d\n", i, success);
+            }
+            i++;
         }
-        i++;
+
+        bool result = success == ECHO_LOOPS;
+
+        sock.close();
+        eth.disconnect();
+        GREENTEA_TESTSUITE_RESULT(result);
     }
-
-    bool result = success == ECHO_LOOPS;
-
-    sock.close();
-    eth.disconnect();
-    GREENTEA_TESTSUITE_RESULT(result);
 }
