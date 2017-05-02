e51c6942cf9c1838601465c28e23ccdc6ba6ec32
arostm <arostm97@gmail.com>
2017-05-02 09:58:00 +0200

    DISCO_L072CZ_LRWAN1: prepipheralPins and PinNames correction

--- targets/TARGET_STM/TARGET_STM32L0/TARGET_DISCO_L072CZ_LRWAN1/PinNames.h
+++ targets/TARGET_STM/TARGET_STM32L0/TARGET_DISCO_L072CZ_LRWAN1/PinNames.h
@@ -46,15 +46,15 @@ typedef enum {
 
 typedef enum {
     PA_0  = 0x00,
-    //PA_1  = 0x01,
+    PA_1  = 0x01,
     PA_2  = 0x02,
-    PA_2_ALT0 = PA_2|ALT0,
     PA_3  = 0x03,
-    PA_3_ALT0 = PA_3|ALT0,
     PA_4  = 0x04,
     PA_5  = 0x05,
-    //PA_6  = 0x06,
-    //PA_7  = 0x07,
+    PA_6  = 0x06,
+    PA_6_ALT0 = PA_6|ALT0,
+    PA_7  = 0x07,
+    PA_7_ALT0 = PA_7|ALT0,
     PA_8  = 0x08,
     PA_9  = 0x09,
     PA_10 = 0x0A,
@@ -65,42 +65,28 @@ typedef enum {
     PA_14_ALT0 = PA_14|ALT0,
     PA_15 = 0x0F,
 
-    //PB_0  = 0x10,
-    //PB_1  = 0x11,
+    PB_0  = 0x10,
+    PB_1  = 0x11,
     PB_2  = 0x12,
-    //PB_3  = 0x13,
-    //PB_4  = 0x14,
+    PB_3  = 0x13,
+    PB_4  = 0x14,
+    PB_4_ALT0 = PB_4|ALT0,
     PB_5  = 0x15,
     PB_5_ALT0 = PB_5|ALT0,
     PB_6  = 0x16,
     PB_7  = 0x17,
     PB_8  = 0x18,
     PB_9  = 0x19,
-    //PB_10 = 0x1A,
-    //PB_11 = 0x1B,
+    PB_10 = 0x1A,
+    PB_11 = 0x1B,
     PB_12 = 0x1C,
     PB_13 = 0x1D,
     PB_14 = 0x1E,
     PB_15 = 0x1F,
 
-    //PC_0  = 0x20,
-    //PC_1  = 0x21,
-    //PC_2  = 0x22,
-    //PC_3  = 0x23,
-    //PC_4  = 0x24,
-    //PC_5  = 0x25,
-    //PC_6  = 0x26,
-    //PC_7  = 0x27,
-    //PC_8  = 0x28,
-    //PC_9  = 0x29,
-    //PC_10 = 0x2A,
-    //PC_11 = 0x2B,
-    //PC_12 = 0x2C,
-    //PC_13 = 0x2D,
-    //PC_14 = 0x2E,
-    //PC_15 = 0x2F,
-
-    //PD_2  = 0x32,
+    PC_0  = 0x20,
+    PC_1  = 0x21,
+    PC_2  = 0x22,
 
     PH_0  = 0x70,
     PH_1  = 0x71,
@@ -108,19 +94,19 @@ typedef enum {
     // ADC internal channels
     ADC_TEMP = 0xF0,
     ADC_VREF = 0xF1,
-    ADC_VLCD = 0xF2,
+    ADC_VBAT = 0xF2,
 
     // Arduino connector namings
     A0          = PA_0,
-    A1          = PA_0, //Alias
+    A1          = PA_0, // Alias
     A2          = PA_4,
-    A3          = PA_4, //Alias
-    A4          = PB_9, //SB11 to be soldered
-    A5          = PB_8, //SB12 to be soldered
+    A3          = PA_4, // Alias
+    A4          = PB_9, // SB11 must be closed
+    A5          = PB_8, // SB12 must be closed
     D0          = PA_3,
     D1          = PA_2,
     D2          = PA_10,
-    D3          = PB_13, //NC
+    D3          = PB_13,
     D4          = PB_5,
     D5          = PB_7,
     D6          = PB_2,
@@ -130,16 +116,16 @@ typedef enum {
     D10         = PB_6,
     D11         = PB_15,
     D12         = PB_14,
-    D13         = PB_13,
+    D13         = PB_13, // SB2 must be closed
     D14         = PB_9,
     D15         = PB_8,
 
     // Generic signals namings
-    LED1        = PB_5, //Green
-    LED2        = PA_5, //Red
-    LED3        = PB_6, //Blue
-    LED4        = PB_7, //Red
-    USER_BUTTON = PB_2,   
+    LED1        = PB_5, // Green
+    LED2        = PA_5, // Red
+    LED3        = PB_6, // Blue
+    LED4        = PB_7, // Red
+    USER_BUTTON = PB_2,
     SERIAL_TX   = PA_2,
     SERIAL_RX   = PA_3,
     USBTX       = SERIAL_TX,
@@ -150,7 +136,7 @@ typedef enum {
     SPI_MISO    = D12,
     SPI_SCK     = D13,
     SPI_CS      = D10,
-    PWM_OUT     = D3,
+    PWM_OUT     = D13,
 
     // Not connected
     NC = (int)0xFFFFFFFF
