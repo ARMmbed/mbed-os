5e3350ce38b28756218b99693bf7c06602174efd
arostm <arostm97@gmail.com>
2017-05-09 11:17:25 +0200

    DISCO_F413ZH: Modification needed to build

--- targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F413xH/device/stm32f413xx.h
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F413xH/device/stm32f413xx.h
@@ -67,7 +67,9 @@
 #define __MPU_PRESENT             1U       /*!< STM32F4XX provides an MPU                     */
 #define __NVIC_PRIO_BITS          4U       /*!< STM32F4XX uses 4 Bits for the Priority Levels */
 #define __Vendor_SysTickConfig    0U       /*!< Set to 1 if different SysTick Config is used  */
+#ifndef __FPU_PRESENT
 #define __FPU_PRESENT             1U       /*!< FPU present                                   */
+#endif
 
 /**
   * @}
--- targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F413xH/device/system_stm32f4xx.h
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F413xH/device/system_stm32f4xx.h
@@ -104,6 +104,7 @@ extern const uint8_t  APBPrescTable[8];     /*!< APB prescalers table values */
   
 extern void SystemInit(void);
 extern void SystemCoreClockUpdate(void);
+extern void SetSysClock(void);
 /**
   * @}
   */
