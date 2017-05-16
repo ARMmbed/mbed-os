9b890198dd38e824fa0b699376e21f828067b76c
gabker01 <gabor.kertesz@arm.com>
2017-05-16 15:18:21 +0200

    Implement low-level Ethernet interface for lwIP
    Based on lwip_ethernetif.c skeleton file,
    use init, receive and transfer
    functionality of SMSC9220 Ethernet driver
    for the lightweight IP stack.
    
    Receive mechanism is interrupt driven.
    
    HW buffer sizes:
    Tx = 4608 bytes (MTU)
    Rx = 10560 bytes
    
    lwIP fine tuning:
    mbed-os/features/FEATURE_LWIP/lwip-interface/lwip/src/include/lwip/opt.h
    
    Change-Id: I0ea95650c65fb32cafb5c2d3dde11420c61dba66
    Signed-off-by: Gabor Kertesz <gabor.kertesz@arm.com>

---
+++ targets/TARGET_ARM_SSG/TARGET_CM3DS_MPS2/mbed_overrides.c
@@ -0,0 +1,24 @@
+/* mbed Microcontroller Library
+
+ * Copyright (c) 2006-2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "smsc9220_eth.h"
+
+/* Provide ethernet devices with a semi-unique MAC address from the UUID */
+void mbed_mac_address(char *mac)
+{
+    smsc9220_read_mac_address(mac);
+}
