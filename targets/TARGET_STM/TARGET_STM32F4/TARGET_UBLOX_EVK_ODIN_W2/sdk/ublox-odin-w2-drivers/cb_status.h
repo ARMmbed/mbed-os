f996c1b5d07cab38aa73fb10731d688e5d807cc1
andreas.larsson <andreas.larsson@connectblue.com>
2017-05-17 23:11:50 +0200

    Added cbSTATUS_RECEIVE_DATA_MODE + macros

--- targets/TARGET_STM/TARGET_STM32F4/TARGET_UBLOX_EVK_ODIN_W2/sdk/ublox-odin-w2-drivers/cb_status.h
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_UBLOX_EVK_ODIN_W2/sdk/ublox-odin-w2-drivers/cb_status.h
@@ -26,7 +26,9 @@
  *=========================================================================*/
 
 #define OK(status) (status == cbSTATUS_OK)
- 
+#define BUSY(status) (status == cbSTATUS_BUSY)
+#define ERR(status) (status == cbSTATUS_ERROR)
+
 /*===========================================================================
  * TYPES
  *=========================================================================*/
@@ -36,6 +38,7 @@
     cbSTATUS_OK,
     cbSTATUS_ERROR,
     cbSTATUS_BUSY,
+    cbSTATUS_RECEIVE_DATA_MODE,
     cbSTATUS_TIMEOUT
  
  } cbRTSL_Status;
