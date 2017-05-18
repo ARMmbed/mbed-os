4a438b6ab268f35357724778bbf33575a1d2b1d9
Kari Severinkangas <kari.severinkangas@arm.com>
2017-05-18 16:36:26 +0300

    Thread minimal end device operating mode added

--- features/nanostack/FEATURE_NANOSTACK/mbed-mesh-api/source/thread_tasklet.c
+++ features/nanostack/FEATURE_NANOSTACK/mbed-mesh-api/source/thread_tasklet.c
@@ -255,7 +255,11 @@ void thread_tasklet_configure_and_connect_to_network(void)
     int8_t status;
     link_configuration_s* temp_link_config=NULL;
 
-    if (MBED_CONF_MBED_MESH_API_THREAD_DEVICE_TYPE == MESH_DEVICE_TYPE_THREAD_SLEEPY_END_DEVICE) {
+
+    if (MBED_CONF_MBED_MESH_API_THREAD_DEVICE_TYPE == MESH_DEVICE_TYPE_THREAD_MINIMAL_END_DEVICE) {
+            thread_tasklet_data_ptr->operating_mode = NET_6LOWPAN_HOST;
+    }
+    else if (MBED_CONF_MBED_MESH_API_THREAD_DEVICE_TYPE == MESH_DEVICE_TYPE_THREAD_SLEEPY_END_DEVICE) {
         thread_tasklet_data_ptr->operating_mode = NET_6LOWPAN_SLEEPY_HOST;
     } else {
         thread_tasklet_data_ptr->operating_mode = NET_6LOWPAN_ROUTER;
