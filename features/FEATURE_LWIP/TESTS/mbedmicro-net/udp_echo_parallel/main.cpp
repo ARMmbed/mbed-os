4f5a1993360d7012a47eacc03d6a0778964c4b18
Brian Daniels <brianddaniels@gmail.com>
2017-06-02 10:37:17 -0500

    Fix bad print message in test

--- features/FEATURE_LWIP/TESTS/mbedmicro-net/udp_echo_parallel/main.cpp
+++ features/FEATURE_LWIP/TESTS/mbedmicro-net/udp_echo_parallel/main.cpp
@@ -152,7 +152,7 @@ public:
         result = success == ECHO_LOOPS;
 
         err = sock.close();
-        printf("[ID:%01u][%02u] Failed to close socket!\n", id, i);
+        printf("[ID:%01u] Failed to close socket!\n", id);
         if (err) {
             result = false;
         }
