77d8a0150736a2c9bc111631823ee07b5ce40225
Dave Wu <dave.wu@analog.com>
2017-06-05 14:47:00 +1000

    Updated legal header block.

--- targets/TARGET_Analog_Devices/TARGET_ADUCM302X/TARGET_ADUCM3029/TARGET_EVAL_ADICUP3029/api/analogin_api.c
+++ targets/TARGET_Analog_Devices/TARGET_ADUCM302X/TARGET_ADUCM3029/TARGET_EVAL_ADICUP3029/api/analogin_api.c
@@ -1,18 +1,43 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2006-2015 ARM Limited
+/*******************************************************************************
+ * Copyright (c) 2010-2017 Analog Devices, Inc.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * All rights reserved.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   - Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   - Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   - Modified versions of the software must be conspicuously marked as such.
+ *   - This software is licensed solely and exclusively for use with processors
+ *     manufactured by or for Analog Devices, Inc.
+ *   - This software may not be combined or merged with other code in any manner
+ *     that would cause the software to become subject to terms and conditions
+ *     which differ from those listed here.
+ *   - Neither the name of Analog Devices, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *   - The use of this software may or may not infringe the patent rights of one
+ *     or more patent holders.  This license does not release you from the
+ *     requirement that you obtain separate licenses from these patent holders
+ *     to use this software.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES, INC. AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-
+ * INFRINGEMENT, TITLE, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ANALOG DEVICES, INC. OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, DAMAGES ARISING OUT OF
+ * CLAIMS OF INTELLECTUAL PROPERTY RIGHTS INFRINGEMENT; PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ ******************************************************************************/
+
 #include "mbed_assert.h"
 #include "analogin_api.h"
 
@@ -65,14 +90,14 @@ void analogin_init(analogin_t *obj, PinName pin)
 
     /* verify read function */
     function = pinmap_function(pin, &PinMap_ADC[0]);
-    MBED_ASSERT(function == 1); 
+    MBED_ASSERT(function == 1);
 
     /* Configure PORT2_MUX registers */
     pin_function(pin, function);
 
     /* Configure active channel */
     channel = adi_pin2channel(pin);
-    MBED_ASSERT(channel != 0xFFFFFFFF); 
+    MBED_ASSERT(channel != 0xFFFFFFFF);
     obj->UserBuffer.nChannels = channel;
 
     /* Open the ADC device */
@@ -148,39 +173,39 @@ uint16_t analogin_read_u16(analogin_t *obj)
 
 /* Retrieve te active channel correspondoing to the input pin */
 static uint32_t adi_pin2channel(PinName pin) {
-    
-    uint32_t activech; 
-   
-    switch(pin) {        
+
+    uint32_t activech;
+
+    switch(pin) {
         case A0:
             activech = ADI_ADC_CHANNEL_0;
-            break;             
+            break;
         case A1:
             activech = ADI_ADC_CHANNEL_1;
-            break;               
+            break;
         case A2:
             activech = ADI_ADC_CHANNEL_2;
-            break;                                 
+            break;
         case A3:
             activech = ADI_ADC_CHANNEL_3;
-            break; 
+            break;
         case A4:
             activech = ADI_ADC_CHANNEL_4;
-            break;  
+            break;
         case A5:
             activech = ADI_ADC_CHANNEL_5;
-            break; 
+            break;
         case A6:
             activech = ADI_ADC_CHANNEL_6;
-            break; 
+            break;
         case A7:
             activech = ADI_ADC_CHANNEL_7;
-            break;                                       
-        default: 
+            break;
+        default:
             activech = (uint32_t) 0xFFFFFFFF;
             break;
-    } 
-    
+    }
+
     return((uint32_t) activech);
 }
 
