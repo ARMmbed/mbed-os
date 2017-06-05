0de35e2a2129e8d960efb153d62276a3cd6472d7
Russ Butler <russ.butler@arm.com>
2017-06-05 18:05:38 -0500

    Fix config store deprecation warnings
    Silence deprecation warnings in the config store C and C++ files. This
    removes warnings that not relevant to applications. Note - using
    these deprecated functions still gives an error outside of these files.

--- features/storage/FEATURE_STORAGE/cfstore/source/cfstore_test.c
+++ features/storage/FEATURE_STORAGE/cfstore/source/cfstore_test.c
@@ -18,6 +18,13 @@
  * test support code implementation file.
  */
 
+// This file is deprecated so deprecation warnings when building it are silenced
+#if   defined ( __CC_ARM )
+#pragma diag_suppress 1361  // Deprecated declaration
+#elif defined ( __GNUC__ )
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
 #include "cfstore_config.h"
 #include "cfstore_debug.h"
 #include "cfstore_test.h"
