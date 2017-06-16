dff0df96d8d6b757a610020a84cc3fa3754241fe
ccli8 <ccli8@nuvoton.com>
2017-06-16 13:34:48 +0800

    Add internal configuration control_01

--- targets/TARGET_NUVOTON/TARGET_M480/device/system_M480.c
+++ targets/TARGET_NUVOTON/TARGET_M480/device/system_M480.c
@@ -88,5 +88,13 @@ void SystemInit (void)
     SPIM_DISABLE_CACHE();
     SPIM_ENABLE_CCM();
     while (! SPIM_IS_CCM_EN());
+    
+#ifndef MBED_CONF_M480_CTRL01_ENABLE
+#define MBED_CONF_M480_CTRL01_ENABLE 1
+#endif
+
+#if (! MBED_CONF_M480_CTRL01_ENABLE)
+    M32(0x4000c018) |= 0x00000080;
+#endif
 }
 /*** (C) COPYRIGHT 2016 Nuvoton Technology Corp. ***/
