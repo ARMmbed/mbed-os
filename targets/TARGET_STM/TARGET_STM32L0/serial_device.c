b5cbaaa0d09ffc77a74bed83a35ca72828b455eb
Laurent MEUNIER <laurent.meunier@st.com>
2017-07-10 18:33:46 +0200

    STM32: Serial - use TXE as tx_irq instead of TC
    TXE indicates that a byte can be written to UART register for sending,
    while TC indicates that last byte was completely sent. So the TXE flag
    can be used in case of interrupt based Serial communication, to allow
    faster and efficient application buffer emptying.
    
    Also TXE flag will be erased from the interrupt when writing to register.
    In case there is nothing to write in the register, the application is
    expected to disable the interrupt.

--- targets/TARGET_STM/TARGET_STM32L0/serial_device.c
+++ targets/TARGET_STM/TARGET_STM32L0/serial_device.c
@@ -201,10 +201,9 @@ static void uart_irq(int id)
     UART_HandleTypeDef * huart = &uart_handlers[id];
     
     if (serial_irq_ids[id] != 0) {
-        if (__HAL_UART_GET_FLAG(huart, UART_FLAG_TC) != RESET) {
-            if (__HAL_UART_GET_IT(huart, UART_IT_TC) != RESET) {
+        if (__HAL_UART_GET_FLAG(huart, UART_FLAG_TXE) != RESET) {
+            if (__HAL_UART_GET_IT(huart, UART_IT_TXE) != RESET) {
                 irq_handler(serial_irq_ids[id], TxIrq);
-                __HAL_UART_CLEAR_FLAG(huart, UART_CLEAR_TCF);
             }
         }
         if (__HAL_UART_GET_FLAG(huart, UART_FLAG_RXNE) != RESET) {
@@ -302,7 +301,7 @@ void serial_irq_set(serial_t *obj, SerialIrq irq, uint32_t enable)
         if (irq == RxIrq) {
             __HAL_UART_ENABLE_IT(huart, UART_IT_RXNE);
         } else { // TxIrq
-            __HAL_UART_ENABLE_IT(huart, UART_IT_TC);
+            __HAL_UART_ENABLE_IT(huart, UART_IT_TXE);
         }
         NVIC_SetVector(irq_n, vector);
         NVIC_EnableIRQ(irq_n);
@@ -316,7 +315,7 @@ void serial_irq_set(serial_t *obj, SerialIrq irq, uint32_t enable)
                 all_disabled = 1;
             }
         } else { // TxIrq
-            __HAL_UART_DISABLE_IT(huart, UART_IT_TC);
+            __HAL_UART_DISABLE_IT(huart, UART_IT_TXE);
             // Check if RxIrq is disabled too
             if ((huart->Instance->CR1 & USART_CR1_RXNEIE) == 0) {
                 all_disabled = 1;
