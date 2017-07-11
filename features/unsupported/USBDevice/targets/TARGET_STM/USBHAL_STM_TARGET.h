bdd4f18acbc3366579c51c41165673641cb77da8
jeromecoutant <jerome.coutant@st.com>
2017-07-11 16:20:48 +0200

    ST USB config files

--- features/unsupported/USBDevice/targets/TARGET_STM/USBHAL_STM_TARGET.h
+++ features/unsupported/USBDevice/targets/TARGET_STM/USBHAL_STM_TARGET.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2016 mbed.org, MIT License
+/* Copyright (c) 2017 mbed.org, MIT License
 *
 * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 * and associated documentation files (the "Software"), to deal in the Software without
@@ -15,4 +15,29 @@
 * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
+
+#if defined(TARGET_DISCO_L476VG)
+#include "USBHAL_STM32L476VG.h"
+
+#elif defined(TARGET_DISCO_L475VG_IOT01A)
+#include "USBHAL_STM32L475VG.h"
+
+#elif defined(TARGET_DISCO_L072CZ_LRWAN1)
+#include "USBHAL_STM32L072CZ.h"
+
+#elif defined(TARGET_NUCLEO_F303ZE)
+#include "USBHAL_STM32F303ZE.h"
+
+#elif defined(TARGET_NUCLEO_F103RB)
+#include "USBHAL_STM32F103RB.h"
+
+#elif defined(TARGET_DISCO_F769NI)
+#include "USBHAL_STM32F769NI.h"
+
+#elif defined(TARGET_DISCO_L053C8)
+#include "USBHAL_STM32L053C8.h"
+
+#else /* default configuration */
 #include "USBHAL_STM_144_64pins.h"
+  		  
+#endif
