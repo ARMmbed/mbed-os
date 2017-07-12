98af79f087e8d95b28614e982a144a122f2dcbda
Christopher Haster <chaster@utexas.edu>
2017-07-12 04:44:33 -0500

    Squashed 'littlefs/' content from commit 663e953
    git-subtree-dir: littlefs
    git-subtree-split: 663e953a5073ac8e459f60d18375f887f14e21a2

---
+++ lfs_util.h
@@ -0,0 +1,45 @@
+/*
+ * lfs utility functions
+ *
+ * Copyright (c) 2017 Christopher Haster
+ * Distributed under the Apache 2.0 license
+ */
+#ifndef LFS_UTIL_H
+#define LFS_UTIL_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+
+
+// Builtin functions
+static inline uint32_t lfs_max(uint32_t a, uint32_t b) {
+    return (a > b) ? a : b;
+}
+
+static inline uint32_t lfs_min(uint32_t a, uint32_t b) {
+    return (a < b) ? a : b;
+}
+
+static inline uint32_t lfs_ctz(uint32_t a) {
+    return __builtin_ctz(a);
+}
+
+static inline uint32_t lfs_npw2(uint32_t a) {
+    return 32 - __builtin_clz(a-1);
+}
+
+static inline int lfs_scmp(uint32_t a, uint32_t b) {
+    return (int)(unsigned)(a - b);
+}
+
+void lfs_crc(uint32_t *crc, const void *buffer, size_t size);
+
+
+// Logging functions
+#define LFS_DEBUG(fmt, ...) printf("lfs debug: " fmt "\n", __VA_ARGS__)
+#define LFS_WARN(fmt, ...)  printf("lfs warn: " fmt "\n", __VA_ARGS__)
+#define LFS_ERROR(fmt, ...) printf("lfs error: " fmt "\n", __VA_ARGS__)
+
+
+#endif
