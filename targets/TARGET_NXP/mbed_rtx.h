hanxinyumeng/mbed

Add mbed support for LPCXpresso54114 board
Signed-off-by: Mahadevan Mahesh <Mahesh.Mahadevan@nxp.com>

targets/TARGET_NXP/mbed_rtx.h
@@ -203,6 +203,21 @@ extern unsigned char            __usr_stack_top__[];
 #define OS_CLOCK                30000000
 #endif
 
+#elif defined(TARGET_LPC54114_M4)
+
+#ifndef INITIAL_SP
+#define INITIAL_SP              (0x20020000UL)
+#endif
+#ifndef OS_TASKCNT
+#define OS_TASKCNT              14
+#endif
+#ifndef OS_MAINSTKSIZE
+#define OS_MAINSTKSIZE          256
+#endif
+#ifndef OS_CLOCK
+#define OS_CLOCK                48000000
+#endif
+
 #endif
 
 #endif  // MBED_MBED_RTX_H

ARMmbed/mbed-os

targets/TARGET_NXP/mbed_rtx.h
@@ -80,6 +80,21 @@
 #define INITIAL_SP              (0x10002000UL)
 #endif
 
+#elif defined(TARGET_LPC54114_M4)
+
+#ifndef INITIAL_SP
+#define INITIAL_SP              (0x20010000UL)
+#endif
+#ifndef OS_TASKCNT
+#define OS_TASKCNT              14
+#endif
+#ifndef OS_MAINSTKSIZE
+#define OS_MAINSTKSIZE          256
+#endif
+#ifndef OS_CLOCK
+#define OS_CLOCK                48000000
+#endif
+
 #endif
 
 #endif  // MBED_MBED_RTX_H