174ed0701399f7afad451e5d0ca1dcf15b1bc74c
Sarah Marsh <Sarah.Marsh@arm.com>
2017-08-04 16:30:17 -0500

    Move test config keys out of targets.json
    Change TestConfig class methods to module methods

--- tools/test_configs/__init__.py
+++ tools/test_configs/__init__.py
@@ -1,34 +1,33 @@
 from os.path import dirname, abspath, join
 
 from tools.utils import json_file_to_dict
-from tools.targets import TARGET_MAP
 
-class TestConfig:
-    CONFIG_DIR = dirname(abspath(__file__))
-    CONFIG_MAP = json_file_to_dict(join(CONFIG_DIR, "config_paths.json"))
+CONFIG_DIR = dirname(abspath(__file__))
+CONFIG_MAP = json_file_to_dict(join(CONFIG_DIR, "config_paths.json"))
+TARGET_CONFIGS = json_file_to_dict(join(CONFIG_DIR, "target_configs.json"))
 
-    @classmethod
-    def get_valid_configs(cls, target_name):
-        target = TARGET_MAP[target_name]
-        config_dict = {}
-        for attr in cls.CONFIG_MAP:
-            if attr in target.device_has:
-                config_dict[attr] = cls.CONFIG_MAP[attr]
-        return config_dict
+def get_valid_configs(target_name):
+    if target_name in TARGET_CONFIGS:
+        target_config = TARGET_CONFIGS[target_name]
+    else:
+        return {}
 
-    @classmethod
-    def get_config_path(cls, conf_name, target_name):
-        configs = cls.get_valid_configs(target_name)
-        if configs and conf_name.upper() in configs:
-            return join(cls.CONFIG_DIR, configs[conf_name.upper()])
-        else:
-            return None
+    config_dict = {}
+    for attr in CONFIG_MAP:
+        if attr in target_config['test_configurations']:
+            config_dict[attr] = CONFIG_MAP[attr]
+    return config_dict
 
-    @classmethod
-    def get_default_config(cls, target_name):
-        configs = cls.get_valid_configs(target_name)
-        if configs:
-            keys = configs.keys()
-            return join(cls.CONFIG_DIR, configs[keys[0]])
-        else:
-            return None
+def get_config_path(conf_name, target_name):
+    configs = get_valid_configs(target_name)
+    if configs and conf_name.upper() in configs:
+        return join(CONFIG_DIR, configs[conf_name.upper()])
+    else:
+        return None
+
+def get_default_config(target_name):
+    if target_name in TARGET_CONFIGS:
+        config_name = TARGET_CONFIGS[target_name]['default_test_configuration']
+        return join(CONFIG_DIR, CONFIG_MAP[config_name])
+    else:
+        return None
