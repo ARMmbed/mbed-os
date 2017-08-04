f9152c2bd6e35acba5fba77fdb781013f74be983
JojoS <sn-mail@onlinehome.de>
2017-08-04 19:05:54 +0200

    exporter based on lpc54114 template from nxpmicro
    - used LPC4088 for testing
    - split template for LPC54114 into common and Cortex-Mx dependet parts like in old LPCXpresso exporter
    - changed fixed linker script name to name from options
    Todo: use flags and configurations from options (like in gae exporter)

--- tools/export/__init__.py
+++ tools/export/__init__.py
@@ -346,7 +346,7 @@ def export_project(src_paths, export_path, target, ide, libraries_paths=None,
                     resource.add(res)
         if isinstance(zip_proj, basestring):
             zip_export(join(export_path, zip_proj), name, resource_dict,
-                       files + exporter.static_files, inc_repos)
+                       files , inc_repos)
         else:
             zip_export(zip_proj, name, resource_dict,
                        files + exporter.static_files, inc_repos)
