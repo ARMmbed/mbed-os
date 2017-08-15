a7723c4b73488be617f821af584864d5119c00aa
cyliangtw <cyliang@nuvoton.com>
2017-08-15 18:16:09 +0800

    Modify Nuvoton common files to avoid conflicting with master

--- targets/TARGET_NUVOTON/nu_bitutil.h
+++ targets/TARGET_NUVOTON/nu_bitutil.h
@@ -17,8 +17,7 @@
 #ifndef NU_BIT_UTIL_H
 #define NU_BIT_UTIL_H
 
-#if defined(TOOLCHAIN_IAR) && ( defined(TARGET_M0) || defined(TARGET_M0P) )
-
+#if defined(__ICCARM__) && ( defined(TARGET_M0) || defined(TARGET_M0P) )
 #include <arm_math.h>
 #endif
 #include "cmsis.h"
