69f3902e95d1b0800f6f077df15243a567aa49c8
Christopher Haster <chaster@utexas.edu>
2017-08-15 17:04:11 -0500

    Remove erase in favor of trim
    For better or worse, erase in the block device API is intended for flash
    erases, which is a low-level operation that must occur before every
    write and limits the write size of the underlying blocks. This is
    different than the erase size found in SD cards, which does not limit
    the write size for blocks.
    
    Following the standard set by ATA and the ioctl function, we are adding
    a seperate operation for the SD card erase behaviour named trim.
    
    See for more info:
    https://en.wikipedia.org/wiki/Trim_(computing)

--- SDBlockDevice.cpp
+++ SDBlockDevice.cpp
@@ -545,9 +545,17 @@ int SDBlockDevice::read(void *b, bd_addr_t addr, bd_size_t size)
     return status;
 }
 
-int SDBlockDevice::erase(bd_addr_t addr, bd_size_t size)
+bool SDBlockDevice::_is_valid_trim(bd_addr_t addr, bd_size_t size)
 {
-    if (!is_valid_erase(addr, size)) {
+    return (
+        addr % _erase_size == 0 &&
+        size % _erase_size == 0 &&
+        addr + size <= this->size());
+}
+
+int SDBlockDevice::trim(bd_addr_t addr, bd_size_t size)
+{
+    if (!_is_valid_trim(addr, size)) {
         return SD_BLOCK_DEVICE_ERROR_PARAMETER;
     }
 
@@ -558,15 +566,7 @@ int SDBlockDevice::erase(bd_addr_t addr, bd_size_t size)
     }
     int status = BD_ERROR_OK;
 
-    // Toss out erases that are too small, this is fine as the block device api
-    // doesn't garuntee the final state of erased bytes and a following program
-    // will still work fine.
-    if (size < _erase_size) {
-        _lock.unlock();
-        return status;
-    }
-
-    size -= _erase_size;
+    size -= _block_size;
     // SDSC Card (CCS=0) uses byte unit address
     // SDHC and SDXC Cards (CCS=1) use block unit address (512 Bytes unit)
     if (SDCARD_V2HC == _card_type) {
@@ -596,11 +596,6 @@ bd_size_t SDBlockDevice::get_read_size() const
 }
 
 bd_size_t SDBlockDevice::get_program_size() const
-{
-    return _block_size;
-}
-
-bd_size_t SDBlockDevice::get_erase_size() const
 {
     return _block_size;
 }
