fbf7d4077847172cca1d55606f6c6d8cf6da99ba
ccli8 <ccli8@nuvoton.com>
2017-08-17 09:39:41 +0800

    [M487] Remove debug code in AES alternative

--- features/mbedtls/targets/TARGET_NUVOTON/TARGET_M480/aes/aes_alt.h
+++ features/mbedtls/targets/TARGET_NUVOTON/TARGET_M480/aes/aes_alt.h
@@ -53,15 +53,8 @@ typedef struct
     uint32_t channel;
     uint32_t swapType;
     uint32_t *iv;
-		unsigned char prv_iv[16];
-#if 1	
+    unsigned char prv_iv[16];
     uint32_t buf[8]; 
-/* For comparsion with software AES for correctness */ 
-#else
-    uint32_t buf[68];           /*!<  unaligned data    */    
-    int nr;                     /*!<  number of rounds  */
-    uint32_t *rk;               /*!<  AES round keys    */    
-#endif    
 }
 mbedtls_aes_context;
 
