177fb7933d91d616218cefb34f534702cfe5a81b
Jimmy Brisson <theotherjimmy@gmail.com>
2017-08-28 12:06:15 -0500

    Rework config_test to use py.test

--- tools/test/config_test/test19/test_data.py
+++
@@ -1,8 +0,0 @@
-# Test that invalid keys in application configuration are not allowed
-
-expected_results = {
-    "K64F": {
-        "desc": "test invalid key in application config",
-        "exception_msg": "Unknown key(s) 'invalid_key'"
-    }
-}
