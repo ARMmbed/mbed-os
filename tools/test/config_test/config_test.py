de3cc63bb3fc9fd0b73916d4beae436e41b06e02
Jimmy Brisson <theotherjimmy@gmail.com>
2017-08-28 12:06:15 -0500

    Rework config_test to use py.test

--- tools/test/config_test/config_test.py
+++ tools/test/config_test/config_test.py
@@ -15,10 +15,14 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
+import os
+import sys
+import json
+import pytest
+from os.path import join, isfile, dirname, abspath
 from tools.build_api import get_config
 from tools.targets import set_targets_json_location, Target
 from tools.config import ConfigException, Config
-import os, sys
 
 # Compare the output of config against a dictionary of known good results
 def compare_config(cfg, expected):
@@ -27,87 +31,43 @@ def compare_config(cfg, expected):
             if cfg[k].value != expected[k]:
                 return "'%s': expected '%s', got '%s'" % (k, expected[k], cfg[k].value)
     except KeyError:
-        raise
         return "Unexpected key '%s' in configuration data" % k
     for k in expected:
-        if k not in ["desc", "expected_macros", "expected_features"] + cfg.keys():
+        if k not in ["expected_macros", "expected_features"] + cfg.keys():
             return "Expected key '%s' was not found in configuration data" % k
     return ""
 
-def test_tree(full_name, name):
-    failed = 0
-    sys.path.append(full_name)
-    if "test_data" in sys.modules:
-       del sys.modules["test_data"]
-    import test_data
-    # If the test defines custom targets, they must exist in a file called
-    # "targets.json" in the test's directory.
-    if os.path.isfile(os.path.join(full_name, "targets.json")):
-        set_targets_json_location(os.path.join(full_name, "targets.json"))
-    else: # uset the regular set of targets
-        set_targets_json_location()
-    for target, expected in test_data.expected_results.items():
-        sys.stdout.write("%s:'%s'(%s) " % (name, expected["desc"], target))
-        sys.stdout.flush()
-        err_msg = None
+def data_path(path):
+    return join(path, "test_data.json")
+
+def is_test(path):
+    return isfile(data_path(path))
+
+root_dir = abspath(dirname(__file__))
+
+@pytest.mark.parametrize("name", filter(lambda d: is_test(join(root_dir, d)),
+                                             os.listdir(root_dir)))
+def test_config(name):
+    test_dir = join(root_dir, name)
+    test_data = json.load(open(data_path(test_dir)))
+    targets_json = os.path.join(test_dir, "targets.json")
+    set_targets_json_location(targets_json if isfile(targets_json) else None)
+    for target, expected in test_data.items():
         try:
-            cfg, macros, features = get_config(full_name, target, "GCC_ARM")
-            macros = Config.config_macros_to_macros(macros)
-        except ConfigException as e:
-            err_msg = e.message
-        if err_msg:
-            if expected.has_key("exception_msg"):
-                if err_msg.find(expected["exception_msg"]) == -1:
-                    print "FAILED!"
-                    sys.stderr.write("    Unexpected error message!\n")
-                    sys.stderr.write("    Expected: '%s'\n" % expected["exception_msg"])
-                    sys.stderr.write("    Got: '%s'\n" % err_msg)
-                    failed += 1
-                else:
-                    print "OK"
-            else:
-                print "FAILED!"
-                sys.stderr.write("    Error while getting configuration!\n")
-                sys.stderr.write("    " + err_msg + "\n")
-                failed += 1
-        else:
+            cfg, macros, features = get_config(test_dir, target, "GCC_ARM")
             res = compare_config(cfg, expected)
+            assert not(res), res
             expected_macros = expected.get("expected_macros", None)
             expected_features = expected.get("expected_features", None)
 
-            if res:
-                print "FAILED!"
-                sys.stdout.write("    " + res + "\n")
-                failed += 1
-            elif expected_macros is not None:
-                if sorted(expected_macros) != sorted(macros):
-                    print "FAILED!"
-                    sys.stderr.write("    List of macros doesn't match\n")
-                    sys.stderr.write("    Expected: '%s'\n" % ",".join(sorted(expected_macros)))
-                    sys.stderr.write("    Got: '%s'\n" % ",".join(sorted(expected_macros)))
-                    failed += 1
-                else:
-                    print "OK"
-            elif expected_features is not None:
-                if sorted(expected_features) != sorted(features):
-                    print "FAILED!"
-                    sys.stderr.write("    List of features doesn't match\n")
-                    sys.stderr.write("    Expected: '%s'\n" % ",".join(sorted(expected_features)))
-                    sys.stderr.write("    Got: '%s'\n" % ",".join(sorted(expected_features)))
-                    failed += 1
-                else:
-                    print "OK"
+            if expected_macros is not None:
+                macros = Config.config_macros_to_macros(macros)
+                assert sorted(expected_macros) == sorted(macros)
+            if expected_features is not None:
+                assert sorted(expected_features) == sorted(features)
+        except ConfigException as e:
+            err_msg = e.message
+            if "exception_msg" not in expected:
+                assert not(err_msg), "Unexpected Error: %s" % e
             else:
-                print "OK"
-    sys.path.remove(full_name)
-    return failed
-
-failed = 0
-root_dir = os.path.abspath(os.path.dirname(__file__))
-tlist = sorted(os.listdir(root_dir), key = lambda e: int(e[4:]) if e.startswith('test') else -1)
-for test_name in tlist:
-    full_name = os.path.join(root_dir, test_name)
-    if os.path.isdir(full_name) and test_name.startswith('test'):
-        failed += test_tree(full_name, test_name)
-sys.exit(failed)
-
+                assert expected["exception_msg"] in err_msg
--- tools/test/config_test/test24/test_data.py
+++
@@ -1,8 +0,0 @@
-# Testing if features can enable other features
-
-expected_results = {
-    "test_target": {
-        "desc": "test recursive features",
-        "expected_features": ["IPV4", "STORAGE", "UVISOR"]
-    }
-}
