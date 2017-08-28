d9d6f593c0ae3fdd178ccdc3118f1c35edff60de
bcostm <bco.stm@gmail.com>
2017-08-28 14:40:36 +0200

    STM32 USBDevice: Add DISCO_F407VG target

--- features/unsupported/USBDevice/targets/TARGET_STM/USBHAL_IP_OTGFSHS.h
+++ features/unsupported/USBDevice/targets/TARGET_STM/USBHAL_IP_OTGFSHS.h
@@ -140,6 +140,7 @@ USBHAL::USBHAL(void) {
     defined(TARGET_NUCLEO_F446ZE) || \
     defined(TARGET_NUCLEO_F767ZI) || \
     defined(TARGET_NUCLEO_F746ZG) || \
+    defined(TARGET_DISCO_F407VG) || \
     defined(TARGET_DISCO_F469NI) || \
     defined(TARGET_DISCO_F746NG_OTG_FS)
     __HAL_RCC_GPIOA_CLK_ENABLE();
