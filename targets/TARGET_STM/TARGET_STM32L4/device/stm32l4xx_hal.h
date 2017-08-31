04b1c3a95f0071fdf998f17eb7f2e95eea4b2c77
Boting Ren <Boting.Ren@arm.com>
2017-08-31 15:21:22 +0900

    Remove HAL_GetUID_12Bytes function from stm32l4xx_hal layer.

--- targets/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal.h
+++ targets/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal.h
@@ -582,7 +582,6 @@ void HAL_ResumeTick(void);
 uint32_t HAL_GetHalVersion(void);
 uint32_t HAL_GetREVID(void);
 uint32_t HAL_GetDEVID(void);
-void HAL_GetUID_12Byte(uint32_t *UID);
 uint32_t HAL_GetUIDw0(void);
 uint32_t HAL_GetUIDw1(void);
 uint32_t HAL_GetUIDw2(void);
