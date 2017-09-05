e0c2ea2876bc0b950a1eb68d8d9e3e1c2c370545
Chris Snow <chris@chrissnow.co.uk>
2017-09-05 18:05:29 +0100

    Ensure CRP is set correctly for IAR, GCC and ARM
    CRP value can be set through a macro in mbed_app such as
    "macros": [
        "CRP=CRP_NONE"
    ]

--- targets/TARGET_NXP/TARGET_LPC176X/device/CRP.c
+++ targets/TARGET_NXP/TARGET_LPC176X/device/CRP.c
@@ -13,6 +13,48 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#if !defined(MBED_APP_START) // do not include CRP if there is a bootloader.
-    long CRP_Key __attribute__((section(".ARM.__at_0x000002FC "))) = 0xFFFFFFFF;
+ 
+ #include "mbed_toolchain.h"
+
+ /* Code Read Protection
+
+NONE	0xFFFFFFFF - No code protection.
+
+CRP1    0x12345678 - Write to RAM command can not access RAM below 0x10000200.
+                   - Read Memory command: disabled.
+                   - Copy RAM to Flash command: cannot write to Sector 0.
+                   - "Go" command: disabled.
+                   - Erase sector(s) command: can erase any individual sector except 
+                   	 sector 0 only, or can erase all sectors at once.
+                   - Compare command: disabled
+
+CRP2    0x87654321 - Write to RAM command: disabled.
+                   - Copy RAM to Flash: disabled.
+                   - Erase command: only allows erase of all sectors.
+
+CRP3    0x43218765 - Access to chip via the SWD pins is disabled. ISP entry
+                     by pulling PIO0_1 LOW is disabled if a valid user code is
+                     present in flash sector 0.
+Caution: If CRP3 is selected, no future factory testing can be
+performed on the device.
+*/
+#if !defined(APPLICATION_ADDR) // Relocate CRP if there is a bootloader.
+    #define APPLICATION_ADDR 0
+#endif
+
+#define CRP_NONE	0xFFFFFFFF
+#define CRP_1		0x12345678 
+#define CRP_2		0x87654321
+#define CRP_3		0x43218765
+
+#ifndef CRP
+#define CRP CRP_NONE
+#endif
+
+#if defined (__ICCARM__)
+    __root const long CRP_Key@APPLICATION_ADDR + 0x000002FC = CRP;
+#elif defined (__ARMCC_VERSION)
+    const long CRP_Key __attribute__((used)) __attribute__((at(APPLICATION_ADDR+0x000002FC))) = CRP;
+#elif defined (__GNUC__)
+    const long CRP_Key __attribute__((used)) __attribute__((section(".CRPSection"))) = CRP;
 #endif
