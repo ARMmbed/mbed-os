9c779577985784dd008b5c04bd824cb0cc54d1af
Piotr Grygorczuk <grygorek@gmail.com>
2017-09-05 15:30:14 +0100

    LPCXpresso LPC1769 board ported
    The blinky example compiles and runs.
    The board has a different eth phy component than mbed LPC1768. It requires a driver.

--- targets/TARGET_NXP/mbed_rtx.h
+++ targets/TARGET_NXP/mbed_rtx.h
@@ -50,7 +50,7 @@
 #define INITIAL_SP              (0x02009000UL)
 #endif
 
-#elif defined(TARGET_LPC1768)
+#elif defined(TARGET_LPC1768) || defined(TARGET_LPC1769)
 
 #ifndef INITIAL_SP
 #define INITIAL_SP              (0x10008000UL)
