54d08d62516c64c07806113b3a5a4af064a5cfdd
0x6d61726b <0x6d61726b@gmail.com>
2017-09-08 15:15:46 +0200

    fixed element calculation

--- platform/CircularBuffer.h
+++ platform/CircularBuffer.h
@@ -108,16 +108,16 @@ public:
     /** Returns the number of available transactions the buffer contains */
     CounterType available() {
         core_util_critical_section_enter();
-        CounterType elements = 0;
+        CounterType elements;
         if (!_full)
         {
             if (_head < _tail)
                 elements = BufferSize + _head - _tail;
             else
                 elements = _head - _tail;
-            
-            elements /= sizeof(T);
         }
+        else
+            elements = BufferSize;
         core_util_critical_section_exit();
         return elements;
     }
