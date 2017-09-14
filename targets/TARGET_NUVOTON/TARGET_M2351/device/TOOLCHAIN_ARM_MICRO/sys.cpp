f7ea847dfed25b4eb5348240c210981a47ba8962
Deepika <deepika.bhavnani@arm.com>
2017-09-14 11:21:19 -0500

    [M2351] ARMC6 compiler related changes

--- targets/TARGET_NUVOTON/TARGET_M2351/device/TOOLCHAIN_ARM_MICRO/sys.cpp
+++ targets/TARGET_NUVOTON/TARGET_M2351/device/TOOLCHAIN_ARM_MICRO/sys.cpp
@@ -9,13 +9,17 @@
 extern "C" {
 #endif
 
+#if defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050)
+#include <arm_compat.h>
+#endif
+
 #include <rt_misc.h>
 #include <stdint.h>
 
 extern char Image$$ARM_LIB_STACK$$ZI$$Limit[];
 extern char Image$$ARM_LIB_HEAP$$Base[];
 extern char Image$$ARM_LIB_HEAP$$ZI$$Limit[];
-extern __value_in_regs struct __initial_stackheap __user_setup_stackheap(uint32_t R0, uint32_t R1, uint32_t R2, uint32_t R3) {
+extern __value_in_regs struct __initial_stackheap _mbed_user_setup_stackheap(uint32_t R0, uint32_t R1, uint32_t R2, uint32_t R3) {
 
     struct __initial_stackheap r;
     r.heap_base = (uint32_t)Image$$ARM_LIB_HEAP$$Base;
