2b8f99361288ffe179c86aca58ccaf9c3aef0b04
Jimmy Brisson <theotherjimmy@gmail.com>
2017-09-19 15:14:43 -0500

    Test memap parsing for ARMC6 and IAR

---
+++ tools/test/memap/parse_test.py
@@ -0,0 +1,36 @@
+import sys
+from io import open
+from os.path import isfile, join, dirname
+import json
+
+import pytest
+
+from tools.memap import MemapParser
+from copy import deepcopy
+
+
+PARSED_ARM_DATA = {
+    "startup/startup.o": {".text": 0xc0},
+    "[lib]/c_p.l/__main.o": {".text": 8},
+    "irqs/irqs.o": {".text": 0x98},
+    "data/data.o": {".data": 0x18, ".bss": 0x198},
+    "main.o": {".text": 0x36},
+}
+
+def test_parse_armcc():
+    memap = MemapParser()
+    memap.parse_map_file_armcc(open(join(dirname(__file__), "arm.map")))
+    assert memap.modules == PARSED_ARM_DATA
+
+PARSED_IAR_GCC_DATA = {
+    "startup/startup.o": {".text": 0xc0},
+    "[lib]/d16M_tlf.a/__main.o": {".text": 8},
+    "irqs/irqs.o": {".text": 0x98},
+    "data/data.o": {".data": 0x18, ".bss": 0x198},
+    "main.o": {".text": 0x36},
+}
+
+def test_parse_iar():
+    memap = MemapParser()
+    memap.parse_map_file_iar(open(join(dirname(__file__), "iar.map")))
+    assert memap.modules == PARSED_IAR_GCC_DATA
