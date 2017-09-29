fde08a1ee8df4cf263a5ec6b5989dec3e2f95a03
Seppo Takalo <seppo.takalo@arm.com>
2017-09-29 12:55:04 +0300

    Fix includes.
    Cannot include header file witin a function (without severe side effects)

--- TESTS/network/wifi/get_interface.cpp
+++ TESTS/network/wifi/get_interface.cpp
@@ -1,5 +1,12 @@
 #include "mbed.h"
+
+#if TARGET_UBLOX_EVK_ODIN_W2
+#include "OdinWiFiInterface.h"
+#elif TARGET_REALTEK_RTL8195AM
+#include "RTWInterface.h"
+#else
 #include "ESP8266Interface.h"
+#endif
 
 WiFiInterface *get_interface()
 {
@@ -9,10 +16,8 @@ WiFiInterface *get_interface()
         delete interface;
 
 #if TARGET_UBLOX_EVK_ODIN_W2
-#include "OdinWiFiInterface.h"
     interface = new OdinWiFiInterface();
 #elif TARGET_REALTEK_RTL8195AM
-#include "RTWInterface.h"
     interface = new RTWInterface();
 #else
     interface = new ESP8266Interface(D1, D0);
