d3b1f92093b12ebe6e973220ce7d6f200e6f189a
Helmut Tschemernjak <helmut@helios.de>
2017-10-01 10:47:09 +0200

    Added NUCLEO_L433RC into build targets

--- tools/export/sw4stm32/__init__.py
+++ tools/export/sw4stm32/__init__.py
@@ -244,6 +244,11 @@ class Sw4STM32(GNUARMEclipse):
             'name': 'NUCLEO-L432KC',
             'mcuId': 'STM32L432KCUx'
         },
+        'NUCLEO_L433RC':
+        {
+            'name': 'NUCLEO-L433RC',
+            'mcuId': 'STM32L433RCUx'
+        },
         'NUCLEO_L476RG':
         {
             'name': 'NUCLEO-L476RG',
