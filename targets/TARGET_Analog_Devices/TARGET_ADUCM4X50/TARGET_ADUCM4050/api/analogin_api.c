33927ee963c90d80002fe496c1d79f117995e602
Dave Wu <dave.wu@analog.com>
2017-10-02 14:28:25 +1100

    Fixed issues raised in the feedback from ARM for the ADuCM4050 platform.

--- targets/TARGET_Analog_Devices/TARGET_ADUCM4X50/TARGET_ADUCM4050/api/analogin_api.c
+++ targets/TARGET_Analog_Devices/TARGET_ADUCM4X50/TARGET_ADUCM4050/api/analogin_api.c
@@ -47,10 +47,6 @@
 #include "pinmap.h"
 #include "PeripheralPins.h"
 
-/** Analogin hal structure. analogin_s is declared in the target's hal
- */
-typedef struct analogin_s analogin_t;
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -99,21 +95,21 @@ void analogin_init(analogin_t *obj, PinName pin)
     obj->UserBuffer.nChannels = channel;
 
     /* Open the ADC device */
-    adi_adc_Open(ADC_DEV_NUM, DeviceMemory, sizeof(DeviceMemory),&hDevice);
+    adi_adc_Open(ADC_DEV_NUM, DeviceMemory, sizeof(DeviceMemory), &hDevice);
     obj->hDevice = hDevice;
 
     /* Power up ADC */
     adi_adc_PowerUp (hDevice, true);
 
     /* Set ADC reference */
-    adi_adc_SetVrefSource (hDevice, ADI_ADC_VREF_SRC_INT_2_50_V);
+    adi_adc_SetVrefSource(hDevice, ADI_ADC_VREF_SRC_INT_2_50_V);
 
     /* Enable ADC sub system */
-    adi_adc_EnableADCSubSystem (hDevice, true);
+    adi_adc_EnableADCSubSystem(hDevice, true);
 
     /* Wait untilthe ADC is ready for sampling */
     while(bReady == false) {
-        adi_adc_IsReady (hDevice, &bReady);
+        adi_adc_IsReady(hDevice, &bReady);
     }
 
     /* Start calibration */
@@ -121,14 +117,14 @@ void analogin_init(analogin_t *obj, PinName pin)
 
     /* Wait until calibration is done */
     while (!bCalibrationDone) {
-        adi_adc_IsCalibrationDone (hDevice, &bCalibrationDone);
+        adi_adc_IsCalibrationDone(hDevice, &bCalibrationDone);
     }
 
     /* Set the delay time */
-    adi_adc_SetDelayTime ( hDevice, obj->DelayCycles);
+    adi_adc_SetDelayTime(hDevice, obj->DelayCycles);
 
     /* Set the acquisition time. (Application need to change it based on the impedence) */
-    adi_adc_SetAcquisitionTime ( hDevice, obj->SampleCycles);
+    adi_adc_SetAcquisitionTime(hDevice, obj->SampleCycles);
 
 }
 
@@ -155,15 +151,15 @@ uint16_t analogin_read_u16(analogin_t *obj)
     ADI_ADC_BUFFER  *pAdcBuffer;
 
     /* Submit the buffer to the driver */
-    adi_adc_SubmitBuffer (hDevice, &obj->UserBuffer);
+    adi_adc_SubmitBuffer(hDevice, &obj->UserBuffer);
 
     /* Enable the ADC */
-    adi_adc_Enable (hDevice, true);
+    adi_adc_Enable(hDevice, true);
 
-    adi_adc_GetBuffer (hDevice, &pAdcBuffer);
+    adi_adc_GetBuffer(hDevice, &pAdcBuffer);
     MBED_ASSERT(pAdcBuffer == &obj->UserBuffer);
 
-    return( (uint16_t) ( ((uint16_t *)pAdcBuffer->pDataBuffer)[(pAdcBuffer->nNumConversionPasses) - 1]) );
+    return( (uint16_t)( ((uint16_t *)pAdcBuffer->pDataBuffer)[(pAdcBuffer->nNumConversionPasses) - 1]) );
 }
 
 /* Retrieve te active channel correspondoing to the input pin */
