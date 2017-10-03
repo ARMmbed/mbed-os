ee9d04e248e6e13d92c8013d66f81377a2525837
Seppo Takalo <seppo.takalo@arm.com>
2017-10-03 12:50:03 +0300

    Fix builds for targets without wifi or without mbed_app.json

--- TESTS/network/wifi/get_interface.cpp
+++ TESTS/network/wifi/get_interface.cpp
@@ -1,11 +1,32 @@
 #include "mbed.h"
 
+// Pick the correct driver based on mbed_app.json
+#define INTERNAL            1
+#define WIFI_ESP8266        2
+#define X_NUCLEO_IDW01M1    3
+
+#if MBED_CONF_APP_WIFI_DRIVER == INTERNAL
+
 #if TARGET_UBLOX_EVK_ODIN_W2
 #include "OdinWiFiInterface.h"
+#define DRIVER OdinWiFiInterface
+
 #elif TARGET_REALTEK_RTL8195AM
 #include "RTWInterface.h"
+#define DRIVER RTWInterface
 #else
+#error [NOT_SUPPORTED] Unsupported Wifi driver
+#endif
+
+#elif MBED_CONF_APP_WIFI_DRIVER == WIFI_ESP8266
 #include "ESP8266Interface.h"
+#define DRIVER ESP8266Interface
+
+#elif MBED_CONF_APP_WIFI_DRIVER == X_NUCLEO_IDW01M1
+#include "SpwfSAInterface.h"
+#define DRIVER SpwfSAInterface
+#else
+#error [NOT_SUPPORTED] Unsupported Wifi driver
 #endif
 
 WiFiInterface *get_interface()
@@ -15,12 +36,10 @@ WiFiInterface *get_interface()
     if (interface)
         delete interface;
 
-#if TARGET_UBLOX_EVK_ODIN_W2
-    interface = new OdinWiFiInterface();
-#elif TARGET_REALTEK_RTL8195AM
-    interface = new RTWInterface();
+#if MBED_CONF_APP_WIFI_DRIVER == INTERNAL
+    interface = new DRIVER();
 #else
-    interface = new ESP8266Interface(D1, D0);
+    interface = new DRIVER(MBED_CONF_APP_WIFI_TX, MBED_CONF_APP_WIFI_RX);
 #endif
     return interface;
 }
