77dd4205095f09b54565bc50adacd9e8416c5d3f
Russ Butler <russ.butler@arm.com>
2017-10-03 16:09:15 -0500

    Increase ticker test time for slower devices
    Increase the hal ticker test time from 30s to 60s to prevent
    a timeout from occurring on slower devices, such as the nrf51.

--- TESTS/mbed_hal/ticker/main.cpp
+++ TESTS/mbed_hal/ticker/main.cpp
@@ -2367,7 +2367,7 @@ static const case_t cases[] = {
 
 static utest::v1::status_t greentea_test_setup(const size_t number_of_cases)
 {
-    GREENTEA_SETUP(30, "default_auto");
+    GREENTEA_SETUP(60, "default_auto");
     return verbose_test_setup_handler(number_of_cases);
 }
 
