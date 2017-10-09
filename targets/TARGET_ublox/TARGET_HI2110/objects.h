249655d86b19791b408539e76dfe7ae472698f7c
Wajahat Abbas <wabb@4mwirelesspk.local>
2017-10-09 17:29:44 +0500

    changes to serial_s struct

--- targets/TARGET_ublox/TARGET_HI2110/objects.h
+++ targets/TARGET_ublox/TARGET_HI2110/objects.h
@@ -47,21 +47,26 @@ struct gpio_irq_s {
     uint32_t ch;   /* Corresponds to the interrupt pin */
 };
 
-struct serial_s {
-    SerialConfig config;
-    PinName rx_pin;
-    PinName tx_pin;
-    volatile uart_ctrl_t *reg_base;
-    uint8_t index;
+struct serial_s {    
+    PinName rx_pin; /* PinName will be used to find if UART1 or UART2 is addressed, PinNames, PortNames, PeripheralNames, Pinmap is yet to be updated */
+    PinName tx_pin; 
+    SerialConfig config; /* This enum is used to initialize the UART being addressed */	
+    volatile struct uart_s *reg_base;
+    uint8_t index; /* IRQ index number */
     uint32_t baud_rate;
+    uint16_t baud_rate_divider; /* calculated from baudrate */
+    uint8_t nco_phase; /* calculated from baudrate */
+    uint8_t flow_control; 
+    bool baudrate_gen_enable;
     bool format_set; /* If true then the struct that follows is populated */
     struct {
-        uint8_t stop_bits;
-        uint8_t data_bits;
-        uint8_t parity;
+        uint8_t rx_stop_bits; /* ref. uart_stop_bits */
+        uint8_t tx_stop_bits;
+        uint8_t data_bits; /* ref. uart_driver_mode */
+        uint8_t parity; /* ref. uart_parity (UART_PARITY_EVEN or UART_PARITY_ODD) */
     } format;
-    irq_setting_t irq_rx_setting;
-    irq_setting_t irq_tx_setting;
+    irq_setting_t irq_rx_setting; /* used in serial_irq_set if IRQ has been set or not */
+    irq_setting_t irq_tx_setting;		
 };
 
 #include "gpio_object.h"
