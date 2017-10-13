3c3b0329d44d44a2d47a28c755658d7f1db6a085
Christopher Haster <chaster@utexas.edu>
2017-10-12 20:42:30 -0500

    Squashed 'littlefs/' changes from 9843402..454b588
    454b588 Updated SPEC.md and DESIGN.md based on recent changes
    f3578e3 Removed clamping to block size in ctz linked-list
    83d4c61 Updated copyright
    539409e Refactored deduplicate/deorphan step to single deorphan step
    2936514 Added atomic move using dirty tag in entry type
    ac9766e Added self-hosting fuzz test using littlefs-fuse
    9db1a86 Added specification document
    
    git-subtree-dir: littlefs
    git-subtree-split: 454b588f73032f9621c264fba280ab7b3a216402

--- lfs_util.h
+++ lfs_util.h
@@ -1,8 +1,19 @@
 /*
  * lfs utility functions
  *
- * Copyright (c) 2017 Christopher Haster
- * Distributed under the Apache 2.0 license
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 #ifndef LFS_UTIL_H
 #define LFS_UTIL_H
@@ -12,7 +23,8 @@
 #include <stdio.h>
 
 
-// Builtin functions
+// Builtin functions, these may be replaced by more
+// efficient implementations in the system
 static inline uint32_t lfs_max(uint32_t a, uint32_t b) {
     return (a > b) ? a : b;
 }
@@ -33,10 +45,12 @@ static inline int lfs_scmp(uint32_t a, uint32_t b) {
     return (int)(unsigned)(a - b);
 }
 
+// CRC-32 with polynomial = 0x04c11db7
 void lfs_crc(uint32_t *crc, const void *buffer, size_t size);
 
 
-// Logging functions
+// Logging functions, these may be replaced by system-specific
+// logging functions
 #define LFS_DEBUG(fmt, ...) printf("lfs debug: " fmt "\n", __VA_ARGS__)
 #define LFS_WARN(fmt, ...)  printf("lfs warn: " fmt "\n", __VA_ARGS__)
 #define LFS_ERROR(fmt, ...) printf("lfs error: " fmt "\n", __VA_ARGS__)
