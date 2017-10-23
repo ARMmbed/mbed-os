10d85eace9d3782ce2040023c96651d160c3a744
Russ Butler <russ.butler@arm.com>
2017-10-23 15:36:03 -0500

    Keep rtc code if low power timer is used
    Keep the RTC code if either DEVICE_RTC or DEVICE_LOWPOWERTIMER is
    defined on the devices which use the RTC for both the rtc api and the
    low power timer api. This allows DEVICE_LOWPOWERTIMER to be enabled while
    DEVICE_RTC is turned off.

--- targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_KL82Z/TARGET_USENSE/mbed_overrides.c
+++ targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_KL82Z/TARGET_USENSE/mbed_overrides.c
@@ -46,7 +46,7 @@ void NMI_Handler(void)
     gpio_init_in(&gpio, PTA4);
 }
 
-#ifdef DEVICE_RTC
+#if DEVICE_RTC || DEVICE_LOWPOWERTIMER
 // Enable the RTC oscillator if available on the board
 void rtc_setup_oscillator(RTC_Type *base)
 {
