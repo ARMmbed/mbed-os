f9655fe99dc37f2a2c4a60e4e7dd333b3bf4a07a
Tony Wu <tung7970@gmail.com>
2017-10-24 23:32:53 +0800

    rtl8195am - remove redundant header file inclusion
    Signed-off-by: Tony Wu <tung7970@gmail.com>

--- targets/TARGET_Realtek/TARGET_AMEBA/TARGET_RTL8195A/device/rtl8195a_init.c
+++ targets/TARGET_Realtek/TARGET_AMEBA/TARGET_RTL8195A/device/rtl8195a_init.c
@@ -15,17 +15,6 @@
  */
 #include "rtl8195a.h"
 
-#if defined(__CC_ARM)
-#include "cmsis_armcc.h"
-#elif (defined(__ARMCC_VERSION) && __ARMCC_VERSION >= 6010050)
-#include "cmsis_armclang.h"
-#elif defined(__GNUC__)
-#include "cmsis_gcc.h"
-#else
-#include <cmsis_iar.h>
-#endif
-
-
 #if defined(__CC_ARM) || \
     (defined (__ARMCC_VERSION) && __ARMCC_VERSION >= 6010050)
 
