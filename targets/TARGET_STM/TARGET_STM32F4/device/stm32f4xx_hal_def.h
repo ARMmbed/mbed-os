9a343e7d9db66954fcf0127d00008d808cb79a38
deepikabhavnani <deepika.bhavnani@arm.com>
2017-10-30 10:07:28 -0500

    Update params in calls to LDREXW/STREXW to uint32_t

--- targets/TARGET_STM/TARGET_STM32F4/device/stm32f4xx_hal_def.h
+++ targets/TARGET_STM/TARGET_STM32F4/device/stm32f4xx_hal_def.h
@@ -129,9 +129,9 @@ static inline  void atomic_set_u32(volatile uint32_t *ptr, uint32_t mask)
 {
 	uint32_t newValue;
 	do {
-		newValue = (uint32_t)__LDREXW(ptr) | mask;
+		newValue = (uint32_t)__LDREXW((volatile uint32_t *)ptr) | mask;
 
-	} while (__STREXW(newValue, ptr));
+	} while (__STREXW(newValue,(volatile uint32_t *) ptr));
 }
 
 
@@ -139,9 +139,9 @@ static inline  void atomic_clr_u32(volatile uint32_t *ptr, uint32_t mask)
 {
 	uint32_t newValue;
 	do {
-		newValue = (uint32_t)__LDREXW(ptr) &~mask;
+		newValue = (uint32_t)__LDREXW((volatile uint32_t *)ptr) &~mask;
 
-	} while (__STREXW(newValue, ptr));
+	} while (__STREXW(newValue,(volatile uint32_t *) ptr));
 }
 
 #endif /* USE_RTOS */
