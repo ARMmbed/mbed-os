334b2d64b01724a467ea99210c752de39cc23b6e
deepikabhavnani <deepika.bhavnani@arm.com>
2017-10-30 10:07:28 -0500

    Update params in calls to LDREXW/STREXW to uint32_t

--- targets/TARGET_STM/TARGET_STM32F4/device/stm32f4xx_hal_def.h
+++ targets/TARGET_STM/TARGET_STM32F4/device/stm32f4xx_hal_def.h
@@ -131,9 +131,9 @@ static inline  void atomic_set_u32(volatile uint32_t *ptr, uint32_t mask)
 {
 	uint32_t newValue;
 	do {
-		newValue = (uint32_t)__LDREXW(ptr) | mask;
+		newValue = (uint32_t)__LDREXW((volatile uint32_t *)ptr) | mask;
 
-	} while (__STREXW(newValue, ptr));
+	} while (__STREXW(newValue,(volatile uint32_t *) ptr));
 }
 
 
@@ -141,9 +141,9 @@ static inline  void atomic_clr_u32(volatile uint32_t *ptr, uint32_t mask)
 {
 	uint32_t newValue;
 	do {
-		newValue = (uint32_t)__LDREXW(ptr) &~mask;
+		newValue = (uint32_t)__LDREXW((volatile uint32_t *)ptr) &~mask;
 
-	} while (__STREXW(newValue, ptr));
+	} while (__STREXW(newValue,(volatile uint32_t *) ptr));
 }
 /* MBED */
 
--- targets/TARGET_STM/TARGET_STM32F7/device/stm32f7xx_hal_def.h
+++ targets/TARGET_STM/TARGET_STM32F7/device/stm32f7xx_hal_def.h
@@ -130,9 +130,9 @@ static inline  void atomic_set_u32(volatile uint32_t *ptr, uint32_t mask)
 {
 	uint32_t newValue;
 	do {
-		newValue = (uint32_t)__LDREXW(ptr) | mask;
+		newValue = (uint32_t)__LDREXW((volatile uint32_t *)ptr) | mask;
 
-	} while (__STREXW(newValue, ptr));
+	} while (__STREXW(newValue,(volatile uint32_t *) ptr));
 }
 
 
@@ -140,9 +140,9 @@ static inline  void atomic_clr_u32(volatile uint32_t *ptr, uint32_t mask)
 {
 	uint32_t newValue;
 	do {
-		newValue = (uint32_t)__LDREXW(ptr) &~mask;
+		newValue = (uint32_t)__LDREXW((volatile uint32_t *)ptr) &~mask;
 
-	} while (__STREXW(newValue, ptr));
+	} while (__STREXW(newValue,(volatile uint32_t *) ptr));
 }
 
 #if  defined ( __GNUC__ ) && !defined ( __CC_ARM )
