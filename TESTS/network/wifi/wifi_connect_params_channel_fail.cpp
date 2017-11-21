043ee4a6f5b7d2dd84eb6b9a4927c84607de2962
Wolfgang Betz <wolfgang.betz@st.com>
2017-11-21 13:42:51 +0100

    Split test for secure & unsecure tests

--- TESTS/network/wifi/wifi_connect_params_channel_fail.cpp
+++ TESTS/network/wifi/wifi_connect_params_channel_fail.cpp
@@ -23,6 +23,8 @@
 
 using namespace utest::v1;
 
+#if defined(MBED_CONF_APP_WIFI_SECURE_SSID)
+
 void wifi_connect_params_channel_fail(void)
 {
     WiFiInterface *wifi = get_interface();
@@ -32,7 +34,8 @@ void wifi_connect_params_channel_fail(void)
         return;
     }
 
-    nsapi_error_t error = wifi->connect(MBED_CONF_APP_WIFI_SECURE_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA2, MBED_CONF_APP_WIFI_CH_UNSECURE);
+    nsapi_error_t error = wifi->connect(MBED_CONF_APP_WIFI_SECURE_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA2, MBED_CONF_APP_WIFI_CH_SECURE);
     TEST_ASSERT(error==NSAPI_ERROR_CONNECTION_TIMEOUT || error==NSAPI_ERROR_NO_CONNECTION);
 }
 
+#endif // defined(MBED_CONF_APP_WIFI_SECURE_SSID)
