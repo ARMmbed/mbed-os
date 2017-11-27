397b53f1268d282305df3cdfbcf29956c42dbc3b
Maciej Rogozinski <marogi@o2.pl>
2017-11-27 18:14:26 +0100

    Wunderbar target based on TARGET_MCU_K24F1M

--- targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_MCU_K24F/TARGET_MCU_K24F1M/device/system_MK24F12.h
+++ targets/TARGET_Freescale/TARGET_MCUXpresso_MCUS/TARGET_MCU_K24F/TARGET_MCU_K24F1M/device/system_MK24F12.h
@@ -112,8 +112,11 @@ extern "C" {
 #endif
 
 /* Define clock source values */
-
+#ifdef TARGET_WUNDERBAR
+#define CPU_XTAL_CLK_HZ                12000000u           /* Value of the external crystal or oscillator clock frequency in Hz */
+#else
 #define CPU_XTAL_CLK_HZ                50000000u           /* Value of the external crystal or oscillator clock frequency in Hz */
+#endif
 #define CPU_XTAL32k_CLK_HZ             32768u              /* Value of the external 32k crystal or oscillator clock frequency in Hz */
 #define CPU_INT_SLOW_CLK_HZ            32768u              /* Value of the slow internal oscillator clock frequency in Hz  */
 #define CPU_INT_FAST_CLK_HZ            4000000u            /* Value of the fast internal oscillator clock frequency in Hz  */
