32060bb8d0611c3709a0443f384f24ad3bbf9aa0
Kevin Bracey <kevin.bracey@arm.com>
2017-12-05 12:45:30 +0200

    Insert EMACInterface class
    Rather than let "EthernetInterface" be the base EMAC NetworkInterface,
    insert an "EMACInterface" class.
    
    EthernetInterface then derives from EMACInterface and EthInterface.
    
    A Wi-Fi driver can derive from EMACInterface and WiFiInterface - this
    will be more logical than deriving from EthernetInterface and
    WiFiInterface.
    
    This does mean adding a couple of virtual inheritances to avoid
    duplicate NetworkInterfaces:
    
                       NetworkInterface
                         /           \
                virtual /             \ virtual
                       /               \
                 EMACInterface     WiFiInterface
                       \               /
                        \             /
                         \           /
                      MyCustomWiFiInterface

--- features/netsocket/EMACInterface.cpp
+++ features/netsocket/EMACInterface.cpp
@@ -14,10 +14,10 @@
  * limitations under the License.
  */
 
-#include "EthernetInterface.h"
+#include "EMACInterface.h"
 
 /* Interface implementation */
-EthernetInterface::EthernetInterface(EMAC &emac, OnboardNetworkStack &stack) :
+EMACInterface::EMACInterface(EMAC &emac, OnboardNetworkStack &stack) :
     _emac(emac),
     _stack(stack),
     _interface(NULL),
@@ -28,7 +28,7 @@ EthernetInterface::EthernetInterface(EMAC &emac, OnboardNetworkStack &stack) :
 {
 }
 
-nsapi_error_t EthernetInterface::set_network(const char *ip_address, const char *netmask, const char *gateway)
+nsapi_error_t EMACInterface::set_network(const char *ip_address, const char *netmask, const char *gateway)
 {
     _dhcp = false;
 
@@ -42,13 +42,13 @@ nsapi_error_t EthernetInterface::set_network(const char *ip_address, const char 
     return NSAPI_ERROR_OK;
 }
 
-nsapi_error_t EthernetInterface::set_dhcp(bool dhcp)
+nsapi_error_t EMACInterface::set_dhcp(bool dhcp)
 {
     _dhcp = dhcp;
     return NSAPI_ERROR_OK;
 }
 
-nsapi_error_t EthernetInterface::connect()
+nsapi_error_t EMACInterface::connect()
 {
     if (!_interface) {
         nsapi_error_t err = _stack.add_ethernet_interface(_emac, true, &_interface);
@@ -65,12 +65,12 @@ nsapi_error_t EthernetInterface::connect()
             DEFAULT_STACK);
 }
 
-nsapi_error_t EthernetInterface::disconnect()
+nsapi_error_t EMACInterface::disconnect()
 {
     return _interface->bringdown();
 }
 
-const char *EthernetInterface::get_mac_address()
+const char *EMACInterface::get_mac_address()
 {
     if (_interface->get_mac_address(_mac_address, sizeof(_mac_address))) {
         return _mac_address;
@@ -78,7 +78,7 @@ const char *EthernetInterface::get_mac_address()
     return NULL;
 }
 
-const char *EthernetInterface::get_ip_address()
+const char *EMACInterface::get_ip_address()
 {
     if (_interface->get_ip_address(_ip_address, sizeof(_ip_address))) {
         return _ip_address;
@@ -87,7 +87,7 @@ const char *EthernetInterface::get_ip_address()
     return NULL;
 }
 
-const char *EthernetInterface::get_netmask()
+const char *EMACInterface::get_netmask()
 {
     if (_interface->get_netmask(_netmask, sizeof(_netmask))) {
         return _netmask;
@@ -96,7 +96,7 @@ const char *EthernetInterface::get_netmask()
     return 0;
 }
 
-const char *EthernetInterface::get_gateway()
+const char *EMACInterface::get_gateway()
 {
     if (_interface->get_gateway(_gateway, sizeof(_gateway))) {
         return _gateway;
@@ -105,23 +105,23 @@ const char *EthernetInterface::get_gateway()
     return 0;
 }
 
-NetworkStack *EthernetInterface::get_stack()
+NetworkStack *EMACInterface::get_stack()
 {
     return &_stack;
 }
 
-void EthernetInterface::attach(
+void EMACInterface::attach(
     Callback<void(nsapi_event_t, intptr_t)> status_cb)
 {
     _interface->attach(status_cb);
 }
 
-nsapi_connection_status_t EthernetInterface::get_connection_status() const
+nsapi_connection_status_t EMACInterface::get_connection_status() const
 {
     return _interface->get_connection_status();
 }
 
-nsapi_error_t EthernetInterface::set_blocking(bool blocking)
+nsapi_error_t EMACInterface::set_blocking(bool blocking)
 {
     _interface->set_blocking(blocking);
     return NSAPI_ERROR_OK;
