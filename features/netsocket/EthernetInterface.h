4578d3daf94b310dc9386e2a07617e139ce70a69
Kevin Bracey <kevin.bracey@arm.com>
2017-12-05 12:45:30 +0200

    Insert EMACInterface class
    Rather than let "EthernetInterface" be the base EMAC NetworkInterface,
    insert an "EMACInterface" class.
    
    EthernetInterface then derives from EMACInterface and EthInterface.
    
    A Wi-Fi driver can derive from EMACInterface and WiFiInterface - this
    will be more logical than deriving from EthernetInterface and
    WiFiInterface.
    
    This does mean adding a couple of virtual inheritances to avoid
    duplicate NetworkInterfaces:
    
                       NetworkInterface
                         /           \
                virtual /             \ virtual
                       /               \
                 EMACInterface     WiFiInterface
                       \               /
                        \             /
                         \           /
                      MyCustomWiFiInterface

--- features/netsocket/EthernetInterface.h
+++ features/netsocket/EthernetInterface.h
@@ -19,14 +19,13 @@
 
 #include "nsapi.h"
 #include "rtos.h"
-#include "EMAC.h"
-#include "OnboardNetworkStack.h"
+#include "EMACInterface.h"
 
 
 /** EthernetInterface class
- *  Implementation of the NetworkStack for an EMAC-based driver
+ *  Implementation of the NetworkStack for an EMAC-based Ethernet driver
  */
-class EthernetInterface : public EthInterface
+class EthernetInterface : public EMACInterface, public EthInterface
 {
 public:
     /** Create an EMAC-based ethernet interface.
@@ -44,110 +43,7 @@ public:
      */
     EthernetInterface(
             EMAC &emac = EMAC::get_default_instance(),
-            OnboardNetworkStack &stack = OnboardNetworkStack::get_default_instance());
-
-    /** Set a static IP address
-     *
-     *  Configures this network interface to use a static IP address.
-     *  Implicitly disables DHCP, which can be enabled in set_dhcp.
-     *  Requires that the network is disconnected.
-     *
-     *  @param ip_address  Null-terminated representation of the local IP address
-     *  @param netmask     Null-terminated representation of the local network mask
-     *  @param gateway     Null-terminated representation of the local gateway
-     *  @return            0 on success, negative error code on failure
-     */
-    virtual nsapi_error_t set_network(
-            const char *ip_address, const char *netmask, const char *gateway);
-
-    /** Enable or disable DHCP on the network
-     *
-     *  Requires that the network is disconnected
-     *
-     *  @param dhcp     False to disable dhcp (defaults to enabled)
-     *  @return         0 on success, negative error code on failure
-     */
-    virtual nsapi_error_t set_dhcp(bool dhcp);
-
-    /** Start the interface
-     *  @return             0 on success, negative on failure
-     */
-    virtual nsapi_error_t connect();
-
-    /** Stop the interface
-     *  @return             0 on success, negative on failure
-     */
-    virtual nsapi_error_t disconnect();
-
-    /** Get the local MAC address
-     *
-     *  Provided MAC address is intended for info or debug purposes and
-     *  may not be provided if the underlying network interface does not
-     *  provide a MAC address
-     *  
-     *  @return         Null-terminated representation of the local MAC address
-     *                  or null if no MAC address is available
-     */
-    virtual const char *get_mac_address();
-
-    /** Get the local IP address
-     *
-     *  @return         Null-terminated representation of the local IP address
-     *                  or null if no IP address has been recieved
-     */
-    virtual const char *get_ip_address();
-
-    /** Get the local network mask
-     *
-     *  @return         Null-terminated representation of the local network mask 
-     *                  or null if no network mask has been recieved
-     */
-    virtual const char *get_netmask();
-
-    /** Get the local gateways
-     *
-     *  @return         Null-terminated representation of the local gateway
-     *                  or null if no network mask has been recieved
-     */
-    virtual const char *get_gateway();
-
-    /** Register callback for status reporting
-     *
-     *  @param status_cb The callback for status changes
-     */
-    virtual void attach(mbed::Callback<void(nsapi_event_t, intptr_t)> status_cb);
-
-    /** Get the connection status
-     *
-     *  @return         The connection status according to nsapi_connection_status_t
-     */
-    virtual nsapi_connection_status_t get_connection_status() const;
-
-    /** Set blocking status of connect() which by default should be blocking
-     *
-     *  @param blocking true if connect is blocking
-     *  @return         0 on success, negative error code on failure
-     */
-    virtual nsapi_error_t set_blocking(bool blocking);
-
-
-protected:
-    /** Provide access to the underlying stack
-     *
-     *  @return The underlying network stack 
-     */
-    virtual NetworkStack *get_stack();
-
-    EMAC &_emac;
-    OnboardNetworkStack &_stack;
-    OnboardNetworkStack::Interface *_interface;
-    bool _dhcp;
-    bool _blocking;
-    char _mac_address[NSAPI_MAC_SIZE];
-    char _ip_address[NSAPI_IPv6_SIZE];
-    char _netmask[NSAPI_IPv4_SIZE];
-    char _gateway[NSAPI_IPv4_SIZE];
-    Callback<void(nsapi_event_t, intptr_t)> _connection_status_cb;
+            OnboardNetworkStack &stack = OnboardNetworkStack::get_default_instance()) : EMACInterface(emac, stack) { }
 };
 
 #endif
