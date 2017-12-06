89c4dd32301e5f9e737b82c06468e881c26c2fc4
mudassar-ublox <31336299+mudassar-ublox@users.noreply.github.com>
2017-12-06 18:28:55 +0500

    Update gpio_api.c
    "gpio_channel_select" function updated according to GPIO FPGA setup. 0-6 GPIO has been configured for KM.

--- targets/TARGET_ublox/TARGET_R5030/gpio_api.c
+++ targets/TARGET_ublox/TARGET_R5030/gpio_api.c
@@ -25,9 +25,9 @@
 *****************************************************************************/
 uint32_t gpio_set(PinName pin)
 {
-  MBED_ASSERT(pin != (PinName)NC);
+	MBED_ASSERT(pin != (PinName)NC);
 
-  return (uint32_t) 1;
+	return (uint32_t) 1;
 }
 
 /*****************************************************************************
@@ -36,21 +36,21 @@ uint32_t gpio_set(PinName pin)
 *****************************************************************************/
 void gpio_init(gpio_t *obj, PinName pin)
 {
-  uint32_t status;
-  uint8_t pioChannel;
+	uint32_t status;
+	uint8_t pioChannel;
 
-  MBED_ASSERT(pin != (PinName)NC);
+	MBED_ASSERT(pin != (PinName)NC);
 
-  obj->reg_base = gpio_base;
+	obj->reg_base = gpio_base;
 
-  obj->pin = pin;
+	obj->pin = pin;
 
-  pioChannel = gpio_channel_select(obj->pin);
-  obj->mask = (1 << (pioChannel & PIO_CHANNEL_SUB_32_MASK));
+	pioChannel = gpio_channel_select(obj->pin);
+	obj->mask = (1 << (pioChannel & PIO_CHANNEL_SUB_32_MASK));
 
-  status = gpio_periph_mux_set(PIO_MUX_GPIO,pioChannel,true);
-  
-  MBED_ASSERT(status == SUCCESS);
+	status = gpio_periph_mux_set(PIO_MUX_GPIO,pioChannel,true);
+
+	MBED_ASSERT(status == SUCCESS);
 }
 
 /*****************************************************************************
@@ -59,28 +59,26 @@ void gpio_init(gpio_t *obj, PinName pin)
 *****************************************************************************/
 void gpio_mode(gpio_t *obj, PinMode mode)
 {
-	MBED_ASSERT(obj->pin != (PinName)NC);
+    MBED_ASSERT(obj->pin != (PinName)NC);
 
-	switch (mode) {
-		case PullNone:
-		{
-			obj->reg_base->pio_phdr_0 = obj->mask; // Pull up disable
-			obj->reg_base->pio_pldr_0 = obj->mask; // Pull down disable
-		}
-		break;
-		case PullDown:
-		{
-			obj->reg_base->pio_pler_0 = obj->mask; // Set pull down
-		}
-		break;
-		case PullUp:
-		{
-			obj->reg_base->pio_pher_0 = obj->mask; // Set pull up
-		}
-		break;
-		default:
-		break;
-	}
+    switch (mode)
+    {
+      case PullNone:
+    	  obj->reg_base->pio_phdr_0 = obj->mask; // Pull up disable
+    	  obj->reg_base->pio_pldr_0 = obj->mask; // Pull down disable
+    	  break;
+
+      case PullDown:
+    	  obj->reg_base->pio_pler_0 = obj->mask; // Set pull down
+    	  break;
+
+      case PullUp:
+    	  obj->reg_base->pio_pher_0 = obj->mask; // Set pull up
+    	  break;
+
+      default:
+    	  break;
+    }
 }
 
 /*****************************************************************************
@@ -91,18 +89,16 @@ void gpio_dir(gpio_t *obj, PinDirection direction)
 {
     MBED_ASSERT(obj->pin != (PinName)NC);
 
-	switch (direction) {
-		case PIN_INPUT:
-		{
-			obj->reg_base->pio_iner_0 = obj->mask; // Enable input
-		}
-		break;
-		case PIN_OUTPUT:
-		{
-			obj->reg_base->pio_oer_0 = obj->mask; // Enable output
-		}
-		break;
-	}
+    switch (direction)
+    {
+      case PIN_INPUT:
+    	  obj->reg_base->pio_iner_0 = obj->mask; // Enable input
+    	  break;
+
+      case PIN_OUTPUT:
+    	  obj->reg_base->pio_oer_0 = obj->mask; // Enable output
+    	  break;
+    }
 }
 
 /*****************************************************************************
@@ -110,27 +106,32 @@ void gpio_dir(gpio_t *obj, PinDirection direction)
 						    GPIO channel setection
 *****************************************************************************/
 uint8_t gpio_channel_select(PinName pin)
-{	
+{
     switch(pin)
     {
-      case LED1:
-        return GPIIO_1_CHANNEL;	
-        break;
+       case LED0:
+    	   return GPIO_0_CHANNEL;
 
-      case LED2:
-        return GPIIO_2_CHANNEL;	
-        break;
-		
-      case LED3:
-        return GPIIO_3_CHANNEL;	
-        break;
+       case LED1:
+    	   return GPIO_1_CHANNEL;
 
-      case LED4:
-        return GPIIO_4_CHANNEL;	
-        break;
+       case LED2:
+    	   return GPIO_2_CHANNEL;
 
-      default:
-        return ERROR;
+       case LED3:
+    	   return GPIO_3_CHANNEL;
+
+       case LED4:
+    	   return GPIO_4_CHANNEL;
+
+       case LED5:
+    	   return GPIO_5_CHANNEL;
+
+       case LED6:
+    	   return GPIO_6_CHANNEL;
+
+       default:
+    	   return ERROR;
     }
 }
 
@@ -140,58 +141,60 @@ uint8_t gpio_channel_select(PinName pin)
 *****************************************************************************/
 uint32_t gpio_periph_mux_set(uint8_t mux, uint8_t pioChannel, bool periphPullUpDownOn)
 {
-  uint32_t status = SUCCESS;
-  uint8_t channel_offset_in_reg = (pioChannel & PIO_CHANNEL_SUB_32_MASK);
-  struct pio_s *pio_channel_regbase;
+    uint32_t status = SUCCESS;
+    uint8_t channel_offset_in_reg = (pioChannel & PIO_CHANNEL_SUB_32_MASK);
+    struct pio_s *pio_channel_regbase;
 
-	/*PIO base address */
-    if((pioChannel >> PIO_CHANNEL_OVER_32_SHIFT) == 0){
-		pio_channel_regbase = (struct pio_s *)PIO_CONTROL_BASE;
-	}
-	else{
-		pio_channel_regbase = (struct pio_s *)(PIO_CONTROL_BASE + 0x220);
-	}
+    /*PIO base address */
+    if((pioChannel >> PIO_CHANNEL_OVER_32_SHIFT) == 0)
+    {
+    	pio_channel_regbase = (struct pio_s *)PIO_CONTROL_BASE;
+    }
+    else
+    {
+    	pio_channel_regbase = (struct pio_s *)(PIO_CONTROL_BASE + 0x220);
+    }
 
 	/*Mux Enable */
     switch(mux)
     {
       case PIO_MUX_GPIO:
-			pio_channel_regbase->pio_per_0 |= (1 << channel_offset_in_reg);
-        break;
+    	  pio_channel_regbase->pio_per_0 |= (1 << channel_offset_in_reg);
+    	  break;
 
       case PIO_MUX_PERIPH_0:
-			pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
-			pio_channel_regbase->pio_asr_0 |= (1 << channel_offset_in_reg);
-        break;
+    	  pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
+    	  pio_channel_regbase->pio_asr_0 |= (1 << channel_offset_in_reg);
+    	  break;
 
       case PIO_MUX_PERIPH_1:
-			pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
-			pio_channel_regbase->pio_bsr_0 |= (1 << channel_offset_in_reg);
-        break;
+    	  pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
+    	  pio_channel_regbase->pio_bsr_0 |= (1 << channel_offset_in_reg);
+    	  break;
 
       case PIO_MUX_PERIPH_2:
-			pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
-			pio_channel_regbase->pio_csr_0 |= (1 << channel_offset_in_reg);
-        break;
+    	  pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
+    	  pio_channel_regbase->pio_csr_0 |= (1 << channel_offset_in_reg);
+    	  break;
 
       case PIO_MUX_PERIPH_3:
-			pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
-			pio_channel_regbase->pio_dsr_0 |= (1 << channel_offset_in_reg);
-        break;
+    	  pio_channel_regbase->pio_pdr_0 = (1 << channel_offset_in_reg);
+    	  pio_channel_regbase->pio_dsr_0 |= (1 << channel_offset_in_reg);
+    	  break;
 
       default:
-        return ERROR;
+    	  return ERROR;
     }
 
-	/*Peripheral Pull UP/Down */
-	if(periphPullUpDownOn)
-	{
-		pio_channel_regbase->pio_percper_0 = (1 << channel_offset_in_reg);
-	}
-	else
-	{
-		pio_channel_regbase->pio_percpdr_0 = (1 << channel_offset_in_reg);
-	}
+    /*Peripheral Pull UP/Down */
+    if(periphPullUpDownOn)
+    {
+    	pio_channel_regbase->pio_percper_0 = (1 << channel_offset_in_reg);
+    }
+    else
+    {
+    	pio_channel_regbase->pio_percpdr_0 = (1 << channel_offset_in_reg);
+    }
 
-  return status;
+    return status;
 }
