1209125940344eaacff0de8e27ceddbcbdf705eb
jeromecoutant <jerome.coutant@st.com>
2017-12-07 13:58:39 +0100

    STM32F7: STDIO_UART_TX and STDIO_UART_RX can be now user defined

--- targets/TARGET_STM/TARGET_STM32F7/TARGET_STM32F746xG/TARGET_DISCO_F746NG/PeripheralNames.h
+++ targets/TARGET_STM/TARGET_STM32F7/TARGET_STM32F746xG/TARGET_DISCO_F746NG/PeripheralNames.h
@@ -57,10 +57,6 @@ typedef enum {
     UART_8 = (int)UART8_BASE
 } UARTName;
 
-#define STDIO_UART_TX  PA_9
-#define STDIO_UART_RX  PB_7
-#define STDIO_UART     UART_1
-
 typedef enum {
     SPI_1 = (int)SPI1_BASE,
     SPI_2 = (int)SPI2_BASE,
