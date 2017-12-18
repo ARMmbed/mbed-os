26f01d6c2574f42baabadbdfaf9f42f2d7158cbd
Steven Cartmell <steve.cartmell@arm.com>
2017-12-18 11:21:24 +0000

    Fix bug allowing I2C::abort_transfer to incorrectly unlock deep sleep mode
    - Add flag to I2C class to track if the I2C instance has locked deep sleep mode.
    - Wrap call to sleep_manager_lock_deep_sleep to only be called if I2C instance
      hasn't already locked deep sleep.
    - Wrap call to sleep_manager_unlock_deep_sleep to only be called if I2C has
      currently locked deep sleep mode.

--- drivers/I2C.h
+++ drivers/I2C.h
@@ -176,11 +176,19 @@ public:
     /** Abort the on-going I2C transfer
      */
     void abort_transfer();
-protected:
+
+  protected:
+    /** Lock deep sleep only if it is not yet locked */
+    void lock_deep_sleep();
+
+    /** Unlock deep sleep only if it has been locked */
+    void unlock_deep_sleep();
+
     void irq_handler_asynch(void);
     event_callback_t _callback;
     CThunk<I2C> _irq;
     DMAUsage _usage;
+    bool _deep_sleep_locked;
 #endif
 
 protected:
