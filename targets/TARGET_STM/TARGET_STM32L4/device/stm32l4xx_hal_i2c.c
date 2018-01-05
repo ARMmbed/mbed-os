0448d64f6292f2ce755e9504daebe3f8be88d659
bcostm <bco.stm@gmail.com>
2018-01-05 22:04:19 +0100

    Add more comments

--- targets/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_i2c.c
+++ targets/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_i2c.c
@@ -2592,6 +2592,7 @@ HAL_StatusTypeDef HAL_I2C_Master_Sequential_Transmit_IT(I2C_HandleTypeDef *hi2c,
     /* Prepare transfer parameters */
     hi2c->pBuffPtr    = pData;
     hi2c->XferCount   = Size;
+    // Added for MBED PR #3324
     hi2c->XferOptions = (XferOptions & (~I2C_RELOAD_MODE));
     hi2c->XferISR     = I2C_Master_ISR_IT;
 
@@ -2665,6 +2666,7 @@ HAL_StatusTypeDef HAL_I2C_Master_Sequential_Receive_IT(I2C_HandleTypeDef *hi2c, 
     /* Prepare transfer parameters */
     hi2c->pBuffPtr    = pData;
     hi2c->XferCount   = Size;
+    // Added for MBED PR #3324
     hi2c->XferOptions = (XferOptions & (~I2C_RELOAD_MODE));
     hi2c->XferISR     = I2C_Master_ISR_IT;
 
--- targets/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_spi.c
+++ targets/TARGET_STM/TARGET_STM32L4/device/stm32l4xx_hal_spi.c
@@ -650,6 +650,7 @@ HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pData, uint
     hspi->ErrorCode = HAL_SPI_ERROR_FLAG;
   }
 
+  // Added for MBED PR #4975
   if (hspi->Init.Direction == SPI_DIRECTION_1LINE) {
       __HAL_SPI_DISABLE(hspi);
   }
