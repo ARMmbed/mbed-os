cc91e5c541654e4443fe8a4c3ed444e9de11b6de
Patrick Frischmann <patrick.frischmann@world-direct.at>
2018-01-15 15:29:56 +0100

    Use mbed_error_vfprintf for log write operations

--- targets/TARGET_WD/lib/logging/wd_logging.c
+++ targets/TARGET_WD/lib/logging/wd_logging.c
@@ -18,34 +18,33 @@ void wd_log(int level, const char *format, va_list args) {
 	if (WD_DEBUG_LEVEL_CURRENT < level) {
 		return;
 	}
-	vfprintf(stderr, format, args);
-	printf("\r\n");
+	mbed_error_vfprintf(format, args);
 }
 
 void wd_log_debug(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
-	va_end(args);
 	wd_log(WD_DEBUG_LEVEL_DEBUG, format, args);
+	va_end(args);
 }
 
 void wd_log_info(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
-	va_end(args);
 	wd_log(WD_DEBUG_LEVEL_INFO, format, args);
+	va_end(args);
 }
 
 void wd_log_warn(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
-	va_end(args);
 	wd_log(WD_DEBUG_LEVEL_WARN, format, args);
+	va_end(args);
 }
 	
 void wd_log_error(const char *format, ...) {
 	va_list args;
 	va_start(args, format);
-	va_end(args);
 	wd_log(WD_DEBUG_LEVEL_ERROR, format, args);
+	va_end(args);
 }
