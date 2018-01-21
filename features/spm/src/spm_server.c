b617df250ae7c48a3759e019813324180a6ea3ff
Oren Cohen <oren.cohen@arm.com>
2018-01-21 15:27:23 +0200

    Add SPM_PANIC and more minor changes

--- features/spm/src/spm_server.c
+++ features/spm/src/spm_server.c
@@ -26,6 +26,17 @@
 
 extern spm_t g_spm;
 
+// This function should do proper validation
+static inline bool is_buffer_accessible(const void *ptr, size_t size)
+{
+    if (NULL == ptr) {
+        return false;
+    }
+
+    PSA_UNUSED(size);
+    return true;
+}
+
 static inline partition_t *partition_get_by_pid(int32_t partition_id)
 {
     for (uint32_t i = 0; i < g_spm.partition_count; ++i) {
@@ -66,8 +77,8 @@ static uint32_t psa_wait(bool wait_any, uint32_t bitmask, uint32_t timeout)
     } else {
         // Make sure the interrupt mask contains only a subset of interrupt signal mask.
         if (bitmask != (curr_partition->flags_interrupts & bitmask)) {
-            spm_panic("%s - interrupt mask 0x%x must have only bits from 0x%x!\n",
-                __func__, bitmask, curr_partition->flags_interrupts);
+            SPM_PANIC("interrupt mask 0x%x must have only bits from 0x%x!\n",
+                bitmask, curr_partition->flags_interrupts);
         }
     }
     
@@ -96,8 +107,8 @@ uint32_t psa_wait_interrupt(uint32_t interrupt_mask, uint32_t timeout)
 
 void psa_get(psa_signal_t signum, psa_msg_t *msg)
 {
-    if (NULL == msg) {
-        spm_panic("%s - 'msg' is NULL!\n", __func__);
+    if (!is_buffer_accessible(msg, sizeof(psa_msg_t))) {
+        SPM_PANIC("msg is inaccessible\n");
     }
 
     // TODO: assert memory range [msg, msg + sizeof(psa_msg_t)] is writable by the caller
@@ -108,12 +119,16 @@ void psa_get(psa_signal_t signum, psa_msg_t *msg)
     // signum must be ONLY ONE of the bits of curr_partition->flags_sf
     bool is_one_bit = ((signum != 0) && !(signum & (signum - 1)));
     if (!is_one_bit || !(signum & (curr_partition->flags_sf | curr_partition->flags_interrupts))) {
-        spm_panic(
-            "%s - signum 0x%x must have only 1 bit ON and must be a subset of 0x%x!\n",
-            __func__,
+        SPM_PANIC(
+            "signum 0x%x must have only 1 bit ON and must be a subset of 0x%x!\n",
             signum,
             curr_partition->flags_sf | curr_partition->flags_interrupts
             );
+    }
+
+    uint32_t active_flags = osThreadFlagsGet();
+    if (0 == (signum & active_flags)) {
+        SPM_PANIC("flag is not active!\n");
     }
 
     active_msg_t *active_msg = &(curr_partition->active_msg);
@@ -144,8 +159,9 @@ void psa_get(psa_signal_t signum, psa_msg_t *msg)
 
 size_t psa_read(psa_handle_t msg_handle, size_t offset, void *buffer, size_t len)
 {
-    SPM_ASSERT(buffer != NULL);
-    SPM_ASSERT(offset <= UINT32_MAX - len);
+    if (!is_buffer_accessible(buffer, len)) {
+        SPM_PANIC("buffer is inaccessible\n");
+    }
 
     active_msg_t *active_msg = NULL;
     spm_msg_handle_get_mem(msg_handle, &active_msg);
@@ -155,6 +171,10 @@ size_t psa_read(psa_handle_t msg_handle, size_t offset, void *buffer, size_t len
 
     PARTITION_STATE_ASSERT(active_msg->channel->dst_sec_func->partition, PARTITION_STATE_ACTIVE);
 
+    if (active_msg->tx_size == 0) {
+        SPM_PANIC("Message size to read from is zero!\n");
+    }
+    
     if (offset >= active_msg->tx_size) {
         return 0;
     }
@@ -168,8 +188,8 @@ size_t psa_read(psa_handle_t msg_handle, size_t offset, void *buffer, size_t len
 
 void psa_write(psa_handle_t msg_handle, size_t offset, const void *buffer, size_t bytes)
 {
-    if (NULL == buffer) {
-        spm_panic("%s - 'buffer' is NULL!\n", __func__);
+    if (!is_buffer_accessible(buffer, bytes)) {
+        SPM_PANIC("buffer is inaccessible\n");
     }
 
     active_msg_t *active_msg = NULL;
@@ -181,21 +201,19 @@ void psa_write(psa_handle_t msg_handle, size_t offset, const void *buffer, size_
     PARTITION_STATE_ASSERT(active_msg->channel->dst_sec_func->partition, PARTITION_STATE_ACTIVE);
 
     if (NULL == active_msg->rx_buf) {
-        spm_panic("%s - active_msg->rx_buf is NULL!\n", __func__);
+        SPM_PANIC("active_msg->rx_buf is NULL!\n");
     }
     if (offset > UINT32_MAX - bytes) {
-        spm_panic(
-            "%s - Integer overflow (offset=%lu, bytes=%lu)!\n",
-            __func__,
+        SPM_PANIC(
+            "Integer overflow (offset=%lu, bytes=%lu)!\n",
             offset,
             bytes
             );
     }
     if (offset + bytes > active_msg->rx_size) {
-        spm_panic(
-            "%s - Buffer overflow of client's response buffer"
+        SPM_PANIC(
+            "Buffer overflow of client's response buffer"
             " (offset=%lu, bytes=%lu, active_msg->rx_size=%lu)!\n",
-            __func__,
             offset,
             bytes,
             active_msg->rx_size
@@ -215,7 +233,7 @@ void psa_end(psa_handle_t msg_handle, psa_error_t retval)
                (active_msg->type <= PSA_IPC_MSG_TYPE_MAX));
 
     if ((active_msg->channel->rhandle != NULL) && (active_msg->type == PSA_IPC_MSG_TYPE_DISCONNECT)){
-        spm_panic("%s - Try closing channel without clearing rhandle first\n", __func__);
+        SPM_PANIC("Try closing channel without clearing rhandle first\n");
     }
 
     secure_func_t *dst_sec_func = active_msg->channel->dst_sec_func;
@@ -246,7 +264,11 @@ void psa_end(psa_handle_t msg_handle, psa_error_t retval)
 void psa_notify(int32_t partition_id)
 {
     partition_t *target_partition = partition_get_by_pid(partition_id);
-    SPM_ASSERT(NULL != target_partition);
+    if (NULL == target_partition) {
+        SPM_PANIC("Could not find partition (partition_id = %d)\n",
+        partition_id
+        );
+    }
 
     int32_t flags = (int32_t)osThreadFlagsSet(target_partition->thread_id, PSA_SIG_DOORBELL_MSK);
     SPM_ASSERT(flags >= 0);
