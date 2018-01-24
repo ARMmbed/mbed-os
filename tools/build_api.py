2aee11893cad0b20de5f3cd7dacb320f29460fe5
Oren Cohen <oren.cohen@arm.com>
2018-01-24 18:12:51 +0200

    Add SPM sources detection for mbed2 compatibility

--- tools/build_api.py
+++ tools/build_api.py
@@ -560,13 +560,11 @@ def build_project(src_paths, build_path, target, toolchain_name,
         # Call unified scan_resources
         resources = scan_resources(src_paths, toolchain, inc_dirs=inc_dirs)
 
-
-        # psa_source_pattern = re.compile(r'spm_.+\.[oc]')
-        # if any(psa_source_pattern.match(basename(f)) for f in itertools.chain(resources.objects, resources.c_sources)):
-
-        # Generate SPM additional code from manifests
-        psa_files_dir = process_manifest_files(resources.psa_manifests, build_path)
-        resources.add(toolchain.scan_resources(psa_files_dir))
+        spm_source_pattern = re.compile(r'spm_.+\.[oc]')
+        if any(spm_source_pattern.match(basename(f)) for f in itertools.chain(resources.objects, resources.c_sources)):
+            # Generate SPM additional code from manifests
+            psa_files_dir = process_manifest_files(resources.psa_manifests, build_path)
+            resources.add(toolchain.scan_resources(psa_files_dir))
 
         # Change linker script if specified
         if linker_script is not None:
