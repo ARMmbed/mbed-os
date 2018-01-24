71c5b06d6f118bf450fce0edaf9c174cf0ebd39b
Oren Cohen <oren.cohen@arm.com>
2018-01-24 17:12:23 +0200

    Increase pytest coverage to 100%

--- tools/test/spm/test_data.py
+++ tools/test/spm/test_data.py
@@ -63,12 +63,10 @@ missing_nspe_callable_secure_functions = [
     {'sfid': 'SFID1', 'signal': 'SFID1', 'minor_version': 1, 'minor_policy': 'strict'}
 ]
 
-
 duplicate_signal_secure_functions = [
     {'sfid': 'SFID3', 'signal': 'SFID1', 'minor_version': 5, 'minor_policy': 'relaxed', 'non_secure_clients': True},
     {'sfid': 'SFID4', 'signal': 'SFID2', 'minor_version': 12, 'minor_policy': 'strict', 'non_secure_clients': False},
 ]
-
 
 missing_minor_version_secure_functions = [
     {'sfid': 'SFID1', 'signal': 'SFID1', 'minor_policy': 'relaxed', 'non_secure_clients': True}
@@ -81,3 +79,94 @@ missing_minor_version_policy_secure_functions = [
 missing_minor_completley_secure_functions = [
     {'sfid': 'SFID2', 'signal': 'SFID2', 'non_secure_clients': False}
 ]
+
+duplicate_signal_irqs = [
+    {"line_num": 22, "signal": "ISR20"}
+]
+
+duplicate_line_num_irqs = [
+    {"line_num": 21, "signal": "ISR22"}
+]
+
+invalid_mmioregion_base = {'base': 'str', 'size': 4096, 'permission': 'READ-ONLY'}
+
+invalid_mmioregion_size = {'base': '0xEEEEEEEE', 'size': 'str', 'permission': 'READ-ONLY'}
+
+test_mock_files = {
+    'manifest1': 1,
+    'manifest2': 2,
+    'template_common1': 3,
+    'template_common2': 4,
+    'template_NAME_3': 5,
+    'template_NAME_4': 6,
+    'gen1': 7,
+    'gen2': 8,
+    'gen3': 9,
+    'gen4': 10,
+    'gen5': 11,
+    'gen6': 12
+}
+
+test_common_template = '''{
+    "num_of_partitions": {{partitions|count}},
+    "partition_names": [
+{% for partition in partitions %}
+        "{{partition.name}}"{{"" if loop.last else ","}}
+{% endfor %}
+    ],
+    "num_of_region_pairs": {{region_pair_list|count}}
+}
+'''
+
+test_partition_template = '''{
+    "name": "{{partition.name}}",
+    "id": "0x{{"%0x"|format(partition.id|int)|upper}}",
+    "type": "{{partition.type}}",
+    "priority": "{{partition.priority|find_priority_key}}",
+    "entry_point": "{{partition.entry_point}}",
+    "stack_size": {{partition.stack_size}},
+    "heap_size": {{partition.heap_size}},
+    "mmio_regions": [
+{% for mmio in partition.mmio_regions %}
+        {
+        {% if mmio.size|int %}
+            "base": "{{mmio.base}}",
+            "size": {{mmio.size}},
+        {% else %}
+            "name": "{{mmio.base}}",
+        {% endif %}
+            "permission": "{{mmio.permission|find_permission_key}}"
+        {{"}" if loop.last else "},"}}
+{% endfor %}
+    ],
+    "secure_functions": [
+{% for sf in partition.secure_functions %}
+        {
+            "sfid": "{{sf.sfid}}",
+            "signal": "{{sf.signal}}",
+            "minor_version": {{sf.minor_version}},
+            "minor_policy": "{{sf.minor_policy}}",
+            "non_secure_clients": {{sf.nspe_callable|lower}}
+        {{"}" if loop.last else "},"}}
+{% endfor %}
+    ],
+    "source_files": [
+{% for src in partition.source_files %}
+        "{{src|basename}}"{{"" if loop.last else ","}}
+{% endfor %}
+    ],
+    "irqs": [
+{% for irq in partition.irqs %}
+        {
+            "line_num": {{irq.line_num}},
+            "signal": "{{irq.signal}}"
+        {{"}" if loop.last else "},"}}
+{% endfor %}
+    ],
+    "extern_sfids": [
+{% for ext_sfid in partition.extern_sfids %}
+        "{{ext_sfid}}"{{"" if loop.last else ","}}
+{% endfor %}
+    ]
+}
+'''
