2e7b5ba27bdb623069b6323d5bb867e3bef31240
Marcus Chang <marcus.chang@arm.com>
2018-02-05 10:46:39 -0800

    Fix linker scripts and startup code for NRF52
    Various bug fixes:
    
     * Moved SPIM3 location in vector table based on new location in SDK 14.2.
     * Updated vector table entries and size in startup code and linker scripts.
     * Added missing vector table RAM section to IAR linker script.

--- targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/TARGET_MCU_NRF52840/device/cmsis_nvic.h
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/TARGET_MCU_NRF52840/device/cmsis_nvic.h
@@ -32,7 +32,7 @@
 #ifndef MBED_CMSIS_NVIC_H
 #define MBED_CMSIS_NVIC_H
 
-#define NVIC_NUM_VECTORS      (16 + 46)   // CORE + MCU Peripherals
+#define NVIC_NUM_VECTORS      (16 + 48)   // CORE + MCU Peripherals
 #define NVIC_USER_IRQ_OFFSET  16
 
 #include "nrf.h"
