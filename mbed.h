8d517f7e460d93d4076d9cc2a738c395a04b27f4
adbridge <anna.bridge@arm.com>
2018-02-09 14:18:46 +0000

    Update Mbed version block for patch release

--- mbed.h
+++ mbed.h
@@ -22,7 +22,7 @@
 // RTOS present, this is valid only for mbed OS 5
 #define MBED_MAJOR_VERSION 5
 #define MBED_MINOR_VERSION 7
-#define MBED_PATCH_VERSION 4
+#define MBED_PATCH_VERSION 5
 
 #else
 // mbed 2
