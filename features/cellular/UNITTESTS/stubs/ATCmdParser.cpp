ea6d59ff1963b5c6da7a85e1561fe247d948a3d4
Teppo Järvelin <teppo.jarvelin@arm.com>
2018-02-13 10:40:24 +0200

    Updated license texts.

--- features/cellular/UNITTESTS/stubs/ATCmdParser.cpp
+++ features/cellular/UNITTESTS/stubs/ATCmdParser.cpp
@@ -1,4 +1,6 @@
-/* Copyright (c) 2017 ARM Limited
+/*
+ * Copyright (c) 2017, Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,11 +13,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @section DESCRIPTION
- *
- * Parser for the AT command syntax
- *
  */
 
 #include "ATCmdParser.h"
