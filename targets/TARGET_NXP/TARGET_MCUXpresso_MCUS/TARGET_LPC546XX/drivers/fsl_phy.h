5a18f8645e28783c72d6381e8c1335ace2af3297
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2018-02-22 13:34:34 -0600

    LPC546XX: Update SDK driver to version 2.3
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- targets/TARGET_NXP/TARGET_MCUXpresso_MCUS/TARGET_LPC546XX/drivers/fsl_phy.h
+++ targets/TARGET_NXP/TARGET_MCUXpresso_MCUS/TARGET_LPC546XX/drivers/fsl_phy.h
@@ -1,9 +1,12 @@
 /*
+ * The Clear BSD License
  * Copyright (c) 2016, Freescale Semiconductor, Inc.
  * Copyright 2016-2017 NXP
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
+ * are permitted (subject to the limitations in the disclaimer below) provided
+ * that the following conditions are met:
  *
  * o Redistributions of source code must retain the above copyright notice, this list
  *   of conditions and the following disclaimer.
@@ -16,6 +19,7 @@
  *   contributors may be used to endorse or promote products derived from this
  *   software without specific prior written permission.
  *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
  * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -111,7 +115,7 @@ extern "C" {
  *
  *  This function initialize the SMI interface and initialize PHY.
  *  The SMI is the MII management interface between PHY and MAC, which should be
- *  firstly initialized before any other operation for PHY.
+ *  firstly initialized before any other operation for PHY. The PHY initialize with auto-negotiation.
  *
  * @param base       ENET peripheral base address.
  * @param phyAddr    The PHY address.
