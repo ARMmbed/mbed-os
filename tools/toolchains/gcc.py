be0b730229cab1363bd588a202f49519a0cd71c7
Andrew Leech <andrew.leech@planetinnovation.com.au>
2018-02-23 14:36:39 +1100

    Allow #include files from build directory when preprocessing linker file.

--- tools/toolchains/gcc.py
+++ tools/toolchains/gcc.py
@@ -211,7 +211,7 @@ class GCC(mbedToolchain):
         if mem_map:
             preproc_output = join(dirname(output), ".link_script.ld")
             cmd = (self.preproc + [mem_map] + self.ld[1:] +
-                   [ "-o", preproc_output])
+                   ["-I%s" % dirname(output), "-o", preproc_output])
             self.cc_verbose("Preproc: %s" % ' '.join(cmd))
             self.default_cmd(cmd)
             mem_map = preproc_output
