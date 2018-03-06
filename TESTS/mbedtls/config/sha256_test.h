4d25e3f539aadf220a8b86aff4cefd38fc91769f
Derek Miller <derek.miller@arm.com>
2018-03-06 16:45:48 -0600

    Add Mbed OS JSON configuration options to the MbedTLS Library
    The MbedTLS library is configured by commenting/uncommenting #defines
    in header files. This does not match the Mbed OS configuration
    system, which uses mbed_lib.json files for libraries, and allows
    application developers to override options from that file.
    This change adds support for the Mbed OS-style configuration.
    Application developers can choose either cloud-specific options
    (currently only Mbed Cloud is supported), or they can choose specific
    ciphersuites that they want supported.
    A number of tests for the configurations options have been added as
    well.

---
+++ TESTS/mbedtls/config/sha256_test.h
@@ -0,0 +1,6 @@
+#ifndef __SHA256_TEST_H__
+#define __SHA256_TEST_H__
+
+int mbedtls_sha256_self_test2( int verbose );
+
+#endif // __SHA256_TEST_H__
---
+++ TESTS/mbedtls/config/sha512_test.h
@@ -0,0 +1,6 @@
+#ifndef __SHA512_TEST_H__
+#define __SHA512_TEST_H__
+
+int mbedtls_sha512_self_test2( int verbose );
+
+#endif // __SHA512_TEST_H__
