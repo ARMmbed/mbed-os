c4a8ce943eef35e06b68cc199dfe560afb97cd45
Fahim Alavi <FA02@4mwirelesspk.local>
2018-03-08 13:59:27 +0500

    Code cleanup 2

--- targets/TARGET_ublox/TARGET_R5030/device/system_KM_app.c
+++ targets/TARGET_ublox/TARGET_R5030/device/system_KM_app.c
@@ -99,18 +99,7 @@ void SystemInit (void)
   ExternalClock = __EXTERNAL_CLOCK;
 }
 
-//# if defined (__Vendor_SysTickConfig) && (__Vendor_SysTickConfig == 1U)
-/**
-  \brief   System Tick Configuration
-  \details Initializes the System Timer and its interrupt, and starts the System Tick Timer.
-           Counter is in free running mode to generate periodic interrupts.
-  \param [in]  ticks  Number of ticks between two interrupts.
-  \return          0  Function succeeded.
-  \return          1  Function failed.
-  \note    When the variable <b>__Vendor_SysTickConfig</b> is set to 1, then the
-           function <b>SysTick_Config</b> is not included. In this case, the file <b><i>device</i>.h</b>
-           must contain a vendor-specific implementation of this function.
- */
+// Initializes the System Timer and its interrupt, and starts the System Tick Timer.
 uint32_t SysTick_Config(uint32_t ticks)
 {
   if ((ticks - 1UL) > SysTick_LOAD_RELOAD_Msk)
@@ -126,8 +115,3 @@ uint32_t SysTick_Config(uint32_t ticks)
                    SysTick_CTRL_ENABLE_Msk;                         /* Enable SysTick IRQ and SysTick Timer */
   return (0UL);                                                     /* Function successful */
 }
-
-
-
-
-//# endif
