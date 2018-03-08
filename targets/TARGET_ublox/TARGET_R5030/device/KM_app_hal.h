adf2db97b3c2df1edb6a57dbc8708fd8ab6b1697
Fahim Alavi <FA02@4mwirelesspk.local>
2018-03-08 14:11:09 +0500

    Code clean up

--- targets/TARGET_ublox/TARGET_R5030/device/KM_app_hal.h
+++ targets/TARGET_ublox/TARGET_R5030/device/KM_app_hal.h
@@ -43,18 +43,6 @@ extern void HAL_Init (void);
  */
 extern void HAL_SysTick_Config(uint32_t);
 
-/**
- * brief   System Tick Configuration
- * details Initializes the System Timer and its interrupt, and starts the System Tick Timer.
- *         Counter is in free running mode to generate periodic interrupts.
- * param [in]  ticks  Number of ticks between two interrupts.
- * return          0  Function succeeded.
-  * return          1  Function failed.
- * note    When the variable <b>__Vendor_SysTickConfig</b> is set to 1, then the
- *         function <b>SysTick_Config</b> is not included. In this case, the file <b><i>device</i>.h</b>
- *         must contain a vendor-specific implementation of this function.
- */
-uint32_t SysTick_Config(uint32_t ticks);
 
 #ifdef __cplusplus
 }
--- targets/TARGET_ublox/TARGET_R5030/device/system_KM_app.h
+++ targets/TARGET_ublox/TARGET_R5030/device/system_KM_app.h
@@ -71,20 +71,18 @@ extern void SystemInit (void);
  */
 extern void SystemCoreClockUpdate (void);
 
-//# if defined (__Vendor_SysTickConfig) && (__Vendor_SysTickConfig == 1U)
 /**
-  \brief   System Tick Configuration
-  \details Initializes the System Timer and its interrupt, and starts the System Tick Timer.
-           Counter is in free running mode to generate periodic interrupts.
-  \param [in]  ticks  Number of ticks between two interrupts.
-  \return          0  Function succeeded.
-  \return          1  Function failed.
-  \note    When the variable <b>__Vendor_SysTickConfig</b> is set to 1, then the
-           function <b>SysTick_Config</b> is not included. In this case, the file <b><i>device</i>.h</b>
-           must contain a vendor-specific implementation of this function.
+ * brief   System Tick Configuration
+ * details Initializes the System Timer and its interrupt, and starts the System Tick Timer.
+ *         Counter is in free running mode to generate periodic interrupts.
+ * param [in]  ticks  Number of ticks between two interrupts.
+ * return          0  Function succeeded.
+ * return          1  Function failed.
+ * note    When the variable <b>__Vendor_SysTickConfig</b> is set to 1, then the
+ *         function <b>SysTick_Config</b> is not included. In this case, the file <b><i>device</i>.h</b>
+ *         must contain a vendor-specific implementation of this function.
  */
 uint32_t SysTick_Config(uint32_t ticks);
-//# endif
 
 #ifdef __cplusplus
 }
