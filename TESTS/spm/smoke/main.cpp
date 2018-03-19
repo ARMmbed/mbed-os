71c283b2fb9282305394ffd10c95589f45206756
Oren Cohen <oren.cohen@arm.com>
2018-03-19 14:32:59 +0200

    Disable SPM on constrained platforms

--- TESTS/spm/smoke/main.cpp
+++ TESTS/spm/smoke/main.cpp
@@ -13,6 +13,11 @@
  * limitations under the License.
  */
 
+
+#if !ENABLE_SPM
+    #error [NOT_SUPPORTED] SPM is not supported on this platform
+#endif
+
 /* -------------------------------------- Includes ----------------------------------- */
 
 #include "greentea-client/test_env.h"
