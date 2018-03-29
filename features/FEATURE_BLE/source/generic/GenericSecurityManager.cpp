c36a8d7dba1a16b32479cc3fbc0e5fcd8c01c29a
paul-szczepanek-arm <33840200+paul-szczepanek-arm@users.noreply.github.com>
2018-03-29 15:24:40 +0100

    let the stack know whether csrk is authenticated

--- features/FEATURE_BLE/source/generic/GenericSecurityManager.cpp
+++ features/FEATURE_BLE/source/generic/GenericSecurityManager.cpp
@@ -711,7 +711,10 @@ void GenericSecurityManager::set_peer_csrk_cb(
         return;
     }
 
-    _pal.set_peer_csrk(cb->connection, *csrk);
+    _pal.set_peer_csrk(
+        cb->connection,
+        *csrk,
+        cb->csrk_mitm_protected);
 }
 
 void GenericSecurityManager::return_csrk_cb(
