d1bbd21e01e0b71bf58a4b2a2d3a6f0a6c960915
Antti Kauppila <antti.kauppila@arm.com>
2018-04-06 09:33:50 +0300

    LoRa: Improved handling of region selection
    - This implements IOTCELL-697
    - This touches API, but does not break it, old ones still work in a same manner!

--- features/lorawan/lorastack/phy/loraphy_target.h
+++ features/lorawan/lorastack/phy/loraphy_target.h
@@ -18,37 +18,50 @@
 #ifndef LORAPHY_TARGET
 #define LORAPHY_TARGET
 
+#define EU868 0
+#define AS923 1
+#define AU915 2
+#define CN470 3
+#define CN779 4
+#define EU433 5
+#define IN865 6
+#define KR920 7
+#define US915 8
+#define US915_HYBRID 9
+
 #ifdef MBED_CONF_LORA_PHY
- #if MBED_CONF_LORA_PHY      == 0
+ #if MBED_CONF_LORA_PHY      == EU868
   #include "lorawan/lorastack/phy/LoRaPHYEU868.h"
   #define LoRaPHY_region LoRaPHYEU868
- #elif MBED_CONF_LORA_PHY    == 1
+ #elif MBED_CONF_LORA_PHY    == AS923
   #include "lorawan/lorastack/phy/LoRaPHYAS923.h"
   #define LoRaPHY_region LoRaPHYAS923
- #elif MBED_CONF_LORA_PHY    == 2
+ #elif MBED_CONF_LORA_PHY    == AU915
   #include "lorawan/lorastack/phy/LoRaPHYAU915.h"
  #define LoRaPHY_region LoRaPHYAU915;
- #elif MBED_CONF_LORA_PHY    == 3
+ #elif MBED_CONF_LORA_PHY    == CN470
   #include "lorawan/lorastack/phy/LoRaPHYCN470.h"
   #define LoRaPHY_region LoRaPHYCN470
- #elif MBED_CONF_LORA_PHY    == 4
+ #elif MBED_CONF_LORA_PHY    == CN779
   #include "lorawan/lorastack/phy/LoRaPHYCN779.h"
   #define LoRaPHY_region LoRaPHYCN779
- #elif MBED_CONF_LORA_PHY    == 5
+ #elif MBED_CONF_LORA_PHY    == EU433
   #include "lorawan/lorastack/phy/LoRaPHYEU433.h"
   #define LoRaPHY_region LoRaPHYEU433
- #elif MBED_CONF_LORA_PHY    == 6
+ #elif MBED_CONF_LORA_PHY    == IN865
   #include "lorawan/lorastack/phy/LoRaPHYIN865.h"
   #define LoRaPHY_region LoRaPHYIN865
- #elif MBED_CONF_LORA_PHY    == 7
+ #elif MBED_CONF_LORA_PHY    == KR920
   #include "lorawan/lorastack/phy/LoRaPHYKR920.h"
   #define LoRaPHY_region LoRaPHYKR920
- #elif MBED_CONF_LORA_PHY    == 8
+ #elif MBED_CONF_LORA_PHY    == US915
   #include "lorawan/lorastack/phy/LoRaPHYUS915.h"
   #define LoRaPHY_region LoRaPHYUS915
- #elif MBED_CONF_LORA_PHY    == 9
+ #elif MBED_CONF_LORA_PHY    == US915_HYBRID
   #include "lorawan/lorastack/phy/LoRaPHYUS915Hybrid.h"
   #define LoRaPHY_region LoRaPHYUS915Hybrid
+ #else
+  #error "Unsupported region, check your configuration."
  #endif //MBED_CONF_LORA_PHY == VALUE
 #else
  #error "Must set LoRa PHY layer parameters."
