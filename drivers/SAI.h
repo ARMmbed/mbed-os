93314e1eb64172afbc394c65bb5cf43c7ec2a57a
kegilbert <kevin.gilbert@arm.com>
2018-04-13 11:44:41 -0500

    Add status code/get call to SAI and children classes
    Move to public inheritance of SAI in children
    
    Allows access to general member methods like status.
    
    Moved internal functions to protected
    
    Cherry-picked examples.json changes that limited Lorawan example tests to Lorawan targets

--- drivers/SAI.h
+++ drivers/SAI.h
@@ -41,11 +41,19 @@ public:
         const sai_format_t *fmt = &sai_mode_i2s32, bool is_input = false,
         uint32_t master_clock = 0, bool internal_mclk = false);
 
+    /** Retreive the object status. Allows the ctor to
+     * funnel init errors/warnings back up to the application.
+     */
+    virtual sai_result_t status(void);
+    
     /** Push a sample to the Fifo & try to read a new sample.
      * it may return 0 if no sample was available.
      */
     virtual bool xfer(uint32_t *value);
 
+    virtual ~SAI() {}
+
+protected:
     /** Acquire exclusive access to this SAI bus
      */
     virtual void lock(void);
@@ -54,17 +62,14 @@ public:
      */
     virtual void unlock(void);
 
-
-public:
-    virtual ~SAI() {}
-
-protected:
+    ////////////////////////////////////////////
     sai_t           _sai;
     PlatformMutex   _mutex;
     bool            _is_input;
+    sai_result_t    _status;
 };
 
-class SAITransmitter : private SAI {
+class SAITransmitter : public SAI {
     public:
         SAITransmitter(PinName mclk, PinName bclk, PinName wclk, PinName sd,
                        const sai_format_t *fmt = &sai_mode_i2s32)
@@ -75,7 +80,7 @@ class SAITransmitter : private SAI {
         }
 };
 
-class SAIReceiver : private SAI {
+class SAIReceiver : public SAI {
     public:
         SAIReceiver(PinName mclk, PinName bclk, PinName wclk, PinName sd,
                     const sai_format_t *fmt = &sai_mode_i2s32)
