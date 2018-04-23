f95bacd23c35f98a548000a16b0f3e0144e6d73b
Simon Pfeifhofer <simon.pfeifhofer@gmail.com>
2018-04-23 12:36:02 +0200

    add _cplusplus-check

--- targets/TARGET_WD/metadata.h
+++ targets/TARGET_WD/metadata.h
@@ -9,6 +9,10 @@
 #ifndef METADATA_H_
 #define METADATA_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 // Utility-Macros
 #define QUOTE(str) #str
 #define EXPAND_AND_QUOTE(str) QUOTE(str)
@@ -16,5 +20,9 @@
 // Derived metadata settings
 #define APPLICATION_NAME_STRING EXPAND_AND_QUOTE(APPLICATION_NAME)
 #define APPLICATION_VERSION_STRING EXPAND_AND_QUOTE(APPLICATION_VERSION)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* METADATA_H_ */
