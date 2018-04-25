b62f25663a1a021322067cc6913ace1a40c2243c
Derek Miller <derek.miller@arm.com>
2018-04-25 13:48:29 -0500

    Added license headers and changed coding style (where appropriate)

--- TESTS/mbedtls/config/ecdh_test.cpp
+++ TESTS/mbedtls/config/ecdh_test.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2013-2018, ARM Limited, All Rights Reserved
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #include <stdio.h>
 #include <string.h>
 #include "mbed.h"
@@ -8,12 +25,10 @@
 #include "ecdh_test.h"
 
 #if defined (MBEDTLS_ECDH_C)
+/* Tests whether the mbedtls_ecdh_* functions are included in the build configuration
+ * It never generates asserts. Instead, failures should show up at compile time
+ */
 int mbedtls_ecdh_test_mbedos( int verbose ) {
-    // This is just a smoke test. It doesn't test the functionality
-    // of the DH algorithm. It just makes sure the function is present to be 
-    // called (it tests the build configuration).
-    // It never generates asserts. Instead, failures should show up at compile
-    // time
     mbedtls_ecdh_make_params( NULL, NULL, NULL, 0, NULL, NULL );
 
     return 0;
