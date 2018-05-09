67140a2706cade7ad4ed565d461bc474ec02deee
Andrew Leech <andrew.leech@planetinnovation.com.au>
2018-05-09 14:17:40 +1000

    Redirect NRF asserts to mbed error() in TARGET_NRF5x and SDK 14.2 Add related details to TARGET_NRF5x Readme's

--- targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_SDK_14_2/libraries/util/nrf_assert.h
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_SDK_14_2/libraries/util/nrf_assert.h
@@ -76,10 +76,6 @@ extern "C" {
 void assert_nrf_callback(uint16_t line_num, const uint8_t *file_name);
 //lint -restore
 
-#if !defined(NDEBUG) && !defined(DEBUG_NRF_USER)
-#define DEBUG_NRF_USER
-#endif
-
 #if (defined(DEBUG_NRF) || defined(DEBUG_NRF_USER))
 #define NRF_ASSERT_PRESENT 1
 #else
