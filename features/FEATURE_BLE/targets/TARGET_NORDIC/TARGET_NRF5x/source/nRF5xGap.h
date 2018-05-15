31f8cd18f7f6905af9afae9915bf8f047cf79a10
Vincent Coubard <vincent.coubard@arm.com>
2018-05-15 10:56:35 +0100

    Nordic BLE: Backport Gap from nRF5

--- features/FEATURE_BLE/targets/TARGET_NORDIC/TARGET_NRF5x/source/nRF5xGap.h
+++ features/FEATURE_BLE/targets/TARGET_NORDIC/TARGET_NRF5x/source/nRF5xGap.h
@@ -44,6 +44,7 @@
 #include "ble/GapAdvertisingData.h"
 #include "ble/Gap.h"
 #include "ble/GapScanningParams.h"
+#include "ble/pal/ConnectionEventMonitor.h"
 
 #include "nrf_soc.h"
 
@@ -51,8 +52,6 @@ extern "C" {
 #include "ble_radio_notification.h"
 #include "app_util_platform.h"
 }
-
-#include "btle_security.h"
 
 void radioNotificationStaticCallback(bool param);
 
@@ -62,9 +61,11 @@ void radioNotificationStaticCallback(bool param);
 
 */
 /**************************************************************************/
-class nRF5xGap : public Gap
-{
+class nRF5xGap : public ::Gap, public ble::pal::ConnectionEventMonitor {
 public:
+    nRF5xGap();
+
+    virtual ~nRF5xGap() { }
     /* Functions that must be implemented from Gap */
     virtual ble_error_t setAddress(AddressType_t  type,  const Address_t address);
     virtual ble_error_t getAddress(AddressType_t *typeP, Address_t address);
@@ -129,6 +130,23 @@ public:
         return BLE_ERROR_UNSPECIFIED;
     }
 
+    virtual ble_error_t enablePrivacy(bool enable);
+
+    virtual ble_error_t setPeripheralPrivacyConfiguration(
+        const PeripheralPrivacyConfiguration_t *configuration
+    );
+
+    virtual ble_error_t getPeripheralPrivacyConfiguration(
+        PeripheralPrivacyConfiguration_t *configuration
+    );
+
+    virtual ble_error_t setCentralPrivacyConfiguration(
+        const CentralPrivacyConfiguration_t *configuration
+    );
+
+    virtual ble_error_t getCentralPrivacyConfiguration(
+        CentralPrivacyConfiguration_t *configuration
+    );
 /* Observer role is not supported by S110, return BLE_ERROR_NOT_IMPLEMENTED */
 #if !defined(TARGET_MCU_NRF51_16K_S110) && !defined(TARGET_MCU_NRF51_32K_S110)
     virtual ble_error_t startRadioScan(const GapScanningParams &scanningParams);
@@ -229,21 +247,41 @@ private:
         radioNotificationTimeout.attach_us(mbed::callback(this, &nRF5xGap::postRadioNotificationCallback), 0);
     }
     friend void radioNotificationStaticCallback(bool param); /* allow invocations of processRadioNotificationEvent() */
+public:
+    /** @note Implements ConnectionEventMonitor.
+     *  @copydoc ConnectionEventMonitor::set_connection_event_handler
+     */
+    virtual void set_connection_event_handler(
+        ConnectionEventMonitor::EventHandler* connection_event_handler
+    );
+
+    /**
+     * @copydoc ::Gap::processDisconnectionEvent
+     */
+    void processDisconnectionEvent(
+        Handle_t handle,
+        DisconnectionReason_t reason
+    );
 
 private:
+    friend void btle_handler(const ble_evt_t *p_ble_evt);
+    friend void btle_handler(const ble_evt_t *p_ble_evt, void *p_context);
+
+    void on_connection(Handle_t handle, const ble_gap_evt_connected_t& evt);
+    void on_advertising_packet(const ble_gap_evt_adv_report_t &evt);
     uint16_t m_connectionHandle;
+    ConnectionEventMonitor::EventHandler* _connection_event_handler;
+
+    bool _privacy_enabled;
+    PeripheralPrivacyConfiguration_t _peripheral_privacy_configuration;
+    CentralPrivacyConfiguration_t _central_privacy_configuration;
+    AddressType_t _non_private_address_type;
+    Address_t _non_private_address;
 
     /*
      * Allow instantiation from nRF5xn when required.
      */
     friend class nRF5xn;
-
-    nRF5xGap() :
-        advertisingPolicyMode(Gap::ADV_POLICY_IGNORE_WHITELIST),
-        scanningPolicyMode(Gap::SCAN_POLICY_IGNORE_WHITELIST),
-        whitelistAddressesSize(0) {
-        m_connectionHandle = BLE_CONN_HANDLE_INVALID;
-    }
 
     nRF5xGap(nRF5xGap const &);
     void operator=(nRF5xGap const &);
