02e8172538613d3dbbed4ad65f587d6b21263648
jeromecoutant <jerome.coutant@st.com>
2018-05-16 14:05:42 +0200

    STM32F3 DISCO : PeripheralPins.c and PinNames.h files alignment

--- targets/TARGET_STM/TARGET_STM32F3/TARGET_STM32F334x8/TARGET_DISCO_F334C8/PinNames.h
+++ targets/TARGET_STM/TARGET_STM32F3/TARGET_STM32F334x8/TARGET_DISCO_F334C8/PinNames.h
@@ -1,6 +1,6 @@
 /* mbed Microcontroller Library
  *******************************************************************************
- * Copyright (c) 2016, STMicroelectronics
+ * Copyright (c) 2018, STMicroelectronics
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -27,6 +27,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *******************************************************************************
  */
+
 #ifndef MBED_PINNAMES_H
 #define MBED_PINNAMES_H
 
@@ -38,6 +39,13 @@ extern "C" {
 #endif
 
 typedef enum {
+    ALT0  = 0x100,
+    ALT1  = 0x200,
+    ALT2  = 0x300,
+    ALT3  = 0x400
+} ALTx;
+
+typedef enum {
     PA_0  = 0x00,
     PA_1  = 0x01,
     PA_2  = 0x02,
@@ -45,24 +53,42 @@ typedef enum {
     PA_4  = 0x04,
     PA_5  = 0x05,
     PA_6  = 0x06,
+    PA_6_ALT0 = PA_6|ALT0,
     PA_7  = 0x07,
+    PA_7_ALT0 = PA_7|ALT0,
+    PA_7_ALT1 = PA_7|ALT1,
+    PA_7_ALT2 = PA_7|ALT2,
     PA_8  = 0x08,
     PA_9  = 0x09,
     PA_10 = 0x0A,
     PA_11 = 0x0B,
+    PA_11_ALT0 = PA_11|ALT0,
+    PA_11_ALT1 = PA_11|ALT1,
     PA_12 = 0x0C,
+    PA_12_ALT0 = PA_12|ALT0,
+    PA_12_ALT1 = PA_12|ALT1,
     PA_13 = 0x0D,
     PA_14 = 0x0E,
     PA_15 = 0x0F,
 
     PB_0  = 0x10,
+    PB_0_ALT0 = PB_0|ALT0,
+    PB_0_ALT1 = PB_0|ALT1,
     PB_1  = 0x11,
+    PB_1_ALT0 = PB_1|ALT0,
+    PB_1_ALT1 = PB_1|ALT1,
     PB_2  = 0x12,
     PB_3  = 0x13,
     PB_4  = 0x14,
+    PB_4_ALT0 = PB_4|ALT0,
+    PB_4_ALT1 = PB_4|ALT1,
     PB_5  = 0x15,
+    PB_5_ALT0 = PB_5|ALT0,
+    PB_5_ALT1 = PB_5|ALT1,
     PB_6  = 0x16,
     PB_7  = 0x17,
+    PB_7_ALT0 = PB_7|ALT0,
+    PB_7_ALT1 = PB_7|ALT1,
     PB_8  = 0x18,
     PB_9  = 0x19,
     PB_10 = 0x1A,
@@ -70,7 +96,10 @@ typedef enum {
     PB_12 = 0x1C,
     PB_13 = 0x1D,
     PB_14 = 0x1E,
+    PB_14_ALT0 = PB_14|ALT0,
     PB_15 = 0x1F,
+    PB_15_ALT0 = PB_15|ALT0,
+    PB_15_ALT1 = PB_15|ALT1,
 
     PC_13 = 0x2D,
     PC_14 = 0x2E,
@@ -144,6 +173,25 @@ typedef enum {
     SPI_SCK     = PA_5,
     SPI_CS      = PA_4,
     PWM_OUT     = PB_6,
+
+/**** No USB pins ***/
+
+/**** No ETHERNET pins ***/
+
+/**** OSCILLATOR pins ****/
+    RCC_OSC32_IN = PC_14,
+    RCC_OSC32_OUT = PC_15,
+    RCC_OSC_IN = PF_0,
+    RCC_OSC_OUT = PF_1,
+
+/**** DEBUG pins ****/
+    SYS_JTCK_SWCLK = PA_14,
+    SYS_JTDI = PA_15,
+    SYS_JTDO_TRACESWO = PB_3,
+    SYS_JTMS_SWDIO = PA_13,
+    SYS_NJTRST = PB_4,
+    SYS_WKUP1 = PA_0,
+    SYS_WKUP2 = PC_13,
 
     // Not connected
     NC = (int)0xFFFFFFFF
