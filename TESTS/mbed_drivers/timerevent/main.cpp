c6ead0780c7941fe001d2f36c2180f802dacd132
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2018-05-17 13:21:14 +0200

    tests-mbed_drivers-timerevent: Disable deep-sleep mode in TestTimerEvent class.
    Since now us ticker is disabled in deep-sleep mode and tests-mbed_drivers-timerevent verifies TimerEvent class which uses us ticker then deep-sleep mode needs to be disabled during the test execution.

--- TESTS/mbed_drivers/timerevent/main.cpp
+++ TESTS/mbed_drivers/timerevent/main.cpp
@@ -39,6 +39,8 @@ private:
 public:
     TestTimerEvent() :
             TimerEvent(), sem(0, 1) {
+
+        sleep_manager_lock_deep_sleep();
     }
 
     TestTimerEvent(const ticker_data_t *data) :
@@ -46,6 +48,7 @@ public:
     }
 
     virtual ~TestTimerEvent() {
+        sleep_manager_unlock_deep_sleep();
     }
 
     // Make these methods publicly accessible
