6781ee88cda42029cc183da62d30da4aebd0aa6d
Martin Kojtal <martin.kojtal@arm.com>
2018-05-25 09:34:14 +0100

    maxim: fix rtc if lp ticker not defined
    lp ticker handler defined only if lp ticker is defined. In case not,
    use only overflow, rtc0 not used.

--- targets/TARGET_Maxim/TARGET_MAX32600/rtc_api.c
+++ targets/TARGET_Maxim/TARGET_MAX32600/rtc_api.c
@@ -33,6 +33,7 @@
 
 #include "rtc_api.h"
 #include "lp_ticker_api.h"
+#include "ticker_api.h"
 #include "cmsis.h"
 #include "rtc_regs.h"
 #include "pwrseq_regs.h"
@@ -73,8 +74,10 @@ void rtc_init(void)
     MXC_PWRSEQ->reg0 |= MXC_F_PWRSEQ_REG0_PWR_RTCEN_RUN;
 
     // Prepare interrupt handlers
+#ifdef DEVICE_LPTICKER
     NVIC_SetVector(RTC0_IRQn, (uint32_t)lp_ticker_irq_handler);
     NVIC_EnableIRQ(RTC0_IRQn);
+#endif
     NVIC_SetVector(RTC3_IRQn, (uint32_t)overflow_handler);
     NVIC_EnableIRQ(RTC3_IRQn);
 
