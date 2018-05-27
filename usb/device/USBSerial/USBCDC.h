c90c8b9dba184dfa263f10e368216961eb91561e
Russ Butler <c1728p9@gmail.com>
2018-05-27 16:44:00 -0500

    Add default values to USBCDC constructor
    Add reasonable defaults to the USBCDC constructor so it is easier to
    use.

--- usb/device/USBSerial/USBCDC.h
+++ usb/device/USBSerial/USBCDC.h
@@ -41,7 +41,7 @@ public:
     * @param product_id Your product_id
     * @param product_release Your product_release
     */
-    USBCDC(bool connect_blocking, uint16_t vendor_id, uint16_t product_id, uint16_t product_release);
+    USBCDC(bool connect_blocking=true, uint16_t vendor_id=0x1f00, uint16_t product_id=0x2012, uint16_t product_release=0x0001);
 
     /**
     * Fully featured constructor
