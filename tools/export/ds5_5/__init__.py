550c3bc653609df35657f80faa2566561f790e40
Jimmy Brisson <theotherjimmy@gmail.com>
2018-06-01 08:53:31 -0500

    Import relpath

--- tools/export/ds5_5/__init__.py
+++ tools/export/ds5_5/__init__.py
@@ -14,7 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from os.path import basename
+from os.path import basename, relpath
 
 from tools.export.exporters import Exporter
 
