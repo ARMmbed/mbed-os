3826efad3a6b537e7a454a001befee55fbaa31bc
Michael Schwarcz <Michael.Schwarcz@arm.com>
2018-06-04 13:52:00 +0300

    Revert unintended lines from previous commit

--- TESTS/secure_time/library_api/main.cpp
+++ TESTS/secure_time/library_api/main.cpp
@@ -554,7 +554,7 @@ utest::v1::status_t test_setup(const size_t number_of_cases)
 {
    // Setup Greentea using a reasonable timeout in seconds
 #ifndef NO_GREENTEA
-//   GREENTEA_SETUP(120, "default_auto");
+   GREENTEA_SETUP(120, "default_auto");
 #endif
    return verbose_test_setup_handler(number_of_cases);
 }
