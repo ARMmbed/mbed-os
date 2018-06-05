869b47ddb3d6ecf690985f0c261819b3a386d28d
bcostm <bco.stm@gmail.com>
2018-06-05 11:21:02 +0200

    Remove code related to timer channel 2

--- targets/TARGET_STM/stm_common.c
+++ targets/TARGET_STM/stm_common.c
@@ -1,7 +1,18 @@
 #include "hal/us_ticker_api.h"
 
-// Overwrite default HAL function
+// Overwrite default HAL functions defined as "weak"
+
 uint32_t HAL_GetTick()
 {
-    return ticker_read_us(get_us_ticker_data()) / 1000;
+    return ticker_read_us(get_us_ticker_data()) / 1000; // 1 ms tick is required for ST HAL
+}
+
+void HAL_SuspendTick(void)
+{
+    // Do nothing
+}
+
+void HAL_ResumeTick(void)
+{
+    // Do nothing
 }
