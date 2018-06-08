8c147495080f5c58b9af3f982a2b700939f287bb
cedrick kukela <cedrick.kukela@connecteddev.com>
2018-06-08 07:27:33 -0400

    adding TARGET MTS_DRAGONFLY_L471QG

---
+++ targets/TARGET_STM/TARGET_STM32L4/TARGET_MTS_DRAGONFLY_L471QG/ublox_low_level_api.h
@@ -0,0 +1,14 @@
+#ifndef UBLOX_LOW_LEVEL_API_H
+#define UBLOX_LOW_LEVEL_API_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ublox_board_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // UBLOX_LOW_LEVEL_H
