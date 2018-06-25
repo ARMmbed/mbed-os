11ed24d1d8d636c1a4f1c0f42c65d88cee7b064b
Marten Lootsma <marten@twtg.io>
2018-06-25 22:17:30 +0200

    Added mbed_rtx.h for saml21

---
+++ targets/TARGET_Atmel/mbed_rtx.h
@@ -0,0 +1,30 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2017 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MBED_MBED_RTX_H
+#define MBED_MBED_RTX_H
+
+#ifndef INITIAL_SP
+
+#if (defined(TARGET_SAML21J18A))
+#define INITIAL_SP              (0x20008000UL)
+#else
+#error "INITIAL_SP is not defined for this target in the mbed_rtx.h file"
+#endif
+
+#endif // INITIAL_SP
+
+#endif  // MBED_MBED_RTX_H
