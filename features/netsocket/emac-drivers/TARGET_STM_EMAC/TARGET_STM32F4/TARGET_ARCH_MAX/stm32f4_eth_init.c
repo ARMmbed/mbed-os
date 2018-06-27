5612dac3b7f64d4716edeb0f1a2811392fbae164
jeromecoutant <jerome.coutant@st.com>
2018-06-27 15:00:51 +0200

    TARGET_STM_EMAC astyle

--- features/netsocket/emac-drivers/TARGET_STM_EMAC/TARGET_STM32F4/TARGET_ARCH_MAX/stm32f4_eth_init.c
+++ features/netsocket/emac-drivers/TARGET_STM_EMAC/TARGET_STM32F4/TARGET_ARCH_MAX/stm32f4_eth_init.c
@@ -3,7 +3,7 @@
 /**
  * Override HAL Eth Init function
  */
-void HAL_ETH_MspInit(ETH_HandleTypeDef* heth)
+void HAL_ETH_MspInit(ETH_HandleTypeDef *heth)
 {
     GPIO_InitTypeDef GPIO_InitStructure;
     if (heth->Instance == ETH) {
@@ -20,7 +20,7 @@ void HAL_ETH_MspInit(ETH_HandleTypeDef* heth)
           RMII_MII_CRS_DV -------------------> PA7
           RMII_MII_RXD0 ---------------------> PC4
           RMII_MII_RXD1 ---------------------> PC5
-          RMII_MII_RXER ---------------------> 
+          RMII_MII_RXER --------------------->
           RMII_MII_TX_EN --------------------> PB11
           RMII_MII_TXD0 ---------------------> PB12
           RMII_MII_TXD1 ---------------------> PB13
@@ -28,7 +28,7 @@ void HAL_ETH_MspInit(ETH_HandleTypeDef* heth)
         /* Configure PA1, PA2 and PA7 */
         GPIO_InitStructure.Speed = GPIO_SPEED_HIGH;
         GPIO_InitStructure.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStructure.Pull = GPIO_NOPULL; 
+        GPIO_InitStructure.Pull = GPIO_NOPULL;
         GPIO_InitStructure.Alternate = GPIO_AF11_ETH;
         GPIO_InitStructure.Pin = GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_7;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStructure);
@@ -44,7 +44,7 @@ void HAL_ETH_MspInit(ETH_HandleTypeDef* heth)
         /* Enable the Ethernet global Interrupt */
         HAL_NVIC_SetPriority(ETH_IRQn, 0x7, 0);
         HAL_NVIC_EnableIRQ(ETH_IRQn);
-        
+
         /* Enable ETHERNET clock  */
         __HAL_RCC_ETH_CLK_ENABLE();
     }
@@ -53,7 +53,7 @@ void HAL_ETH_MspInit(ETH_HandleTypeDef* heth)
 /**
  * Override HAL Eth DeInit function
  */
-void HAL_ETH_MspDeInit(ETH_HandleTypeDef* heth)
+void HAL_ETH_MspDeInit(ETH_HandleTypeDef *heth)
 {
     if (heth->Instance == ETH) {
         /* Peripheral clock disable */
@@ -66,7 +66,7 @@ void HAL_ETH_MspDeInit(ETH_HandleTypeDef* heth)
           RMII_MII_CRS_DV -------------------> PA7
           RMII_MII_RXD0 ---------------------> PC4
           RMII_MII_RXD1 ---------------------> PC5
-          RMII_MII_RXER ---------------------> 
+          RMII_MII_RXER --------------------->
           RMII_MII_TX_EN --------------------> PB11
           RMII_MII_TXD0 ---------------------> PB12
           RMII_MII_TXD1 ---------------------> PB13
