6064af0434228ba0a907552bbd085885969d0da5
jeromecoutant <jerome.coutant@st.com>
2018-07-17 14:28:30 +0200

    Systimer test : compilation error with MBED_TICKLESS
    Issue occurs when "MBED_TICKLESS" is added in mbed_app.json,
    not when "MBED_TICKLESS" is added in targets.json

--- TESTS/mbedmicro-rtos-mbed/systimer/main.cpp
+++ TESTS/mbedmicro-rtos-mbed/systimer/main.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#if !MBED_TICKLESS
+#ifndef MBED_TICKLESS
 #error [NOT_SUPPORTED] Tickless mode not supported for this target.
 #endif
 
