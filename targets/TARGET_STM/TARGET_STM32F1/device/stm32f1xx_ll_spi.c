7449c11e2f4a2ebf97154c5622db44008f34c011
bcostm <bco.stm@gmail.com>
2018-07-25 13:05:51 +0200

    F1 ST CUBE V1.6.1
    - stm32f1xx hal V1.1.2

--- targets/TARGET_STM/TARGET_STM32F1/device/stm32f1xx_ll_spi.c
+++ targets/TARGET_STM/TARGET_STM32F1/device/stm32f1xx_ll_spi.c
@@ -2,8 +2,6 @@
   ******************************************************************************
   * @file    stm32f1xx_ll_spi.c
   * @author  MCD Application Team
-  * @version V1.1.0
-  * @date    14-April-2017
   * @brief   SPI LL module driver.
   ******************************************************************************
   * @attention
