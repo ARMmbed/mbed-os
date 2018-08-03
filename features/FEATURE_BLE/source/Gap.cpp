0dbcf97742dd013373d9ad78cafc2ea96bf6b8c6
paul-szczepanek-arm <33840200+paul-szczepanek-arm@users.noreply.github.com>
2018-08-03 09:54:46 +0100

    set correct default privacy configuration for centrals

--- features/FEATURE_BLE/source/Gap.cpp
+++ features/FEATURE_BLE/source/Gap.cpp
@@ -79,7 +79,7 @@ const Gap::PeripheralPrivacyConfiguration_t Gap::default_peripheral_privacy_conf
 
 const Gap::CentralPrivacyConfiguration_t Gap::default_central_privacy_configuration = {
     /* use_non_resolvable_random_address */ false,
-    /* resolution_strategy */ CentralPrivacyConfiguration_t::DO_NOT_RESOLVE
+    /* resolution_strategy */ CentralPrivacyConfiguration_t::RESOLVE_AND_FORWARD
 };
 
 void Gap::processConnectionEvent(
