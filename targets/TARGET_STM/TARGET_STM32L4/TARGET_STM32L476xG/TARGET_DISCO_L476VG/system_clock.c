a6cf01f9481cde6a2803f60d49f6e098b93efa7a
jeromecoutant <jerome.coutant@st.com>
2018-08-13 15:56:53 +0200

    STM32L4 assert in SetSysClock replaced

--- targets/TARGET_STM/TARGET_STM32L4/TARGET_STM32L476xG/TARGET_DISCO_L476VG/system_clock.c
+++ targets/TARGET_STM/TARGET_STM32L4/TARGET_STM32L476xG/TARGET_DISCO_L476VG/system_clock.c
@@ -32,7 +32,7 @@
 
 #include "stm32l4xx.h"
 #include "nvic_addr.h"
-#include "mbed_assert.h"
+#include "mbed_error.h"
 
 /*!< Uncomment the following line if you need to relocate your vector Table in
      Internal SRAM. */
@@ -134,8 +134,8 @@ void SetSysClock(void)
                 if (SetSysClock_PLL_MSI() == 0)
 #endif
                 {
-                    while (1) {
-                        MBED_ASSERT(1);
+                    {
+                        error("SetSysClock failed\n");
                     }
                 }
             }
