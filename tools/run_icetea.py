449ef92b4db484163c9941c4d581bb4a5e4e18d2
Olli-Pekka Puolitaival <olli-pekka.puolitaival@arm.com>
2018-08-20 12:48:59 +0300

    Update licenses

--- tools/run_icetea.py
+++ tools/run_icetea.py
@@ -1,8 +1,6 @@
 #! /usr/bin/env python2
 """
-mbed SDK
-Copyright (c) 2011-2013 ARM Limited
-
+Copyright 2018 ARM Limited
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
@@ -14,7 +12,6 @@ distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-
 """
 
 from __future__ import print_function, division, absolute_import
