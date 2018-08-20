23da904f8ea51ebc4fff2f5a2614eca3a290ae7e
Amanda Butler <Amanda.Butler@arm.com>
2018-08-20 15:36:37 -0500

    Copy edit CellularSIM.h
    Copy edit for parallelism, and fix typo and capitalization from before these changes.

--- features/cellular/framework/API/CellularSIM.h
+++ features/cellular/framework/API/CellularSIM.h
@@ -55,33 +55,33 @@ public:
      *
      *  @param sim_pin  PIN for the SIM card
      *  @return         NSAPI_ERROR_OK on success
-     *                  NSAPI_ERROR_DEVICE_ERROR on case of failure
+     *                  NSAPI_ERROR_DEVICE_ERROR on failure
      */
     virtual nsapi_error_t set_pin(const char *sim_pin) = 0;
 
-    /** Change sim pin code.
+    /** Change SIM pin code.
      *
-     *  @param sim_pin  Current PIN for sim
-     *  @param new_pin  New PIN for sim
+     *  @param sim_pin  Current PIN for SIM
+     *  @param new_pin  New PIN for SIM
      *  @return         NSAPI_ERROR_OK on success
-     *                  NSAPI_ERROR_DEVICE_ERROR on case of failure
+     *                  NSAPI_ERROR_DEVICE_ERROR on failure
      */
     virtual nsapi_error_t change_pin(const char *sim_pin, const char *new_pin) = 0;
 
     /** Change is pin query needed after boot
      *
      *  @param sim_pin      Valid PIN for SIM card
-     *  @param query_pin    False is PIN query not needed, True if PIN query needed after boot.
+     *  @param query_pin    False if PIN query not needed, True if PIN query needed after boot.
      *  @return         NSAPI_ERROR_OK on success
-     *                  NSAPI_ERROR_DEVICE_ERROR on case of failure
+     *                  NSAPI_ERROR_DEVICE_ERROR on failure
      */
     virtual nsapi_error_t set_pin_query(const char *sim_pin, bool query_pin) = 0;
 
-    /** Get sim card's state
+    /** Get SIM card's state
      *
      *  @param state    current state of SIM
      *  @return         NSAPI_ERROR_OK on success
-     *                  NSAPI_ERROR_DEVICE_ERROR on case of failure
+     *                  NSAPI_ERROR_DEVICE_ERROR on failure
      */
     virtual nsapi_error_t get_sim_state(SimState &state) = 0;
 
@@ -91,7 +91,7 @@ public:
      *  @param imsi     preallocated char* which after successful request contains imsi
      *  @return         NSAPI_ERROR_OK on success
      *                  NSAPI_ERROR_PARAMETER if imsi if null
-     *                  NSAPI_ERROR_DEVICE_ERROR on case of other failures
+     *                  NSAPI_ERROR_DEVICE_ERROR on other failures
      */
     virtual nsapi_error_t get_imsi(char *imsi) = 0;
 
@@ -100,7 +100,7 @@ public:
      *  @param buf      SIM ICCID as zero terminated string
      *  @param buf_size max length of SIM ICCID is MAX_ICCID_LENGTH
      *  @return         NSAPI_ERROR_OK on success
-     *                  NSAPI_ERROR_DEVICE_ERROR on case of failure
+     *                  NSAPI_ERROR_DEVICE_ERROR on failure
      */
     virtual nsapi_error_t get_iccid(char *buf, size_t buf_size) = 0;
 };
