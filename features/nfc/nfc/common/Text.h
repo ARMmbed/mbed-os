dcf38eec50db6455cd8107cc16541be6906a6f46
Vincent Coubard <vincent.coubard@arm.com>
2018-08-24 16:16:37 +0100

    NFC: Add NDEF message framework.

---
+++ features/nfc/nfc/common/Text.h
@@ -0,0 +1,145 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2018 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NFC_COMMON_TEXT_H_
+#define NFC_COMMON_TEXT_H_
+
+#include "platform/Span.h"
+#include <nfc/ndef/RecordParser.h>
+#include <nfc/ndef/MessageBuilder.h>
+#include <stdint.h>
+
+namespace mbed {
+namespace nfc {
+namespace common {
+
+/**
+ * @addtogroup nfc
+ * @{
+ */
+
+/**
+ * Represent the well known type text.
+ */
+class Text {
+public:
+    /**
+     * Encoding of the text.
+     */
+    enum encoding_t {
+        UTF8 = 0,//!< UTF8
+        UTF16 = 1//!< UTF16
+    };
+
+    /**
+     * Construct an empty text element.
+     */
+    Text();
+
+    /**
+     * Construct a text element from a data buffer and an encoding.
+     * @param text_encoding The encoding of the text.
+     * @param language_code The string of the language code.
+     * @param text The text buffer.
+     */
+    Text(
+        encoding_t text_encoding,
+        const Span<const uint8_t> &language_code,
+        const Span<const uint8_t> &text
+    );
+
+    /**
+     * Copy construct a text element.
+     * @param to_copy
+     */
+    Text(const Text &to_copy);
+
+    /**
+     * Destroy a text element.
+     */
+    ~Text();
+
+    /**
+     * Copy assignment of another text element.
+     * @param to_copy The Text instance to copy
+     * @return a reference to this object.
+     */
+    Text& operator=(const Text &to_copy);
+
+    /**
+     * Copy a text from an external buffer.
+     * @param language_code The language code of the text.
+     * @param text The text to copy.
+     */
+    void set_text(
+        encoding_t text_encoding,
+        const Span<const uint8_t> &language_code,
+        const Span<const uint8_t> &text
+    );
+
+    /**
+     * Get the encoding of the text.
+     * @return The encoding of the text.
+     */
+    encoding_t get_encoding() const;
+
+    /**
+     * Return the language code.
+     * @return The language code.
+     */
+    Span<const uint8_t> get_language_code() const;
+
+    /**
+     * Return the text contained in this object.
+     * @return The text contained in this object.
+     */
+    Span<const uint8_t> get_text() const;
+
+    /**
+     * Append into a message builder
+     */
+    bool append_as_record(
+        ndef::MessageBuilder &message_builder,
+        bool is_last_record = false
+    );
+
+private:
+    friend class TextParser;
+
+    void move_data(uint8_t *text, size_t size);
+
+    uint8_t *_text_record;
+    size_t _text_record_size;
+};
+
+/**
+ * Parse a Text.
+ */
+class TextParser : public ndef::GenericRecordParser<TextParser, Text> {
+public:
+    virtual bool do_parse(const ndef::Record &record, Text &text);
+};
+
+/**
+ * @}
+ */
+
+} // namespace common
+} // namespace nfc
+} // namespace mbed
+
+
+#endif /* NFC_COMMON_TEXT_H_ */
---
+++ features/nfc/source/nfc/ndef/MessageBuilder.cpp
@@ -0,0 +1,296 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2018 ARM Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MessageBuilder.h"
+#include <cstring>
+
+namespace mbed {
+namespace nfc {
+namespace ndef {
+
+MessageBuilder::MessageBuilder(const Span<uint8_t> &buffer) :
+    _message_buffer(buffer),
+    _position(0),
+    _message_started(false),
+    _message_ended(false),
+    _in_chunk(false)
+{ }
+
+bool MessageBuilder::append_record(
+   const RecordType &type,
+   const RecordPayload &payload,
+   bool is_last_record
+) {
+    Record record(
+        type,
+        payload,
+        /* id */ RecordID(),
+        /* chunk */ false,
+        is_last_record
+    );
+
+    return append_record(record);
+}
+
+bool MessageBuilder::append_record(const Record &record)
+{
+    if (_message_ended) {
+        return false;
+    }
+
+    if (record.type.value.size() > 255) {
+        return false;
+    }
+
+    if (record.id.size() > 255) {
+        return false;
+    }
+
+    if (!record.id.empty() && _in_chunk) {
+        if (record.chunk) {
+            // middle chunk
+            return false;
+        } else if (record.type.tnf == RecordType::unchanged) {
+            // terminating chunk
+            return false;
+        }
+    }
+
+    if (_in_chunk && record.type.tnf != RecordType::unchanged) {
+        return false;
+    }
+
+    if (!_in_chunk && record.chunk && record.type.tnf == RecordType::unchanged) {
+        return false;
+    }
+
+    if (record.type.tnf == RecordType::empty) {
+        if (!record.type.value.empty()) {
+            return false;
+        }
+
+        if (!record.id.empty()) {
+            return false;
+        }
+
+        if (!record.payload.empty()) {
+            return false;
+        }
+    }
+
+    if (record.type.tnf == RecordType::well_known_type ||
+        record.type.tnf == RecordType::media_type ||
+        record.type.tnf == RecordType::absolute_uri ||
+        record.type.tnf == RecordType::external_type
+    ) {
+        if (record.type.value.empty()) {
+            return false;
+        }
+    }
+
+    if (record.type.tnf == RecordType::unknown && !record.type.value.empty()) {
+        return false;
+    }
+
+    size_t record_size = compute_record_size(record);
+    if (record_size > (_message_buffer.size() - _position)) {
+        return false;
+    }
+
+    append_header(record);
+    append_type_length(record);
+    append_payload_length(record);
+    append_id_length(record);
+    append_type(record);
+    append_id(record);
+    append_payload(record);
+
+    if (record.chunk) {
+        _in_chunk = true;
+    } else if (record.type.tnf == RecordType::unchanged) {
+        // last chunk reached
+        _in_chunk = false;
+    }
+
+    return true;
+}
+
+void MessageBuilder::reset()
+{
+    _position = 0;
+    _message_started = false;
+    _message_ended = false;
+    _in_chunk = false;
+}
+
+void MessageBuilder::reset(const Span<uint8_t> &buffer)
+{
+    _message_buffer = buffer;
+    _position = 0;
+    _message_started = false;
+    _message_ended = false;
+    _in_chunk = false;
+}
+
+bool MessageBuilder::is_message_complete() const
+{
+    return _message_ended;
+}
+
+Span<const uint8_t> MessageBuilder::get_message() const
+{
+    if (is_message_complete()) {
+        return _message_buffer.first(_position);
+    } else {
+        return Span<const uint8_t>();
+    }
+}
+
+size_t MessageBuilder::compute_record_size(const Record &record)
+{
+    size_t record_size = 0;
+    record_size = 1; /* header */
+    record_size += 1; /* type length */
+    record_size += is_short_payload(record) ? 1 : 4;
+
+    if (!record.id.empty()) {
+        record_size += 1;
+    }
+
+    record_size += record.type.value.size();
+    record_size += record.id.size();
+    record_size += record.payload.size();
+
+    return record_size;
+}
+
+void MessageBuilder::append_header(const Record &record)
+{
+    uint8_t header = 0;
+    if (!_message_started) {
+        header |= Header::message_begin_bit;
+        _message_started = true;
+    }
+
+    if (record.last_record) {
+        header |= Header::message_end_bit;
+        _message_ended = true;
+    }
+
+    if (record.chunk) {
+        header |= Header::chunk_flag_bit;
+    }
+
+    if (is_short_payload(record)) {
+        header |= Header::short_record_bit;
+    }
+
+    if (record.id.size()) {
+        header |= Header::id_length_bit;
+    }
+
+    header |= record.type.tnf;
+    _message_buffer[_position++] = header;
+}
+
+void MessageBuilder::append_type_length(const Record &record)
+{
+    _message_buffer[_position++] = record.type.value.size();
+}
+
+void MessageBuilder::append_payload_length(const Record &record)
+{
+    if (record.payload.empty()) {
+        _message_buffer[_position++] = 0;
+        return;
+    }
+
+    if (is_short_payload(record)) {
+        _message_buffer[_position++] = record.payload.size();
+    } else {
+        // TODO: proper host to network
+        uint32_t size = record.payload.size();
+        _message_buffer[_position++] = (size >> 24) & 0xFF;
+        _message_buffer[_position++] = (size >> 16) & 0xFF;
+        _message_buffer[_position++] = (size >> 8) & 0xFF;
+        _message_buffer[_position++] = size & 0xFF;
+    }
+}
+
+void MessageBuilder::append_id_length(const Record &record)
+{
+    if (record.id.empty()) {
+        return;
+    }
+
+    _message_buffer[_position++] = record.id.size();
+}
+
+void MessageBuilder::append_type(const Record &record)
+{
+    if (record.type.value.empty()) {
+        return;
+    }
+
+    memcpy(
+        _message_buffer.data() + _position,
+        record.type.value.data(),
+        record.type.value.size()
+    );
+    _position += record.type.value.size();
+}
+
+void MessageBuilder::append_id(const Record &record)
+{
+    if (record.id.empty()) {
+        return;
+    }
+
+    memcpy(
+        _message_buffer.data() + _position,
+        record.id.data(),
+        record.id.size()
+    );
+    _position += record.id.size();
+}
+
+void MessageBuilder::append_payload(const Record &record)
+{
+    if (record.payload.empty()) {
+        return;
+    }
+
+    memcpy(
+        _message_buffer.data() + _position,
+        record.payload.data(),
+        record.payload.size()
+    );
+    _position += record.payload.size();
+}
+
+bool MessageBuilder::is_short_payload(const Record &record)
+{
+    if (record.payload.size() <= 255) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+} // namespace ndef
+} // namespace nfc
+} // namespace mbed
+
