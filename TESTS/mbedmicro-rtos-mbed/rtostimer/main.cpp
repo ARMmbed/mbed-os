b98bd91a985d550a759aabf9da8d2f781d0d5917
Qinghao Shi <Qinghao.Shi@arm.com>
2018-08-29 13:07:42 +0100

    increase timeout for rtostimer test to 10s

--- TESTS/mbedmicro-rtos-mbed/rtostimer/main.cpp
+++ TESTS/mbedmicro-rtos-mbed/rtostimer/main.cpp
@@ -334,7 +334,7 @@ void test_isr_calls_fail()
 
 utest::v1::status_t test_setup(const size_t number_of_cases)
 {
-    GREENTEA_SETUP(5, "default_auto");
+    GREENTEA_SETUP(10, "default_auto");
     return verbose_test_setup_handler(number_of_cases);
 }
 
