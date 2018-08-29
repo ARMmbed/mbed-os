cf1fa063815563ee36f0c222708c94d1ff87cfb2
Kimmo Vaisanen <kimmo.vaisanen@arm.com>
2018-08-29 13:50:53 +0300

    Uplink (unstable)

--- features/lorawan/lorastack/mac/LoRaMac.cpp
+++ features/lorawan/lorastack/mac/LoRaMac.cpp
@@ -1,3 +1,4 @@
+#include <stdio.h>
 /**
  / _____)             _              | |
 ( (____  _____ ____ _| |_ _____  ____| |__
@@ -310,6 +311,26 @@ loramac_event_info_status_t LoRaMac::handle_join_accept_frame(const uint8_t *pay
             return LORAMAC_EVENT_INFO_STATUS_CRYPTO_FAIL;
         }
 
+        printf("nwk_skey: ");
+        for (int i = 0; i < 16; i++)
+            printf("%02X ", _params.keys.nwk_skey[i]);
+        printf("\r\n");
+
+        printf("app_skey: ");
+        for (int i = 0; i < 16; i++)
+            printf("%02X ", _params.keys.app_skey[i]);
+        printf("\r\n");
+
+        printf("snwk_sintkey: ");
+        for (int i = 0; i < 16; i++)
+            printf("%02X ", _params.keys.snwk_sintkey[i]);
+        printf("\r\n");
+
+        printf("nwk_senckey: ");
+        for (int i = 0; i < 16; i++)
+            printf("%02X ", _params.keys.nwk_senckey[i]);
+        printf("\r\n");
+
         _params.net_id = (uint32_t) _params.rx_buffer[payload_start + 3];
         _params.net_id |= ((uint32_t) _params.rx_buffer[payload_start + 4] << 8);
         _params.net_id |= ((uint32_t) _params.rx_buffer[payload_start + 5] << 16);
@@ -495,7 +516,7 @@ bool LoRaMac::extract_mac_commands_only(const uint8_t *payload,
         uint8_t buffer[15];
 
         if (_params.server_type == LW1_1) {
-            if (0 != _lora_crypto.encrypt_payload(payload + 8, fopts_len,
+            if (0 != _lora_crypto.decrypt_payload(payload + 8, fopts_len,
                                                   _params.keys.nwk_senckey, sizeof(_params.keys.nwk_senckey) * 8,
                                                   _params.dev_addr, DOWN_LINK,
                                                   _params.dl_frame_counter,
@@ -1843,14 +1864,13 @@ lorawan_status_t LoRaMac::prepare_frame(loramac_mhdr_t *machdr,
             const uint8_t mac_commands_len = _mac_commands.get_mac_cmd_length();
 
             if ((payload != NULL) && (_params.tx_buffer_len > 0)) {
-
                 if (mac_commands_len <= LORA_MAC_COMMAND_MAX_FOPTS_LENGTH) {
                     fctrl->bits.fopts_len += mac_commands_len;
 
                     // Update FCtrl field with new value of OptionsLength
                     _params.tx_buffer[0x05] = fctrl->value;
-
                     const uint8_t *buffer = _mac_commands.get_mac_commands_buffer();
+
                     if (_params.server_type == LW1_1) {
                         if (0 != _lora_crypto.encrypt_payload(buffer, mac_commands_len,
                                                               _params.keys.nwk_senckey,
@@ -1910,6 +1930,8 @@ lorawan_status_t LoRaMac::prepare_frame(loramac_mhdr_t *machdr,
                 status = LORAWAN_STATUS_CRYPTO_FAIL;
             }
 
+            tr_info("_params.ul_frame_counter = %d", _params.ul_frame_counter);
+
             if (_params.server_type == LW1_1) {
                 if (_params.is_srv_ack_requested) {
                     args = _params.counterForAck;
@@ -1929,6 +1951,8 @@ lorawan_status_t LoRaMac::prepare_frame(loramac_mhdr_t *machdr,
                 _params.tx_buffer[_params.tx_buffer_len + 1] = (mic2 >> 8) & 0xFF;
                 _params.tx_buffer[_params.tx_buffer_len + 2] = mic & 0xFF;
                 _params.tx_buffer[_params.tx_buffer_len + 3] = (mic >> 8) & 0xFF;
+
+                tr_info("LoRaWAN 1.1.x MIC1 = 0x%x, MIC2 = 0x%x", mic, mic2);
             } else {
                 _params.tx_buffer[_params.tx_buffer_len + 0] = mic & 0xFF;
                 _params.tx_buffer[_params.tx_buffer_len + 1] = (mic >> 8) & 0xFF;
@@ -1937,6 +1961,11 @@ lorawan_status_t LoRaMac::prepare_frame(loramac_mhdr_t *machdr,
             }
 
             _params.tx_buffer_len += LORAMAC_MFR_LEN;
+
+            printf("tx_buf (%d):", _params.tx_buffer_len);
+            for (int b = 0; b < _params.tx_buffer_len; b++)
+                printf("%02X ", _params.tx_buffer[b]);
+            printf("\r\n");
         }
         break;
         case FRAME_TYPE_PROPRIETARY:
