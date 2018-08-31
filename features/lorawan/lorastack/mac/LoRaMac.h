c11b0ff56577ef87a70d445b64c913281978c6d2
Kimmo Vaisanen <kimmo.vaisanen@arm.com>
2018-08-31 15:25:40 +0300

    Fix MIC calculation

--- features/lorawan/lorastack/mac/LoRaMac.h
+++ features/lorawan/lorastack/mac/LoRaMac.h
@@ -647,7 +647,10 @@ private:
     void set_tx_continuous_wave(uint8_t channel, int8_t datarate, int8_t tx_power,
                                 float max_eirp, float antenna_gain, uint16_t timeout);
 
-    lorawan_status_t calculate_mic();
+    /**
+     * Calculate MIC for user data messages
+     */
+    lorawan_status_t calculate_userdata_mic();
 
 private:
     typedef mbed::ScopedLock<LoRaMac> Lock;
