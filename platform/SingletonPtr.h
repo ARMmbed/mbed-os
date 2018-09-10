199d1667ff1d0558f631fb21774bda9f8eace368
Kevin Bracey <kevin.bracey@arm.com>
2018-09-10 11:00:08 +0300

    Make SingletonPtr methods const
    Make get() and operators * and -> of SingletonPtr const - they are
    logically const and thread-safe, despite the construction on first call.
    This construction is "invisible" to the caller of those methods.

--- platform/SingletonPtr.h
+++ platform/SingletonPtr.h
@@ -88,7 +88,7 @@ struct SingletonPtr {
      * @returns
      *   A pointer to the singleton
      */
-    T *get()
+    T *get() const
     {
         if (NULL == _ptr) {
             singleton_lock();
@@ -108,7 +108,7 @@ struct SingletonPtr {
      * @returns
      *   A pointer to the singleton
      */
-    T *operator->()
+    T *operator->() const
     {
         return get();
     }
@@ -118,15 +118,15 @@ struct SingletonPtr {
      * @returns
      *   A reference to the singleton
      */
-    T &operator*()
+    T &operator*() const
     {
         return *get();
     }
 
     // This is zero initialized when in global scope
-    T *_ptr;
+    mutable T *_ptr;
     // Force data to be 4 byte aligned
-    uint32_t _data[(sizeof(T) + sizeof(uint32_t) - 1) / sizeof(uint32_t)];
+    mutable uint32_t _data[(sizeof(T) + sizeof(uint32_t) - 1) / sizeof(uint32_t)];
 };
 
 #endif
