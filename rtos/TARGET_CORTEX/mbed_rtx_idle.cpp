50316d00bd13eca6b6e5d864f50c1b5dcb6ab822
Russ Butler <russ.butler@arm.com>
2018-09-21 16:53:42 -0500

    Improve RTOS behavior with deep sleep
    Only deep sleep when the wakeup time is more than
    MBED_CONF_TARGET_DEEP_SLEEP_LATENCY ms in the future.
    This ensures that RTOS events are handled at the correct time. Note -
    when deep sleep is allow interrupt latency may still be as high as
    10ms.

--- rtos/TARGET_CORTEX/mbed_rtx_idle.cpp
+++ rtos/TARGET_CORTEX/mbed_rtx_idle.cpp
@@ -90,6 +90,13 @@ uint32_t OS_Tick_GetInterval (void) {
 static void default_idle_hook(void)
 {
     uint32_t ticks_to_sleep = osKernelSuspend();
+    const bool block_deep_sleep = ticks_to_sleep <= MBED_CONF_TARGET_DEEP_SLEEP_LATENCY;
+
+    if (block_deep_sleep) {
+        sleep_manager_lock_deep_sleep();
+    } else {
+        ticks_to_sleep -= MBED_CONF_TARGET_DEEP_SLEEP_LATENCY;
+    }
     os_timer->suspend(ticks_to_sleep);
 
     bool event_pending = false;
@@ -106,6 +113,11 @@ static void default_idle_hook(void)
         // Ensure interrupts get a chance to fire
         __ISB();
     }
+
+    if (block_deep_sleep) {
+        sleep_manager_unlock_deep_sleep();
+    }
+
     osKernelResume(os_timer->resume());
 }
 
