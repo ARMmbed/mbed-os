e00ced5cf987b80d30fe1aa6e2b6715bbc78e27b
Antti Kauppila <antti.kauppila@arm.com>
2018-09-24 14:34:27 +0300

    Auto merge problems solved

--- features/lorawan/lorastack/mac/LoRaMacCommand.cpp
+++ features/lorawan/lorastack/mac/LoRaMacCommand.cpp
@@ -404,7 +404,6 @@ lorawan_status_t LoRaMacCommand::add_reset_ind(uint8_t version)
         mac_cmd_buffer[mac_cmd_buf_idx++] = MOTE_MAC_RESET_IND;
         mac_cmd_buffer[mac_cmd_buf_idx++] = version & 0x0F;
         ret = LORAWAN_STATUS_OK;
-        mac_cmd_in_next_tx = true;
     }
     return ret;
 }
@@ -416,7 +415,6 @@ lorawan_status_t LoRaMacCommand::add_rekey_ind(uint8_t version)
         mac_cmd_buffer[mac_cmd_buf_idx++] = MOTE_MAC_REKEY_IND;
         mac_cmd_buffer[mac_cmd_buf_idx++] = version & 0x0F;
         ret = LORAWAN_STATUS_OK;
-        mac_cmd_in_next_tx = true;
     }
     return ret;
 }
@@ -428,7 +426,6 @@ lorawan_status_t LoRaMacCommand::add_device_mode_indication(uint8_t classType)
         mac_cmd_buffer[mac_cmd_buf_idx++] = MOTE_DEVICE_MODE_IND;
         mac_cmd_buffer[mac_cmd_buf_idx++] = classType;
         ret = LORAWAN_STATUS_OK;
-        mac_cmd_in_next_tx = true;
     }
     return ret;
 }
@@ -439,7 +436,6 @@ lorawan_status_t LoRaMacCommand::add_device_time_req()
     if (cmd_buffer_remaining() > 0) {
         mac_cmd_buffer[mac_cmd_buf_idx++] = MOTE_MAC_DEVICE_TIME_REQ;
         ret = LORAWAN_STATUS_OK;
-        mac_cmd_in_next_tx = true;
     }
     return ret;
 }
