1f2e56bc4385e6d590bb312134a331730baa1b8b
Jimmy Brisson <theotherjimmy@gmail.com>
2018-09-25 12:52:25 -0500

    Tools: Don't traceback on missing linker script
    ### Description
    
    The `mbed compile` would traceback when no linker script is found.
    This PR changes that behavior to make that into a NotSupportedException,
    which has decent user behavior.
    
    Fixes #7723
    
    ### Pull request type
    
        [x] Fix
        [ ] Refactor
        [ ] Target update
        [ ] Functionality change
        [ ] Breaking change

--- tools/build_api.py
+++ tools/build_api.py
@@ -570,6 +570,8 @@ def build_project(src_paths, build_path, target, toolchain_name,
         # Change linker script if specified
         if linker_script is not None:
             resources.add_file_ref(linker_script, linker_script)
+        if not resources.get_file_refs(FileType.LD_SCRIPT):
+            raise NotSupportedException("No Linker Script found!")
 
         # Compile Sources
         objects = toolchain.compile_sources(resources, sorted(resources.get_file_paths(FileType.INC_DIR)))
