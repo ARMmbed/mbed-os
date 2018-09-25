b355c750a6419502423914cc0d1733f49898a219
Veijo Pesonen <veijo.pesonen@arm.com>
2018-09-25 09:22:26 +0300

    Makes mbedgt-netsocket cases to accept port 0 as destination

--- TESTS/netsocket/udp/udpsocket_sendto_invalid.cpp
+++ TESTS/netsocket/udp/udpsocket_sendto_invalid.cpp
@@ -32,7 +32,6 @@ void UDPSOCKET_SENDTO_INVALID()
     TEST_ASSERT(sock.sendto(NULL, 9, NULL, 0) < 0);
     TEST_ASSERT(sock.sendto("", 9, NULL, 0) < 0);
     TEST_ASSERT(sock.sendto("", 0, NULL, 0) < 0);
-    TEST_ASSERT_EQUAL(5, sock.sendto(MBED_CONF_APP_ECHO_SERVER_ADDR, 0, "hello", 5));
     TEST_ASSERT_EQUAL(0, sock.sendto(MBED_CONF_APP_ECHO_SERVER_ADDR, 9, NULL, 0));
     TEST_ASSERT_EQUAL(5, sock.sendto(MBED_CONF_APP_ECHO_SERVER_ADDR, 9, "hello", 5));
 
