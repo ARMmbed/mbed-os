9e6b1246a4919ed29e1193bf96f5c6bc2d578fea
Bence Kaposzta <bence.kaposzta@arm.com>
2018-10-12 09:13:04 +0200

    This commit adds EMAC driver for CM3DS that uses an SMSC LAN 9220 Ethernet controller. To ensure proper operation, some methods needed to be updated in the SMSC9220's native driver as well. It passes all related Greentea tests, however when supervised by the Python environment it tends to fail because of Timeout.
    The current timeout is set to 1200s that seems to be a little bit short
    to finish all test cases, the timeout happens towards the end of the
    last test case.
    
    Change-Id: I914608c34828b493a80e133cd132537a297bfc84
    Signed-off-by: Bence Kaposzta <bence.kaposzta@arm.com>

--- targets/TARGET_ARM_SSG/TARGET_CM3DS_MPS2/mbed_overrides.c
+++ targets/TARGET_ARM_SSG/TARGET_CM3DS_MPS2/mbed_overrides.c
@@ -1,6 +1,6 @@
 /* mbed Microcontroller Library
 
- * Copyright (c) 2006-2017 ARM Limited
+ * Copyright (c) 2006-2018 Arm Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,10 +15,12 @@
  * limitations under the License.
  */
 
-#include "smsc9220_eth.h"
+#include "platform_devices.h"
+#include "smsc9220_eth_drv.h"
 
 /* Provide ethernet devices with a semi-unique MAC address from the UUID */
 void mbed_mac_address(char *mac)
 {
-    smsc9220_read_mac_address(mac);
+    smsc9220_read_mac_address(&SMSC9220_ETH_DEV, mac);
 }
+
