c21c599b250c9015930b6df2d789759d60bac60c
Mahesh Mahadevan <mahesh.mahadevan@nxp.com>
2018-10-19 11:20:42 -0500

    Update the QSPI unit test for NXP LPC546XX board
    Signed-off-by: Mahesh Mahadevan <mahesh.mahadevan@nxp.com>

--- TESTS/mbed_hal/qspi/flash_configs/NXP/LPC546XX/flash_config.h
+++ TESTS/mbed_hal/qspi/flash_configs/NXP/LPC546XX/flash_config.h
@@ -13,24 +13,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#ifndef MBED_QSPI_FLASH_CONFIG_H
+#define MBED_QSPI_FLASH_CONFIG_H
 
-#ifndef MBED_FLASH_CONFIGS_H
-#define MBED_FLASH_CONFIGS_H
+#include "../../MT25Q_config.h"
 
-#if defined(TARGET_DISCO_L475VG_IOT01A)
-#include "STM/DISCO_L475VG_IOT01A/flash_config.h"
-#elif defined(TARGET_NRF52840)
-#include "NORDIC/NRF52840_DK/flash_config.h"
-#elif defined(TARGET_DISCO_F413ZH)
-#include "STM/DISCO_F413ZH/flash_config.h"
-#elif defined(TARGET_EFM32GG11_STK3701)
-#include "SiliconLabs/EFM32GG11_STK3701/flash_config.h"
-#elif defined(TARGET_K82F)
-#include "NXP/K82F/flash_config.h"
-#elif defined(TARGET_KL82Z)
-#include "NXP/KL82Z/flash_config.h"
-#elif defined(TARGET_LPC546XX)
-#include "NXP/LPC546XX/flash_config.h"
-#endif
-
-#endif // MBED_FLASH_CONFIGS_H
+#endif // MBED_QSPI_FLASH_CONFIG_H
