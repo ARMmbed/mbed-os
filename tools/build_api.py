aa5d2afa4d3aaa3a6140457ce52ff6e867ca7d9d
Jimmy Brisson <theotherjimmy@gmail.com>
2018-10-19 10:25:49 -0500

    Stop shouting at users in error messages

--- tools/build_api.py
+++ tools/build_api.py
@@ -533,7 +533,7 @@ def build_project(src_paths, build_path, target, toolchain_name,
         if linker_script is not None:
             resources.add_file_ref(linker_script, linker_script)
         if not resources.get_file_refs(FileType.LD_SCRIPT):
-            raise NotSupportedException("No Linker Script found!")
+            raise NotSupportedException("No Linker Script found")
 
         # Compile Sources
         objects = toolchain.compile_sources(resources, sorted(resources.get_file_paths(FileType.INC_DIR)))
