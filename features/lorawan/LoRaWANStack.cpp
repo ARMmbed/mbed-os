b1a668745e47a4b1daace94e13ad8ad87fef2872
Antti Kauppila <antti.kauppila@arm.com>
2018-10-23 02:45:07 +0300

    Auto merge problem fixed

--- features/lorawan/LoRaWANStack.cpp
+++ features/lorawan/LoRaWANStack.cpp
@@ -792,17 +792,6 @@ void LoRaWANStack::process_reception(const uint8_t *const payload, uint16_t size
 
     _loramac.on_radio_rx_done(payload, size, rssi, snr, mbed::callback(this, &LoRaWANStack::mlme_confirm_handler));
 
-    if (_loramac.get_mlme_confirmation()->pending) {
-        _loramac.post_process_mlme_request();
-        mlme_confirm_handler();
-
-        if (_loramac.get_mlme_confirmation()->req_type == MLME_JOIN) {
-            _ready_for_rx = true;
-            return;
-        }
-    }
-    make_rx_metadata_available();
-
     if (!joined) {
         _ready_for_rx = true;
         return;
