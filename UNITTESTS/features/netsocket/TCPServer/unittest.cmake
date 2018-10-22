3bc33f6c3e637be31588d2734c62aa24d072b1c6
Taiki <github@taiki.fr>
2018-10-22 14:32:59 +0200

    Use the parsing status as a way to detect if the IP string represent a valid IPv6 address

--- UNITTESTS/features/netsocket/TCPServer/unittest.cmake
+++ UNITTESTS/features/netsocket/TCPServer/unittest.cmake
@@ -13,7 +13,10 @@ set(unittest-sources
   ../features/netsocket/TCPSocket.cpp
   ../features/netsocket/TCPServer.cpp
   ../features/frameworks/nanostack-libservice/source/libip4string/ip4tos.c
+  ../features/frameworks/nanostack-libservice/source/libip6string/ip6tos.c
   ../features/frameworks/nanostack-libservice/source/libip4string/stoip4.c
+  ../features/frameworks/nanostack-libservice/source/libip6string/stoip6.c
+  ../features/frameworks/nanostack-libservice/source/libBits/common_functions.c  
 )
 
 set(unittest-test-sources
