0a1e3a0c520eceb8275d95b48139620333f2c073
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2018-10-23 12:18:45 +0200

    mbed_rtc_time.h - documentation fix
    Make example code consistent with:
    https://os.mbed.com/teams/mbed_example/code/time_HelloWorld/file/0c6401d671c6/main.cpp/

--- platform/mbed_rtc_time.h
+++ platform/mbed_rtc_time.h
@@ -40,10 +40,10 @@ extern "C" {
  * int main() {
  *     set_time(1256729737);  // Set RTC time to Wed, 28 Oct 2009 11:35:37
  *
- *     while(1) {
+ *     while (true) {
  *         time_t seconds = time(NULL);
  *
- *         printf("Time as seconds since January 1, 1970 = %d\n", seconds);
+ *         printf("Time as seconds since January 1, 1970 = %u\n", (unsigned int)seconds);
  *
  *         printf("Time as a basic string = %s", ctime(&seconds));
  *
