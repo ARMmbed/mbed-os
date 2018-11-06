d27566c9556a078d5a630c4e45f89194f7a4b67d
Russ Butler <russ.butler@arm.com>
2018-11-06 15:57:28 -0600

    Rename MpuXnLock
    Rename MpuXnLock  to ScopedMpuXnLock so it has the same naming
    convention as ScopedMutexLock. Also make this class inherit from
    NonCopyable to prevent misuse.

--- mbed.h
+++ mbed.h
@@ -95,7 +95,7 @@
 #include "platform/DirHandle.h"
 #include "platform/CriticalSectionLock.h"
 #include "platform/DeepSleepLock.h"
-#include "platform/MpuXnLock.h"
+#include "platform/ScopedMpuXnLock.h"
 #include "platform/mbed_stats.h"
 
 // mbed Non-hardware components
