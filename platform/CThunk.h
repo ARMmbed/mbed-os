17097a962b80d69cb9deb5d0724734bd3178fe17
Martin Kojtal <martin.kojtal@arm.com>
2018-11-09 11:31:20 +0000

    platform: add spdx license

--- platform/CThunk.h
+++ platform/CThunk.h
@@ -13,6 +13,7 @@
  * - ideally suited for class object receiving interrupts (NVIC_SetVector)
  *
  * Copyright (c) 2014-2015 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
