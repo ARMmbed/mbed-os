cfd79596c85cba0be6642bd4488fe1b7b610a36e
Martin Kojtal <martin.kojtal@arm.com>
2018-11-09 11:27:45 +0000

    hal: add spdx license

--- hal/analogin_api.h
+++ hal/analogin_api.h
@@ -3,6 +3,7 @@
 /** @{*/
 /* mbed Microcontroller Library
  * Copyright (c) 2006-2013 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
