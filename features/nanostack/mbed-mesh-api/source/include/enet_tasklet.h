db28f2edce461610c1ce4d100d7ae28d01fc982f
earl manning <earl.manning@arm.com>
2018-11-09 14:05:45 -0600

    Match enet_tasklet_disconnect() declaration to definition
    The declaration of enet_tasklet_disconnect() was declared in the header to return a void.  In definition, it returned int8_t.  This fix is just to correct the header to match the code.

--- features/nanostack/mbed-mesh-api/source/include/enet_tasklet.h
+++ features/nanostack/mbed-mesh-api/source/include/enet_tasklet.h
@@ -25,7 +25,7 @@ extern "C" {
 void enet_tasklet_init(void);
 uint8_t enet_tasklet_network_init(int8_t);
 int8_t enet_tasklet_connect(void (*)(mesh_connection_status_t mesh_status), int8_t nwk_interface_id);
-void enet_tasklet_disconnect();
+int8_t enet_tasklet_disconnect();
 
 #ifdef __cplusplus
 }
