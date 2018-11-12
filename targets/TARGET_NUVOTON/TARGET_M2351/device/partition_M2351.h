1d8c00eccb4bdaedb0369439dd908b71b5c44cee
Deepika <deepika.bhavnani@arm.com>
2018-11-11 21:07:21 -0600

    Update parition sizes in M2351 - based on #8607
    RAM/ROM sizes in tools were updated to report entire device size, and in M2351
    they were used earlier to report secure/non-secure partition size.
    
    M2351 files are updated to take full RAM/ROM device size and derive secure
    and non-secure partition size based on that.

--- targets/TARGET_NUVOTON/TARGET_M2351/device/partition_M2351.h
+++ targets/TARGET_NUVOTON/TARGET_M2351/device/partition_M2351.h
@@ -11,17 +11,16 @@
 #ifndef PARTITION_M2351
 #define PARTITION_M2351
 
-#ifndef MBED_ROM_SIZE
-    #define NU_TZ_SECURE_FLASH_SIZE     0x40000
-#else
-    #define NU_TZ_SECURE_FLASH_SIZE     MBED_ROM_SIZE
+#ifndef MBED_ROM_SIZE_S
+#define MBED_ROM_SIZE_S      (0x40000)
 #endif
 
-#ifndef APPLICATION_RAM_SIZE
-    #define NU_TZ_SECURE_SRAM_SIZE      0x8000
-#else
-    #define NU_TZ_SECURE_SRAM_SIZE      APPLICATION_RAM_SIZE
+#ifndef MBED_RAM_SIZE_S
+#define MBED_RAM_SIZE_S      (0x8000)
 #endif
+
+#define NU_TZ_SECURE_FLASH_SIZE     MBED_ROM_SIZE_S
+#define NU_TZ_SECURE_SRAM_SIZE      MBED_RAM_SIZE_S
 
 #if defined(__CC_ARM) || (defined(__ARMCC_VERSION) && (__ARMCC_VERSION >= 6010050))
 
