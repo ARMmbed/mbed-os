5a16ea489e0826027283330e57a995e006e49303
chao_king <37656088+ChazJin@users.noreply.github.com>
2018-11-13 16:11:02 +0800

    Re add target support for GD32F307VG
    1. Mainly change TARGET_Gigadevice --> TARGET_GigaDevice
    2. Add license header

---
+++ targets/TARGET_GigaDevice/TARGET_GD32F30X/TATGET_GD32F307VG/device/cmsis.h
@@ -0,0 +1,13 @@
+/* mbed Microcontroller Library - CMSIS
+ * Copyright (C) 2009-2018 ARM Limited. All rights reserved.
+ * 
+ * A generic CMSIS include header, pulling in GD32F307VG specifics
+ */
+
+#ifndef MBED_CMSIS_H
+#define MBED_CMSIS_H
+
+#include "gd32f30x.h"
+#include "cmsis_nvic.h"
+
+#endif  /* MBED_CMSIS_H */
