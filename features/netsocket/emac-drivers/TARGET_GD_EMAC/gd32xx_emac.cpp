49b25c96991b592dc4a19fabe71446a05872c1b2
chao_king <37656088+ChazJin@users.noreply.github.com>
2018-11-13 17:33:51 +0800

    Update licence

--- features/netsocket/emac-drivers/TARGET_GD_EMAC/gd32xx_emac.cpp
+++ features/netsocket/emac-drivers/TARGET_GD_EMAC/gd32xx_emac.cpp
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2018 Gigadevice
+ * Copyright (c) 2018 Gigadevice Semiconductor Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
