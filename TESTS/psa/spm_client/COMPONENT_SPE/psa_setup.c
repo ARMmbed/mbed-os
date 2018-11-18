9cc017fd48830a33d3e0338eff9e1da5fab4d505
Alexander Zilberkant <alexander.zilberkant@arm.com>
2018-11-18 20:14:05 +0200

    Normalize license headers
    - add SPDX license identifier
    - add missing license headers
    - update year in license headers

--- TESTS/psa/spm_client/COMPONENT_SPE/psa_setup.c
+++ TESTS/psa/spm_client/COMPONENT_SPE/psa_setup.c
@@ -1,4 +1,6 @@
-/* Copyright (c) 2017 ARM Limited
+/* Copyright (c) 2017-2018 ARM Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- components/TARGET_PSA/spm/TARGET_IGNORE/neg_dual_partition/NEG_DUAL_PART1/psa_neg_dual_part1_ifs.h
+++ components/TARGET_PSA/spm/TARGET_IGNORE/neg_dual_partition/NEG_DUAL_PART1/psa_neg_dual_part1_ifs.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2017 ARM Limited
+/* Copyright (c) 2017-2018 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
