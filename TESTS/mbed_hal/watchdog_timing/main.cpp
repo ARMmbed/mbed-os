fed02b0fa80a2b8e655047b2da47f366daddb8f9
Donatien Garnier <donatien.garnier@arm.com>
2018-11-19 16:55:27 +0000

    Update license headers

--- TESTS/mbed_hal/watchdog_timing/main.cpp
+++ TESTS/mbed_hal/watchdog_timing/main.cpp
@@ -1,5 +1,6 @@
 /* mbed Microcontroller Library
  * Copyright (c) 2018 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
