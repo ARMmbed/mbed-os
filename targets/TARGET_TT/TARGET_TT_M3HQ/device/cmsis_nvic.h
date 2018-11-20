3502fad243c25634f059a16d9d6ded6db735fe21
panyz0725@thundersoft.com <panyz0725@thundersoft.com>
2018-11-20 11:16:48 +0800

    Add platform TT_M3HQ

---
+++ targets/TARGET_TT/TARGET_TT_M3HQ/device/cmsis_nvic.h
@@ -0,0 +1,13 @@
+/* mbed Microcontroller Library - cmsis_nvic for TMPM3HQ
+ * Copyright (c) 2011 ARM Limited. All rights reserved.
+ *
+ * CMSIS-style functionality to support dynamic vectors
+ */
+
+#ifndef MBED_CMSIS_NVIC_H
+#define MBED_CMSIS_NVIC_H
+
+#define NVIC_NUM_VECTORS             (182)
+#define NVIC_RAM_VECTOR_ADDRESS      0x20000000    // Location of vectors in RAM
+
+#endif
