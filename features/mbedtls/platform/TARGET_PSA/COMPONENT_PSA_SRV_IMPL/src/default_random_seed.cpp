07c4da236969e86f383b3cf8c7a5d36c8200b6d1
Netanel Gonen <netanel.gonen@arm.com>
2018-11-20 17:52:36 +0200

    update random seed uid define name

--- features/mbedtls/platform/TARGET_PSA/COMPONENT_PSA_SRV_IMPL/src/default_random_seed.cpp
+++ features/mbedtls/platform/TARGET_PSA/COMPONENT_PSA_SRV_IMPL/src/default_random_seed.cpp
@@ -6,18 +6,12 @@
 int mbed_default_seed_read(unsigned char *buf, size_t buf_len)
 {
     psa_its_status_t rc = psa_its_get(MBED_RANDOM_SEED_ITS_UID, 0, buf_len, buf);
-    /* Make sure that in case of an error the value will be negative
-     * Mbed TLS errors are negative values */
-    rc  = rc < 0 ? rc : (-1 * rc);
-    return (rc);
+    return (-1 * rc);
 }
 
 int mbed_default_seed_write(unsigned char *buf, size_t buf_len)
 {
-    psa_its_status_t rc = psa_its_set(MBED_RANDOM_SEED_ITS_UID, buf_len, buf, 0);
-    /* Make sure that in case of an error the value will be negative
-     * Mbed TLS errors are negative values */
-    rc  = rc < 0 ? rc : (-1 * rc);
-    return (rc);
+    psa_its_status_t rc = psa_its_set(MBEDTLS_RANDOM_SEED_ITS_UID, buf_len, buf, 0);
+    return (-1 * rc);
 }
 
