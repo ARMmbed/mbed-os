1029821f753e21422fef2cd0967db136bd43ac1a
c_jin <chao.jin@gigadevice.com>
2018-11-21 11:38:53 +0800

    Add SPDX identifier

--- targets/TARGET_GigaDevice/TARGET_GD32F30X/PortNames.h
+++ targets/TARGET_GigaDevice/TARGET_GD32F30X/PortNames.h
@@ -1,6 +1,8 @@
 /* mbed Microcontroller Library
  * Copyright (c) 2018 GigaDevice Semiconductor Inc.
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
