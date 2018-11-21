1479dd9c0af8f9a812ee7d375179791182277236
Alex Volinski <alex.volinski@arm.com>
2018-11-21 17:33:15 +0200

    Change naming of MBEDTLS_RANDOM_SEED_ITS

--- features/mbedtls/platform/TARGET_PSA/COMPONENT_PSA_SRV_IMPL/src/default_random_seed.cpp
+++ features/mbedtls/platform/TARGET_PSA/COMPONENT_PSA_SRV_IMPL/src/default_random_seed.cpp
@@ -5,13 +5,13 @@
 
 int mbed_default_seed_read(unsigned char *buf, size_t buf_len)
 {
-    psa_its_status_t rc = psa_its_get(MBED_RANDOM_SEED_ITS_UID, 0, buf_len, buf);
+    psa_its_status_t rc = psa_its_get(PSA_CRYPTO_ITS_RANDOM_SEED_UID, 0, buf_len, buf);
     return (-1 * rc);
 }
 
 int mbed_default_seed_write(unsigned char *buf, size_t buf_len)
 {
-    psa_its_status_t rc = psa_its_set(MBEDTLS_RANDOM_SEED_ITS_UID, buf_len, buf, 0);
+    psa_its_status_t rc = psa_its_set(PSA_CRYPTO_ITS_RANDOM_SEED_UID, buf_len, buf, 0);
     return (-1 * rc);
 }
 
