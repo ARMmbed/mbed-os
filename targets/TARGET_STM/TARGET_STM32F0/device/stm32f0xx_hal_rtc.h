6b226ffcef8456571da9af914f767df164eba0d2
jeromecoutant <jerome.coutant@st.com>
2018-11-22 16:52:09 +0100

    STM32 RTC update for easy maintenance

--- targets/TARGET_STM/TARGET_STM32F0/device/stm32f0xx_hal_rtc.h
+++ targets/TARGET_STM/TARGET_STM32F0/device/stm32f0xx_hal_rtc.h
@@ -43,6 +43,7 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f0xx_hal_def.h"
+#include "stm32f0xx_ll_rtc.h"
 
 /** @addtogroup STM32F0xx_HAL_Driver
   * @{
