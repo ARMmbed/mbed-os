c263c04e93bc49b18371bbff87d0a185d95230c0
Filip Jagodzinski <filip.jagodzinski@arm.com>
2018-11-27 10:50:54 +0100

    Docs: Watchdog: Update code comments
    * capitalize driver class name,
    * reword test docs for the driver & HAL,
    * capitalize Mbed name,
    * reword the comments explaining the SERIAL_FLUSH_TIME_MS macro.

--- TESTS/mbed_hal/watchdog_reset/watchdog_reset_tests.h
+++ TESTS/mbed_hal/watchdog_reset/watchdog_reset_tests.h
@@ -1,4 +1,4 @@
-/* mbed Microcontroller Library
+/* Mbed Microcontroller Library
  * Copyright (c) 2018 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  *
@@ -27,46 +27,45 @@
 
 /** Test watchdog reset
  *
- * Given a device with a watchdog started
- * When a watchdog timeout expires
- * Then the device is restarted
+ * Given a device with a watchdog started,
+ * when a watchdog timeout expires,
+ * then the device is restarted.
  */
 void test_simple_reset();
 
 /** Test watchdog reset in sleep mode
  *
- * Given a device supporting sleep mode, with a watchdog started
- * When the device is in sleep mode and watchdog timeout expires
- * Then the device is restarted
+ * Given a device with a watchdog started,
+ * when the watchdog timeout expires while the device is in sleep mode,
+ * then the device is restarted.
  */
 void test_sleep_reset();
 
 /** Test watchdog reset in deepsleep mode
  *
- * Given a device supporting deepsleep mode, with watchdog started
- * When the device is in deepsleep mode and watchdog timeout expires
- * Then the device is restarted
+ * Given a device with a watchdog started,
+ * when the watchdog timeout expires while the device is in deepsleep mode,
+ * then the device is restarted.
  */
 void test_deepsleep_reset();
 
-/** Test stopped watchdog can be started again and reset the device
+/** Test watchdog reset after watchdog restart
  *
- * Given a device supporting 'disable_watchdog' feature, with watchdog started
- * When the watchdog is stopped before timeout expires
- * Then the device is not restarted
- * When the watchdog is started again and it's timeout expires
- * Then the device is restarted
+ * Given a device with a watchdog started,
+ * when the watchdog is stopped before its timeout expires,
+ * then the device is not restarted.
+ * When the watchdog is started again and its timeout expires,
+ * then the device is restarted.
  */
 void test_restart_reset();
 
-/** Test kicking the watchdog prevents reset
+/** Test watchdog kick
  *
- * Given a device with watchdog started
- * When the watchdog is kicked before timeout expires
- * Then the device restart is prevented
- * When the watchdog is *NOT* kicked again before next timeout expires
- * Then the device is restarted
- *
+ * Given a device with a watchdog started,
+ * when the watchdog is kicked before its timeout expires,
+ * then the device restart is prevented.
+ * When the watchdog is *NOT* kicked again before next timeout expires,
+ * then the device is restarted.
  */
 void test_kick_reset();
 
