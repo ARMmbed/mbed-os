4e94694b5fcaafa5c4b1abc14136e32a020d2b06
ccli8 <ccli8@nuvoton.com>
2018-11-28 16:26:21 +0800

    Avoid re-configuring UART in serial_init() for the same H/W UART
    The same H/W UART may be shared by multiple serial_t objects. This fix tries to avoid
    re-configuring the same H/W UART in serial_init() when there are multiple serial_t
    objects constructed. To re-configure UART, call serial_baud() and serial_format()
    explicitly. This can avoid confusion when e.g. a newly constructed serial_t object
    changes baudrate unexpectedly in serial_init().

--- targets/TARGET_NUVOTON/TARGET_M2351/serial_api.c
+++ targets/TARGET_NUVOTON/TARGET_M2351/serial_api.c
@@ -200,35 +200,33 @@ void serial_init(serial_t *obj, PinName tx, PinName rx)
     struct nu_uart_var *var = (struct nu_uart_var *) modinit->var;
 
     if (! var->ref_cnt) {
-        do {
-            /* Reset module
-             *
-             * NOTE: We must call secure version (from non-secure domain) because SYS/CLK regions are secure.
-             */
-            SYS_ResetModule_S(modinit->rsetidx);
+        /* Reset module
+         *
+         * NOTE: We must call secure version (from non-secure domain) because SYS/CLK regions are secure.
+         */
+        SYS_ResetModule_S(modinit->rsetidx);
 
-            /* Select IP clock source
-             *
-             * NOTE: We must call secure version (from non-secure domain) because SYS/CLK regions are secure.
-             */
-            CLK_SetModuleClock_S(modinit->clkidx, modinit->clksrc, modinit->clkdiv);
-            
-            /* Enable IP clock
-             *
-             * NOTE: We must call secure version (from non-secure domain) because SYS/CLK regions are secure.
-             */
-            CLK_EnableModuleClock_S(modinit->clkidx);
+        /* Select IP clock source
+         *
+         * NOTE: We must call secure version (from non-secure domain) because SYS/CLK regions are secure.
+         */
+        CLK_SetModuleClock_S(modinit->clkidx, modinit->clksrc, modinit->clkdiv);
 
-            pinmap_pinout(tx, PinMap_UART_TX);
-            pinmap_pinout(rx, PinMap_UART_RX);
-        } while (0);
+        /* Enable IP clock
+         *
+         * NOTE: We must call secure version (from non-secure domain) because SYS/CLK regions are secure.
+         */
+        CLK_EnableModuleClock_S(modinit->clkidx);
+
+        pinmap_pinout(tx, PinMap_UART_TX);
+        pinmap_pinout(rx, PinMap_UART_RX);
+
+        // Configure the UART module and set its baudrate
+        serial_baud(obj, 9600);
+        // Configure data bits, parity, and stop bits
+        serial_format(obj, 8, ParityNone, 1);
     }
     var->ref_cnt ++;
-
-    // Configure the UART module and set its baudrate
-    serial_baud(obj, 9600);
-    // Configure data bits, parity, and stop bits
-    serial_format(obj, 8, ParityNone, 1);
 
     obj->serial.vec = var->vec;
     obj->serial.irq_en = 0;
