fee5e42f95459b0070a5d76aa02630afd2c5de0e
Oren Cohen <oren.cohen@arm.com>
2018-12-02 11:14:49 +0200

    Fix PSA crypto partiotion and tests
    * Styling
    * Correct error codes on failing connection
    * Add panics where needed
    * correct skip defines
    * Fix psa_spm_init_refence_counter bug

--- TESTS/psa/crypto_init/COMPONENT_SPE/psa_setup.c
+++ TESTS/psa/crypto_init/COMPONENT_SPE/psa_setup.c
@@ -27,21 +27,21 @@
 #include "spm_internal.h"
 #include "handles_manager.h"
 #include "cmsis.h"
-#include "psa_psa_f_partition.h"
+#include "psa_crypto_srv_partition.h"
 #include "psa_its_partition.h"
 
-extern const uint32_t psa_f_external_sids[4];
+extern const uint32_t crypto_srv_external_sids[4];
 
 spm_partition_t g_partitions[2] = {
     {
-        .partition_id = PSA_F_ID,
+        .partition_id = CRYPTO_SRV_ID,
         .thread_id = 0,
-        .flags_rot_srv = PSA_F_WAIT_ANY_SID_MSK,
+        .flags_rot_srv = CRYPTO_SRV_WAIT_ANY_SID_MSK,
         .flags_interrupts = 0,
         .rot_services = NULL,
-        .rot_services_count = PSA_F_ROT_SRV_COUNT,
-        .extern_sids = psa_f_external_sids,
-        .extern_sids_count = PSA_F_EXT_ROT_SRV_COUNT,
+        .rot_services_count = CRYPTO_SRV_ROT_SRV_COUNT,
+        .extern_sids = crypto_srv_external_sids,
+        .extern_sids_count = CRYPTO_SRV_EXT_ROT_SRV_COUNT,
         .irq_mapper = NULL,
     },
     {
@@ -65,7 +65,7 @@ const mem_region_t *mem_regions = NULL;
 const uint32_t mem_region_count = 0;
 
 // forward declaration of partition initializers
-void psa_f_init(spm_partition_t *partition);
+void crypto_srv_init(spm_partition_t *partition);
 void its_init(spm_partition_t *partition);
 
 uint32_t init_partitions(spm_partition_t **partitions)
@@ -74,7 +74,7 @@ uint32_t init_partitions(spm_partition_t **partitions)
         SPM_PANIC("partitions is NULL!\n");
     }
 
-    psa_f_init(&(g_partitions[0]));
+    crypto_srv_init(&(g_partitions[0]));
     its_init(&(g_partitions[1]));
 
     *partitions = g_partitions;
