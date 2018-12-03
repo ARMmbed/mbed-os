198fe703b8d7dcdf486bc18d66f280e0077f2aae
ThunderSoft_mbed_os <44602518+ThunderSoft123@users.noreply.github.com>
2018-12-03 09:33:53 +0800

    Update gpio_object.h

--- targets/TARGET_TT/TARGET_TT_M3HQ/gpio_object.h
+++ targets/TARGET_TT/TARGET_TT_M3HQ/gpio_object.h
@@ -1,6 +1,6 @@
 /* mbed Microcontroller Library
- * (C)Copyright TOSHIBA ELECTRONIC DEVICES & STORAGE CORPORATION 2017 All rights reserved
- *
+ * (C)Copyright TOSHIBA ELECTRONIC DEVICES & STORAGE CORPORATION 2018 All rights reserved
+ * SPDX-License-Identifier: Apache-2.0
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
