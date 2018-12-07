b5b0dcc03eaaedf521e58059129fb69f1ae4a8cc
Cruz Monrreal <Cruz.Monrreal@arm.com>
2018-12-07 14:43:58 -0600

    Fix '=' to '==' in ATHandler.cpp
    Found via https://github.com/ARMmbed/mbed-os/issues/9014

--- features/cellular/framework/AT/ATHandler.cpp
+++ features/cellular/framework/AT/ATHandler.cpp
@@ -959,7 +959,7 @@ bool ATHandler::consume_to_tag(const char *tag, bool consume_tag)
             break;
             // compares c against tag at current position and if this match fails
             // compares c against tag[0] and also resets match_pos to 0
-        } else if (c == tag[match_pos] || ((match_pos = 1) && (c == tag[--match_pos]))) {
+        } else if (c == tag[match_pos] || ((match_pos == 1) && (c == tag[--match_pos]))) {
             match_pos++;
             if (match_pos == strlen(tag)) {
                 if (!consume_tag) {
