da4d70800f6955a1f89234d77b070035594f33ad
Netanel Gonen <netanel.gonen@arm.com>
2018-12-11 17:49:15 +0200

    fix clean arg message

--- tools/test_crypto.py
+++ tools/test_crypto.py
@@ -39,7 +39,7 @@ def get_parser():
         '-c',
         '--clean',
         action='store_true',
-        help="choose a specific test to compile"
+        help="clean the files created by running this script"
     )
 
     parser.add_argument(
