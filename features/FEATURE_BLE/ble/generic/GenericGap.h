e8eee620a831f1606dec720791f0e40ba63ed8c3
paul-szczepanek-arm <33840200+paul-szczepanek-arm@users.noreply.github.com>
2018-12-11 15:08:16 +0000

    fix max payload and hci length values

--- features/FEATURE_BLE/ble/generic/GenericGap.h
+++ features/FEATURE_BLE/ble/generic/GenericGap.h
@@ -50,7 +50,6 @@ class GenericGap :
 public:
     /* TODO: move to config */
     static const uint8_t MAX_ADVERTISING_SETS = 15;
-    static const size_t MAX_HCI_DATA_LENGTH = 251;
 
     /**
      * Construct a GenericGap.
@@ -91,6 +90,10 @@ public:
     /** @copydoc Gap::getMaxAdvertisingDataLength
      */
     virtual uint16_t getMaxAdvertisingDataLength();
+
+    /** @copydoc Gap::getMaxConnectableAdvertisingDataLength
+     */
+    virtual uint16_t getMaxConnectableAdvertisingDataLength();
 
     /** @copydoc Gap::createAdvertisingSet
      */
