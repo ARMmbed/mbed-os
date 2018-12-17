6f6cdc948b2fa74fa8972f632b3bd15b519a1ce3
jeromecoutant <jerome.coutant@st.com>
2018-12-17 17:04:54 +0100

    DNS tests: improve debug messages

--- TESTS/netsocket/dns/asynchronous_dns_simultaneous_repeat.cpp
+++ TESTS/netsocket/dns/asynchronous_dns_simultaneous_repeat.cpp
@@ -40,7 +40,7 @@ void ASYNCHRONOUS_DNS_SIMULTANEOUS_REPEAT()
         // For 1st round can fail to no memory, on other rounds some of the addresses are found from cache
         TEST_ASSERT(result_ok == MBED_CONF_APP_DNS_SIMULT_QUERIES || result_ok == MBED_CONF_APP_DNS_SIMULT_QUERIES + 1);
         TEST_ASSERT(result_no_mem == 1 || result_no_mem == 0); // last query fails for 1st round to no memory as expected
-        TEST_ASSERT(result_dns_failure == 0);
-        TEST_ASSERT(result_exp_timeout == 0);
+        TEST_ASSERT_EQUAL(0, result_dns_failure);
+        TEST_ASSERT_EQUAL(0, result_exp_timeout);
     }
 }
