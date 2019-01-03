879d234b729cff1ca4553d5b7eb9a91652ab95f9
offirko <offir.kochalsky@arm.com>
2019-01-03 15:11:27 +0200

    Updated test to consider tdb_internal non default configuration as well

--- features/storage/TESTS/kvstore/direct_access_devicekey_test/main.cpp
+++ features/storage/TESTS/kvstore/direct_access_devicekey_test/main.cpp
@@ -54,7 +54,7 @@ static inline uint32_t align_down(uint64_t val, uint64_t size)
 int  get_virtual_TDBStore_position(uint32_t conf_start_address, uint32_t conf_size, bool is_conf_tdb_internal,
                                    uint32_t *tdb_start_address, uint32_t *tdb_end_address)
 {
-    uint32_t bd_final_size;
+    uint32_t bd_final_size = conf_size;;
     uint32_t flash_end_address;
     uint32_t flash_start_address;
     uint32_t aligned_start_address;
@@ -78,25 +78,42 @@ int  get_virtual_TDBStore_position(uint32_t conf_start_address, uint32_t conf_si
     }
 
     if (!request_default) {
-        aligned_start_address = align_down(conf_start_address, flash.get_sector_size(conf_start_address));
-        if (aligned_start_address < flash_first_writable_sector_address) {
-            aligned_start_address = flash_first_writable_sector_address;
+        if (conf_start_address == 0) {
+            conf_start_address = flash_end_address - conf_size;
         }
-        bd_final_size = (flash_end_address - aligned_start_address);
-        if (bd_final_size < 2 * 4096) {
-            flash.deinit();
-            return -2;
+        aligned_start_address = align_down(conf_start_address, flash.get_sector_size(conf_start_address));
+        if (conf_size == 0) {
+            bd_final_size = (flash_end_address - aligned_start_address);
+        }
+
+        if ((conf_size == 0) || (conf_start_address == 0)) {
+            if (aligned_start_address < flash_first_writable_sector_address) {
+                flash.deinit();
+                return -2;
+            }
         }
     } else {
         if (is_conf_tdb_internal == true) {
             aligned_start_address = flash_first_writable_sector_address;
-            bd_final_size = (flash_end_address - aligned_start_address);
-            if (bd_final_size < 2 * 4096) {
-                flash.deinit();
-                return -2;
+            bd_size_t spare_size_for_app = 0;
+            bd_addr_t curr_addr = aligned_start_address;
+            int spare_sectors_for_app = 2;
+            int min_sectors_for_storage = 2;
+            for (int i = 0; i < spare_sectors_for_app + min_sectors_for_storage - 1; i++) {
+                bd_size_t sector_size = flash.get_sector_size(curr_addr);
+                curr_addr += sector_size;
+                if (curr_addr >= flash_end_address) {
+                    spare_size_for_app = 0;
+                    break;
+                }
+
+                if (i < spare_sectors_for_app) {
+                    spare_size_for_app += sector_size;
+                }
             }
+            aligned_start_address += spare_size_for_app;
+            bd_final_size = (flash_end_address - aligned_start_address);
         } else {
-            //update start_address to double the size for TDBStore needs
             aligned_start_address = flash_end_address - (flash.get_sector_size(flash_end_address - 1) * 2);
             if (aligned_start_address < flash_first_writable_sector_address) {
                 flash.deinit();
