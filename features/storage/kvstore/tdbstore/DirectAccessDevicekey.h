a1788e929d15373f7dbd02707c10772c8b4e6b90
offirko <offir.kochalsky@arm.com>
2019-01-07 15:51:16 +0200

    Added utility function to help locate expected internal flash tdbstore position. (Supporting FILESYSTEM and TDB_EXTERNAL configuration only)

--- features/storage/kvstore/tdbstore/DirectAccessDevicekey.h
+++ features/storage/kvstore/tdbstore/DirectAccessDevicekey.h
@@ -39,4 +39,24 @@
 int direct_access_to_devicekey(uint32_t tdb_start_offset, uint32_t tdb_end_offset, void *data_buf,
                                size_t data_buf_size, size_t *actual_data_size_ptr);
 
+/**
+ * @brief Returns the expected tdb internal position.
+ *        Limitations:
+ *        1. supporting FILESYSTEM or TDB_EXTERNAL configuration only
+ *        2. supporting either both start_address and size are explicitly given (different than zero)
+ *           or both of them are zero (default configuration)
+ *
+ * @param[out]  out_tdb_start_offset Expected FlashIAP Start address offset of tdb store
+ * @param[out]  out_tdb_end_offset   Expected FlashIAP End address offset of tdb store
+ *
+ * @returns MBED_ERROR_FAILED_OPERATION    internal flash failure
+ *          MBED_ERROR_UNSUPPORTED         kvstore configuration different than FILESYSTEM or TDB_EXTERNAL
+ *                                         or start_address or size are different than 0 and the other isn't
+ *          MBED_ERROR_INVALID_OPERATION   tdb_start_offset or tdb_end_offset are not aligned or lower
+ *                                         than end of code segment (when using default configuration)
+ *          MBED_SUCCESS                   successful
+ *
+ */
+int get_expected_internal_TDBStore_position(uint32_t *out_tdb_start_offset, uint32_t *out_tdb_end_offset);
+
 #endif /* MBED_DIRECT_ACCESS_DEVICEKEY_H */
