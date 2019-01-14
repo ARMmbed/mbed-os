5b979148aaf4e0429f083392a35ced104c0af83d
Andriy.Lishchynskyi <anjo@cypress.com>
2019-01-14 17:02:29 +0200

    Reworked launch configuration creation mechanism - switched to single template. Specific data comes from the JSON file

--- tools/export/cdt/__init__.py
+++ tools/export/cdt/__init__.py
@@ -1,16 +1,49 @@
 import re
-
+import os
+import json
+from collections import namedtuple
+from tools.targets import TARGET_MAP
 from os.path import join, exists
 from os import makedirs, remove
 import shutil
-from jinja2.exceptions import TemplateNotFound
 
 from tools.export.makefile import Makefile, GccArm, Armc5, IAR
+
+_eclipse_defs = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'cdt_definitions.json')
+
+with open(_eclipse_defs, 'r') as f:
+    _CONFIGS_OPTIONS = json.load(f)
+
+supported_launches = ['debug', 'program']
 
 class Eclipse(Makefile):
     """Generic Eclipse project. Intended to be subclassed by classes that
     specify a type of Makefile.
     """
+    def get_target_config(self, ctx, configuration):
+        """Retrieve info from cdt_definitions.json"""
+        tgt = TARGET_MAP[self.target]
+        defaults = _CONFIGS_OPTIONS['default']
+        eclipse_config = defaults['generic']
+        if configuration in defaults:
+            eclipse_config.update(defaults[configuration])
+
+        target_specific = _CONFIGS_OPTIONS['targets']
+        if tgt.name in target_specific:
+            target_info = target_specific[tgt.name]['generic']
+            if configuration in target_specific[tgt.name]:
+                target_info.update(target_specific[tgt.name][configuration])
+            
+            eclipse_config.update(target_info)
+
+        #special case for gdbClientOtherOptions param - in some cases it may contain dynamical values, fill in it here
+        eclipse_config['gdbClientOtherOptions'] = eclipse_config['gdbClientOtherOptions'].format(
+                                                    elf_location=ctx['elf_location'])
+
+        Eclipsedevice = namedtuple('Eclipsedevice', eclipse_config.keys())
+        return Eclipsedevice(**eclipse_config)
+
     def generate(self):
         """Generate Makefile, .cproject & .project Eclipse project file,
         py_ocd_settings launch file, and software link .p2f file
@@ -26,22 +59,22 @@ class Eclipse(Makefile):
             'include_paths': [starting_dot.sub('%s/' % self.project_name, inc) for inc in self.resources.inc_dirs],
             'load_exe': str(self.LOAD_EXE).lower()
         }
-
+        
+        launch_cfgs = {}
+        for launch_name in supported_launches:
+            launch = dict(ctx.items() + {'device': self.get_target_config(ctx, launch_name)}.items())
+            launch_cfgs.update({launch_name: launch})
+            
         if not exists(join(self.export_dir,'eclipse-extras')):
             makedirs(join(self.export_dir,'eclipse-extras'))
 
-        templates = ['%s.tmpl' % (self.target.lower())] + \
-            ['%s.tmpl' % (label.lower()) for label
-            in self.toolchain.target.extra_labels] + \
-            ['%s.tmpl' % 'pyocd_settings']
-
-        for templatefile in templates:
-            try:
-                self.gen_file('cdt/%s' % templatefile, ctx, join('eclipse-extras',
-                           '{target}_{project}_{launch}.launch'.format(target=self.target,
-                                                                    project=self.project_name, launch=templatefile)))
-            except TemplateNotFound:
-                pass
+        for launch_name, ctx in launch_cfgs.items():
+            self.gen_file('cdt/%s' % 'pyocd_settings.tmpl', ctx, join('eclipse-extras',
+                            '{target}_{project}_{conf}_{launch}.launch'.format(
+                                                                        target=self.target,
+                                                                        project=self.project_name,
+                                                                        conf=launch_name,
+                                                                        launch='pyocd_settings')))
 
         self.gen_file('cdt/necessary_software.tmpl', ctx,
                       join('eclipse-extras','necessary_software.p2f'))
