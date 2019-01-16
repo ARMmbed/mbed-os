fc968e6cef1fefa4fc625d34130fb6eabe040d37
Garrett <loverdeg@embeddedplanet.com>
2019-01-16 16:24:28 -0500

    Revert get_config.py make.py test.py

--- tools/test.py
+++ tools/test.py
@@ -65,10 +65,6 @@ if __name__ == '__main__':
                           type=argparse_filestring_type,
                             default=None, help="The source (input) directory (for sources other than tests). Defaults to current directory.", action="append")
 
-        parser.add_argument("--custom-targets", dest="custom_targets_directory",
-                            type=argparse_filestring_type,
-                            default=None, help="Specify directory containing custom_targets.json", action="append")
-
         parser.add_argument("--build", dest="build_dir", type=argparse_dir_not_parent(ROOT),
                           default=None, help="The build (output) directory")
 
