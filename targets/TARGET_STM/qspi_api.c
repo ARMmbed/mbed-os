82b131aa59b7fcea2f22cf208108cdc22d0bf861
Russ Butler <russ.butler@arm.com>
2019-01-18 18:57:18 -0600

    Use dedicated PinMap for each QSPI data line
    Split PinMap_QSPI_DATA into PinMap_QSPI_DATA0 - PinMap_QSPI_DATA3.
    This allows pins to be selected more accurately.

--- targets/TARGET_STM/qspi_api.c
+++ targets/TARGET_STM/qspi_api.c
@@ -158,10 +158,10 @@ qspi_status_t qspi_init(qspi_t *obj, PinName io0, PinName io1, PinName io2, PinN
 
     obj->handle.Init.ClockMode = mode == 0 ? QSPI_CLOCK_MODE_0 : QSPI_CLOCK_MODE_3;
 
-    QSPIName qspiio0name = (QSPIName)pinmap_peripheral(io0, PinMap_QSPI_DATA);
-    QSPIName qspiio1name = (QSPIName)pinmap_peripheral(io1, PinMap_QSPI_DATA);
-    QSPIName qspiio2name = (QSPIName)pinmap_peripheral(io2, PinMap_QSPI_DATA);
-    QSPIName qspiio3name = (QSPIName)pinmap_peripheral(io3, PinMap_QSPI_DATA);
+    QSPIName qspiio0name = (QSPIName)pinmap_peripheral(io0, PinMap_QSPI_DATA0);
+    QSPIName qspiio1name = (QSPIName)pinmap_peripheral(io1, PinMap_QSPI_DATA1);
+    QSPIName qspiio2name = (QSPIName)pinmap_peripheral(io2, PinMap_QSPI_DATA2);
+    QSPIName qspiio3name = (QSPIName)pinmap_peripheral(io3, PinMap_QSPI_DATA3);
     QSPIName qspiclkname = (QSPIName)pinmap_peripheral(sclk, PinMap_QSPI_SCLK);
     QSPIName qspisselname = (QSPIName)pinmap_peripheral(ssel, PinMap_QSPI_SSEL);
 
@@ -179,13 +179,13 @@ qspi_status_t qspi_init(qspi_t *obj, PinName io0, PinName io1, PinName io2, PinN
 
     // pinmap for pins (enable clock)
     obj->io0 = io0;
-    pinmap_pinout(io0, PinMap_QSPI_DATA);
+    pinmap_pinout(io0, PinMap_QSPI_DATA0);
     obj->io1 = io1;
-    pinmap_pinout(io1, PinMap_QSPI_DATA);
+    pinmap_pinout(io1, PinMap_QSPI_DATA1);
     obj->io2 = io2;
-    pinmap_pinout(io2, PinMap_QSPI_DATA);
+    pinmap_pinout(io2, PinMap_QSPI_DATA2);
     obj->io3 = io3;
-    pinmap_pinout(io3, PinMap_QSPI_DATA);
+    pinmap_pinout(io3, PinMap_QSPI_DATA3);
 
     obj->sclk = sclk;
     pinmap_pinout(sclk, PinMap_QSPI_SCLK);
