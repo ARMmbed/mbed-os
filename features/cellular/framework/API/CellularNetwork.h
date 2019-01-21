5788107da32c0d3dd6a40d8f370aea0933b69f80
Seppo Takalo <seppo.takalo@arm.com>
2019-01-21 11:47:41 +0200

    Add missing copyright header.

--- features/cellular/framework/API/CellularNetwork.h
+++ features/cellular/framework/API/CellularNetwork.h
@@ -1,4 +1,10 @@
-/*
+/* Copyright (c) 2017,2018 Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
