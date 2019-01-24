b08c0e23611925df12f91eca752ceeea54294f31
Oren Cohen <oren.cohen@arm.com>
2019-01-24 12:33:38 +0200

    Create new PSA compliant headers

---
+++ components/TARGET_PSA/inc/psa/internal_trusted_storage.h
@@ -0,0 +1,23 @@
+/* Copyright (c) 2017-2018 ARM Limited
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __MBED_INTERNAL_TRUSTED_STORAGE_H__
+#define __MBED_INTERNAL_TRUSTED_STORAGE_H__
+
+#include "psa_prot_internal_storage.h"
+
+#endif // __MBED_INTERNAL_TRUSTED_STORAGE_H__
