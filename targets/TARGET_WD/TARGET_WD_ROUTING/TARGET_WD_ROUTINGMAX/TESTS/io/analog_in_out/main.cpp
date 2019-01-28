8fccb4f36a7148946e8b04b0f93522b178fd218d
Gregor.Mayramhof <gregor.mayramhof@world-direct.at>
2019-01-28 12:23:34 +0100

    #89831 tweaked test timout to match additional waiting time

--- targets/TARGET_WD/TARGET_WD_ROUTING/TARGET_WD_ROUTINGMAX/TESTS/io/analog_in_out/main.cpp
+++ targets/TARGET_WD/TARGET_WD_ROUTING/TARGET_WD_ROUTINGMAX/TESTS/io/analog_in_out/main.cpp
@@ -147,7 +147,7 @@ Case cases[] = {
 
 utest::v1::status_t greentea_test_setup(const size_t number_of_cases)
 {
-    GREENTEA_SETUP(150, "default_auto");
+    GREENTEA_SETUP(200, "default_auto");
     return greentea_test_setup_handler(number_of_cases);
 }
 
