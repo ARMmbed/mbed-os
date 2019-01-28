7dbce3e15a58f1fc158a8428833c374a5a299edc
Veijo Pesonen <veijo.pesonen@arm.com>
2019-01-28 12:43:51 +0200

    Test UDPSOCKET_SENDTO_INVALID allows NSAPI_ERROR_UNSUPPORTED

--- TESTS/netsocket/udp/udpsocket_sendto_invalid.cpp
+++ TESTS/netsocket/udp/udpsocket_sendto_invalid.cpp
@@ -32,7 +32,12 @@ void UDPSOCKET_SENDTO_INVALID()
     TEST_ASSERT(sock.sendto(NULL, 9, NULL, 0) < 0);
     TEST_ASSERT(sock.sendto("", 9, NULL, 0) < 0);
     TEST_ASSERT(sock.sendto("", 0, NULL, 0) < 0);
-    TEST_ASSERT_EQUAL(0, sock.sendto(MBED_CONF_APP_ECHO_SERVER_ADDR, 9, NULL, 0));
+
+    nsapi_error_t result = sock.sendto(MBED_CONF_APP_ECHO_SERVER_ADDR, 9, NULL, 0);
+    if (result != NSAPI_ERROR_UNSUPPORTED) {
+        TEST_ASSERT_EQUAL(0, result);
+    }
+
     TEST_ASSERT_EQUAL(5, sock.sendto(MBED_CONF_APP_ECHO_SERVER_ADDR, 9, "hello", 5));
 
     TEST_ASSERT_EQUAL(NSAPI_ERROR_OK, sock.close());
