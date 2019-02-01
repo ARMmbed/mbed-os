b8888ad41ee433d069606752886d24f07d151a90
Michal Paszta <michal.paszta@mobica.com>
2019-02-01 14:22:05 +0200

    Greentea UDPSOCKET_RECV_TIMEOUT increase time tolerance

--- TESTS/netsocket/udp/udpsocket_recv_timeout.cpp
+++ TESTS/netsocket/udp/udpsocket_recv_timeout.cpp
@@ -62,7 +62,7 @@ void UDPSOCKET_RECV_TIMEOUT()
         if (recvd == NSAPI_ERROR_WOULD_BLOCK) {
             osSignalWait(SIGNAL_SIGIO, SIGIO_TIMEOUT);
             printf("MBED: recvfrom() took: %dms\n", timer.read_ms());
-            TEST_ASSERT_INT_WITHIN(50, 150, timer.read_ms());
+            TEST_ASSERT_INT_WITHIN(51, 150, timer.read_ms());
             continue;
         } else if (recvd < 0) {
             printf("[bt#%02d] network error %d\n", i, recvd);
