a14acfad14843916cbb24d995c3edd7b81721c42
Hasnain Virk <hasnain.virk@arm.com>
2019-02-04 16:57:53 +0200

    Updating unit tests
    Updating unit test in response to the change in the link ADR related
    APIs.

--- UNITTESTS/features/lorawan/lorawanstack/Test_LoRaWANStack.cpp
+++ UNITTESTS/features/lorawan/lorawanstack/Test_LoRaWANStack.cpp
@@ -494,6 +494,10 @@ TEST_F(Test_LoRaWANStack, handle_rx)
     ind.buffer = ind_buf;
     ind.buffer_size = 150;
     ind.type = MCPS_UNCONFIRMED;
+    ind.port = 15;
+    ind.is_data_recvd = true;
+    ind.fpending_status = false;
+    LoRaMac_stub::dev_class_value = CLASS_A;
     radio._ev->rx_done(NULL, 0, 0, 0);
 
     //data == NULL || LENGTH == 0 (2 cases)
