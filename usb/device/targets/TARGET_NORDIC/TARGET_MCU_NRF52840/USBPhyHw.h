153e60d5e9b96cf16887f3da535ca245f9701a9b
George Beckstein <donbeckstein@live.com>
2019-02-09 07:29:49 -0500

    Trying virtual status stage and generated interrupt

--- usb/device/targets/TARGET_NORDIC/TARGET_MCU_NRF52840/USBPhyHw.h
+++ usb/device/targets/TARGET_NORDIC/TARGET_MCU_NRF52840/USBPhyHw.h
@@ -77,12 +77,6 @@ private:
 		USB_HW_EVENT_POWER = 2
     } usb_hw_event_type_t;
 
-    typedef enum usb_hw_ctrl_stage_t {
-    		USB_HW_CTRL_STAGE_SETUP = 0,
-		USB_HW_CTRL_STAGE_DATA = 1,
-		USB_HW_CTRL_STAGE_STATUS = 2
-    } usb_hw_ctrl_stage_t;
-
     // Event type to process
     usb_hw_event_type_t usb_event_type;
 
@@ -94,9 +88,6 @@ private:
 
     // Buffer to hold setup packet
     nrf_drv_usbd_setup_t setup_buf;
-
-    // Setup bytes remaining
-    uint32_t setup_remaining;
 
     // Nordic transfer structures for each in/out endpoint
     nrf_drv_usbd_transfer_t transfer_buf[18];
