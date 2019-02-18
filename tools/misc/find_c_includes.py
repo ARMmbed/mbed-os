34cdefc89ea3411dba23a6b25897083ca4935182
Martin Kojtal <martin.kojtal@arm.com>
2019-02-18 15:45:33 +0000

    tools: fix hashbang

--- tools/misc/find_c_includes.py
+++ tools/misc/find_c_includes.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 """
 Copyright (c) 2016-2019 ARM Limited. All rights reserved.
 
@@ -15,8 +17,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-#!/usr/bin/env python
 
 import os
 import re
