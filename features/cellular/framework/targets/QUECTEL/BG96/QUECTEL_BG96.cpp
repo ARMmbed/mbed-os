50a276ead8bf66e9dcf909d52288577aff0553f0
Ari Parkkila <ari.parkkila@arm.com>
2019-02-17 23:53:11 -0800

    Cellular: Fix Quectel BG96 driver RTS/CTS setup

--- features/cellular/framework/targets/QUECTEL/BG96/QUECTEL_BG96.cpp
+++ features/cellular/framework/targets/QUECTEL/BG96/QUECTEL_BG96.cpp
@@ -73,7 +73,7 @@ void QUECTEL_BG96::set_ready_cb(Callback<void()> callback)
 CellularDevice *CellularDevice::get_default_instance()
 {
     static UARTSerial serial(MBED_CONF_QUECTEL_BG96_TX, MBED_CONF_QUECTEL_BG96_RX, MBED_CONF_QUECTEL_BG96_BAUDRATE);
-#if defined (MBED_CONF_UBLOX_AT_RTS) && defined(MBED_CONF_UBLOX_AT_CTS)
+#if defined (MBED_CONF_QUECTEL_BG96_RTS) && defined(MBED_CONF_QUECTEL_BG96_CTS)
     tr_debug("QUECTEL_BG96 flow control: RTS %d CTS %d", MBED_CONF_QUECTEL_BG96_RTS, MBED_CONF_QUECTEL_BG96_CTS);
     serial.set_flow_control(SerialBase::RTSCTS, MBED_CONF_QUECTEL_BG96_RTS, MBED_CONF_QUECTEL_BG96_CTS);
 #endif
