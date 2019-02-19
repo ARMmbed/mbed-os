9d1a43349daa300ffc231929aae2d05d06eab25b
Seppo Takalo <seppo.takalo@arm.com>
2019-02-19 12:22:37 +0200

    Update copyright years

--- TESTS/network/multihoming/multihoming_tests.h
+++ TESTS/network/multihoming/multihoming_tests.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, ARM Limited, All Rights Reserved
+ * Copyright (c) 2019, ARM Limited, All Rights Reserved
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
