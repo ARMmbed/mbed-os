faf03e02dcc964b760d261899a41d37acf91eec2
Guillermo Alonso <galonso@tecnofingers.com>
2019-02-20 17:48:07 +0100

    added licence identifier texts

--- targets/TARGET_STM/TARGET_STM32L4/TARGET_STM32L476xG/TARGET_RHOMBIO_L476DMW1K/PeripheralNames.h
+++ targets/TARGET_STM/TARGET_STM32L4/TARGET_STM32L476xG/TARGET_RHOMBIO_L476DMW1K/PeripheralNames.h
@@ -1,4 +1,6 @@
 /* mbed Microcontroller Library
+
+* SPDX-License-Identifier: Apache-2.0
  *******************************************************************************
  * Copyright (c) 2015, STMicroelectronics
  * All rights reserved.
