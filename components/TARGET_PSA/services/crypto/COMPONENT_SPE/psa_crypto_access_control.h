37cc25726caaba52cf9d990a46332483f4372973
itayzafrir <itay.zafrir@arm.com>
2019-02-20 17:40:51 +0200

    Add internal documentation to access control header file
    This file is for internal use only.

--- components/TARGET_PSA/services/crypto/COMPONENT_SPE/psa_crypto_access_control.h
+++ components/TARGET_PSA/services/crypto/COMPONENT_SPE/psa_crypto_access_control.h
@@ -22,14 +22,19 @@
 
 #include "crypto_platform.h"
 
+/* initialize the module, resets all tracked information */
 void psa_crypto_access_control_init(void);
 
+/* deinitialize the module, resets all tracked information */
 void psa_crypto_access_control_destroy(void);
 
+/* tracks and associates the key_handle with partition_id */
 void psa_crypto_access_control_register_handle(psa_key_handle_t key_handle, int32_t partition_id);
 
+/* removes tracking of the key_handle */
 void psa_crypto_access_control_unregister_handle(psa_key_handle_t key_handle);
 
+/* checks if the key_handle is associated with the partition_id, returns 0 is false otherwise 1 */
 uint8_t psa_crypto_access_control_is_handle_permitted(psa_key_handle_t key_handle, int32_t partition_id);
 
 #endif /* PSA_CRYPTO_ACCESS_CONTROL_H */
