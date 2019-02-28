8e9b0e21781234f425bdbef1c7bfc35a9a9f5eae
Michael Schwarcz <michael.schwarcz@arm.com>
2019-02-28 22:51:38 +0200

    wait_us using wait_ns

--- platform/mbed_wait_api_no_rtos.c
+++ platform/mbed_wait_api_no_rtos.c
@@ -37,9 +37,19 @@ void wait_ms(int ms)
 
 void wait_us(int us)
 {
+#if DEVICE_USTICKER
     const ticker_data_t *const ticker = get_us_ticker_data();
     uint32_t start = ticker_read(ticker);
     while ((ticker_read(ticker) - start) < (uint32_t)us);
+#else // fallback to wait_ns for targets without usticker
+    while (us > 1000) {
+        us -= 1000;
+        wait_ns(1000000);
+    }
+    if (us > 0) {
+        wait_ns(us * 1000);
+    }
+#endif // DEVICE_USTICKER
 }
 
 #endif // #ifndef MBED_CONF_RTOS_PRESENT
