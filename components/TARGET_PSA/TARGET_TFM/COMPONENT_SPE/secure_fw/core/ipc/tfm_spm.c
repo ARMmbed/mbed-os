7a2c7d7df4d12776689b10e5fa77963f8473193f
Michael Schwarcz <michael.schwarcz@arm.com>
2019-03-03 14:51:39 +0200

    TF-M patch: Fix tfm_memory_check issue (TF-M issue #224)
    - Add Secure/non-secure code and secure heap to memory check
    - Link to bug tracking: https://developer.trustedfirmware.org/T224

--- components/TARGET_PSA/TARGET_TFM/COMPONENT_SPE/secure_fw/core/ipc/tfm_spm.c
+++ components/TARGET_PSA/TARGET_TFM/COMPONENT_SPE/secure_fw/core/ipc/tfm_spm.c
@@ -466,6 +466,8 @@ static uint32_t tfm_spm_partition_get_priority_ext(uint32_t partition_idx)
 /* Macros to pick linker symbols and allow references to sections in all level*/
 #define REGION_DECLARE_EXT(a, b, c) extern uint32_t REGION_NAME(a, b, c)
 
+REGION_DECLARE_EXT(Image$$, ARM_LIB_HEAP, $$ZI$$Base);
+REGION_DECLARE_EXT(Image$$, ARM_LIB_HEAP, $$ZI$$Limit);
 REGION_DECLARE_EXT(Image$$, ER_TFM_DATA, $$ZI$$Base);
 REGION_DECLARE_EXT(Image$$, ER_TFM_DATA, $$ZI$$Limit);
 REGION_DECLARE_EXT(Image$$, ER_TFM_DATA, $$RW$$Base);
@@ -521,7 +523,19 @@ int32_t tfm_memory_check(void *buffer, size_t len, int32_t ns_caller)
         if (memory_check_range(buffer, len, base, limit) == IPC_SUCCESS) {
             return IPC_SUCCESS;
         }
+
+        base = (uintptr_t)NS_CODE_START;
+        limit = (uintptr_t)(NS_CODE_START + NS_CODE_SIZE);
+        if (memory_check_range(buffer, len, base, limit) == IPC_SUCCESS) {
+            return IPC_SUCCESS;
+        }
     } else {
+        base = (uintptr_t)&REGION_NAME(Image$$, ARM_LIB_HEAP, $$ZI$$Base);
+        limit = (uintptr_t)&REGION_NAME(Image$$, ARM_LIB_HEAP, $$ZI$$Limit);
+        if (memory_check_range(buffer, len, base, limit) == IPC_SUCCESS) {
+            return IPC_SUCCESS;
+        }
+
         base = (uintptr_t)&REGION_NAME(Image$$, ER_TFM_DATA, $$RW$$Base);
         limit = (uintptr_t)&REGION_NAME(Image$$, ER_TFM_DATA, $$RW$$Limit);
         if (memory_check_range(buffer, len, base, limit) == IPC_SUCCESS) {
@@ -543,6 +557,12 @@ int32_t tfm_memory_check(void *buffer, size_t len, int32_t ns_caller)
         base = (uintptr_t)&REGION_NAME(Image$$, TFM_UNPRIV_SCRATCH, $$ZI$$Base);
         limit = (uintptr_t)&REGION_NAME(Image$$, TFM_UNPRIV_SCRATCH,
                                         $$ZI$$Limit);
+        if (memory_check_range(buffer, len, base, limit) == IPC_SUCCESS) {
+            return IPC_SUCCESS;
+        }
+
+        base = (uintptr_t)S_CODE_START;
+        limit = (uintptr_t)(S_CODE_START + S_CODE_SIZE);
         if (memory_check_range(buffer, len, base, limit) == IPC_SUCCESS) {
             return IPC_SUCCESS;
         }
