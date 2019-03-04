615a9f65488e6675ec545a79fbc6a7b04a16d071
Laurent Meunier <laurent.meunier@st.com>
2019-03-04 15:11:29 +0100

    STM32WB: Update headers

--- features/FEATURE_BLE/targets/TARGET_STM/TARGET_NUCLEO_WB55RG/hw_ipcc.c
+++ features/FEATURE_BLE/targets/TARGET_STM/TARGET_NUCLEO_WB55RG/hw_ipcc.c
@@ -1,7 +1,7 @@
 /**
- ******************************************************************************
+  ******************************************************************************
   * File Name          : Target/hw_ipcc.c
-  * Description        : Hardware IPCC source file for BLE 
+  * Description        : Hardware IPCC source file for BLE
   *                      middleWare.
   ******************************************************************************
   * @attention
@@ -9,10 +9,10 @@
   * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
   * All rights reserved.</center></h2>
   *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                             www.st.com/SLA0044
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
   *
   ******************************************************************************
   */
--- features/FEATURE_BLE/targets/TARGET_STM/TARGET_NUCLEO_WB55RG/tl_mbox.c
+++ features/FEATURE_BLE/targets/TARGET_STM/TARGET_NUCLEO_WB55RG/tl_mbox.c
@@ -1,21 +1,21 @@
 /**
- ******************************************************************************
- * @file    tl_mbox.c
- * @author  MCD Application Team
- * @brief   Transport layer for the mailbox interface
- ******************************************************************************
- * @attention
- *
- * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
- * All rights reserved.</center></h2>
- *
- * This software component is licensed by ST under Ultimate Liberty license
- * SLA0044, the "License"; You may not use this file except in compliance with
- * the License. You may obtain a copy of the License at:
- *                             www.st.com/SLA0044
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file    tl_mbox.c
+  * @author  MCD Application Team
+  * @brief   Transport layer for the mailbox interface
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
 
 
 /* Includes ------------------------------------------------------------------*/
