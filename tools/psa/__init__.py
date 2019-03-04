a9fdf1f632e92f346f037f789a7b0d77e00442b7
Michael Schwarcz <michael.schwarcz@arm.com>
2019-03-04 15:20:21 +0200

    Add assertions and tests

--- tools/psa/__init__.py
+++ tools/psa/__init__.py
@@ -1,16 +1,23 @@
 from os.path import basename
+from tools.resources import FileType
 
 
-def find_secure_image(notify, resources, ns_image_path, configured_s_image_path, image_type):
+def find_secure_image(notify, resources, ns_image_path, configured_s_image_filename, image_type):
     """ Find secure image. """
+
+    assert ns_image_path and configured_s_image_filename, 'ns_image_path and configured_s_image_path are mandatory'
+    assert image_type in [FileType.BIN, FileType.HEX], 'image_type must be of type BIN or HEX'
+
     image_files = resources.get_file_paths(image_type)
-    secure_image = next((f for f in image_files if basename(f) == configured_s_image_path), None)
+    assert image_files, 'No image files found for this target'
+
+    secure_image = next((f for f in image_files if basename(f) == configured_s_image_filename), None)
     secure_image = next((f for f in image_files if basename(f) == basename(ns_image_path)), secure_image)
 
     if secure_image:
         notify.debug("Secure image file found: %s." % secure_image)
     else:
-        notify.debug("Secure image file %s not found. Aborting." % configured_s_image_path)
+        notify.debug("Secure image file %s not found. Aborting." % configured_s_image_filename)
         raise Exception("Required secure image not found.")
 
     return secure_image
