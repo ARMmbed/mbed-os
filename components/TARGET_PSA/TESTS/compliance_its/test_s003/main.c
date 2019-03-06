1d56ad44a6e0f7040d64f020ce27ae1ad0c895e9
Nir Sonnenschein <nir.sonnenschein@arm.com>
2019-03-06 10:53:57 +0200

    use common flag to allow test 3 for PS and ITS
    add a compilation flag to allow tests which fill up the entire device storage.
    storage test 003 (for both PS and its) is unsuitable for the regular CI because:
    1. it takes a long time causing timeouts
    2. it places a very high attrition load on the storage of the CI boards
        and this can cause them to fail much sooner.

--- components/TARGET_PSA/TESTS/compliance_its/test_s003/main.c
+++ components/TARGET_PSA/TESTS/compliance_its/test_s003/main.c
@@ -2,12 +2,13 @@
 #include "pal_mbed_os_intf.h"
 #include "lifecycle.h"
 
-#ifdef ITS_TEST
-void test_entry_s003(val_api_t *val_api, psa_api_t *psa_api);
-#elif PS_TEST
 #ifndef PS_ALLOW_ENTIRE_STORAGE_FILL
 #error [NOT_SUPPORTED] Test is too long for CI, thus always fails on timeout.
 #endif
+
+#ifdef ITS_TEST
+void test_entry_s003(val_api_t *val_api, psa_api_t *psa_api);
+#elif PS_TEST
 void test_entry_p003(val_api_t *val_api, psa_api_t *psa_api);
 #endif
 
