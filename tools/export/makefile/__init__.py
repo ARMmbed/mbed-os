f0f133f3ecc6a1eee94213b69831d5358c5ab432
Naveen Kaje <Naveen.Kaje@arm.com>
2019-03-06 14:34:09 -0600

    tools: fix the path generated to the sct file
    The sct file path generated in the online compiler
    is incorrect. Fix that by changing the correct_scatter_shebang
    API to accept a FileRef object instead and use the path.
    
    This change should go with change in online compiler that removes
    the override for correct_scatter_shebang.

--- tools/export/makefile/__init__.py
+++ tools/export/makefile/__init__.py
@@ -1,6 +1,6 @@
 """
 mbed SDK
-Copyright (c) 2011-2016 ARM Limited
+Copyright (c) 2011-2019 ARM Limited
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -272,11 +272,10 @@ class Arm(Makefile):
         if self.resources.linker_script:
             sct_file = self.resources.get_file_refs(FileType.LD_SCRIPT)[-1]
             new_script = self.toolchain.correct_scatter_shebang(
-                sct_file.path, join("..", dirname(sct_file.name)))
+                sct_file, dirname(sct_file.name))
             if new_script is not sct_file:
                 self.resources.add_files_to_type(
                     FileType.LD_SCRIPT, [new_script])
-                self.generated_files.append(new_script)
         return super(Arm, self).generate()
 
 class Armc5(Arm):
