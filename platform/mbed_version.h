7dd791e2f992d872b2dab4a7f061d05f7283090a
adbridge <anna.bridge@arm.com>
2019-03-06 15:49:59 +0000

    Update Mbed version block

--- platform/mbed_version.h
+++ platform/mbed_version.h
@@ -30,21 +30,21 @@
   *
   * @note 99 is default value for development version (master branch)
   */
-#define MBED_MAJOR_VERSION  99
+#define MBED_MAJOR_VERSION 5
 
 /** MBED_MINOR_VERSION
   * Mbed OS minor version
   *
   * @note 99 is default value for development version (master branch)
   */
-#define MBED_MINOR_VERSION  99
+#define MBED_MINOR_VERSION 12
 
 /** MBED_PATCH_VERSION
   * Mbed OS patch version
   *
   * @note 99 is default value for development version (master branch)
   */
-#define MBED_PATCH_VERSION  99
+#define MBED_PATCH_VERSION 0
 
 #define MBED_ENCODE_VERSION(major, minor, patch) ((major)*10000 + (minor)*100 + (patch))
 
