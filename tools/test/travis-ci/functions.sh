7a36f93bf5bdfdc789a0f3adbc8022bf5909293c
Cruz Monrreal II <Cruz.Monrreal@arm.com>
2019-03-07 14:22:12 -0600

    Removed nearly all usage of 'apt-get'.
    Primary cause of false Travis CI failures was running 'apt-get update'.
    Refactored all instances where this was needed with manually fetching and installing dependencies in S3 instace.
    
    This also includes GCC installation.

--- tools/test/travis-ci/functions.sh
+++ tools/test/travis-ci/functions.sh
@@ -18,21 +18,128 @@
 
 set -o pipefail
 
-info() { echo "I: ${1}"; }
-die() { echo "E: ${1}" 1>&2; exit ${2:-1}; }
+info() { echo -e "I: ${1}"; }
+die() { echo -e "E: ${1}" 1>&2; exit ${2:-1}; }
 
 set_status()
 {
-  local _name=${NAME}
-  local _payload=$(<<< "
+  local job_name=${NAME}
+  local payload=$(<<< "
   {
     'state': '${1}',
     'description': '${2}',
-    'context': 'travis-ci/${_name}',
+    'context': 'travis-ci/${job_name}',
     'target_url': 'https://travis-ci.org/${TRAVIS_REPO_SLUG}/jobs/${TRAVIS_JOB_ID}'
   }" tr '"' '"')
 
   curl --silent --output /dev/null --user "${MBED_BOT}" --request POST \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/statuses/${TRAVIS_PULL_REQUEST_SHA:-$TRAVIS_COMMIT}" \
     --data @- <<< "${payload}"
+}
+
+
+_install_gcc()
+{
+  local url="${deps_url}/gcc6-linux.tar.bz2"
+  local gcc_path="${deps_dir}/gcc/gcc-arm-none-eabi-6-2017-q2-update/"
+  local archive="gcc.tar.bz2"
+  
+  info "URL: ${url}"
+
+  if [ ! -d "${deps_dir}/gcc" ]; then
+    
+    info "Downloading archive"
+    curl --location "${url}" --output "${deps_dir}/${archive}"
+    ls -al "${deps_dir}"
+    
+    info "Extracting 'gcc'"
+    mkdir -p "${deps_dir}/gcc"
+    tar -xf "${deps_dir}/${archive}" -C "${deps_dir}/gcc"
+    rm "${deps_dir}/${archive}"
+
+  fi
+  
+  info "Installing 'gcc'"
+  export "PATH=${gcc_path}/bin:${PATH}"
+} 
+
+
+_fetch_deps()
+{
+  local pkg="${1}"
+  local dep_list="${2}"
+
+  info "Fetching '${pkg}' archives"
+
+  while read dep; do
+    
+    curl --location "${deps_url}/${dep}.deb" \
+      --output "${deps_dir}/${dep}.deb" \
+      || die "Download failed ('${dep}')" \
+      && info "Fetched ${deps_url}/${dep}.deb" &
+
+  done <<< "${dep_list}"
+
+  wait
+}
+
+_install_deps()
+{
+  local pkg="${1}"
+  local dep_list="${2}"
+
+  # Assume that if the first package isn't cached, none are.
+  local first_dep=$(<<< "${dep_list}" head -n1)
+  [ ! -f "${deps_dir}/${first_dep}.deb" ] && _fetch_deps "${pkg}" "${dep_list}"
+
+  # Install dependencies
+  info "Installing '${pkg}' packages"
+  sudo dpkg -i $(<<< "${dep_list}" sed -e 's_^ *__' -e 's_^\(.*\)$_'"${deps_dir}"'/\1.deb_' | tr $'\n' ' ')
+}
+
+
+source_pkg()
+{
+  local pkg="${1}"
+
+  local aspell_deps="aspell
+    aspell-en
+    dictionaries-common
+    libaspell15"
+
+  local libfuse_deps="libfuse-dev
+    libpcre3-dev
+    libpcre32-3
+    libpcrecpp0v5
+    libselinux1-dev
+    libsepol1-dev
+    libc-bin"
+
+
+  case "${pkg}" in
+
+    "fuse" )
+      sudo apt-get -o=dir::cache="${deps_dir}/apt-get" install fuse \
+        || die "Installation failed"
+      ;;
+    
+    "aspell" )
+      _install_deps aspell "${aspell_deps}" \
+        || die "Installation failed"
+      ;;
+    "libfuse-dev" )
+      _install_deps libfuse-dev "${libfuse_deps}" \
+        || die "Installation failed"
+      ;;
+
+    "gcc" )
+      _install_gcc \
+        || die "Installation failed"
+      ;;
+
+    * )
+      die "Package not supported: '${pkg}'"
+      ;;
+
+  esac
 }
