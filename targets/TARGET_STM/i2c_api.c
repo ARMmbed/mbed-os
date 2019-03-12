c57fb11910d48ada149f40c7ae3954aa850a01fb
Steve Cartmell <steve.cartmell@arm.com>
2019-03-12 15:08:39 +0000

    fix(hal-i2c): Disable 10-bit addressing I2C capabilities on STM32

--- targets/TARGET_STM/i2c_api.c
+++ targets/TARGET_STM/i2c_api.c
@@ -328,7 +328,7 @@ void i2c_get_capabilities(i2c_capabilities_t *capabilities)
     capabilities->minimum_frequency = 100000;
     capabilities->maximum_frequency = 1000000;
     capabilities->supports_slave_mode = true;
-    capabilities->supports_10bit_addressing = true;
+    capabilities->supports_10bit_addressing = false;
     capabilities->supports_multi_master = true;
 }
 
