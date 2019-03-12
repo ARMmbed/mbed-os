8ca16c4c856e0bbd7a7fd4c01acf5a0fbfccc1f0
ccli8 <ccli8@nuvoton.com>
2019-03-12 09:25:52 +0800

    [M2351] Add license header

--- targets/TARGET_NUVOTON/TARGET_M2351/device/partition_M2351.h
+++ targets/TARGET_NUVOTON/TARGET_M2351/device/partition_M2351.h
@@ -1,12 +1,20 @@
-/**************************************************************************//**
- * @file     partition_M2351.c
- * @version  V3.00
- * @brief    SAU configuration for secure/nonsecure region settings.
+/*
+ * Copyright (c) 2019-2020, Nuvoton Technology Corporation
  *
- * @note
- * Copyright (C) 2016 Nuvoton Technology Corp. All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0
  *
- ******************************************************************************/
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef PARTITION_M2351
 #define PARTITION_M2351
