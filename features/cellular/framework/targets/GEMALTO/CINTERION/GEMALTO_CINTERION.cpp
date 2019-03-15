744e6486733384889c219be25ad456763285c2a5
Sebastian Stockhammer <sebastian.stockhammer@rosenberger.com>
2019-03-15 09:32:26 +0100

    Disable dual stack mode for ehs5-e

--- features/cellular/framework/targets/GEMALTO/CINTERION/GEMALTO_CINTERION.cpp
+++ features/cellular/framework/targets/GEMALTO/CINTERION/GEMALTO_CINTERION.cpp
@@ -161,7 +161,7 @@ void GEMALTO_CINTERION::init_module_ehs5e()
         1,  // AT_CGAUTH
         1,  // PROPERTY_IPV4_STACK
         1,  // PROPERTY_IPV6_STACK
-        1,  // PROPERTY_IPV4V6_STACK
+        0,  // PROPERTY_IPV4V6_STACK
     };
     AT_CellularBase::set_cellular_properties(cellular_properties);
     _module = ModuleEHS5E;
