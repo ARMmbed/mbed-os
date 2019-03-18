9b2acb25a3a794fa3952488336518638d971cc3d
Laurent Meunier <laurent.meunier@st.com>
2019-03-18 16:14:31 +0100

    STM32WB: handle extended reset
    as suggested during review, apply same sequence as in:
    https://github.com/paul-szczepanek-arm/mbed-os/commit/b96c0ac986234457c0c2450ce79a3c9bd1336290

--- features/FEATURE_BLE/targets/TARGET_STM/TARGET_NUCLEO_WB55RG/stm32wb_HCIDriver.cpp
+++ features/FEATURE_BLE/targets/TARGET_STM/TARGET_NUCLEO_WB55RG/stm32wb_HCIDriver.cpp
@@ -281,8 +281,7 @@ public:
             case HCI_OPCODE_LE_WRITE_DEF_DATA_LEN:
                 if (hciCoreCb.extResetSeq)
                 {
-                    /* send first extended command */
-                    (*hciCoreCb.extResetSeq)(pMsg, opcode);
+                    HciReadLocalVerInfoCmd();
                 }
                 else
                 {
@@ -295,6 +294,7 @@ public:
                 }
                 break;
 
+            case HCI_OPCODE_READ_LOCAL_VER_INFO:
             case HCI_OPCODE_LE_READ_MAX_ADV_DATA_LEN:
             case HCI_OPCODE_LE_READ_NUM_SUP_ADV_SETS:
             case HCI_OPCODE_LE_READ_PER_ADV_LIST_SIZE:
