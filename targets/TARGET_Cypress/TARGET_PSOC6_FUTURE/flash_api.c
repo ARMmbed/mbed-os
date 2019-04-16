ARMmbed/mbed-os

commit e16d2d81d97002656926e98afd58769e94cbb030

Reduce flash page size from 512 to 32 bytes in PSOC6 based boards
Page size in all PSOC6 boards is 512 bytes. This is very problematic in
all storage applications. This change reduces the page size (in flash_api's
flash_program_page API) to 32 by reading the original page, modifying it
with programmed data and programming it back. The number 32 for page size
conforms to the number of times (16) this action can be done.

targets/TARGET_Cypress/TARGET_PSOC6_FUTURE/flash_api.c
@@ -47,10 +47,27 @@ int32_t flash_program_page(flash_t *obj, uint32_t address, const uint8_t *data, 
 {
     (void)(obj);
     int32_t status = 0;
-    if (Cy_Flash_ProgramRow(address, (const uint32_t *)data) != CY_FLASH_DRV_SUCCESS) {
-        status = -1;
+    static uint8_t prog_buf[CY_FLASH_SIZEOF_ROW];
+    while (size) {
+        uint32_t offset = address % CY_FLASH_SIZEOF_ROW;
+        uint32_t chunk_size;
+        if (offset + size > CY_FLASH_SIZEOF_ROW) {
+            chunk_size = CY_FLASH_SIZEOF_ROW - offset;
+        } else {
+            chunk_size = size;
+        }
+        uint32_t row_address = address / CY_FLASH_SIZEOF_ROW * CY_FLASH_SIZEOF_ROW;
+        memcpy(prog_buf, (const void *)row_address, CY_FLASH_SIZEOF_ROW);
+        memcpy(prog_buf + offset, data, chunk_size);
+
+        if (Cy_Flash_ProgramRow(row_address, (const uint32_t *)prog_buf) != CY_FLASH_DRV_SUCCESS) {
+            status = -1;
+        }
+        address += chunk_size;
+        size -= chunk_size;
     }
 
+    Cy_SysLib_ClearFlashCacheAndBuffer();
     return status;
 }
 
@@ -67,7 +84,7 @@ uint32_t flash_get_sector_size(const flash_t *obj, uint32_t address)
 uint32_t flash_get_page_size(const flash_t *obj)
 {
     (void)(obj);
-    return CY_FLASH_SIZEOF_ROW;
+    return CY_FLASH_EFFECTIVE_PAGE_SIZE;
 }
 
 uint32_t flash_get_start_address(const flash_t *obj)

targets/TARGET_Cypress/TARGET_PSOC6/flash_api.c
@@ -47,10 +47,27 @@ int32_t flash_program_page(flash_t *obj, uint32_t address, const uint8_t *data, 
 {
     (void)(obj);
     int32_t status = 0;
-    if (Cy_Flash_ProgramRow(address, (const uint32_t *)data) != CY_FLASH_DRV_SUCCESS) {
-        status = -1;
+    static uint8_t prog_buf[CY_FLASH_SIZEOF_ROW];
+    while (size) {
+        uint32_t offset = address % CY_FLASH_SIZEOF_ROW;
+        uint32_t chunk_size;
+        if (offset + size > CY_FLASH_SIZEOF_ROW) {
+            chunk_size = CY_FLASH_SIZEOF_ROW - offset;
+        } else {
+            chunk_size = size;
+        }
+        uint32_t row_address = address / CY_FLASH_SIZEOF_ROW * CY_FLASH_SIZEOF_ROW;
+        memcpy(prog_buf, (const void *)row_address, CY_FLASH_SIZEOF_ROW);
+        memcpy(prog_buf + offset, data, chunk_size);
+
+        if (Cy_Flash_ProgramRow(row_address, (const uint32_t *)prog_buf) != CY_FLASH_DRV_SUCCESS) {
+            status = -1;
+        }
+        address += chunk_size;
+        size -= chunk_size;
     }
 
+    Cy_SysLib_ClearFlashCacheAndBuffer();
     return status;
 }
 
@@ -67,7 +84,7 @@ uint32_t flash_get_sector_size(const flash_t *obj, uint32_t address)
 uint32_t flash_get_page_size(const flash_t *obj)
 {
     (void)(obj);
-    return CY_FLASH_SIZEOF_ROW;
+    return CY_FLASH_EFFECTIVE_PAGE_SIZE;
 }
 
 uint32_t flash_get_start_address(const flash_t *obj)

targets/TARGET_Cypress/TARGET_PSOC6/psoc6pdl/drivers/include/cy_flash.h
@@ -377,6 +377,8 @@ extern "C" {
 
 /** Flash row size */
 #define CY_FLASH_SIZEOF_ROW                (CPUSS_FLASHC_PA_SIZE * 4u)
+/** Flash effective page size */
+#define CY_FLASH_EFFECTIVE_PAGE_SIZE       32
 /** Long words flash row size */
 #define CY_FLASH_SIZEOF_ROW_LONG_UNITS     (CY_FLASH_SIZEOF_ROW / sizeof(uint32_t))
 

targets/TARGET_Cypress/TARGET_PSOC6_FUTURE/device/drivers/peripheral/flash/cy_flash.h
@@ -332,6 +332,8 @@ extern "C" {
 
 /** Flash row size */
 #define CY_FLASH_SIZEOF_ROW                (CPUSS_FLASHC_PA_SIZE * 4u)
+/** Flash effective page size */
+#define CY_FLASH_EFFECTIVE_PAGE_SIZE       32
 /** Number of flash rows */
 #define CY_FLASH_NUMBER_ROWS               (CY_FLASH_SIZE / CY_FLASH_SIZEOF_ROW)
 /** Long words flash row size */

commit f81fbab9f00cf5c01975a8bbc53a037a705f1d0b

FUTURE_SEQUANA: Fix flash_api bug introduced with e16d2d81d9
PDL Flash API requires that the data buffer is 32-bit aligned, otherwise
programming can hung. Buffer declared as uint8_t array is not always
properly aligned, e.g. with gcc 6 when -Os option is used.

targets/TARGET_Cypress/TARGET_PSOC6_FUTURE/flash_api.c
@@ -48,7 +48,7 @@ int32_t flash_program_page(flash_t *obj, uint32_t address, const uint8_t *data, 
 {
     (void)(obj);
     int32_t status = 0;
-    static uint8_t prog_buf[CY_FLASH_SIZEOF_ROW];
+    static uint32_t prog_buf[CY_FLASH_SIZEOF_ROW / sizeof(uint32_t)];
     while (size) {
         uint32_t offset = address % CY_FLASH_SIZEOF_ROW;
         uint32_t chunk_size;
@@ -59,7 +59,7 @@ int32_t flash_program_page(flash_t *obj, uint32_t address, const uint8_t *data, 
         }
         uint32_t row_address = address / CY_FLASH_SIZEOF_ROW * CY_FLASH_SIZEOF_ROW;
         memcpy(prog_buf, (const void *)row_address, CY_FLASH_SIZEOF_ROW);
-        memcpy(prog_buf + offset, data, chunk_size);
+        memcpy((uint8_t *)prog_buf + offset, data, chunk_size);
 
         if (Cy_Flash_ProgramRow(row_address, (const uint32_t *)prog_buf) != CY_FLASH_DRV_SUCCESS) {
             status = -1;