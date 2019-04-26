72d91e7235423ce3b184600e7aa4f5ba6edb89e0
Unknown <snelson@senetco.com>
2019-04-26 13:58:44 -0400

    PHY Class B configuration
    - Initialize Class B configuration
    - Implement class B receiver configuration
    - For regions such as US915 with derived rx_config(),  changes to the base class implementation makes the derivation unnecessary , so these region specific versions have been removed

--- features/lorawan/lorastack/phy/LoRaPHYAU915.h
+++ features/lorawan/lorastack/phy/LoRaPHYAU915.h
@@ -57,8 +57,6 @@ public:
     LoRaPHYAU915();
     virtual ~LoRaPHYAU915();
 
-    virtual bool rx_config(rx_config_params_t *config);
-
     virtual bool tx_config(tx_config_params_t *config, int8_t *txPower,
                            lorawan_time_t *txTimeOnAir);
 
@@ -76,6 +74,12 @@ public:
                                               lorawan_time_t *aggregate_timeoff);
 
     virtual uint8_t apply_DR_offset(int8_t dr, int8_t dr_offset);
+
+    virtual uint32_t get_rx1_frequency(uint8_t channel);
+
+    virtual uint32_t get_beacon_frequency(uint32_t beacon_time);
+
+    virtual uint32_t get_ping_slot_frequency(uint32_t dev_addr, uint32_t beacon_time);
 
 private:
 
