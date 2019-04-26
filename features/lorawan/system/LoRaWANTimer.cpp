94836df52b53b57c56c251bf06d8d3baa585d5bc
Unknown <snelson@senetco.com>
2019-04-26 12:52:06 -0400

    Switch to millisecond resolution  gps time
    The existing gps time  resolution is seconds, while class b beacon and ping slot scheduling requires millisecond resolution.
    This set of changes switches gps time to  millisecond resolution.

--- features/lorawan/system/LoRaWANTimer.cpp
+++ features/lorawan/system/LoRaWANTimer.cpp
@@ -66,20 +66,18 @@ void LoRaWANTimeHandler::stop(timer_event_t &obj)
     obj.timer_id = 0;
 }
 
-lorawan_time_t LoRaWANTimeHandler::get_gps_time(void)
-{
-    if (_gps_time == 0) {
-        return _gps_time;
-    }
-
-    return _gps_time + floor((get_current_time() - _monotonic_tick_time) / 1000);
-}
-
-void LoRaWANTimeHandler::set_gps_time(lorawan_time_t gps_time)
+void LoRaWANTimeHandler::set_gps_time(lorawan_gps_time_t gps_time)
 {
     // store in seconds
     _gps_time = gps_time;
     // store snapshot of current tick time in (ms)
     _monotonic_tick_time = get_current_time();
+}
 
+lorawan_gps_time_t LoRaWANTimeHandler::get_gps_time(void)
+{
+    if(_gps_time == 0){
+        return _gps_time;
+    }
+    return _gps_time + get_current_time() - _monotonic_tick_time;
 }
