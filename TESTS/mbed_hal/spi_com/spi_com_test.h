18a958792ba7e911cef5f989b74d7cc09c908d1f
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2019-05-07 15:19:00 +0200

    tests-mbed_hal-spi_com: update usage of spi_get_capabilities() and specify pins for testing in the test header.

--- TESTS/mbed_hal/spi_com/spi_com_test.h
+++ TESTS/mbed_hal/spi_com/spi_com_test.h
@@ -27,6 +27,34 @@
 extern "C" {
 #endif
 
+#if defined(TARGET_NUCLEO_F429ZI)
+
+#define SPI_TEST_MASTER_CS     PA_4
+#define SPI_TEST_MASTER_SCK    PA_5
+#define SPI_TEST_MASTER_MISO   PA_6
+#define SPI_TEST_MASTER_MOSI   PB_5
+#define SPI_TEST_SLAVE_CS      PE_4
+#define SPI_TEST_SLAVE_SCK     PE_2
+#define SPI_TEST_SLAVE_MISO    PE_5
+#define SPI_TEST_SLAVE_MOSI    PE_6
+
+#elif defined(TARGET_K66F)
+
+#define SPI_TEST_MASTER_CS     PTB20 // pin 3 on j6 header
+#define SPI_TEST_MASTER_SCK    PTD12
+#define SPI_TEST_MASTER_MISO   PTB23
+#define SPI_TEST_MASTER_MOSI   PTD13
+#define SPI_TEST_SLAVE_CS      PTD0
+#define SPI_TEST_SLAVE_SCK     PTD1
+#define SPI_TEST_SLAVE_MISO    PTD2
+#define SPI_TEST_SLAVE_MOSI    PTD3
+
+#else
+#error [NOT_SUPPORTED] Provide SPI pins for the communication test in spi_com.h.
+#endif
+
+
+
 /** Test that the SPI transfer can be performed in various configurations.
  *
  * Given board provides at least 2 SPI peripherals, SPI slave and master mode.
