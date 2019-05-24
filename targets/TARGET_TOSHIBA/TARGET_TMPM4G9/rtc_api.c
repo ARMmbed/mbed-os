4bbc29f32ce3217c704824c02ac6ab2270d03cd1
Ganesh Ramachandran <rganesh@TOSHIBA-TSIP.COM>
2019-05-24 10:23:39 +0530

    Added SPDX identifier

--- targets/TARGET_TOSHIBA/TARGET_TMPM4G9/rtc_api.c
+++ targets/TARGET_TOSHIBA/TARGET_TMPM4G9/rtc_api.c
@@ -1,5 +1,6 @@
 /* mbed Microcontroller Library
  * (C)Copyright TOSHIBA ELECTRONIC DEVICES & STORAGE CORPORATION 2019 All rights reserved
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
