9f66347bd3871f5bdefdeffc9c9b0ec44f44efd3
George Psimenos <georgios.psimenos@arm.com>
2019-05-28 15:58:19 +0100

    Make buffer parameter const

--- drivers/MbedCRC.h
+++ drivers/MbedCRC.h
@@ -146,7 +146,7 @@ public:
      *  @param  crc  CRC is the output value
      *  @return  0 on success, negative error code on failure
      */
-    int32_t compute(void *buffer, crc_data_size_t size, uint32_t *crc)
+    int32_t compute(const void *buffer, crc_data_size_t size, uint32_t *crc)
     {
         MBED_ASSERT(crc != NULL);
         int32_t status = 0;
@@ -193,7 +193,7 @@ public:
      *  @note: CRC as output in compute_partial is not final CRC value, call `compute_partial_stop`
      *         to get final correct CRC value.
      */
-    int32_t compute_partial(void *buffer, crc_data_size_t size, uint32_t *crc)
+    int32_t compute_partial(const void *buffer, crc_data_size_t size, uint32_t *crc)
     {
         int32_t status = 0;
 
