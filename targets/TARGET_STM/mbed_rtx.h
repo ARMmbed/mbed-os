7d298aa0360bdeecaf5c090d4a0d3d68ab9749ff
toyowata <toyomasa.watarai@gmail.com>
2019-06-05 14:35:25 +0900

    Move Seeed ARCH_MAX to TARGET_STM32F407xE MCU

--- targets/TARGET_STM/mbed_rtx.h
+++ targets/TARGET_STM/mbed_rtx.h
@@ -91,6 +91,7 @@
 #elif (defined(TARGET_STM32F207ZG) ||\
        defined(TARGET_STM32F405RG) ||\
        defined(TARGET_STM32F407VG) ||\
+       defined(TARGET_STM32F407VE) ||\
        defined(TARGET_STM32F411RE) ||\
        defined(TARGET_STM32F446RE) ||\
        defined(TARGET_STM32F446VE) ||\
