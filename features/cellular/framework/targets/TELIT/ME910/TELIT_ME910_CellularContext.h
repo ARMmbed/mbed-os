a214a3cb59573e75d923bef44e88502ae14956f7
Chris Trowbridge <trowbridgec@embeddedplanet.com>
2019-06-06 14:29:15 -0400

    Modify ME910 driver to follow power on/off best practices and add custom get_context() function

---
+++ features/cellular/framework/targets/TELIT/ME910/TELIT_ME910_CellularContext.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2018, Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef TELIT_ME910_CELLULARCONTEXT_H_
+#define TELIT_ME910_CELLULARCONTEXT_H_
+
+#include "AT_CellularContext.h"
+
+namespace mbed {
+
+class TELIT_ME910_CellularContext: public AT_CellularContext {
+public:
+    TELIT_ME910_CellularContext(ATHandler &at, CellularDevice *device, const char *apn, bool cp_req = false, bool nonip_req = false);
+    virtual ~TELIT_ME910_CellularContext();
+protected:
+    virtual bool get_context();
+};
+
+} /* namespace mbed */
+
+#endif // TELIT_ME910_CELLULARCONTEXT_H_
