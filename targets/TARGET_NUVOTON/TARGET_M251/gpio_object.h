d111aff98b949a9e7d2aa1d1928dcae885d02041
Chun-Chieh Li <ccli8@nuvoton.com>
2019-06-11 17:21:28 +0800

    Support Nuvoton's NUMAKER_M252KG target

---
+++ targets/TARGET_NUVOTON/TARGET_M251/gpio_object.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019, Nuvoton Technology Corporation
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef MBED_GPIO_OBJECT_H
+#define MBED_GPIO_OBJECT_H
+
+#include "mbed_assert.h"
+#include "cmsis.h"
+#include "PortNames.h"
+#include "PeripheralNames.h"
+#include "PinNames.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct {
+    PinName         pin;
+    uint32_t        mask;
+    PinDirection    direction;
+    PinMode         mode;
+} gpio_t;
+
+static inline void gpio_write(gpio_t *obj, int value)
+{
+    MBED_ASSERT(obj->pin != (PinName)NC);
+    uint32_t pin_index = NU_PININDEX(obj->pin);
+    uint32_t port_index = NU_PINPORT(obj->pin);
+
+    GPIO_PIN_DATA(port_index, pin_index) = value ? 1 : 0;
+}
+
+static inline int gpio_read(gpio_t *obj)
+{
+    MBED_ASSERT(obj->pin != (PinName)NC);
+    uint32_t pin_index = NU_PININDEX(obj->pin);
+    uint32_t port_index = NU_PINPORT(obj->pin);
+
+    return (GPIO_PIN_DATA(port_index, pin_index) ? 1 : 0);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
