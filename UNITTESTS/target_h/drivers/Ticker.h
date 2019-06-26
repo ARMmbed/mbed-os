e40a76575faedf42362a1f6578ad9d14ed1d9c4c
Martin Kojtal <martin.kojtal@arm.com>
2019-06-26 09:33:48 +0100

    watchdog: fix copyright

--- UNITTESTS/target_h/drivers/Ticker.h
+++ UNITTESTS/target_h/drivers/Ticker.h
@@ -1,5 +1,5 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2006-2013 ARM Limited
+/*
+ * Copyright (c) 2018-2019 Arm Limited and affiliates.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
