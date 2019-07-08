d77fb26e9f2d2f67fd3c6802ead4915e4dc11642
Volodymyr Medvid <vmed@cypress.com>
2019-07-08 18:25:01 +0300

    PSOC6: fix license headers per review feedback

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CKIT_062_WIFI_BT/SDIO_HOST/SDIO_HOST.c
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CY8CKIT_062_WIFI_BT/SDIO_HOST/SDIO_HOST.c
@@ -1,16 +1,25 @@
 /***************************************************************************//**
-* \file cy_sdio.c
-* \version 1.00
+* \file SDIO_HOST.c
 *
 * \brief
 *  This file provides the source code to the API for the UDB based SDIO driver.
 *
-*******************************************************************************
+********************************************************************************
 * \copyright
-* Copyright 2016, Cypress Semiconductor Corporation.  All rights reserved.
-* You may use this file only in accordance with the license, terms, conditions,
-* disclaimers, and limitations in the end user license agreement accompanying
-* the software package with which this file was provided.
+* Copyright 2016-2019 Cypress Semiconductor Corporation
+* SPDX-License-Identifier: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
 *******************************************************************************/
 
 #include "SDIO_HOST.h"
--- targets/TARGET_Cypress/TARGET_PSOC6/common/cy_network_buffer.c
+++ targets/TARGET_Cypress/TARGET_PSOC6/common/cy_network_buffer.c
@@ -1,16 +1,27 @@
-/*******************************************************************************
-* File Name: cy_network_buffer.c
+/***************************************************************************//**
+* \file cy_network_buffer.h
 *
-* Description:
+* \brief
 * Basic set of APIs for dealing with network packet buffers. This is used by WHD
 * for relaying data between the network stack and the connectivity chip.
-* 
+*
 ********************************************************************************
-* Copyright (c) 2018-2019 Cypress Semiconductor.  All rights reserved.
-* You may use this file only in accordance with the license, terms, conditions, 
-* disclaimers, and limitations in the end user license agreement accompanying 
-* the software package with which this file was provided.
-********************************************************************************/
+* \copyright
+* Copyright 2018-2019 Cypress Semiconductor Corporation
+* SPDX-License-Identifier: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
 
 #if defined(TARGET_WHD)
 
