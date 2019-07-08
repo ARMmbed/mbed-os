d542c3d66b4190937b84ac62b477a606106a48c9
Volodymyr Medvid <vmed@cypress.com>
2019-07-08 18:25:01 +0300

    PSOC6: fix license headers per review feedback

--- targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CYW943012P6EVB_01/SDIO_HOST/SDIO_HOST.h
+++ targets/TARGET_Cypress/TARGET_PSOC6/TARGET_CYW943012P6EVB_01/SDIO_HOST/SDIO_HOST.h
@@ -1,17 +1,26 @@
 /***************************************************************************//**
 * \file SDIO_HOST.h
-* \version 1.00
 *
 * \brief
-*  This file provides types definition, constants and function definition for 
+*  This file provides types definition, constants and function definition for
 *  the SDIO driver.
 *
-*******************************************************************************
+********************************************************************************
 * \copyright
-* Copyright 2016-2019, Cypress Semiconductor Corporation.  All rights reserved.
-* You may use this file only in accordance with the license, terms, conditions,
-* disclaimers, and limitations in the end user license agreement accompanying
-* the software package with which this file was provided.
+* Copyright 2016-2019 Cypress Semiconductor Corporation
+* SPDX-License-Identifier: Apache-2.0
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
 *******************************************************************************
 * \addtogroup group_udb_sdio_changelog
 * <table class="doxtable">
