26343e960e9276feaa3291481b12fa375bc605a3
Filip Jagodzinski <filip.jagodzinski@arm.com>
2019-07-09 16:05:04 +0200

    FVP_MPS2: Fix serial_init when FC is not used
    After adding DEVICE_SERIAL_FC guards to serial_api.h
    serial_set_flow_control is not available. In case of this
    implementation, this function is a no-op and may be safely removed.

--- targets/TARGET_ARM_FM/TARGET_FVP_MPS2/serial_api.c
+++ targets/TARGET_ARM_FM/TARGET_FVP_MPS2/serial_api.c
@@ -153,7 +153,6 @@ void serial_init(serial_t *obj, PinName tx, PinName rx)
     }
     uart_data[obj->index].sw_rts.pin = NC;
     uart_data[obj->index].sw_cts.pin = NC;
-    serial_set_flow_control(obj, FlowControlNone, NC, NC);
 
     is_stdio_uart = (uart == STDIO_UART) ? (1) : (0);
 
