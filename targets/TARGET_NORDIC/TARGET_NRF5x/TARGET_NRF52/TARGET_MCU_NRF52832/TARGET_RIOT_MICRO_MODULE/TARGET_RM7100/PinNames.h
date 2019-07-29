42db954d06fa1930786fb5dc91b569ae5394edf8
Reda Maher <redamaher@riotmicro.com>
2019-07-29 21:17:15 +0200

    Add Riot Micro cellular module targets

---
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/TARGET_MCU_NRF52832/TARGET_RIOT_MICRO_MODULE/TARGET_RM7100/PinNames.h
@@ -0,0 +1,141 @@
+#ifndef MBED_PINNAMES_H
+#define MBED_PINNAMES_H
+
+#include "cmsis.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum {
+    PIN_INPUT,
+    PIN_OUTPUT
+} PinDirection;
+
+#define PORT_SHIFT 3
+
+typedef enum {
+    // nRF52 pin names
+    p0  = 0,
+    p1  = 1,
+    p2  = 2,
+    p3  = 3,
+    p4  = 4,
+    p5  = 5,
+    p6  = 6,
+    p7  = 7,
+    p8  = 8,
+    p9  = 9,
+    p10 = 10,
+    p11 = 11,
+    p12 = 12,
+    p13 = 13,
+    p14 = 14,
+    p15 = 15,
+    p16 = 16,
+    p17 = 17,
+    p18 = 18,
+    p19 = 19,
+    p20 = 20,
+    p21 = 21,
+    p22 = 22,
+    p23 = 23,
+    p24 = 24,
+    p25 = 25,
+    p26 = 26,
+    p27 = 27,
+    p28 = 28,
+    p29 = 29,
+    p30 = 30,
+    p31 = 31,
+    NC = (int)0xFFFFFFFF, // Not connected
+
+    P0_0  = p0,
+    P0_1  = p1,
+    P0_2  = p2,
+    P0_3  = p3,
+    P0_4  = p4,
+    P0_5  = p5,
+    P0_6  = p6,
+    P0_7  = p7,
+
+    P0_8  = p8,
+    P0_9  = p9,
+    P0_10 = p10,
+    P0_11 = p11,
+    P0_12 = p12,
+    P0_13 = p13,
+    P0_14 = p14,
+    P0_15 = p15,
+
+    P0_16 = p16,
+    P0_17 = p17,
+    P0_18 = p18,
+    P0_19 = p19,
+    P0_20 = p20,
+    P0_21 = p21,
+    P0_22 = p22,
+    P0_23 = p23,
+
+    P0_24 = p24,
+    P0_25 = p25,
+    P0_26 = p26,
+    P0_27 = p27,
+    P0_28 = p28,
+    P0_29 = p29,
+    P0_30 = p30,
+    P0_31 = p31,
+
+    MDMCHEN  = p13,
+    MDMREMAP = p14,
+    MDMRST   = p25,
+
+    MDM_UART0_TXD = p15,
+    MDM_UART0_RXD = p16,
+    MDM_UART1_TXD = p17,
+    MDM_UART1_RXD = p18,
+
+    MDM_UART3_TXD = p19,
+    MDM_UART3_RXD = p20,
+    MDM_UART3_RTS = NC,
+    MDM_UART3_CTS = NC,
+
+    LED1    = P0_10,
+    LED2    = P0_22,
+
+    RX_PIN_NUMBER  =  NC,
+    TX_PIN_NUMBER  =  NC,
+    CTS_PIN_NUMBER =  NC,
+    RTS_PIN_NUMBER =  NC,
+
+    // mBed interface Pins
+    USBTX = TX_PIN_NUMBER,
+    USBRX = RX_PIN_NUMBER,
+    STDIO_UART_TX = TX_PIN_NUMBER,
+    STDIO_UART_RX = RX_PIN_NUMBER,
+    STDIO_UART_CTS = CTS_PIN_NUMBER,
+    STDIO_UART_RTS = RTS_PIN_NUMBER,
+
+    I2C_SDA0 = p12,
+    I2C_SCL0 = p11,
+
+    A0 = p2,
+    A1 = p28,
+    A2 = p4,
+    A3 = p29,
+    A4 = p30,
+    A5 = p31,
+} PinName;
+
+typedef enum {
+    PullNone = 0,
+    PullDown = 1,
+    PullUp = 3,
+    PullDefault = PullUp
+} PinMode;
+
+#ifdef __cplusplus
+}
+
+#endif
+#endif
