9d653af2ccdac5712a53e80d03a83fafb5a46455
cyliangtw <cyliang@nuvoton.com>
2019-08-01 17:53:24 +0800

    re-license all of M261 hal files to be Apache

--- targets/TARGET_NUVOTON/TARGET_M261/PeripheralNames.h
+++ targets/TARGET_NUVOTON/TARGET_M261/PeripheralNames.h
@@ -1,5 +1,6 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019-2020 Nuvoton
+/* 
+ * Copyright (c) 2019-2020 Nuvoton Technology Corporation
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
