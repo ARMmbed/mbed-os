9f2e9aa57692acb99c54d630bdc44691f24b9ce3
Vincent Veron <vincent.veron@st.com>
2019-08-06 11:20:55 +0200

    Add EMAC support for NUCLEO_H743ZI
    This port is based on :
        * CurryGuy ethernet branch :
            https://github.com/CurryGuy/mbed-os/tree/feature-stm32h7-emac
        * STM32 Cube example :
            Applications/LwIP/LwIP_HTTP_Server_Netconn_RTOS example
    
    Signed-off-by: Vincent Veron <vincent.veron@st.com>

--- features/netsocket/emac-drivers/TARGET_STM_EMAC/stm32xx_emac.h
+++ features/netsocket/emac-drivers/TARGET_STM_EMAC/stm32xx_emac.h
@@ -167,6 +167,7 @@ private:
     osThreadId_t rmii_watchdog_thread; /**< Watchdog processing thread */
 #endif
     rtos::Mutex TXLockMutex;/**< TX critical section mutex */
+    rtos::Mutex RXLockMutex;/**< RX critical section mutex */
     emac_link_input_cb_t emac_link_input_cb; /**< Callback for incoming data */
     emac_link_state_change_cb_t emac_link_state_cb; /**< Link state change callback */
     EMACMemoryManager *memory_manager; /**< Memory manager */
