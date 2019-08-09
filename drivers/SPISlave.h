3a38c6bae07438accfa297346d64d1ba1a2bca87
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2019-08-09 09:28:57 +0200

    SPISlave: Add explicit pinmap support

--- drivers/SPISlave.h
+++ drivers/SPISlave.h
@@ -71,6 +71,14 @@ public:
      */
     SPISlave(PinName mosi, PinName miso, PinName sclk, PinName ssel);
 
+    /** Create a SPI slave connected to the specified pins.
+     *
+     *  @note Either mosi or miso can be specified as NC if not used.
+     *
+     *  @param explicit_pinmap reference to strucure which holds static pinmap.
+     */
+    SPISlave(const spi_pinmap_t &pinmap);
+
     /** Configure the data transmission format.
      *
      *  @param bits Number of bits per SPI frame (4 - 16).
