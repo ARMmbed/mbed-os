d31879a1d4df023153dacd37212facc58d96fbff
Kevin Bracey <kevin.bracey@arm.com>
2019-08-13 10:46:48 +0300

    Atomics: GCC fix for M23 (again)
    Add another missing unified syntax directive. Was previously fixed in
    03f1ac3ffd, but the same problem was not addressed in the pending PR that
    added the `NEWVAL_2OP` assembler.

--- platform/internal/mbed_atomic_impl.h
+++ platform/internal/mbed_atomic_impl.h
@@ -141,6 +141,7 @@ extern "C" {
 #elif defined __clang__ || defined __GNUC__
 #define DO_MBED_LOCKFREE_NEWVAL_2OP_ASM(OP, Constants, M)       \
     __asm volatile (                                            \
+        ".syntax unified\n\t"                                   \
         "LDREX"#M "\t%[newValue], %[value]\n\t"                 \
         #OP       "\t%[newValue], %[arg]\n\t"                   \
         "STREX"#M "\t%[fail], %[newValue], %[value]\n\t"        \
