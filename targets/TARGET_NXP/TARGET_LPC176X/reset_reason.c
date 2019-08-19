4fc9190abf06f97520a9b0b2344bcc80f6f1f804
Chris Snow <Chris@chrissnow.co.uk>
2019-08-19 11:58:02 +0100

    SPDX identifier and license amended

--- targets/TARGET_NXP/TARGET_LPC176X/reset_reason.c
+++ targets/TARGET_NXP/TARGET_LPC176X/reset_reason.c
@@ -1,5 +1,5 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2006-2017 ARM Limited
+/* Copyright (c) 2017-2019 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,6 +12,11 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * @section DESCRIPTION
+ *
+ * LPC1768 Reset Reason
+ *
  */
 #include "reset_reason_api.h"
 
