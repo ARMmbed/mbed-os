8ab6aecbe959b8517aeb9fa61608209eeb5bc137
Vincent Veron <vincent.veron@st.com>
2019-08-21 15:02:45 +0200

    Move stm32xx_emac_config.h to family specific folder
    This allows to specify which hal version to use for each family.
    It can also be used to modify the thread stack size.
    
    Signed-off-by: Vincent Veron <vincent.veron@st.com>

--- features/netsocket/emac-drivers/TARGET_STM_EMAC/TARGET_STM32H7/stm32xx_emac_config.h
+++ features/netsocket/emac-drivers/TARGET_STM_EMAC/TARGET_STM32H7/stm32xx_emac_config.h
@@ -17,6 +17,8 @@
 #ifndef STM32XX_EMAC_CONFIG_H__
 #define STM32XX_EMAC_CONFIG_H__
 
+#define ETH_IP_VERSION_V2
+
 #define THREAD_STACKSIZE              512
 
 #endif // #define STM32XX_EMAC_CONFIG_H__
