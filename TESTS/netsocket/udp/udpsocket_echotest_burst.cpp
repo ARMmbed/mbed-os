05e94afca37864ca7497213776ed89cfe8223f49
Michal Paszta <michal.paszta@mobica.com>
2019-08-22 14:32:59 +0300

    Replace TEST_ASSERT_INT_WITHIN usage in netsocket tests
    IPCore's netsocket tests are expected to run all tests even if some of them fail. The TEST_ASSERT_INT_WITHIN macro sets a global variable Unity.CurrentTestFailed which prevents further checks and gives incorrect failure count. Other assertion macros in Unity do not do that.

--- TESTS/netsocket/udp/udpsocket_echotest_burst.cpp
+++ TESTS/netsocket/udp/udpsocket_echotest_burst.cpp
@@ -146,7 +146,7 @@ void UDPSOCKET_ECHOTEST_BURST()
     // Packet loss up to 30% tolerated
     TEST_ASSERT_DOUBLE_WITHIN(TOLERATED_LOSS_RATIO, EXPECTED_LOSS_RATIO, loss_ratio);
     // 70% of the bursts need to be successful
-    TEST_ASSERT_INT_WITHIN(3 * (BURST_CNT / 10), BURST_CNT, ok_bursts);
+    TEST_ASSERT(BURST_CNT - ok_bursts < 3 * (BURST_CNT / 10));
 
     TEST_ASSERT_EQUAL(NSAPI_ERROR_OK, sock.close());
 }
@@ -229,7 +229,7 @@ PKT_OK:
     // Packet loss up to 30% tolerated
     TEST_ASSERT_DOUBLE_WITHIN(TOLERATED_LOSS_RATIO, EXPECTED_LOSS_RATIO, loss_ratio);
     // 70% of the bursts need to be successful
-    TEST_ASSERT_INT_WITHIN(3 * (BURST_CNT / 10), BURST_CNT, ok_bursts);
+    TEST_ASSERT(BURST_CNT - ok_bursts < 3 * (BURST_CNT / 10));
 
     TEST_ASSERT_EQUAL(NSAPI_ERROR_OK, sock.close());
 }
