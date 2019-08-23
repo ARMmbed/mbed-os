73cfa4e88c055c70c0370624535d668e86395e61
Kyle Kearney <kyle@cypress.com>
2019-08-23 11:02:07 -0700

    Add QSPI flash configs for several Cypress targets

--- TESTS/mbed_hal/qspi/flash_configs/flash_configs.h
+++ TESTS/mbed_hal/qspi/flash_configs/flash_configs.h
@@ -66,8 +66,24 @@
 #elif defined(TARGET_LPC546XX)
 #include "NXP/LPC546XX/flash_config.h"
 
-#elif (defined(TARGET_CY8CPROTO_062_4343W) || defined(TARGET_CY8CKIT_062_WIFI_BT) || defined(TARGET_CY8CKIT_062_BLE))
+#elif(  defined(TARGET_CY8CKIT_062_BLE) ||          \
+        defined(TARGET_CY8CKIT_062_WIFI_BT) ||      \
+        defined(TARGET_CY8CKIT_062S2_43012) ||      \
+        defined(TARGET_CY8CKIT_062S2_4343W) ||      \
+        defined(TARGET_CY8CKIT_064S2_4343W) ||      \
+        defined(TARGET_CY8CPROTO_062_4343W) ||      \
+        defined(TARGET_CY8CPROTO_062S2_43012) ||    \
+        defined(TARGET_CY8CPROTO_062S3_4343W) ||    \
+        defined(TARGET_CYW943012P6EVB_01) ||        \
+        defined(TARGET_CYW9P62S1_43438EVB_01))
 #include "S25FL512S_config.h"
+
+#elif   defined(TARGET_CYW9P62S1_43012EVB_01)
+#include "S25FS128S_config.h"
+
+#elif   defined(TARGET_CY8CPROTO_064_SB)
+#include "S25FL128S_config.h"
 
 #endif
 #endif // MBED_FLASH_CONFIGS_H
+
