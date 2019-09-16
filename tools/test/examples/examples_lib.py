95f04e104a3cf6a89740e1fdcf66c7543a6b43e2
Olli-Pekka Puolitaival <olli-pekka.puolitaival@arm.com>
2019-09-16 13:29:21 +0300

    Make able to define build jobs amount when building examples

--- tools/test/examples/examples_lib.py
+++ tools/test/examples/examples_lib.py
@@ -349,7 +349,7 @@ def export_repos(config, ides, targets, examples):
     return results
 
 
-def compile_repos(config, toolchains, targets, profile, verbose, examples):
+def compile_repos(config, toolchains, targets, profile, verbose, examples, jobs=0):
     """Compiles combinations of example programs, targets and compile chains.
 
        The results are returned in a [key: value] dictionary format:
@@ -398,7 +398,7 @@ def compile_repos(config, toolchains, targets, profile, verbose, examples):
                                                                 valid_choices(example['toolchains'], toolchains),
                                                                 example['features']):
                     
-                    build_command = ["mbed-cli", "compile", "-t", toolchain, "-m", target] + (['-vv'] if verbose else [])
+                    build_command = ["mbed-cli", "compile", "-t", toolchain, "-m", target, "-j", str(jobs)] + (['-vv'] if verbose else [])
                     if profile:
                         build_command.append("--profile")
                         build_command.append(profile)
