fef3fd2ea0403fa446d74374b6677064a093de9f
Ari Parkkila <ari.parkkila@arm.com>
2019-09-26 04:33:53 -0700

    Allow OS_THREAD_LIBSPACE_NUM as a macro

--- rtos/source/TARGET_CORTEX/mbed_rtx_conf.h
+++ rtos/source/TARGET_CORTEX/mbed_rtx_conf.h
@@ -82,7 +82,9 @@
 // Provide Mbed-specific instead.
 #define RTX_NO_MULTITHREAD_CLIB
 // LIBSPACE default value set for ARMCC
+#ifndef OS_THREAD_LIBSPACE_NUM
 #define OS_THREAD_LIBSPACE_NUM      4
+#endif
 
 #define OS_IDLE_THREAD_NAME         "rtx_idle"
 #define OS_TIMER_THREAD_NAME        "rtx_timer"
