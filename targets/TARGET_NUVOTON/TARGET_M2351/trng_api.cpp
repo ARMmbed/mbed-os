cd89efc604234beb5e3f8c6dcb0f22f5822e9269
Chun-Chieh Li <ccli8@nuvoton.com>
2019-10-01 17:34:20 +0800

    NUVOTON: Re-implement TRNG HAL with TRNG H/W
    Targets supporting TRNG H/W:
    
    -   NU_PFM_M2351_*
    -   NUMAKER_IOT_M263A

---
+++ targets/TARGET_NUVOTON/TARGET_M2351/trng_api.cpp
@@ -0,0 +1,142 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2017-2018 Nuvoton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#if DEVICE_TRNG
+
+#include "cmsis.h"
+#include <limits.h>
+#include "crypto-misc.h"
+#include "hal/trng_api.h"
+#include "platform/mbed_toolchain.h"
+#include "platform/mbed_critical.h"
+#include "platform/mbed_error.h"
+#include "nu_modutil.h"
+#include "hal_secure.h"
+#include "partition_M2351.h"
+
+#if defined(SCU_INIT_PNSSET5_VAL) && (SCU_INIT_PNSSET5_VAL & (1 << 25))
+#error("We just support secure TRNG")
+#endif
+
+#if defined (__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3L)
+
+/* Module init definition: modname, clkidx, clksrc, clkdiv, rstidx, irqnum, misc */
+static const struct nu_modinit_s trng_modinit = {TRNG_0, TRNG_MODULE, 0, 0, TRNG_RST, TRNG_IRQn, NULL};
+
+/* TRNG init counter. TRNG is kept active as it is non-zero. */
+static uint16_t trng_init_counter = 0U;
+
+#endif
+
+void trng_init(trng_t *obj)
+{
+    trng_init_s(obj);
+}
+
+void trng_free(trng_t *obj)
+{
+    trng_free_s(obj);
+}
+
+int trng_get_bytes(trng_t *obj, uint8_t *output, size_t length, size_t *output_length)
+{
+    uint32_t output_length_;
+    int32_t rc = trng_get_bytes_s(obj, output, (uint32_t) length, &output_length_);
+    if (output_length) {
+        *output_length = output_length_;
+    }
+    return rc;
+}
+
+#if defined (__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+
+extern "C"
+__NONSECURE_ENTRY
+void trng_init_s(MBED_UNUSED void *obj)
+{
+    core_util_critical_section_enter();
+    if (trng_init_counter == USHRT_MAX) {
+        core_util_critical_section_exit();
+        MBED_ERROR(MBED_MAKE_ERROR(MBED_MODULE_HAL, MBED_ERROR_CODE_OVERFLOW), \
+                   "TRNG initialization counter would overflow");
+    }
+    ++ trng_init_counter;
+    if (trng_init_counter == 1) {
+        /* Enable IP clock (secure version) */
+        CLK_EnableModuleClock_S(trng_modinit.clkidx);
+
+        /* Reset IP (secure version) */
+        SYS_ResetModule_S(trng_modinit.rsetidx);
+
+        TRNG_T *trng_base = (TRNG_T *) NU_MODBASE(trng_modinit.modname);
+
+        trng_base->ACT |= TRNG_ACT_ACT_Msk;
+        while (!(trng_base->CTL & TRNG_CTL_READY_Msk));
+    }
+    core_util_critical_section_exit();
+}
+
+extern "C"
+__NONSECURE_ENTRY
+void trng_free_s(MBED_UNUSED void *obj)
+{
+    core_util_critical_section_enter();
+    if (trng_init_counter == 0) {
+        core_util_critical_section_exit();
+        MBED_ERROR(MBED_MAKE_ERROR(MBED_MODULE_HAL, MBED_ERROR_CODE_UNDERFLOW), \
+                   "TRNG initialization counter would underflow");
+    }
+    -- trng_init_counter;
+    if (trng_init_counter == 0) {
+        TRNG_T *trng_base = (TRNG_T *) NU_MODBASE(trng_modinit.modname);
+
+        trng_base->ACT &= ~TRNG_ACT_ACT_Msk;
+
+        /* Disable IP clock (secure version) */
+        CLK_DisableModuleClock_S(trng_modinit.clkidx);
+    }
+    core_util_critical_section_exit();
+}
+
+extern "C"
+__NONSECURE_ENTRY
+int32_t trng_get_bytes_s(MBED_UNUSED void *obj, uint8_t *output, uint32_t length, uint32_t *output_length)
+{
+    /* Check augument validity */
+    if (!output && length) {
+        return -1;
+    }
+
+    uint8_t *output_ind = output;
+    uint8_t *output_end = output + length;
+
+    TRNG_T *trng_base = (TRNG_T *) NU_MODBASE(trng_modinit.modname);
+
+    for (; output_ind != output_end; output_ind ++) {
+        trng_base->CTL |= TRNG_CTL_TRNGEN_Msk;
+        while (!(trng_base->CTL & TRNG_CTL_DVIF_Msk));
+        *output_ind = trng_base->DATA & 0xff;
+    }
+
+    if (output_length) {
+        *output_length = length;
+    }
+
+    return 0;
+}
+
+#endif
+#endif
