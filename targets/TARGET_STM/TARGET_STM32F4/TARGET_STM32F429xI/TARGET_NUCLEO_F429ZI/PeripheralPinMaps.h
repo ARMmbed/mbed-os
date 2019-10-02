82aefe7c3550ef7b2adf38578ef5d4a716335824
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2019-10-02 15:06:28 +0200

    NUCLEO_F429ZI add CAN pinmaps

--- targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F429xI/TARGET_NUCLEO_F429ZI/PeripheralPinMaps.h
+++ targets/TARGET_STM/TARGET_STM32F4/TARGET_STM32F429xI/TARGET_NUCLEO_F429ZI/PeripheralPinMaps.h
@@ -376,5 +376,7 @@ MSTD_CONSTEXPR_OBJ_11 PinMap PinMap_CAN_TD[] = {
 #define PINMAP_SPI_MISO PinMap_SPI_MISO
 #define PINMAP_SPI_SSEL PinMap_SPI_SSEL
 #define PINMAP_PWM PinMap_PWM
+#define PINMAP_CAN_TD PinMap_CAN_TD
+#define PINMAP_CAN_RD PinMap_CAN_RD
 
 #endif
