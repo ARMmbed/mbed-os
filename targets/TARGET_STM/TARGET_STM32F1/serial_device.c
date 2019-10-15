e862438fad64a782baac14247d83eb978d10768d
Laurent Meunier <laurent.meunier@st.com>
2019-10-15 15:55:10 +0200

    Clearing UART TC Flag prevents deep sleep, so do not clear it
    The TC flag is used in function serial_is_tx_ongoing to check if there is
    an ongoing serial transmission. So this Flag must not be cleared at the
    end of the transmission, otherwise, serial_is_tx_ongoing will notify that
    TX is ongoing.
    
    The impact is that it may prevent deep sleep to be entered.
    
    Also there is no need to clear this flag at the end of the transaction
    because it will be cleared automatically by HW when a new transmission
    starts.

--- targets/TARGET_STM/TARGET_STM32F1/serial_device.c
+++ targets/TARGET_STM/TARGET_STM32F1/serial_device.c
@@ -443,13 +443,6 @@ uint8_t serial_rx_active(serial_t *obj)
     return (((HAL_UART_GetState(huart) & HAL_UART_STATE_BUSY_RX) == HAL_UART_STATE_BUSY_RX) ? 1 : 0);
 }
 
-void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart)
-{
-    if (__HAL_UART_GET_FLAG(huart, UART_FLAG_TC) != RESET) {
-        __HAL_UART_CLEAR_FLAG(huart, UART_FLAG_TC);
-    }
-}
-
 void HAL_UART_ErrorCallback(UART_HandleTypeDef *huart)
 {
     if (__HAL_UART_GET_FLAG(huart, UART_FLAG_PE) != RESET) {
@@ -554,9 +547,6 @@ void serial_tx_abort_asynch(serial_t *obj)
 
     __HAL_UART_DISABLE_IT(huart, UART_IT_TC);
     __HAL_UART_DISABLE_IT(huart, UART_IT_TXE);
-
-    // clear flags
-    __HAL_UART_CLEAR_FLAG(huart, UART_FLAG_TC);
 
     // reset states
     huart->TxXferCount = 0;
