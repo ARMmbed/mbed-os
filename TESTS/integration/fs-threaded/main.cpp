c3bf795e48288b4e000e7b8c6fb696668190aaa7
MarceloSalazar <Marcelo.Salazar@arm.com>
2019-10-17 16:45:15 +0100

    Split integration tests in fs/net

--- TESTS/integration/fs-threaded/main.cpp
+++ TESTS/integration/fs-threaded/main.cpp
@@ -27,7 +27,7 @@
 #include "utest/utest.h"
 #include "unity/unity.h"
 #include "greentea-client/test_env.h"
-#include "common_defines_test.h"
+#include "common_defines_fs_test.h"
 #include "file_test.h"
 
 #if !INTEGRATION_TESTS
