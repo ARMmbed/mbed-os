401c6927c99c275fb50c9b559c332673c450b8d5
Giampaolo Mancini <giampaolo@trampolineup.com>
2019-10-21 15:40:39 +0200

    Add SPDX identifier

--- targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/watchdog_api.c
+++ targets/TARGET_NORDIC/TARGET_NRF5x/TARGET_NRF52/watchdog_api.c
@@ -1,6 +1,7 @@
 /*
  * Copyright (c) 2019 Trampoline SRL
  * Copyright (c) 2019 Giampaolo Mancini <giampaolo@trampolineup.com>
+ * SPDX-License-Identifier: Apache-2.0 
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
