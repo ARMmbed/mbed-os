dfa96924da202e949c25b3eefa7c7f5e6de44e93
Kimmo Vaisanen <kimmo.vaisanen@arm.com>
2019-11-07 12:16:10 +0200

    Fix flag checks to allow disabling unsecure tests
    Now unsecure tests can be disabled by just removing MBED_CONF_APP_WIFI_UNSECURE_SSID
    configuration from json.

--- TESTS/network/wifi/main.cpp
+++ TESTS/network/wifi/main.cpp
@@ -34,15 +34,15 @@
     !defined(MBED_CONF_APP_WIFI_CH_SECURE)     || \
     !defined(MBED_CONF_APP_WIFI_PASSWORD)      || \
     !defined(MBED_CONF_APP_WIFI_SECURE_SSID)   || \
-    !defined MBED_CONF_APP_WIFI_SECURE_PROTOCOL)
+    !defined(MBED_CONF_APP_WIFI_SECURE_PROTOCOL))
 #error [NOT_SUPPORTED] Requires parameters from mbed_app.json (for secure connections)
 #else
 
 #if defined(MBED_CONF_APP_WIFI_UNSECURE_SSID) &&  \
-    !defined(MBED_CONF_APP_AP_MAC_UNSECURE)    || \
+   (!defined(MBED_CONF_APP_AP_MAC_UNSECURE)    || \
     !defined(MBED_CONF_APP_MAX_SCAN_SIZE)      || \
     !defined(MBED_CONF_APP_WIFI_CH_UNSECURE)   || \
-    !defined(MBED_CONF_APP_WIFI_UNSECURE_SSID)
+    !defined(MBED_CONF_APP_WIFI_UNSECURE_SSID))
 #error [NOT_SUPPORTED] Requires parameters from mbed_app.json (for unsecure connections)
 #else
 
