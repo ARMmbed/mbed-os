c8db120ade36b0e85e864910c364c43784298897
Devaraj Ranganna <devaraj.ranganna@arm.com>
2019-11-28 16:49:23 +0000

    Exporters are not supported for TF-M targets
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

--- tools/export/cmsis/__init__.py
+++ tools/export/cmsis/__init__.py
@@ -129,7 +129,10 @@ class CMSIS(Exporter):
     @classmethod
     def is_target_supported(cls, target_name):
         target = TARGET_MAP[target_name]
-        return cls.TOOLCHAIN in target.supported_toolchains
+        if not target.is_TFM_target:
+            return cls.TOOLCHAIN in target.supported_toolchains
+        else:
+            return False
 
     def make_key(self, src):
         """turn a source file into its group name"""
