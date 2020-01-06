bd3cd3a199efb86898cb3ecb4da68de707e67109
PARKJIHOON <jh6186.park@samsung.com>
2020-01-06 20:25:21 +0900

    tidy up PR comments #1
    Signed-off-by: PARKJIHOON <jh6186.park@samsung.com>

--- features/mbedtls/targets/TARGET_Samsung/sha/sha256_alt.c
+++ features/mbedtls/targets/TARGET_Samsung/sha/sha256_alt.c
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2015-2016 Nuvoton
+ * Copyright (c) 2006-2019 ARM Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,25 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+/****************************************************************************
+ *
+ * Copyright 2019 Samsung Electronics All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ ****************************************************************************/
+
 
 #include "mbedtls/sha256.h"
 #include "mbedtls/platform_util.h"
