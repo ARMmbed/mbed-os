nuc972/mbed

[M2351] Enforce locked entry into TFM for platform-specific NSC calls
On M2351, some spaces like SYS/CLK are hard-wired to secure and cannot change.
To access these spaces from non-secure world, we must provide platform-specific NSC
functions. With TFM introduced, we must synchronize NSC calls into TFM to keep TFM
in sync instead of straight NSC calls. To achieve this goal, we go with the following
approach:

1. Like PSA APIs, enforce locked entry through tfm_ns_lock_dispatch().
2. Run platform-specific secure functions in default secure partition, in which SYS/CLK
   spaces have been configured to be accessible.

targets/TARGET_NUVOTON/TARGET_M2351/device/stddriver_secure.h
@@ -28,60 +28,114 @@ extern "C" {
  * Guard access to secure module from non-secure domain before SYS_ResetModule.
  * Its synopsis is the same as SYS_ResetModule.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void SYS_ResetModule_S(uint32_t u32ModuleIndex);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t SYS_ResetModule_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure CLK_SetModuleClock
  *
  * Guard access to secure module from non-secure domain before CLK_SetModuleClock.
  * Its synopsis is the same as CLK_SetModuleClock.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void CLK_SetModuleClock_S(uint32_t u32ModuleIndex, uint32_t u32ClkSrc, uint32_t u32ClkDiv);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_SetModuleClock_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure CLK_EnableModuleClock
  *
  * Guard access to secure module from non-secure domain before CLK_EnableModuleClock.
  * Its synopsis is the same as CLK_EnableModuleClock.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void CLK_EnableModuleClock_S(uint32_t u32ModuleIndex);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_EnableModuleClock_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure CLK_DisableModuleClock
  *
  * Guard access to secure module from non-secure domain before CLK_DisableModuleClock.
  * Its synopsis is the same as CLK_DisableModuleClock.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void CLK_DisableModuleClock_S(uint32_t u32ModuleIndex);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_DisableModuleClock_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Is RTC clock enabled? */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 int32_t CLK_IsRTCClockEnabled_S(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_IsRTCClockEnabled_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure SYS_LockReg
  *
  * Guard access to secure module from non-secure domain before SYS_LockReg.
  * Its synopsis is the same as SYS_LockReg.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void SYS_LockReg_S(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t SYS_LockReg_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure SYS_UnlockReg
  *
  * Guard access to secure module from non-secure domain before SYS_UnlockReg.
  * Its synopsis is the same as SYS_UnlockReg.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void SYS_UnlockReg_S(void);
-
-/* Secure CLK_Idle */
+#if (TFM_LVL > 0)
 __NONSECURE_ENTRY
-void CLK_Idle_S(void);
+int32_t SYS_UnlockReg_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
-/* Secure CLK_PowerDown */
+/* Secure SYS_UnlockReg > CLK_Idle > SYS_LockReg */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
-void CLK_PowerDown_S(void);
+#endif
+void nu_idle_s(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t nu_idle_veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
+
+/* Secure SYS_UnlockReg > CLK_PowerDown > SYS_LockReg */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
+void nu_powerdown_s(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t nu_powerdown_veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 #ifdef __cplusplus
 }

targets/TARGET_NUVOTON/TARGET_M2351/pinmap.c
@@ -21,6 +21,9 @@
 #include "mbed_error.h"
 #include "partition_M2351.h"
 #include "hal_secure.h"
+#if defined(DOMAIN_NS) && (DOMAIN_NS == 1L) && (TFM_LVL > 0)
+#include "nu_tfm_ns_lock_sup.h"
+#endif
 
 /**
  * Configure pin multi-function
@@ -77,16 +80,16 @@ void pin_mode(PinName pin, PinMode mode)
      */
 }
 
-#if defined (__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
-__NONSECURE_ENTRY
-void pin_function_s(int32_t pin, int32_t data)
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+
+static void pin_function_impl(int32_t pin, int32_t data, bool nonsecure_caller)
 {
     MBED_ASSERT(pin != (PinName)NC);
     uint32_t pin_index = NU_PINNAME_TO_PIN(pin);
     uint32_t port_index = NU_PINNAME_TO_PORT(pin);
     
     /* Guard access to secure GPIO from non-secure domain */
-    if (cmse_nonsecure_caller() && 
+    if (nonsecure_caller && 
         (! (SCU_INIT_IONSSET_VAL & (1 << (port_index + 0))))) {
         error("Non-secure domain tries to control secure or undefined GPIO.");
     }
@@ -97,4 +100,35 @@ void pin_function_s(int32_t pin, int32_t data)
     // E.g.: SYS->GPA_MFPL  = (SYS->GPA_MFPL & (~SYS_GPA_MFPL_PA0MFP_Msk) ) | SYS_GPA_MFPL_PA0MFP_SC0_CD  ;
     *GPx_MFPx  = (*GPx_MFPx & (~MFP_Msk)) | data;
 }
+
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t pin_function_veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3)
+{
+    int32_t pin = (int32_t) arg0;
+    int32_t data = (int32_t) arg1;
+    pin_function_impl(pin, data, cmse_nonsecure_caller());
+    return 0;
+}
+#endif
+
+#endif
+
+#if defined(DOMAIN_NS) && (DOMAIN_NS == 1) && (TFM_LVL > 0)
+
+void pin_function_s(int32_t pin, int32_t data)
+{
+    nu_tfm_ns_lock_dispatch(pin_function_veneer, pin, data, 0, 0);
+}
+
+#elif defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+
+#if (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
+void pin_function_s(int32_t pin, int32_t data)
+{
+    pin_function_impl(pin, data, cmse_nonsecure_caller());
+}
+
 #endif

ARMmbed/mbed-os

targets/TARGET_NUVOTON/TARGET_M2351/device/stddriver_secure.h
@@ -28,56 +28,124 @@ extern "C" {
  * Guard access to secure module from non-secure domain before SYS_ResetModule.
  * Its synopsis is the same as SYS_ResetModule.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void SYS_ResetModule_S(uint32_t u32ModuleIndex);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t SYS_ResetModule_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure CLK_SetModuleClock
  *
  * Guard access to secure module from non-secure domain before CLK_SetModuleClock.
  * Its synopsis is the same as CLK_SetModuleClock.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void CLK_SetModuleClock_S(uint32_t u32ModuleIndex, uint32_t u32ClkSrc, uint32_t u32ClkDiv);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_SetModuleClock_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure CLK_EnableModuleClock
  *
  * Guard access to secure module from non-secure domain before CLK_EnableModuleClock.
  * Its synopsis is the same as CLK_EnableModuleClock.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void CLK_EnableModuleClock_S(uint32_t u32ModuleIndex);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_EnableModuleClock_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure CLK_DisableModuleClock
  *
  * Guard access to secure module from non-secure domain before CLK_DisableModuleClock.
  * Its synopsis is the same as CLK_DisableModuleClock.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void CLK_DisableModuleClock_S(uint32_t u32ModuleIndex);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_DisableModuleClock_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure SYS_LockReg
  *
  * Guard access to secure module from non-secure domain before SYS_LockReg.
  * Its synopsis is the same as SYS_LockReg.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void SYS_LockReg_S(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t SYS_LockReg_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 /* Secure SYS_UnlockReg
  *
  * Guard access to secure module from non-secure domain before SYS_UnlockReg.
  * Its synopsis is the same as SYS_UnlockReg.
  */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
 __NONSECURE_ENTRY
+#endif
 void SYS_UnlockReg_S(void);
-
-/* Secure CLK_Idle */
+#if (TFM_LVL > 0)
 __NONSECURE_ENTRY
+int32_t SYS_UnlockReg_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
+
+/* Secure  CLK_Idle */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
 void CLK_Idle_S(void);
-
-/* Secure CLK_PowerDown */
+#if (TFM_LVL > 0)
 __NONSECURE_ENTRY
+int32_t CLK_Idle_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
+
+/* Secure  CLK_PowerDown */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
 void CLK_PowerDown_S(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t CLK_PowerDown_Veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
+
+/* Secure SYS_UnlockReg > CLK_Idle > SYS_LockReg */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
+void nu_idle_s(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t nu_idle_veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
+
+/* Secure SYS_UnlockReg > CLK_PowerDown > SYS_LockReg */
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U) && (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
+void nu_powerdown_s(void);
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t nu_powerdown_veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3);
+#endif
 
 #ifdef __cplusplus
 }

targets/TARGET_NUVOTON/TARGET_M2351/pinmap.c
@@ -22,6 +22,9 @@
 #include "mbed_error.h"
 #include "partition_M2351.h"
 #include "hal_secure.h"
+#if defined(DOMAIN_NS) && (DOMAIN_NS == 1L) && (TFM_LVL > 0)
+#include "tfm_ns_lock.h"
+#endif
 
 /**
  * Configure pin multi-function
@@ -93,16 +96,16 @@ const PeripheralList *pinmap_restricted_peripherals()
     return &peripheral_list;
 }
 
-#if defined (__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
-__NONSECURE_ENTRY
-void pin_function_s(int32_t pin, int32_t data)
+#if defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+
+static void pin_function_impl(int32_t pin, int32_t data, bool nonsecure_caller)
 {
     MBED_ASSERT(pin != (PinName)NC);
     uint32_t pin_index = NU_PINNAME_TO_PIN(pin);
     uint32_t port_index = NU_PINNAME_TO_PORT(pin);
     
     /* Guard access to secure GPIO from non-secure domain */
-    if (cmse_nonsecure_caller() && 
+    if (nonsecure_caller && 
         (! (SCU_INIT_IONSSET_VAL & (1 << (port_index + 0))))) {
         error("Non-secure domain tries to control secure or undefined GPIO.");
     }
@@ -113,4 +116,35 @@ void pin_function_s(int32_t pin, int32_t data)
     // E.g.: SYS->GPA_MFPL  = (SYS->GPA_MFPL & (~SYS_GPA_MFPL_PA0MFP_Msk) ) | SYS_GPA_MFPL_PA0MFP_SC0_CD  ;
     *GPx_MFPx  = (*GPx_MFPx & (~MFP_Msk)) | data;
 }
+
+#if (TFM_LVL > 0)
+__NONSECURE_ENTRY
+int32_t pin_function_veneer(uint32_t arg0, uint32_t arg1, uint32_t arg2, uint32_t arg3)
+{
+    int32_t pin = (int32_t) arg0;
+    int32_t data = (int32_t) arg1;
+    pin_function_impl(pin, data, cmse_nonsecure_caller());
+    return 0;
+}
+#endif
+
+#endif
+
+#if defined(DOMAIN_NS) && (DOMAIN_NS == 1) && (TFM_LVL > 0)
+
+void pin_function_s(int32_t pin, int32_t data)
+{
+    tfm_ns_lock_dispatch(pin_function_veneer, pin, data, 0, 0);
+}
+
+#elif defined(__ARM_FEATURE_CMSE) && (__ARM_FEATURE_CMSE == 3U)
+
+#if (TFM_LVL == 0)
+__NONSECURE_ENTRY
+#endif
+void pin_function_s(int32_t pin, int32_t data)
+{
+    pin_function_impl(pin, data, cmse_nonsecure_caller());
+}
+
 #endif