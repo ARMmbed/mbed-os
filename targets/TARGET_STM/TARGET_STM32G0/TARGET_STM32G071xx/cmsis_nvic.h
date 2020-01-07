9448ded04483cd0c70532898cb67124da705c2d2
jeromecoutant <jerome.coutant@st.com>
2020-01-07 17:00:30 +0100

    STM32G0: Update G071xx toolchain files with default files

--- targets/TARGET_STM/TARGET_STM32G0/TARGET_STM32G071xx/cmsis_nvic.h
+++ targets/TARGET_STM/TARGET_STM32G0/TARGET_STM32G071xx/cmsis_nvic.h
@@ -1,9 +1,10 @@
 /* mbed Microcontroller Library
  * SPDX-License-Identifier: BSD-3-Clause
  ******************************************************************************
+ * @attention
  *
- * Copyright (c) 2014 STMicroelectronics.
- * All rights reserved.
+ * <h2><center>&copy; Copyright (c) 2016-2020 STMicroelectronics.
+ * All rights reserved.</center></h2>
  *
  * This software component is licensed by ST under BSD 3-Clause license,
  * the "License"; You may not use this file except in compliance with the
@@ -11,15 +12,35 @@
  *                        opensource.org/licenses/BSD-3-Clause
  *
  ******************************************************************************
- */
+*/
 
 #ifndef MBED_CMSIS_NVIC_H
 #define MBED_CMSIS_NVIC_H
 
-// CORE: 16 vectors = 64 bytes from 0x00 to 0x3F
-// MCU Peripherals: 32 vectors = 128 bytes from 0x40 to 0xBF
-// Total: 48 vectors = 192 bytes (0xC0) to be reserved in RAM
-#define NVIC_NUM_VECTORS        48
-#define NVIC_RAM_VECTOR_ADDRESS 0x20000000    // Vectors positioned at start of RAM
+#if !defined(MBED_ROM_START)
+#define MBED_ROM_START  0x8000000
+#endif
+
+#if !defined(MBED_ROM_SIZE)
+#if defined (TARGET_NUCLEO_G071RB)
+#define MBED_ROM_SIZE  0x20000
+#else
+ //  0x10000 STM32G071C8Ux STM32G071K8TxN STM32G071R8Tx STM32G071K8Ux STM32G071G8UxN STM32G071C8Tx STM32G071K8Tx STM32G071K8UxN STM32G071G8Ux 
+ //   0x8000 STM32G071K6Tx STM32G071G6Ux STM32G071C6Tx STM32G071K6Ux STM32G071R6Tx STM32G071C6Ux 
+ //  0x20000 STM32G071GBUxN STM32G071CBUx STM32G071KBTx STM32G071KBUxN STM32G071RBTx STM32G071EBYx STM32G071GBUx STM32G071CBTx STM32G071KBUx STM32G071RBIx STM32G071KBTxN 
+#error "MBED_ROM_SIZE not defined"
+#endif
+#endif
+
+#if !defined(MBED_RAM_START)
+#define MBED_RAM_START  0x20000000
+#endif
+
+#if !defined(MBED_RAM_SIZE)
+#define MBED_RAM_SIZE  0x9000
+#endif
+
+#define NVIC_NUM_VECTORS        47
+#define NVIC_RAM_VECTOR_ADDRESS MBED_RAM_START
 
 #endif
