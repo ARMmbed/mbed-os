75c0486e2f2a283aa52b4fceaf729f5cdd9e1a38
Shuopeng Deng <desp@cypress.com>
2020-01-15 09:42:02 -0800

    fix astyle violations

--- targets/TARGET_Cypress/TARGET_PSOC6/PeripheralPins.c
+++ targets/TARGET_Cypress/TARGET_PSOC6/PeripheralPins.c
@@ -22,22 +22,18 @@
 
 static PinMap default_nc_pin_map = {NC, NC, 0};
 
-void try_create_pin_map(PinMap **pin_map, const cyhal_resource_pin_mapping_t* resource_map, size_t resource_size)
+void try_create_pin_map(PinMap **pin_map, const cyhal_resource_pin_mapping_t *resource_map, size_t resource_size)
 {
-    if (*pin_map == NULL)
-    {
-        if (resource_size == sizeof(cyhal_resource_pin_mapping_t) && resource_map[0].pin == NC)
-        {
+    if (*pin_map == NULL) {
+        if (resource_size == sizeof(cyhal_resource_pin_mapping_t) && resource_map[0].pin == NC) {
             // If there are no valid connection, then use default NC pin map.
             *pin_map = &default_nc_pin_map;
-        }
-        else {
-            *pin_map = (PinMap*)malloc(resource_size + sizeof(PinMap));
+        } else {
+            *pin_map = (PinMap *)malloc(resource_size + sizeof(PinMap));
             if (*pin_map == NULL) {
                 // If malloc fails, then use default NC pin map.
                 *pin_map = &default_nc_pin_map;
-            }
-            else {
+            } else {
                 memcpy(*pin_map, resource_map, resource_size);
                 (*pin_map)[resource_size / sizeof(PinMap)].pin = NC;
                 (*pin_map)[resource_size / sizeof(PinMap)].peripheral = NC;
@@ -47,13 +43,12 @@ void try_create_pin_map(PinMap **pin_map, const cyhal_resource_pin_mapping_t* re
     }
 }
 
-void try_create_combined_pin_map(PinMap **pin_map, const cyhal_resource_pin_mapping_t* resource_map0, size_t resource_size0, const cyhal_resource_pin_mapping_t* resource_map1, size_t resource_size1)
+void try_create_combined_pin_map(PinMap **pin_map, const cyhal_resource_pin_mapping_t *resource_map0, size_t resource_size0, const cyhal_resource_pin_mapping_t *resource_map1, size_t resource_size1)
 {
     /*
      * This functions is similar to try_create_pin_map except it create a PinMap from multple cyhal_resource_pin_mapping_t
      */
-    if (*pin_map == NULL)
-    {
+    if (*pin_map == NULL) {
         if (resource_size0 == sizeof(cyhal_resource_pin_mapping_t) && resource_map0[0].pin == NC) {
             resource_size0 = 0;
         }
@@ -64,16 +59,14 @@ void try_create_combined_pin_map(PinMap **pin_map, const cyhal_resource_pin_mapp
         if (resource_size0 == 0 && resource_size1 == 0) {
             // If there are no valid connection, then use default NC pin map.
             *pin_map = &default_nc_pin_map;
-        }
-        else {
-            *pin_map = (PinMap*)malloc(resource_size0 + resource_size1 + sizeof(PinMap));
+        } else {
+            *pin_map = (PinMap *)malloc(resource_size0 + resource_size1 + sizeof(PinMap));
             if (*pin_map == NULL) {
                 // If malloc fails, then use default NC pin map.
                 *pin_map = &default_nc_pin_map;
-            }
-            else {
+            } else {
                 memcpy(*pin_map, resource_map0, resource_size0);
-                memcpy(((void*)(*pin_map)) + resource_size0, resource_map1, resource_size1);
+                memcpy(((void *)(*pin_map)) + resource_size0, resource_map1, resource_size1);
                 (*pin_map)[(resource_size0 + resource_size1) / sizeof(PinMap)].pin = NC;
                 (*pin_map)[(resource_size0 + resource_size1) / sizeof(PinMap)].peripheral = NC;
                 (*pin_map)[(resource_size0 + resource_size1) / sizeof(PinMap)].function = 0;
