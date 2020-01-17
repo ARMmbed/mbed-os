88c9dd85dd67bb3df68464fe07292e45b484ce50
Arun S <arsm@cypress.com>
2020-01-17 16:33:34 +0530

    Pairing fails when IPv6 enabled in SoftAP intf Issue: udp_sendto() Fails for multicast IPV6 packet when interface is switched from SoftAP to STA mode. When SoftAP start is called, add_ethernet_interface() will be called internally to add interface details into netif_list. When switching from SoftAP to STA mode, add_ethernet_interface() will be called again to append the interace details into netif_list. When udp_sendto() is called, ip6_route() will return interface as NULL since it consider device as single interface. Fix: SoftAP mode Stop, call remove_ethernet_interface() to remove the interface from the netif_list.

--- features/netsocket/emac-drivers/TARGET_Cypress/COMPONENT_WHD/interface/WhdSoftAPInterface.cpp
+++ features/netsocket/emac-drivers/TARGET_Cypress/COMPONENT_WHD/interface/WhdSoftAPInterface.cpp
@@ -201,6 +201,16 @@ int WhdSoftAPInterface::stop(void)
     if (res != WHD_SUCCESS) {
         return whd_toerror(res);
     }
+
+    // remove the interface added in start
+    if (_interface) {
+        nsapi_error_t err = _stack.remove_ethernet_interface(&_interface);
+        if (err != NSAPI_ERROR_OK) {
+            return err;
+        }
+        _iface_shared.iface_softap = NULL;
+    }
+
     return NSAPI_ERROR_OK;
 }
 
