aa4e7b17aae082b6b53a55095b3321df0713efb2
Martin Kojtal <martin.kojtal@arm.com>
2020-02-20 08:45:04 +0000

    rtos: add SPDX identifiers

--- rtos/source/Kernel.cpp
+++ rtos/source/Kernel.cpp
@@ -1,6 +1,8 @@
 /* mbed Microcontroller Library
  * Copyright (c) 2017 ARM Limited
  *
+ * SPDX-License-Identifier: MIT
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
