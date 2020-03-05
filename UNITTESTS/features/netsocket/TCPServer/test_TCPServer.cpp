874e36e05a8ad05bbe0e180cbbc15798245e9eb9
Kevin Bracey <kevin.bracey@arm.com>
2020-03-05 16:24:39 +0200

    Fully enforce NonCopyable
    Make NonCopyable fully operational so it gives compile errors in all
    build profiles.

--- UNITTESTS/features/netsocket/TCPServer/test_TCPServer.cpp
+++ UNITTESTS/features/netsocket/TCPServer/test_TCPServer.cpp
@@ -54,7 +54,7 @@ TEST_F(TestTCPServer, constructor)
 
 TEST_F(TestTCPServer, constructor_parameters)
 {
-    TCPServer serverParam = TCPServer(&stack);
+    TCPServer serverParam(&stack);
     const SocketAddress a("127.0.0.1", 1024);
     EXPECT_EQ(serverParam.connect(a), NSAPI_ERROR_OK);
 }
