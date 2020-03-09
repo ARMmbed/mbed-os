07f5a30d83529bf21c0a351ce53f7f0fc939b2d0
Martin Kojtal <martin.kojtal@arm.com>
2020-03-09 15:03:03 +0000

    cmake: add function to get TARGET/FEATURE etc
    creating util cmake to write up some functions we can use
    
    Our current build system has some rules like TARGET_/FEATURE_ to select what is needed. We provide mbed_os_add_cmake_directory function that does the same trick.
    
    If you have:
    
    TARGET_ST/
    TARGET_NXP
    
    add CMakeLists.txt here and use this function to select proper target CMake inside these folders.
    
    mbed_os_add_cmake_directory(TARGET, "${MBED_OS_TARGET_KEYS}")
    
    This means, in this folder the selection is conditional based on TARGET and it checks TARGET_KEYS - they come from tools

--- util.cmake
+++ util.cmake
@@ -3,9 +3,12 @@
 # TODO : this logic is for any TARGET, FEATURE etc,
 #  thus this should be done genericaly
 # Add TARGET_ directory to the CMake tree
-function(mbed_os_add_target_directory)
-    foreach(key ${ARGV})
-        string(PREPEND key "TARGET_")
+
+# KEYWORD - TARGET_ or similar
+# KEYWORD_LIST - list of labels that are used to include directories
+function(mbed_os_add_cmake_directory KEYWORD KEYWORDLIST)
+    foreach(key ${KEYWORDLIST})
+        string(PREPEND key ${KEYWORD} "_")
         # assumption: relative path, no need to check for absolute here
         set(path ${CMAKE_CURRENT_SOURCE_DIR}/${key})
         if (EXISTS ${path})
