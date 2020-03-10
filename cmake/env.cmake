f5f82fad15f563f1585d7b12b1e4cc7e1c129e3e
Bartek Szatkowski <bulislaw@linux.com>
2020-03-10 17:26:58 +0000

    Create a boilerplate and move things around

---
+++ cmake/env.cmake
@@ -0,0 +1,12 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# TODO: @mbed-os-tools This needs to come from tools
+# Build system provides "target" list.
+# Note: This must be placed before subdirectories
+set(MBED_OS_TARGET_LABELS Freescale;MCUXpresso_MCUS;KSDK2_MCUS;FRDM;KPSDK_MCUS;KPSDK_CODE;MCU_K64F;Freescale_EMAC;PSA CACHE INTERNAL "")
+
+# TODO: @mbed-os-tools This needs to come from tools
+set(MBED_OS_TOOLCHAIN GCC_ARM CACHE INTERNAL "")
+
+# TODO: Follow what is for targets above, get somehow from tools what componenets/modules are enabled
+set(MBED_OS_COMPONENTS CACHE INTERNAL "")
