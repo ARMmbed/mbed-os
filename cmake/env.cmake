1fdcdb81fb48468e74b7dc7572825147426100af
Martin Kojtal <martin.kojtal@arm.com>
2020-03-16 12:17:13 +0000

    cmake: move toolchain flag to toolchain gcc file

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -5,8 +5,5 @@
 # Note: This must be placed before subdirectories
 set(MBED_OS_TARGET_LABELS Freescale;MCUXpresso_MCUS;KSDK2_MCUS;FRDM;KPSDK_MCUS;KPSDK_CODE;MCU_K64F;Freescale_EMAC;PSA;CORTEX_M CACHE INTERNAL "")
 
-# TODO: @mbed-os-tools This needs to come from tools
-set(MBED_OS_TOOLCHAIN GCC_ARM CACHE INTERNAL "")
-
 # TODO: Follow what is for targets above, get somehow from tools what componenets/modules are enabled
 set(MBED_OS_COMPONENTS CACHE INTERNAL "")
