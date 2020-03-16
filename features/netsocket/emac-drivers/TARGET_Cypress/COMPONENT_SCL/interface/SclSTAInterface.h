89c70fbd9ae01c25c539bcb0371f988e07a2a2d6
Dustin Crossman <dcro@cypress.com>
2020-03-16 09:40:34 -0700

    Fix licenses in COMPONENT_SCL.

--- features/netsocket/emac-drivers/TARGET_Cypress/COMPONENT_SCL/interface/SclSTAInterface.h
+++ features/netsocket/emac-drivers/TARGET_Cypress/COMPONENT_SCL/interface/SclSTAInterface.h
@@ -1,33 +1,18 @@
 /*
- * (c) (2019-2020), Cypress Semiconductor Corporation. All rights reserved.
+ * Copyright 2018-2020 Cypress Semiconductor Corporation
+ * SPDX-License-Identifier: Apache-2.0
  *
- * This software, including source code, documentation and related materials
- * ("Software"), is owned by Cypress Semiconductor Corporation or one of its
- * subsidiaries ("Cypress") and is protected by and subject to worldwide patent
- * protection (United States and foreign), United States copyright laws and
- * international treaty provisions. Therefore, you may use this Software only
- * as provided in the license agreement accompanying the software package from
- * which you obtained this Software ("EULA").
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * If no EULA applies, Cypress hereby grants you a personal, non-exclusive,
- * non-transferable license to copy, modify, and compile the Software source
- * code solely for use in connection with Cypress' integrated circuit products.
- * Any reproduction, modification, translation, compilation, or representation
- * of this Software except as specified above is prohibited without the express
- * written permission of Cypress.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Disclaimer: THIS SOFTWARE IS PROVIDED AS-IS, WITH NO WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, NONINFRINGEMENT, IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Cypress
- * reserves the right to make changes to the Software without notice. Cypress
- * does not assume any liability arising out of the application or use of the
- * Software or any product or circuit described in the Software. Cypress does
- * not authorize its products for use in any products where a malfunction or
- * failure of the Cypress product may reasonably be expected to result in
- * significant property damage, injury or death ("High Risk Product"). By
- * including Cypress' product in a High Risk Product, the manufacturer of such
- * system or application assumes all risk of such use and in doing so agrees to
- * indemnify Cypress against all liability.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef SCL_STA_INTERFACE_H
