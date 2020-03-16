e1c63fc56d1f7687278613e80b185d40c1c10a79
Martin Kojtal <martin.kojtal@arm.com>
2020-03-16 13:50:18 +0000

    cmake: add RTX toolchain/core logic

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -3,7 +3,9 @@
 # TODO: @mbed-os-tools This needs to come from tools
 # Build system provides "target" list.
 # Note: This must be placed before subdirectories
-set(MBED_OS_TARGET_LABELS Freescale;MCUXpresso_MCUS;KSDK2_MCUS;FRDM;KPSDK_MCUS;KPSDK_CODE;MCU_K64F;Freescale_EMAC;PSA;CORTEX_M CACHE INTERNAL "")
+set(MBED_OS_TARGET_LABELS Freescale;MCUXpresso_MCUS;KSDK2_MCUS;FRDM;KPSDK_MCUS;KPSDK_CODE;MCU_K64F;Freescale_EMAC;PSA;CORTEX_M;RTOS_M4_M7 CACHE INTERNAL "")
 
 # TODO: Follow what is for targets above, get somehow from tools what componenets/modules are enabled
 set(MBED_OS_COMPONENTS CACHE INTERNAL "")
+
+set(MBED_OS_TARGET_CORE M4_M7 CACHE INTERNAL "")
