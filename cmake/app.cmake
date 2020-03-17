355811acc582aa683b2afbf0955353e38c6d5a3f
Martin Kojtal <martin.kojtal@arm.com>
2020-03-17 13:29:38 +0000

    cmake: make app generic
    remove toolchain specific things from it

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -50,15 +50,10 @@ add_subdirectory(gen_config)
 # Link the example libs
 target_link_libraries(app mbed-os gen_config)
 
-get_property(linkerfile GLOBAL PROPERTY MBED_OS_TARGET_LINKER_FILE)
-
 add_custom_command(TARGET app PRE_LINK
-                COMMAND "arm-none-eabi-cpp" -E -P -Wl,--gc-sections -Wl,--wrap,main -Wl,--wrap,_malloc_r -Wl,--wrap,_free_r -Wl,--wrap,_realloc_r -Wl,--wrap,_memalign_r -Wl,--wrap,_calloc_r -Wl,--wrap,exit -Wl,--wrap,atexit -Wl,-n -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -DMBED_ROM_START=0x0 -DMBED_ROM_SIZE=0x100000 -DMBED_RAM_START=0x20000000 -DMBED_RAM_SIZE=0x30000 -DMBED_RAM1_START=0x1fff0000 -DMBED_RAM1_SIZE=0x10000 -DMBED_BOOT_STACK_SIZE=1024 -DXIP_ENABLE=0 ${linkerfile} -o ${CMAKE_CURRENT_BINARY_DIR}/mbed-os-example-blinky_pp.link_script.ld
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/mbed-os-example-blinky_pp.link_script.ld"
+                ${CMAKE_PRE_BUILD_COMMAND}
                 )
 
 add_custom_command(TARGET app POST_BUILD
-                COMMAND ${ELF2BIN} -O binary $<TARGET_FILE:app> $<TARGET_FILE:app>.bin
-                COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.bin"
+                ${CMAKE_POST_BUILD_COMMAND}
                 )
