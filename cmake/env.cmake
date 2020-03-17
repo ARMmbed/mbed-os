5979ea91ffed870e539ba3dfbd0595117e9034c0
Martin Kojtal <martin.kojtal@arm.com>
2020-03-17 14:10:28 +0000

    cmake: move system to env.cmake

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -1,5 +1,14 @@
 # SPDX-License-Identifier: Apache-2.0
 
+# Compiler setup
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+# Project setup
+enable_language(C CXX ASM)
+
 # TODO: @mbed-os-tools This needs to come from tools
 set_property(GLOBAL PROPERTY MBED_OS_COMPONENTS "")
 
