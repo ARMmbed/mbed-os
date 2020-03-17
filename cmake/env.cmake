6522c364d938dc2b1a845e39c44a86190883de6c
Martin Kojtal <martin.kojtal@arm.com>
2020-03-17 13:56:43 +0000

    cmake target: move target definitions

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -1,12 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # TODO: @mbed-os-tools This needs to come from tools
-# Build system provides "target" list.
-# Note: This must be placed before subdirectories
-set_property(GLOBAL PROPERTY MBED_OS_TARGET_LABELS Freescale;MCUXpresso_MCUS;KSDK2_MCUS;FRDM;KPSDK_MCUS;KPSDK_CODE;MCU_K64F;Freescale_EMAC;PSA;CORTEX_M;RTOS_M4_M7)
-
-# TODO: @mbed-os-tools This needs to come from tools
 set_property(GLOBAL PROPERTY MBED_OS_COMPONENTS "")
 
-# Placeholder, use the same command to overwrite provide target linker script file
-set_property(GLOBAL PROPERTY MBED_OS_TARGET_LINKER_FILE "")
