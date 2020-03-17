7aee53448709aebe138718a06563b8d4e43a3a5b
Martin Kojtal <martin.kojtal@arm.com>
2020-03-17 15:52:47 +0000

    cmake: components rename to modules

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -9,8 +9,9 @@ set(CMAKE_CXX_COMPILER_WORKS TRUE)
 # Project setup
 enable_language(C CXX ASM)
 
+# Enabled modules within a project
 # TODO: @mbed-os-tools This needs to come from tools
 # If this is a list like rtos/wifi/networking, we need to figure out the path in Mbed OS (have dictionary or
-# something similar). The best would be if this list contains list of components paths (rtos/ features/ble etc)
-set_property(GLOBAL PROPERTY MBED_OS_COMPONENTS rtos)
+# something similar). The best would be if this list contains list of module paths (rtos/ features/ble etc)
+set_property(GLOBAL PROPERTY MBED_OS_MODULES rtos)
 
