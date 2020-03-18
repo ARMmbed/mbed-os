cf47735495b8577403e6222157ee880209260921
Martin Kojtal <martin.kojtal@arm.com>
2020-03-18 11:40:44 +0000

    cmake: fix linker script generation, needs better clean up

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -27,6 +27,22 @@ add_subdirectory(gen_config)
 # Link the example libs
 target_link_libraries(app mbed-os gen_config)
 
+# I have  to  leave this here as linker is processed after mbed-os added, and can't be in toolchain.cmake
+# as its global symbol is empty at that stage, this needs more work
+# TODO: This property + pre/post should be moved
+get_property(linkerfile GLOBAL PROPERTY MBED_OS_TARGET_LINKER_FILE)
+
+# TODO: get project name to inject into ld
+# TODO: @mbed-os-tools this pre/post build commands should get details from target + profile
+set(CMAKE_PRE_BUILD_COMMAND COMMAND "arm-none-eabi-cpp" -E -P -Wl,--gc-sections -Wl,--wrap,main -Wl,--wrap,_malloc_r -Wl,--wrap,_free_r -Wl,--wrap,_realloc_r -Wl,--wrap,_memalign_r -Wl,--wrap,_calloc_r -Wl,--wrap,exit -Wl,--wrap,atexit -Wl,-n -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -DMBED_ROM_START=0x0 -DMBED_ROM_SIZE=0x100000 -DMBED_RAM_START=0x20000000 -DMBED_RAM_SIZE=0x30000 -DMBED_RAM1_START=0x1fff0000 -DMBED_RAM1_SIZE=0x10000 -DMBED_BOOT_STACK_SIZE=1024 -DXIP_ENABLE=0 ${linkerfile} -o ${CMAKE_CURRENT_BINARY_DIR}/app.link_script.ld
+                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+                BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/app.link_script.ld")
+
+# TODO: @mbed-os-tools this pre/post build commands should get details from target + profile
+set(CMAKE_POST_BUILD_COMMAND COMMAND ${ELF2BIN} -O binary $<TARGET_FILE:app> $<TARGET_FILE:app>.bin
+COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.bin")
+
+
 # Custom pre/post build steps
 add_custom_command(TARGET app PRE_LINK ${CMAKE_PRE_BUILD_COMMAND})
 add_custom_command(TARGET app POST_BUILD ${CMAKE_POST_BUILD_COMMAND})
