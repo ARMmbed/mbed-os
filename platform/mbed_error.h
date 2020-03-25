406e7a2f4ff67a17a643f5bceb3f4a2104c149f9
phst <philipp.steiner@zuehlke.com>
2020-03-25 16:05:55 +0100

    Fix Astyle error 2

--- platform/mbed_error.h
+++ platform/mbed_error.h
@@ -951,7 +951,7 @@ MBED_NORETURN void error(const char *format, ...) MBED_PRINTF(1, 2);
 typedef void (*mbed_error_hook_t)(const mbed_error_ctx *error_ctx);
 
 /**
- * Callback/Error hook function. If application implementation needs to receive this callback when an error is reported, 
+ * Callback/Error hook function. If application implementation needs to receive this callback when an error is reported,
  * mbed_error_hook function should be overridden with custom implementation. When an error happens in the system error handling
  * implementation will invoke this callback with the mbed_error_status_t reported and the error context at the time of error.
  * @param  error_ctx        Error context structure associated with this error.
