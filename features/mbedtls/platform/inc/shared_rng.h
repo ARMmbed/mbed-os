5f19b6b03a71f2f9b6585fb03a573de608a5532d
Antti Kauppila <antti.kauppila@arm.com>
2020-03-31 16:16:30 +0300

    Licence updated

--- features/mbedtls/platform/inc/shared_rng.h
+++ features/mbedtls/platform/inc/shared_rng.h
@@ -1,7 +1,7 @@
 /*
  *  shared_rng.h
  *
- *  Copyright (C) 2019, Arm Limited, All Rights Reserved
+ *  Copyright (C) 2019-2020, Arm Limited, All Rights Reserved
  *  SPDX-License-Identifier: Apache-2.0
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may
