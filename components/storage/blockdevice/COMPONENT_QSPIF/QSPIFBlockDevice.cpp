32d70027a599fe67f94b8c14e0d17a71eea11f7b
Uruloke <38957400+Uruloke@users.noreply.github.com>
2020-05-08 11:13:34 +0200

    Fix setting the QE bit on ISSI Flash memories
    By default QSPIFBlockDevice assumes a Flash memory have two Status registers and when writing the Status registers it writes 2 bytes using the WRSR instruction. This will not work on a ISSI Flash memory as they only have a single status register and attempting to write two bytes using WRSR will cause it to ignore the request and be unable to set the QE bit.
    
    ISSI datasheet specifies that the WRSR instruction only write a single byte so override the number of status registers for ISSI Flash memories.

--- components/storage/blockdevice/COMPONENT_QSPIF/QSPIFBlockDevice.cpp
+++ components/storage/blockdevice/COMPONENT_QSPIF/QSPIFBlockDevice.cpp
@@ -1062,6 +1062,11 @@ int QSPIFBlockDevice::_handle_vendor_quirks()
             _read_status_reg_2_inst = QSPIF_INST_RDCR;
             _attempt_4_byte_addressing = false;
             break;
+        case 0x9d:
+            // ISSI devices have only one status register
+            tr_debug("Applying quirks for ISSI");
+            _num_status_registers = 1;
+            break;
     }
 
     return 0;
@@ -1420,17 +1425,19 @@ qspi_status_t QSPIFBlockDevice::_qspi_read_status_registers(uint8_t *reg_buffer)
 
     // Read Status Register 2 (and beyond, if applicable)
     unsigned int read_length = _num_status_registers - 1; // We already read status reg 1 above
-    status = _qspi_send_general_command(_read_status_reg_2_inst, QSPI_NO_ADDRESS_COMMAND,
-                                        NULL, 0,
-                                        (char *) &reg_buffer[1], read_length);
-    if (QSPI_STATUS_OK == status) {
-        tr_debug("Reading Status Register 2 Success: value = 0x%x", (int) reg_buffer[1]);
-        if (_num_status_registers > 2) {
-            tr_debug("Reading Register 3 Success: value = 0x%x", (int) reg_buffer[2]);
+    if (read_length > 0) {
+        status = _qspi_send_general_command(_read_status_reg_2_inst, QSPI_NO_ADDRESS_COMMAND,
+                                            NULL, 0,
+                                            (char *) &reg_buffer[1], read_length);
+        if (QSPI_STATUS_OK == status) {
+            tr_debug("Reading Status Register 2 Success: value = 0x%x", (int) reg_buffer[1]);
+            if (_num_status_registers > 2) {
+                tr_debug("Reading Register 3 Success: value = 0x%x", (int) reg_buffer[2]);
+            }
+        } else {
+            tr_error("Reading Status Register 2 failed");
+            return status;
         }
-    } else {
-        tr_error("Reading Status Register 2 failed");
-        return status;
     }
 
     return QSPI_STATUS_OK;
