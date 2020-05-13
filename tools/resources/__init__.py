6cf5fc6900cdf7143e45d1755ca90626ecba7329
Jamie Smith <jsmith@crackofdawn.onmicrosoft.com>
2020-05-13 01:10:33 -0700

    Add support for unignore rules in .mbedignore files

--- tools/resources/__init__.py
+++ tools/resources/__init__.py
@@ -425,6 +425,7 @@ class Resources(object):
                 path, base_path, [join(e, "*") for e in exclude_paths])
 
         for root, dirs, files in walk(path, followlinks=True):
+            #print("root=%s, dirs=%s=" % (root, str(dirs)))
             # Check if folder contains .mbedignore
             if IGNORE_FILENAME in files:
                 real_base = relpath(root, base_path)
@@ -434,8 +435,6 @@ class Resources(object):
             root_path = join(relpath(root, base_path))
             if self._ignoreset.is_ignored(join(root_path,"")):
                 self.ignore_dir(join(into_path, root_path))
-                dirs[:] = []
-                continue
 
             for d in copy(dirs):
                 dir_path = join(root, d)
@@ -451,13 +450,21 @@ class Resources(object):
                         relpath(dir_path, base_path)
                     ))
                     dirs.remove(d)
-                elif (d.startswith('.') or d in self._legacy_ignore_dirs or
-                      self._ignoreset.is_ignored(join(root_path, d, ""))):
+
+                # hardcoded ignore rules -- don't want to visit any of these subdirs
+                elif d.startswith('.') or d in self._legacy_ignore_dirs:
                     self.ignore_dir(join(
                         into_path,
                         relpath(dir_path, base_path)
                     ))
                     dirs.remove(d)
+
+                # manual ignore rules -- may want to visit subdirs if they have unignored files
+                elif self._ignoreset.is_ignored(join(root_path, d, "")):
+                    self.ignore_dir(join(
+                        into_path,
+                        relpath(dir_path, base_path)
+                    ))
 
             # Add root to include paths
             root = root.rstrip("/")
@@ -602,3 +609,38 @@ class Resources(object):
         for t in res_filter.file_types:
             self._file_refs[t] = set(filter(
                 res_filter.predicate, self._file_refs[t]))
+
+
+class ResourceFilter(object):
+    def __init__(self, file_types):
+        self.file_types = file_types
+
+    def predicate(self, ref):
+        raise NotImplemented
+
+
+class SpeOnlyResourceFilter(ResourceFilter):
+    def __init__(self):
+        ResourceFilter.__init__(
+            self, [FileType.ASM_SRC, FileType.C_SRC, FileType.CPP_SRC])
+
+    def predicate(self, ref):
+        return 'COMPONENT_SPE' in ref.name
+
+
+class OsAndSpeResourceFilter(ResourceFilter):
+    def __init__(self):
+        ResourceFilter.__init__(
+            self, [FileType.ASM_SRC, FileType.C_SRC, FileType.CPP_SRC])
+
+    def predicate(self, ref):
+        return ROOT in abspath(ref.name) or 'COMPONENT_SPE' in ref.name
+
+
+class PsaManifestResourceFilter(ResourceFilter):
+    def __init__(self):
+        ResourceFilter.__init__(
+            self, [FileType.JSON])
+
+    def predicate(self, ref):
+        return not ref.name.endswith('_psa.json')
--- tools/resources/ignore.py
+++ tools/resources/ignore.py
@@ -32,10 +32,21 @@ class MbedIgnoreSet(object):
     def __init__(self):
         self._ignore_patterns = []
         self._ignore_regex = re.compile("$^")
+        self._unignore_patterns = []
+        self._unignore_regex = re.compile("$^")
 
     def is_ignored(self, file_path):
         """Check if file path is ignored by any .mbedignore thus far"""
-        return self._ignore_regex.match(normcase(file_path))
+        ignore_match = self._ignore_regex.match(normcase(file_path))
+        unignore_match = self._unignore_regex.match(normcase(file_path))
+
+        if not ignore_match:
+            return False
+
+        if unignore_match is not None and len(unignore_match.group(0)) >= len(ignore_match.group(0)):
+            return False
+
+        return True
 
     def add_ignore_patterns(self, in_name, patterns):
         """Ignore all files and directories matching the paterns in 
@@ -45,14 +56,43 @@ class MbedIgnoreSet(object):
         in_name - the filename prefix that this ignore will apply to
         patterns - the list of patterns we will ignore in the future
         """
+
+        if len(patterns) == 0:
+            return
+
         if in_name == ".":
             self._ignore_patterns.extend(normcase(p) for p in patterns)
         else:
             self._ignore_patterns.extend(
                 normcase(join(in_name, pat)) for pat in patterns)
-        if self._ignore_patterns:
-            self._ignore_regex = re.compile("|".join(
-                fnmatch.translate(p) for p in self._ignore_patterns))
+
+        self._ignore_regex = re.compile("|".join(
+            fnmatch.translate(p) for p in self._ignore_patterns))
+
+    def add_unignore_patterns(self, in_name, patterns):
+        """Un-ignore all files and directories matching the patterns in
+        directories named by in_name.
+
+        Un-ignore rules take precedence based on depth.  So ignoring
+        a/b/* then unignoring a/b/c.cpp would allow c.cpp to build.
+        But unignoring a/* then ignoring a/b/* would prevent c.cpp from building.
+
+        Positional arguments:
+        in_name - the filename prefix that this unignore will apply to
+        patterns - the list of patterns we will unignore in the future
+        """
+
+        if len(patterns) == 0:
+            return
+
+        if in_name == ".":
+            self._unignore_patterns.extend(normcase(p) for p in patterns)
+        else:
+            self._unignore_patterns.extend(
+                normcase(join(in_name, pat)) for pat in patterns)
+
+        self._unignore_regex = re.compile("|".join(
+            fnmatch.translate(p) for p in self._unignore_patterns))
 
     def add_mbedignore(self, in_name, filepath):
         """Add a series of patterns to the ignored paths
@@ -62,8 +102,25 @@ class MbedIgnoreSet(object):
         patterns - the list of patterns we will ignore in the future
         """
         with open (filepath) as f:
-            patterns = [l.strip() for l in f
-                        if l.strip() != "" and not l.startswith("#")]
-            self.add_ignore_patterns(in_name, patterns)
+
+            ignore_patterns = []
+            unignore_patterns = []
+
+            for line in f:
+                line_text = line.strip()
+
+                if line_text == "" or line_text.startswith("#"):
+                    # no content on this line
+                    continue
+
+                if line_text.startswith("!"):
+                    # unignore rule
+                    unignore_patterns.append(line_text[1:])
+                else:
+                    # ignore rule
+                    ignore_patterns.append(line_text)
+
+            self.add_ignore_patterns(in_name, ignore_patterns)
+            self.add_unignore_patterns(in_name, unignore_patterns)
 
 
