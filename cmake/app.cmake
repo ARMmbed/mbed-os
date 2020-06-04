1e156fdae5914b42e727ca37515e5a40bd0757b9
Hugues Kamba <hugues.kamba@arm.com>
2020-06-04 13:13:07 +0100

    cmake: rename KEYWORD to PREFIX
    Manually applied https://github.com/0xc0170/mbed-os/commit/290cfcd49b8fa0e002905134582737f85d1718df

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -4,11 +4,13 @@
 #  thus this should be done genericaly
 # Add TARGET_ directory to the CMake tree
 
-# KEYWORD - TARGET_ or similar
+# PREFIX - TARGET or similar (can be empty string)
 # KEYWORD_LIST - list of labels that are used to include directories
-function(mbed_add_cmake_directory KEYWORD KEYWORDLIST)
+function(mbed_add_cmake_directory PREFIX KEYWORDLIST)
     foreach(key ${KEYWORDLIST})
-        string(PREPEND key ${KEYWORD} "_")
+        if(NOT "${PREFIX}" STREQUAL "")
+            string(PREPEND key ${PREFIX} "_")
+        endif()
         # assumption: relative path, no need to check for absolute here
         set(path ${CMAKE_CURRENT_SOURCE_DIR}/${key})
         if (EXISTS ${path})
