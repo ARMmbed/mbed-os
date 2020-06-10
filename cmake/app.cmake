18f189d1250f85e886dd257650996bb83f5b8d2e
Hugues Kamba <hugues.kamba@arm.com>
2020-06-10 18:55:01 +0100

    CMake: Add support for NRF52840_DK
    * Ensure the target specific code can be successfully built
    * Ensure all features supported by the target can successfully build
    * Ensure elf2bin tool is called correctly for each supported toolchain
    * Add ability to generate *.hex along with *.bin file

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -43,13 +43,36 @@ get_property(linkerfile GLOBAL PROPERTY MBED_OS_TARGET_LINKER_FILE)
 
 # TODO: get project name to inject into ld
 # TODO: @mbed-os-tools this pre/post build commands should get details from target + profile
-set(CMAKE_PRE_BUILD_COMMAND COMMAND "arm-none-eabi-cpp" -E -P -Wl,--gc-sections -Wl,--wrap,main -Wl,--wrap,_malloc_r -Wl,--wrap,_free_r -Wl,--wrap,_realloc_r -Wl,--wrap,_memalign_r -Wl,--wrap,_calloc_r -Wl,--wrap,exit -Wl,--wrap,atexit -Wl,-n -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -DMBED_ROM_START=0x0 -DMBED_ROM_SIZE=0x100000 -DMBED_RAM_START=0x20000000 -DMBED_RAM_SIZE=0x30000 -DMBED_RAM1_START=0x1fff0000 -DMBED_RAM1_SIZE=0x10000 -DMBED_BOOT_STACK_SIZE=1024 -DXIP_ENABLE=0 ${linkerfile} -o ${CMAKE_CURRENT_BINARY_DIR}/app.link_script.ld
-                WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-                BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/app.link_script.ld")
+set(CMAKE_PRE_BUILD_COMMAND
+    COMMAND "arm-none-eabi-cpp" -E -P -Wl,--gc-sections -Wl,--wrap,main -Wl,--wrap,_malloc_r -Wl,--wrap,_free_r -Wl,--wrap,_realloc_r -Wl,--wrap,_memalign_r -Wl,--wrap,_calloc_r -Wl,--wrap,exit -Wl,--wrap,atexit -Wl,-n -mcpu=cortex-m4 -mthumb -mfpu=fpv4-sp-d16 -mfloat-abi=softfp -DMBED_ROM_START=0x0 -DMBED_ROM_SIZE=0x100000 -DMBED_RAM_START=0x20000000 -DMBED_RAM_SIZE=0x30000 -DMBED_RAM1_START=0x1fff0000 -DMBED_RAM1_SIZE=0x10000 -DMBED_BOOT_STACK_SIZE=1024 -DXIP_ENABLE=0 ${linkerfile} -o ${CMAKE_CURRENT_BINARY_DIR}/app.link_script.ld
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/app.link_script.ld"
+)
 
 # TODO: @mbed-os-tools this pre/post build commands should get details from target + profile
-set(CMAKE_POST_BUILD_COMMAND COMMAND ${ELF2BIN} -O binary $<TARGET_FILE:app> $<TARGET_FILE:app>.bin
-COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.bin")
+if(MBED_OS_TOOLCHAIN STREQUAL "GCC_ARM")
+    set(CMAKE_POST_BUILD_COMMAND
+        COMMAND ${ELF2BIN} -O binary $<TARGET_FILE:app> $<TARGET_FILE:app>.bin
+        COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.bin"
+        COMMAND ${ELF2BIN} -O ihex $<TARGET_FILE:app> $<TARGET_FILE:app>.hex
+        COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.hex"
+    )
+elseif(MBED_OS_TOOLCHAIN STREQUAL "ARM")
+    set(CMAKE_POST_BUILD_COMMAND
+        COMMAND ${ELF2BIN} --bin  -o $<TARGET_FILE:app>.bin $<TARGET_FILE:app>
+        COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.bin"
+        COMMAND ${ELF2BIN} --i32combined  -o $<TARGET_FILE:app>.hex $<TARGET_FILE:app>
+        COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.hex"
+    )
+elseif(MBED_OS_TOOLCHAIN STREQUAL "IAR")
+    set(CMAKE_POST_BUILD_COMMAND
+        COMMAND ${ELF2BIN} --bin $<TARGET_FILE:app> $<TARGET_FILE:app>.bin
+        COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.bin"
+        COMMAND ${ELF2BIN} --ihex $<TARGET_FILE:app> $<TARGET_FILE:app>.hex
+        COMMAND ${CMAKE_COMMAND} -E echo "-- built: $<TARGET_FILE:app>.hex"
+    )
+endif()
+
 
 
 # Custom pre/post build steps
