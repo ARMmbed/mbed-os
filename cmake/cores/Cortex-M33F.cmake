b2a0949a3bdea86af9f8af37fcad325833c609cd
Rob Walton <rob.walton@arm.com>
2020-07-09 14:58:29 +0100

    Add per-core cmake files

---
+++ cmake/cores/Cortex-M33F.cmake
@@ -0,0 +1,54 @@
+# Copyright (c) 2020 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+if(MBED_TOOLCHAIN strequal "GCC_ARM")
+
+set(GCC_FLAGS
+    -mthumb
+    -mfpu=fpv5-sp-d16
+    -mfloat-abi=softfp
+    -march=armv8-m.main
+)
+
+set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS}
+    ${GCC_FLAGS}
+)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS}
+    ${GCC_FLAGS}
+)
+set(CMAKE_ASM_FLAGS ${CMAKE_ASM_FLAGS}
+    ${GCC_FLAGS}
+)
+set(CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_LINK_FLAGS}
+    ${GCC_FLAGS}
+)
+
+elseif(MBED_TOOLCHAIN strequal "ARMC6")
+
+set(ARM_FLAGS
+    --cpu=Cortex-M33F
+)
+
+set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS}
+    ${ARM_FLAGS}
+)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS}
+    ${ARM_FLAGS}
+)
+set(CMAKE_ASM_FLAGS ${CMAKE_ASM_FLAGS}
+    ${ARM_FLAGS}
+)
+set(CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_LINK_FLAGS}
+    ${ARM_FLAGS}
+)
+
+endif()
+
+add_definitions(
+    __CORTEX_M33
+    ARM_MATH_ARMV8MML
+    DOMAIN_NS=1
+    __FPU_PRESENT=1U
+    __CMSIS_RTOS
+    __MBED_CMSIS_RTOS_CM
+)
---
+++ cmake/cores/Cortex-M4F.cmake
@@ -0,0 +1,53 @@
+# Copyright (c) 2020 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+if(MBED_TOOLCHAIN strequal "GCC_ARM")
+
+set(GCC_FLAGS
+    -mthumb
+    -mcpu=cortex-m4
+    -mfpu=fpv4-sp-d16
+    -mfloat-abi=softfp
+)
+
+set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS}
+    ${GCC_FLAGS}
+)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS}
+    ${GCC_FLAGS}
+)
+set(CMAKE_ASM_FLAGS ${CMAKE_ASM_FLAGS}
+    ${GCC_FLAGS}
+)
+set(CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_LINK_FLAGS}
+    ${GCC_FLAGS}
+)
+
+elseif(MBED_TOOLCHAIN strequal "ARMC6")
+
+set(ARM_FLAGS
+    --cpu=Cortex-M4.fp.sp
+)
+
+set(CMAKE_C_FLAGS ${CMAKE_C_FLAGS}
+    ${ARM_FLAGS}
+)
+set(CMAKE_CXX_FLAGS ${CMAKE_CXX_FLAGS}
+    ${ARM_FLAGS}
+)
+set(CMAKE_ASM_FLAGS ${CMAKE_ASM_FLAGS}
+    ${ARM_FLAGS}
+)
+set(CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_LINK_FLAGS}
+    ${ARM_FLAGS}
+)
+
+endif()
+
+add_definitions(
+    __CORTEX_M4
+    ARM_MATH_CM4
+    __FPU_PRESENT=1
+    __CMSIS_RTOS
+    __MBED_CMSIS_RTOS_CM
+)
