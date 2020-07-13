9aa52b75361422a5a47c1c08e71da41c1aa4e86d
Jaeden Amero <jaeden.amero@arm.com>
2020-07-13 17:02:20 +0100

    cmake: Add top-level profile selecting cmake file

--- cmake/app.cmake
+++ cmake/app.cmake
@@ -5,6 +5,7 @@ include(${MBED_ROOT}/cmake/toolchain.cmake)
 include(${MBED_ROOT}/cmake/env.cmake)
 include(${MBED_ROOT}/cmake/util.cmake)
 include(${MBED_ROOT}/cmake/core.cmake)
+include(${MBED_ROOT}/cmake/profile.cmake)
 
 # if the environment does not specify build type, set to Debug
 if(NOT CMAKE_BUILD_TYPE)
