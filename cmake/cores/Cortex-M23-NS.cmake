f292de6e21a88e65b4cf77b383f96b07684e4676
Hugues Kamba <hugues.kamba@arm.com>
2020-07-20 17:52:23 +0100

    CMake: Add CMake scripts

---
+++ cmake/cores/Cortex-M23-NS.cmake
@@ -0,0 +1,51 @@
+# Copyright (c) 2020 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set(CMAKE_SYSTEM_PROCESSOR cortex-m23)
+
+if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+
+set(GCC_FLAGS " \
+    -mthumb \
+")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
+    ${GCC_FLAGS} \
+")
+
+elseif(MBED_TOOLCHAIN STREQUAL "ARMC6")
+
+set(ARM_FLAGS " \
+    --cpu=Cortex-M23 \
+")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
+    ${ARM_FLAGS} \
+")
+
+endif()
+
+add_definitions(
+    -D__CORTEX_M23
+    -DARM_MATH_ARMV8MBL
+    -D__CMSIS_RTOS
+    -D__MBED_CMSIS_RTOS_CM
+)
---
+++ cmake/cores/Cortex-M33F-NS.cmake
@@ -0,0 +1,55 @@
+# Copyright (c) 2020 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+
+if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+
+set(GCC_FLAGS " \
+    -mthumb \
+    -mfpu=fpv5-sp-d16 \
+    -mfloat-abi=softfp \
+    -march=armv8-m.main \
+")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
+    ${GCC_FLAGS} \
+")
+
+elseif(MBED_TOOLCHAIN STREQUAL "ARMC6")
+
+set(ARM_FLAGS " \
+    --cpu=Cortex-M33F \
+")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
+    ${ARM_FLAGS} \
+")
+
+endif()
+
+add_definitions(
+    -D__CORTEX_M33
+    -DARM_MATH_ARMV8MML
+    -D__FPU_PRESENT=1U
+    -D__CMSIS_RTOS
+    -D__MBED_CMSIS_RTOS_CM
+)
