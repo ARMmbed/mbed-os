90ca9122801ea00b3f9139b1da480a3fb36a48f4
Hugues Kamba <hugues.kamba@arm.com>
2020-07-20 17:52:23 +0100

    CMake: Add CMake scripts

---
+++ cmake/env.cmake
@@ -0,0 +1,16 @@
+# SPDX-License-Identifier: Apache-2.0
+
+# Compiler setup
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_CROSSCOMPILING TRUE)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+# Project setup
+enable_language(C CXX ASM)
+
+# Enabled components within a project
+# TODO: @mbed-os-tools This needs to come from tools
+# If this is a list like rtos/wifi/networking, we need to figure out the path in Mbed OS (have dictionary or
+# something similar). The best would be if this list contains list of module paths (rtos/ features/ble etc)
+set_property(GLOBAL PROPERTY MBED_COMPONENTS "")
