a7d5c55777e4a13dcd83316131017f046de2aef3
Hugues Kamba <hugues.kamba@arm.com>
2020-07-20 17:52:23 +0100

    CMake: Add CMake scripts

---
+++ cmake/cores/Cortex-M0.cmake
@@ -0,0 +1,51 @@
+# Copyright (c) 2020 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+set(CMAKE_SYSTEM_PROCESSOR cortex-m0)
+
+if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+
+set(GCC_FLAGS " \
+    -mthumb \
+")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
+    ${GCC_FLAGS} \
+")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
+    ${GCC_FLAGS} \
+")
+
+elseif(MBED_TOOLCHAIN STREQUAL "ARMC6")
+
+set(ARM_FLAGS " \
+    --cpu=Cortex-M0 \
+")
+
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
+    ${ARM_FLAGS} \
+")
+set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
+    ${ARM_FLAGS} \
+")
+
+endif()
+
+add_definitions(
+    -D__CORTEX_M0
+    -DARM_MATH_CM0
+    -D__CMSIS_RTOS
+    -D__MBED_CMSIS_RTOS_CM
+)
