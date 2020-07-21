3b7e059232111b2ea87c677c05586b94bba39576
Devaraj Ranganna <devaraj.ranganna@arm.com>
2020-07-21 13:39:27 +0100

    cmake: Using Arm compiler from Mbed Studio
    Added a command line option `-DMBEDIDE=ON` which allows using Arm
    compiler shipped with Mbed Studio on local machines.
    
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,4 +1,8 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+option(MBEDIDE "Use Arm compiler from Mbed Studio" OFF)
+if(MBEDIDE)
+    set(MBED_STUDIO_ARM_COMPILER "--ide=mbed")
+endif()
 include(${MBED_ROOT}/cmake/toolchains/${MBED_TOOLCHAIN}.cmake)
