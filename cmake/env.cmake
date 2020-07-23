16a57ddb5bc5fc31f341202802c2505f31e76607
Martin Kojtal <martin.kojtal@arm.com>
2020-07-23 14:11:22 +0100

    CMake env: remove components, needs design first

--- cmake/env.cmake
+++ cmake/env.cmake
@@ -9,9 +9,3 @@ set(CMAKE_CXX_COMPILER_WORKS TRUE)
 
 # Project setup
 enable_language(C CXX ASM)
-
-# Enabled components within a project
-# TODO: @mbed-os-tools This needs to come from tools
-# If this is a list like rtos/wifi/networking, we need to figure out the path in Mbed OS (have dictionary or
-# something similar). The best would be if this list contains list of module paths (rtos/ features/ble etc)
-set_property(GLOBAL PROPERTY MBED_COMPONENTS "")
