cd9d52d3d816aad9a5b18096501da5b987c7b220
Hugues Kamba <hugues.kamba@arm.com>
2020-07-23 20:23:56 +0100

    CMake: Use modern CMake techniques to set toolchain options
    Use target_compile_options(), target_compile_definitions(),
    and target_link_options() to set toolchain options.

--- cmake/profiles/develop.cmake
+++ cmake/profiles/develop.cmake
@@ -1,57 +1,87 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set(DEVELOP_DEFINITIONS
-    -DMBED_TRAP_ERRORS_ENABLED=1
-)
+list(APPEND link_options)
 
 if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+    list(APPEND c_compile_options
+        "-c"
+        "-std=gnu11"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${c_compile_options}>
+    )
 
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    -c \
-    -std=gnu11 \
-    ")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}  \
-    -std=gnu++14 \
-    -fno-rtti \
-    -Wvla \
-    ")
-    set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    -x assembler-with-cpp \
-    ")
-    set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    -Wl,--gc-sections \
-    -Wl,--wrap,main \
-    -Wl,--wrap,_malloc_r \
-    -Wl,--wrap,_free_r \
-    -Wl,--wrap,_realloc_r \
-    -Wl,--wrap,_memalign_r \
-    -Wl,--wrap,_calloc_r \
-    -Wl,--wrap,exit \
-    -Wl,--wrap,atexit \
-    -Wl,-n \
-    ")
+    list(APPEND cxx_compile_options
+        "-std=gnu++14"
+        "-fno-rtti"
+        "-Wvla"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:CXX>:${cxx_compile_options}>
+    )
 
+    list(APPEND asm_compile_options
+        "-x" "assembler-with-cpp"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:ASM>:${asm_compile_options}>
+    )
+
+    list(APPEND link_options
+        "-Wl,--gc-sections"
+        "-Wl,--wrap,main"
+        "-Wl,--wrap,_malloc_r"
+        "-Wl,--wrap,_free_r"
+        "-Wl,--wrap,_realloc_r"
+        "-Wl,--wrap,_memalign_r"
+        "-Wl,--wrap,_calloc_r"
+        "-Wl,--wrap,exit"
+        "-Wl,--wrap,atexit"
+        "-Wl,-n"
+    )
 elseif(MBED_TOOLCHAIN STREQUAL "ARM")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-        -std=gnu11 \
-    ")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-        -fno-rtti \
-        -fno-c++-static-destructors \
-        -std=gnu++14 \
-    ")
-    set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-        --show_full_path \
-        --legacyalign \
-        --inline \
-        --any_contingency \
-        --keep=os_cb_sections \
-    ")
+    list(APPEND c_compile_options
+        "-std=gnu11"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${c_compile_options}>
+    )
 
-    set(DEVELOP_DEFINITIONS ${DEVELOP_DEFINITIONS}
-        -D__ASSERT_MSG
+    list(APPEND cxx_compile_options
+        "-std=gnu++14"
+        "-fno-rtti"
+        "-fno-c++-static-destructors"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:CXX>:${cxx_compile_options}>
+    )
+
+    list(APPEND link_options
+        "--show_full_path"
+        "--legacyalign"
+        "--inline"
+        "--any_contingency"
+        "--keep=os_cb_sections"
+    )
+
+    target_compile_definitions(mbed-os
+        PUBLIC
+            __ASSERT_MSG
     )
 endif()
 
-add_definitions(${DEVELOP_DEFINITIONS})
+target_compile_definitions(mbed-os
+    PUBLIC
+        MBED_TRAP_ERRORS_ENABLED=1
+)
+
+target_link_options(mbed-os
+    PUBLIC
+        ${link_options}
+)
