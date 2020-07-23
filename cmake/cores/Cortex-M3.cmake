d661b60987f199249233008f1cb3088fed418eda
Hugues Kamba <hugues.kamba@arm.com>
2020-07-23 20:23:56 +0100

    CMake: Use modern CMake techniques to set toolchain options
    Use target_compile_options(), target_compile_definitions(),
    and target_link_options() to set toolchain options.

--- cmake/cores/Cortex-M3.cmake
+++ cmake/cores/Cortex-M3.cmake
@@ -4,48 +4,41 @@
 set(CMAKE_SYSTEM_PROCESSOR cortex-m3)
 
 if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+    list(APPEND common_toolchain_options
+        "-mthumb"
+    )
 
-set(GCC_FLAGS " \
-    -mthumb \
-")
+    target_compile_options(mbed-os
+        PUBLIC
+            ${common_toolchain_options}
+    )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${GCC_FLAGS} \
-")
-
+    target_link_options(mbed-os
+        PUBLIC
+            ${common_toolchain_options}
+    )
 elseif(MBED_TOOLCHAIN STREQUAL "ARM")
+    list(APPEND compile_options
+        "-mcpu=cortex-m3"
+    )
 
-set(ARM_FLAGS " \
-    -mcpu=cortex-m3 \
-")
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${compile_options}>
+            $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
+            $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M3>
+    )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${ARM_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${ARM_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    --cpu=Cortex-M3 \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    --cpu=Cortex-M3 \
-")
-
+    target_link_options(mbed-os
+        PUBLIC
+            "--cpu=Cortex-M3"
+    )
 endif()
 
-add_definitions(
-    -D__CORTEX_M3
-    -DARM_MATH_CM3
-    -D__CMSIS_RTOS
-    -D__MBED_CMSIS_RTOS_CM
+target_compile_definitions(mbed-os
+    PUBLIC
+        __CORTEX_M3
+        ARM_MATH_CM3
+        __CMSIS_RTOS
+        __MBED_CMSIS_RTOS_CM
 )
