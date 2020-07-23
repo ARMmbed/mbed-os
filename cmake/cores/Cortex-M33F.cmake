32e65468d7aa220c80dbf8ebbb407c8b5af61c09
Hugues Kamba <hugues.kamba@arm.com>
2020-07-23 20:23:56 +0100

    CMake: Use modern CMake techniques to set toolchain options
    Use target_compile_options(), target_compile_definitions(),
    and target_link_options() to set toolchain options.

--- cmake/cores/Cortex-M33F.cmake
+++ cmake/cores/Cortex-M33F.cmake
@@ -4,55 +4,48 @@
 set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
 
 if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+    list(APPEND common_toolchain_options
+        "-mthumb"
+        "-mfpu=fpv5-sp-d16"
+        "-mfloat-abi=softfp"
+        "-march=armv8-m.main"
+    )
 
-set(GCC_FLAGS " \
-    -mthumb \
-    -mfpu=fpv5-sp-d16 \
-    -mfloat-abi=softfp \
-    -march=armv8-m.main \
-")
+    target_compile_options(mbed-os
+        PUBLIC
+            ${common_toolchain_options}
+    )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${GCC_FLAGS} \
-")
-
+    target_link_options(mbed-os
+        PUBLIC
+            ${common_toolchain_options}
+    )
 elseif(MBED_TOOLCHAIN STREQUAL "ARM")
+    list(APPEND compile_options
+        "-mcpu=cortex-m33+nodsp"
+        "-mfpu=fpv5-sp-d16"
+        "-mfloat-abi=hard"
+    )
 
-set(ARM_FLAGS " \
-    -mcpu=cortex-m33+nodsp \
-    -mfpu=fpv5-sp-d16 \
-    -mfloat-abi=hard \
-")
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${compile_options}>
+            $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
+            $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M33.no_dsp>
+    )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${ARM_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${ARM_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    --cpu=Cortex-M33.no_dsp \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    --cpu=Cortex-M33.no_dsp \
-")
-
+    target_link_options(mbed-os
+        PUBLIC
+            "--cpu=Cortex-M33.no_dsp"
+    )
 endif()
 
-add_definitions(
-    -D__CORTEX_M33
-    -DARM_MATH_ARMV8MML
-    -DDOMAIN_NS=1
-    -D__FPU_PRESENT=1U
-    -D__CMSIS_RTOS
-    -D__MBED_CMSIS_RTOS_CM
+target_compile_definitions(mbed-os
+    PUBLIC
+        __CORTEX_M33
+        ARM_MATH_ARMV8MML
+        DOMAIN_NS=1
+        __FPU_PRESENT=1U
+        __CMSIS_RTOS
+        __MBED_CMSIS_RTOS_CM
 )
--- cmake/toolchains/GCC_ARM.cmake
+++ cmake/toolchains/GCC_ARM.cmake
@@ -5,49 +5,49 @@ set(CMAKE_ASM_COMPILER "arm-none-eabi-gcc")
 set(CMAKE_C_COMPILER "arm-none-eabi-gcc")
 set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
 set(ELF2BIN "arm-none-eabi-objcopy")
-set(LD_SYS_LIBS " \
-    -Wl,--start-group \
-    -lstdc++ \
-    -lsupc++ \
-    -lm \
-    -lc \
-    -lgcc \
-    -lnosys \
-    -Wl,--end-group \
-")
-set(COMMON_FLAGS " \
-    -Wall \
-    -Wextra \
-    -Wno-unused-parameter \
-    -Wno-missing-field-initializers \
-    -fmessage-length=0 \
-    -fno-exceptions \
-    -ffunction-sections \
-    -fdata-sections \
-    -funsigned-char \
-    -MMD \
-    -fomit-frame-pointer \
-    -Os \
-    -g \
-")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${COMMON_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${COMMON_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    ${COMMON_FLAGS} \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${COMMON_FLAGS} \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${LD_SYS_LIBS} \
-    -T ${CMAKE_BINARY_DIR}/app.link_script.ld \
-")
 
-add_definitions(
-    -DTOOLCHAIN_GCC_ARM
-    -DTOOLCHAIN_GCC
+list(APPEND link_options
+    "-Wl,--start-group"
+    "-lstdc++"
+    "-lsupc++"
+    "-lm"
+    "-lc"
+    "-lgcc"
+    "-lnosys"
+    "-Wl,--end-group"
+    "-T"
+    "${CMAKE_BINARY_DIR}/app.link_script.ld"
+)
+
+list(APPEND common_options
+    "-Wall"
+    "-Wextra"
+    "-Wno-unused-parameter"
+    "-Wno-missing-field-initializers"
+    "-fmessage-length=0"
+    "-fno-exceptions"
+    "-ffunction-sections"
+    "-fdata-sections"
+    "-funsigned-char"
+    "-MMD"
+    "-fomit-frame-pointer"
+    "-Os"
+    "-g"
+)
+
+target_compile_options(mbed-os
+    PUBLIC
+        ${common_options}
+)
+
+target_compile_definitions(mbed-os
+    PUBLIC
+        TOOLCHAIN_GCC_ARM
+        TOOLCHAIN_GCC
+)
+
+target_link_options(mbed-os
+    PUBLIC
+        ${common_options}
+        ${link_options}
 )
