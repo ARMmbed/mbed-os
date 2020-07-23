fa48622f9712d3f19dc84eb26a5c7945c8812e6c
Hugues Kamba <hugues.kamba@arm.com>
2020-07-23 20:23:56 +0100

    CMake: Use modern CMake techniques to set toolchain options
    Use target_compile_options(), target_compile_definitions(),
    and target_link_options() to set toolchain options.

--- cmake/toolchains/ARM.cmake
+++ cmake/toolchains/ARM.cmake
@@ -6,35 +6,45 @@ set(CMAKE_C_COMPILER      "armclang")
 set(CMAKE_CXX_COMPILER    "armclang")
 set(CMAKE_AR              "armar")
 set(ELF2BIN               "fromelf")
-set(COMMON_FLAGS " \
-    ${MBED_STUDIO_ARM_COMPILER} \
-    -c \
-    --target=arm-arm-none-eabi \
-    -Oz \
-    -Wno-armcc-pragma-push-pop \
-    -Wno-armcc-pragma-anon-unions \
-    -Wno-reserved-user-defined-literal \
-    -Wno-deprecated-register \
-    -fdata-sections \
-    -fno-exceptions \
-    -fshort-enums \
-    -fshort-wchar \
-")
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${COMMON_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${COMMON_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    ${MBED_STUDIO_ARM_COMPILER} \
-    --cpreproc \
-    --cpreproc_opts=--target=arm-arm-none-eabi,-mcpu=cortex-m4,-D,__FPU_PRESENT,-D,MBED_CONF_PLATFORM_CRASH_CAPTURE_ENABLED \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${MBED_STUDIO_ARM_COMPILER} \
-")
 
-add_definitions(
-    -DTOOLCHAIN_ARM
+list(APPEND common_options
+    "${MBED_STUDIO_ARM_COMPILER}"
+    "-c"
+    "--target=arm-arm-none-eabi"
+    "-Oz"
+    "-Wno-armcc-pragma-push-pop"
+    "-Wno-armcc-pragma-anon-unions"
+    "-Wno-reserved-user-defined-literal"
+    "-Wno-deprecated-register"
+    "-fdata-sections"
+    "-fno-exceptions"
+    "-fshort-enums"
+    "-fshort-wchar"
+)
+
+target_compile_options(mbed-os
+    PUBLIC
+        $<$<COMPILE_LANGUAGE:C>:${common_options}>
+)
+
+target_compile_options(mbed-os
+    PUBLIC
+        $<$<COMPILE_LANGUAGE:CXX>:${common_options}>
+)
+
+target_compile_options(mbed-os
+    PUBLIC
+        $<$<COMPILE_LANGUAGE:ASM>:${MBED_STUDIO_ARM_COMPILER}>
+        $<$<COMPILE_LANGUAGE:ASM>:--cpreproc>
+        $<$<COMPILE_LANGUAGE:ASM>:--cpreproc_opts=--target=arm-arm-none-eabi,-mcpu=cortex-m4,-D,__FPU_PRESENT,-D,MBED_CONF_PLATFORM_CRASH_CAPTURE_ENABLED>
+)
+
+target_compile_definitions(mbed-os
+    PUBLIC
+        TOOLCHAIN_ARM
+)
+
+target_link_options(mbed-os
+    PUBLIC
+        ${MBED_STUDIO_ARM_COMPILER}
 )
