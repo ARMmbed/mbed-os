45a22228e5e35dc69f72e71c7a5037ead5fefb3e
Hugues Kamba <hugues.kamba@arm.com>
2020-07-23 20:23:56 +0100

    CMake: Use modern CMake techniques to set toolchain options
    Use target_compile_options(), target_compile_definitions(),
    and target_link_options() to set toolchain options.

--- cmake/cores/Cortex-M23-NS.cmake
+++ cmake/cores/Cortex-M23-NS.cmake
@@ -4,48 +4,41 @@
 set(CMAKE_SYSTEM_PROCESSOR cortex-m23)
 
 if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
+    list(APPEND common_toolchain_options
+        "-mthumb"
+    )
 
-set(GCC_FLAGS " \
-    -mthumb \
-")
+    target_compile_options(mbed-os
+        PUBLIC
+            ${common_toolchain_options}
+    )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    ${GCC_FLAGS} \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${GCC_FLAGS} \
-")
-
+    target_link_options(mbed-os
+        PUBLIC
+            ${common_toolchain_options}
+    )
 elseif(MBED_TOOLCHAIN STREQUAL "ARM")
+    list(APPEND compile_options
+        "-mcpu=cortex-m23"
+    )
 
-set(ARM_FLAGS " \
-    -mcpu=cortex-m23 \
-")
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${compile_options}>
+            $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
+            $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M23>
+    )
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-    ${ARM_FLAGS} \
-")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-    ${ARM_FLAGS} \
-")
-set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    --cpu=Cortex-M23 \
-")
-set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    --cpu=Cortex-M23 \
-")
-
+    target_link_options(mbed-os
+        PUBLIC
+            "--cpu=Cortex-M23"
+    )
 endif()
 
-add_definitions(
-    -D__CORTEX_M23
-    -DARM_MATH_ARMV8MBL
-    -D__CMSIS_RTOS
-    -D__MBED_CMSIS_RTOS_CM
+target_compile_definitions(mbed-os
+    PUBLIC
+        __CORTEX_M23
+        ARM_MATH_ARMV8MBL
+        __CMSIS_RTOS
+        __MBED_CMSIS_RTOS_CM
 )
--- cmake/profiles/release.cmake
+++ cmake/profiles/release.cmake
@@ -1,57 +1,89 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set(RELEASE_DEFINITIONS
-    -DNDEBUG
-)
+list(APPEND link_options)
 
 if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-        -c \
-        -std=gnu11 \
-    ")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-        -c \
-        -std=gnu++14 \
-        -fno-rtti \
-        -Wvla \
-    ")
-    set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-        -c \
-        -x assembler-with-cpp \
-    ")
-    set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-        -Wl,--gc-sections \
-        -Wl,--wrap,main \
-        -Wl,--wrap,_malloc_r \
-        -Wl,--wrap,_free_r \
-        -Wl,--wrap,_realloc_r \
-        -Wl,--wrap,_memalign_r \
-        -Wl,--wrap,_calloc_r \
-        -Wl,--wrap,exit \
-        -Wl,--wrap,atexit \
-        -Wl,-n \
-    ")
-elseif(MBED_TOOLCHAIN STREQUAL "ARM")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
-        -std=gnu11 \
-    ")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-        -fno-rtti \
-        -fno-c++-static-destructors \
-        -std=gnu++14 \
-    ")
-    set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-        --show_full_path \
-        --legacyalign \
-        --inline \
-        --any_contingency \
-        --keep=os_cb_sections \
-    ")
+    list(APPEND c_compile_options
+        "-c"
+        "-std=gnu11"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${c_compile_options}>
+    )
 
-    set(RELEASE_DEFINITIONS ${RELEASE_DEFINITIONS}
-        -D__ASSERT_MSG
+    list(APPEND cxx_compile_options
+        "-c"
+        "-std=gnu++14"
+        "-fno-rtti"
+        "-Wvla"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:CXX>:${cxx_compile_options}>
+    )
+
+    list(APPEND asm_compile_options
+        "-c"
+        "-x" "assembler-with-cpp"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:ASM>:${asm_compile_options}>
+    )
+
+    list(APPEND link_options
+        "-Wl,--gc-sections"
+        "-Wl,--wrap,main"
+        "-Wl,--wrap,_malloc_r"
+        "-Wl,--wrap,_free_r"
+        "-Wl,--wrap,_realloc_r"
+        "-Wl,--wrap,_memalign_r"
+        "-Wl,--wrap,_calloc_r"
+        "-Wl,--wrap,exit"
+        "-Wl,--wrap,atexit"
+        "-Wl,-n"
+    )
+elseif(MBED_TOOLCHAIN STREQUAL "ARM")
+    list(APPEND c_compile_options
+        "-std=gnu11"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:C>:${c_compile_options}>
+    )
+
+    list(APPEND cxx_compile_options
+        "-std=gnu++14"
+        "-fno-rtti"
+        "-fno-c++-static-destructors"
+    )
+    target_compile_options(mbed-os
+        PUBLIC
+            $<$<COMPILE_LANGUAGE:CXX>:${cxx_compile_options}>
+    )
+
+    list(APPEND link_options
+        "--show_full_path"
+        "--legacyalign"
+        "--inline"
+        "--any_contingency"
+        "--keep=os_cb_sections"
+    )
+
+    target_compile_definitions(mbed-os
+        PUBLIC
+            __ASSERT_MSG
     )
 endif()
 
-add_definitions(${RELEASE_DEFINITIONS})
+target_compile_definitions(mbed-os
+    PUBLIC
+        NDEBUG
+)
+
+target_link_options(mbed-os
+    PUBLIC
+        ${link_options}
+)
