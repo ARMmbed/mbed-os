7cefe31a6824639d69407f9bf8ae3f3499286127
Devaraj Ranganna <devaraj.ranganna@arm.com>
2020-07-23 17:23:30 +0100

    cmake: Armclang - Fix assembler and linker flags
    For Armclang, change compiler flag to `-mcpu` and assembler and linker
    flag to `--cpu`.
    
    Signed-off-by: Devaraj Ranganna <devaraj.ranganna@arm.com>

--- cmake/cores/Cortex-M33F-NS.cmake
+++ cmake/cores/Cortex-M33F-NS.cmake
@@ -28,7 +28,9 @@ set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
 elseif(MBED_TOOLCHAIN STREQUAL "ARM")
 
 set(ARM_FLAGS " \
-    --cpu=Cortex-M33F \
+    -mcpu=cortex-m33+nodsp \
+    -mfpu=fpv5-sp-d16 \
+    -mfloat-abi=hard \
 ")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} \
@@ -38,10 +40,10 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
     ${ARM_FLAGS} \
 ")
 set(CMAKE_ASM_FLAGS "${CMAKE_ASM_FLAGS} \
-    ${ARM_FLAGS} \
+    --cpu=Cortex-M33.no_dsp \
 ")
 set(CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} \
-    ${ARM_FLAGS} \
+    --cpu=Cortex-M33.no_dsp \
 ")
 
 endif()
