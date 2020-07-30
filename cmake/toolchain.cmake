ba6f485031aca8d82701d48e0f40657d5dbe6c38
Hugues Kamba <hugues.kamba@arm.com>
2020-07-30 16:42:41 +0100

    CMake: Remove add_executable
    Instead provide functions to set the pre and post link commands.

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -3,7 +3,7 @@
 
 option(MBEDIDE "Use Arm compiler from Mbed Studio" OFF)
 if(MBEDIDE)
-    set(MBED_STUDIO_ARM_COMPILER "--ide=mbed")
+    set_property(GLOBAL PROPERTY MBED_STUDIO_ARM_COMPILER "--ide=mbed")
 endif()
 include(${MBED_ROOT}/cmake/toolchains/${MBED_TOOLCHAIN}.cmake)
 
