ef11e06cfbc3580fda7e504d6b912fbc76eb6983
Hugues Kamba <hugues.kamba@arm.com>
2020-07-30 16:42:41 +0100

    CMake: Remove add_executable
    Instead provide functions to set the pre and post link commands.

--- cmake/toolchains/ARM.cmake
+++ cmake/toolchains/ARM.cmake
@@ -1,14 +1,16 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set(CMAKE_ASM_COMPILER    "armasm")
-set(CMAKE_C_COMPILER      "armclang")
-set(CMAKE_CXX_COMPILER    "armclang")
-set(CMAKE_AR              "armar")
-set(ELF2BIN               "fromelf")
+set(CMAKE_ASM_COMPILER "armasm")
+set(CMAKE_C_COMPILER "armclang")
+set(CMAKE_CXX_COMPILER "armclang")
+set(CMAKE_AR "armar")
+set(ARM_ELF2BIN "fromelf")
+set_property(GLOBAL PROPERTY ELF2BIN ${ARM_ELF2BIN})
 
+get_property(mbed_studio_arm_compiler GLOBAL PROPERTY MBED_STUDIO_ARM_COMPILER)
 list(APPEND common_options
-    "${MBED_STUDIO_ARM_COMPILER}"
+    "${mbed_studio_arm_compiler}"
     "-c"
     "--target=arm-arm-none-eabi"
     "-Wno-armcc-pragma-push-pop"
@@ -33,7 +35,7 @@ target_compile_options(mbed-os
 
 target_compile_options(mbed-os
     PUBLIC
-        $<$<COMPILE_LANGUAGE:ASM>:${MBED_STUDIO_ARM_COMPILER}>
+        $<$<COMPILE_LANGUAGE:ASM>:${mbed_studio_arm_compiler}>
         $<$<COMPILE_LANGUAGE:ASM>:--cpreproc>
         $<$<COMPILE_LANGUAGE:ASM>:--cpreproc_opts=--target=arm-arm-none-eabi,-mcpu=cortex-m4,-D,__FPU_PRESENT,-D,MBED_CONF_PLATFORM_CRASH_CAPTURE_ENABLED>
 )
@@ -45,5 +47,5 @@ target_compile_definitions(mbed-os
 
 target_link_options(mbed-os
     PUBLIC
-        ${MBED_STUDIO_ARM_COMPILER}
+        ${mbed_studio_arm_compiler}
 )
