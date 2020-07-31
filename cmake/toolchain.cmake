823d765a76bc42c3ca4df35ff82f30ba700ee65d
Hugues Kamba <hugues.kamba@arm.com>
2020-07-31 19:23:34 +0100

    CMake: Move MBED_STUDIO_ARM_COMPILER to ARM.cmake module
    As its name implies, the flag is specific to the ARM toolchain.

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,10 +1,6 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-option(MBEDIDE "Use Arm compiler from Mbed Studio" OFF)
-if(MBEDIDE)
-    set_property(GLOBAL PROPERTY MBED_STUDIO_ARM_COMPILER "--ide=mbed")
-endif()
 include(${MBED_ROOT}/cmake/toolchains/${MBED_TOOLCHAIN}.cmake)
 
 # Compiler setup
