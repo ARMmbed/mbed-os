26fb3db6dfcc8b1c5c48c2f777eca996d38510a1
Hugues Kamba <hugues.kamba@arm.com>
2020-08-04 19:23:25 +0100

    CMake: Do not hard code target in CPU core CMake module
    Provide a function in each CPU core module to set toolchain options.
    All the functions have the same interface therefore the CMake source
    file including the module always calls the same function regardless
    of which cpu core module is actually included.
    When the `mbed-os` target is broken up into multiple libraries, a
    library other than `mbed-os` might need to set the toolchain options.
    This will be possible by simply including the module and calling the
    function with the target and toolchain for which to set the options.

--- cmake/cores/Cortex-M0+.cmake
+++ cmake/cores/Cortex-M0+.cmake
@@ -1,45 +1,46 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
+# Sets cpu core options
+function(mbed_set_cpu_core_options target mbed_toolchain)
+    if(${mbed_toolchain} STREQUAL "GCC_ARM")
+        list(APPEND common_toolchain_options
+            "-mthumb"
+            "-mcpu=cortex-m0plus"
+        )
 
-if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
-    list(APPEND common_toolchain_options
-        "-mthumb"
-        "-mcpu=cortex-m0plus"
-    )
+        target_compile_options(${target}
+            PUBLIC
+                ${common_toolchain_options}
+        )
 
-    target_compile_options(mbed-os
+        target_link_options(${target}
+            PUBLIC
+                ${common_toolchain_options}
+        )
+    elseif(${mbed_toolchain} STREQUAL "ARM")
+        list(APPEND compile_options
+            "-mcpu=cortex-m0plus"
+        )
+
+        target_compile_options(${target}
+            PUBLIC
+                $<$<COMPILE_LANGUAGE:C>:${compile_options}>
+                $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
+                $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M0plus>
+        )
+
+        target_link_options(${target}
+            PUBLIC
+                "--cpu=Cortex-M0plus"
+        )
+    endif()
+
+    target_compile_definitions(${target}
         PUBLIC
-            ${common_toolchain_options}
+            __CORTEX_M0PLUS
+            ARM_MATH_CM0PLUS
+            __CMSIS_RTOS
+            __MBED_CMSIS_RTOS_CM
     )
-
-    target_link_options(mbed-os
-        PUBLIC
-            ${common_toolchain_options}
-    )
-elseif(MBED_TOOLCHAIN STREQUAL "ARM")
-    list(APPEND compile_options
-        "-mcpu=cortex-m0plus"
-    )
-
-    target_compile_options(mbed-os
-        PUBLIC
-            $<$<COMPILE_LANGUAGE:C>:${compile_options}>
-            $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
-            $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M0plus>
-    )
-
-    target_link_options(mbed-os
-        PUBLIC
-            "--cpu=Cortex-M0plus"
-    )
-endif()
-
-target_compile_definitions(mbed-os
-    PUBLIC
-        __CORTEX_M0PLUS
-        ARM_MATH_CM0PLUS
-        __CMSIS_RTOS
-        __MBED_CMSIS_RTOS_CM
-)
+endfunction()
