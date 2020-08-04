d4f353414474fb79dc0e7e3867b8f7272aea397d
Hugues Kamba <hugues.kamba@arm.com>
2020-08-04 19:34:31 +0100

    CMake: Do not hard code target in toolchain CMake module
    Provide a function in each toolchain module to set the toolchain
    specific options. All the functions have the same interface
    therefore the CMake source file including the module always calls
    the same function regardless of which toolchain module is actually
    included.
    When the `mbed-os` target is broken up into multiple libraries, a
    library other than `mbed-os` might need to set the toolchain options.
    This will be possible by simply including the module and calling the
    function with the target for which to set the options.

--- cmake/toolchains/GCC_ARM.cmake
+++ cmake/toolchains/GCC_ARM.cmake
@@ -7,47 +7,51 @@ set(CMAKE_CXX_COMPILER "arm-none-eabi-g++")
 set(GCC_ELF2BIN "arm-none-eabi-objcopy")
 set_property(GLOBAL PROPERTY ELF2BIN ${GCC_ELF2BIN})
 
-list(APPEND link_options
-    "-Wl,--start-group"
-        "-lstdc++"
-        "-lsupc++"
-        "-lm"
-        "-lc"
-        "-lgcc"
-        "-lnosys"
-    "-Wl,--end-group"
-    "-specs=nosys.specs"
-    "-T" "${CMAKE_BINARY_DIR}/${APP_TARGET}.link_script.ld"
-)
 
-list(APPEND common_options
-    "-Wall"
-    "-Wextra"
-    "-Wno-unused-parameter"
-    "-Wno-missing-field-initializers"
-    "-fmessage-length=0"
-    "-fno-exceptions"
-    "-ffunction-sections"
-    "-fdata-sections"
-    "-funsigned-char"
-    "-MMD"
-    "-fomit-frame-pointer"
-    "-g3"
-)
+# Sets toolchain options
+function(mbed_set_toolchain_options target)
+    list(APPEND link_options
+        "-Wl,--start-group"
+            "-lstdc++"
+            "-lsupc++"
+            "-lm"
+            "-lc"
+            "-lgcc"
+            "-lnosys"
+        "-Wl,--end-group"
+        "-specs=nosys.specs"
+        "-T" "${CMAKE_BINARY_DIR}/${APP_TARGET}.link_script.ld"
+    )
 
-target_compile_options(mbed-os
-    PUBLIC
-        ${common_options}
-)
+    list(APPEND common_options
+        "-Wall"
+        "-Wextra"
+        "-Wno-unused-parameter"
+        "-Wno-missing-field-initializers"
+        "-fmessage-length=0"
+        "-fno-exceptions"
+        "-ffunction-sections"
+        "-fdata-sections"
+        "-funsigned-char"
+        "-MMD"
+        "-fomit-frame-pointer"
+        "-g3"
+    )
 
-target_compile_definitions(mbed-os
-    PUBLIC
-        TOOLCHAIN_GCC_ARM
-        TOOLCHAIN_GCC
-)
+    target_compile_options(${target}
+        PUBLIC
+            ${common_options}
+    )
 
-target_link_options(mbed-os
-    PUBLIC
-        ${common_options}
-        ${link_options}
-)
+    target_compile_definitions(${target}
+        PUBLIC
+            TOOLCHAIN_GCC_ARM
+            TOOLCHAIN_GCC
+    )
+
+    target_link_options(${target}
+        PUBLIC
+            ${common_options}
+            ${link_options}
+    )
+endfunction()
