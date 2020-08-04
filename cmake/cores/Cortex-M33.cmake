213976356838a9c36dc2d0a99d57d6ba3e302c98
Hugues Kamba <hugues.kamba@arm.com>
2020-08-04 19:23:25 +0100

    CMake: Do not hard code target in CPU core CMake module
    Provide a function in each CPU core module to set toolchain options.
    All the functions have the same interface therefore the CMake source
    file including the module always calls the same function regardless
    of which cpu core module is actually included.
    When the `mbed-os` target is broken up into multiple libraries, a
    library other than `mbed-os` might need to set the toolchain options.
    This will be possible by simply including the module and calling the
    function with the target and toolchain for which to set the options.

--- cmake/cores/Cortex-M33.cmake
+++ cmake/cores/Cortex-M33.cmake
@@ -1,49 +1,50 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+# Sets cpu core options
+function(mbed_set_cpu_core_options target mbed_toolchain)
+    list(APPEND options)
 
-list(APPEND options)
+    if(${mbed_toolchain} STREQUAL "GCC_ARM")
+        list(APPEND common_toolchain_options
+            "-mthumb"
+            "-march=armv8-m.main"
+        )
 
-if(MBED_TOOLCHAIN STREQUAL "GCC_ARM")
-    list(APPEND common_toolchain_options
-        "-mthumb"
-        "-march=armv8-m.main"
-    )
+        target_compile_options(${target}
+            PUBLIC
+                ${common_toolchain_options}
+        )
 
-    target_compile_options(mbed-os
+        target_link_options(${target}
+            PUBLIC
+                ${common_toolchain_options}
+        )
+    elseif(${mbed_toolchain} STREQUAL "ARM")
+        list(APPEND compile_options
+            "-mcpu=cortex-m33+nodsp"
+            "-mfpu=none"
+        )
+
+        target_compile_options(${target}
+            PUBLIC
+                $<$<COMPILE_LANGUAGE:C>:${compile_options}>
+                $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
+                $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M33.no_dsp.no_fp>
+        )
+
+        target_link_options(${target}
+            PUBLIC
+                "--cpu=Cortex-M33.no_dsp.no_fp"
+        )
+    endif()
+
+    target_compile_definitions(${target}
         PUBLIC
-            ${common_toolchain_options}
+            __CORTEX_M33
+            ARM_MATH_ARMV8MML
+            DOMAIN_NS=1
+            __CMSIS_RTOS
+            __MBED_CMSIS_RTOS_CM
     )
-
-    target_link_options(mbed-os
-        PUBLIC
-            ${common_toolchain_options}
-    )
-elseif(MBED_TOOLCHAIN STREQUAL "ARM")
-    list(APPEND compile_options
-        "-mcpu=cortex-m33+nodsp"
-        "-mfpu=none"
-    )
-
-    target_compile_options(mbed-os
-        PUBLIC
-            $<$<COMPILE_LANGUAGE:C>:${compile_options}>
-            $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
-            $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M33.no_dsp.no_fp>
-    )
-
-    target_link_options(mbed-os
-        PUBLIC
-            "--cpu=Cortex-M33.no_dsp.no_fp"
-    )
-endif()
-
-target_compile_definitions(mbed-os
-    PUBLIC
-        __CORTEX_M33
-        ARM_MATH_ARMV8MML
-        DOMAIN_NS=1
-        __CMSIS_RTOS
-        __MBED_CMSIS_RTOS_CM
-)
+endfunction()
