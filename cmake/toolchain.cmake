a4cc53d34ea17064f936df1c8aa5b62d4b51adaf
Hugues Kamba <hugues.kamba@arm.com>
2020-08-04 19:29:19 +0100

    CMake: Remove toolchain module dependency on cpu core CMake modules
    Set `CMAKE_SYSTEM_PROCESSOR` in toolchain.cmake based on
    `MBED_CPU_CORE`.
    This information is needed by the ARM toolchain (and possibly other
    toolchains), it therefore makes sense to set in `toolchain.cmake`.

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,6 +1,46 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
+# Set the system processor depending on the CPU core type
+if (MBED_CPU_CORE STREQUAL Cortex-A9)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-a9)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M0+)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m0plus)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M0)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m0)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M1)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m1)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M23-NS)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m23)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M23)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m23)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M3)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m3)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M33-NS)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M33)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M33F-NS)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M33F)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M33FE-NS)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M33FE)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m33)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M4)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M4F)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m4)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M7)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m7)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M7F)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m7)
+elseif (MBED_CPU_CORE STREQUAL Cortex-M7FD)
+    set(CMAKE_SYSTEM_PROCESSOR cortex-m7)
+endif()
+
+
 include(${MBED_ROOT}/cmake/toolchains/${MBED_TOOLCHAIN}.cmake)
 
 # Compiler setup
