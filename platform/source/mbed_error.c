a6ce2c53a0c6e0ec466c036ee730f1eac85bcfbe
Martin Kojtal <martin.kojtal@arm.com>
2020-08-20 08:49:30 +0100

    platform: move internal headers to internal/
    They belong to internal folder to follow our guideline, not in source as they were.

--- platform/source/mbed_error.c
+++ platform/source/mbed_error.c
@@ -26,7 +26,7 @@
 #include "platform/mbed_power_mgmt.h"
 #include "platform/mbed_stats.h"
 #include "platform/internal/mbed_fault_handler.h"
-#include "source/mbed_error_hist.h"
+#include "platform/internal/mbed_error_hist.h"
 #include "drivers/MbedCRC.h"
 #include "mbed_rtx.h"
 #ifdef MBED_CONF_RTOS_PRESENT
