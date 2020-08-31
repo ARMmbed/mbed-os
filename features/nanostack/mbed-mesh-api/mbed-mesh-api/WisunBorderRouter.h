207250fe840acdbba999b5c52bf11af4dc95d006
Arto Kinnunen <arto.kinnunen@arm.com>
2020-08-31 14:36:33 +0300

    Update WisunBorderRouter class
    Add set_dns_query_result to WisunBorderRouter.

--- features/nanostack/mbed-mesh-api/mbed-mesh-api/WisunBorderRouter.h
+++ features/nanostack/mbed-mesh-api/mbed-mesh-api/WisunBorderRouter.h
@@ -260,6 +260,19 @@ public:
      * */
     mesh_error_t get_radius_shared_secret(uint16_t *shared_secret_len, uint8_t *shared_secret);
 
+    /**
+     * \brief Set DNS query result to Nanostack cache.
+     *
+     * Function sets DNS query result to Nanostack cache to get distributed to the devices in the Wi-SUN network.
+     * Function must be called for a running Wi-SUN Border Router instance.
+     *
+     * \param address resolved address of domain_name.
+     * \param domain_name name of the domain. Must be non-NULL.
+     * \return MESH_ERROR_NONE on success.
+     * \return error value in case of failure.
+     * */
+    mesh_error_t set_dns_query_result(SocketAddress *address, char *domain_name);
+
 private:
     mesh_error_t configure();
     mesh_error_t apply_configuration(int8_t mesh_if_id);
