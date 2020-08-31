877078003cdfcfdb3e8bafa758fa422e0a03c861
Roman Okhrimenko <roman.okhrimenko@cypress.com>
2020-08-31 22:30:49 +0300

    Rename CY8CKIT_064B0S2_4343W to CY8CKIT064B0S2_4343W, which fits in 20 characters limit

--- hal/tests/TESTS/mbed_hal/qspi/flash_configs/flash_configs.h
+++ hal/tests/TESTS/mbed_hal/qspi/flash_configs/flash_configs.h
@@ -67,7 +67,7 @@
         defined(TARGET_CY8CKIT_062S2_43012) ||      \
         defined(TARGET_CY8CKIT_062S2_4343W) ||      \
         defined(TARGET_CY8CKIT_064S2_4343W) ||      \
-        defined(TARGET_CY8CKIT_064B0S2_4343W) ||    \
+        defined(TARGET_CY8CKIT064B0S2_4343W) ||    \
         defined(TARGET_CY8CPROTO_062_4343W) ||      \
         defined(TARGET_CY8CPROTO_062S2_43012) ||    \
         defined(TARGET_CY8CPROTO_062S3_4343W) ||    \
