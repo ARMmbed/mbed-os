955535a974aba58790f9c7be5e7510ea3c1319b6
Martin Kojtal <martin.kojtal@arm.com>
2020-09-01 12:31:29 +0100

    CMake cores: fix flag  mpcu for asm

--- cmake/cores/Cortex-M4F.cmake
+++ cmake/cores/Cortex-M4F.cmake
@@ -40,7 +40,7 @@ function(mbed_set_cpu_core_options target mbed_toolchain)
             PUBLIC
                 $<$<COMPILE_LANGUAGE:C>:${compile_options}>
                 $<$<COMPILE_LANGUAGE:CXX>:${compile_options}>
-                $<$<COMPILE_LANGUAGE:ASM>:--cpu=Cortex-M4>
+                $<$<COMPILE_LANGUAGE:ASM>:-mcpu=Cortex-M4>
         )
 
         target_link_options(${target}
