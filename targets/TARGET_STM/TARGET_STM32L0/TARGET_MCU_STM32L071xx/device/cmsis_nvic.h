3c39eeb8ddd2b18a5584c1c53a4ca61b34666fad
Andrea Gilardoni <andrea.gilardoni96@gmail.com>
2020-09-01 10:08:41 +0200

    fixing SPDX identifiers in all files

--- targets/TARGET_STM/TARGET_STM32L0/TARGET_MCU_STM32L071xx/device/cmsis_nvic.h
+++ targets/TARGET_STM/TARGET_STM32L0/TARGET_MCU_STM32L071xx/device/cmsis_nvic.h
@@ -1,18 +1,17 @@
 /* mbed Microcontroller Library
  * SPDX-License-Identifier: BSD-3-Clause
  ******************************************************************************
- * @attention
  *
- * <h2><center>&copy; Copyright (c) 2016-2020 STMicroelectronics.
- * All rights reserved.</center></h2>
+ * Copyright (c) 2020 STMicroelectronics. 
+ * All rights reserved.
  *
  * This software component is licensed by ST under BSD 3-Clause license,
- * the "License"; You may not use this file except in compliance with the
+ * the "License"; You may not use this file except in compliance with the 
  * License. You may obtain a copy of the License at:
  *                        opensource.org/licenses/BSD-3-Clause
  *
  ******************************************************************************
-*/
+ */
 
 #ifndef MBED_CMSIS_NVIC_H
 #define MBED_CMSIS_NVIC_H
--- targets/TARGET_STM/TARGET_STM32L0/TARGET_MCU_STM32L071xx/device/stm32l071xx.h
+++ targets/TARGET_STM/TARGET_STM32L0/TARGET_MCU_STM32L071xx/device/stm32l071xx.h
@@ -1,3 +1,18 @@
+/* mbed Microcontroller Library
+ * SPDX-License-Identifier: BSD-3-Clause
+ ******************************************************************************
+ *
+ * Copyright (c) 2020 STMicroelectronics. 
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the 
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
 #ifndef __STM32L071xx_H
 #define __STM32L071xx_H
 
