369fbbdde00b0cef7c7a56b7a68911aec55b3de9
Vincent Coubard <vincent.coubard@arm.com>
2020-09-04 11:26:33 +0100

    BLE: Add resolvable_address_timeout_t datatype.

--- connectivity/FEATURE_BLE/include/ble/gap/Types.h
+++ connectivity/FEATURE_BLE/include/ble/gap/Types.h
@@ -122,6 +122,16 @@ typedef Duration<uint16_t, 10000, Range<0x0A, 0x4000> > sync_timeout_t;
 typedef Duration<uint16_t, 1250, Range<0x06, 0xFFFF> > periodic_interval_t;
 
 /**
+ * Resolvable address timeout.
+ *
+ * The duration is in seconds and ranges from 1 to 0xA1B8. The default value is
+ * 900 seconds.
+ */
+using resolvable_address_timeout_t = Duration<
+    uint16_t, second_t::TIME_BASE, Range<1, 0xA1B8, 0x0384>
+>;
+
+/**
  * Number of connection events that can be skipped by the slave.
  *
  * It ranges from 0 to 0x1F3.
