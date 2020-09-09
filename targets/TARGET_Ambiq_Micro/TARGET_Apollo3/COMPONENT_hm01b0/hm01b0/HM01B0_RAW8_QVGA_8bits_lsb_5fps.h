27bb9bc7a52f4a4338a760c0dff95aeddad8f416
oclyke <owen.lyke@sparkfun.com>
2020-09-08 21:23:18 -0400

    add license to hm01b0 component

--- targets/TARGET_Ambiq_Micro/TARGET_Apollo3/COMPONENT_hm01b0/hm01b0/HM01B0_RAW8_QVGA_8bits_lsb_5fps.h
+++ targets/TARGET_Ambiq_Micro/TARGET_Apollo3/COMPONENT_hm01b0/hm01b0/HM01B0_RAW8_QVGA_8bits_lsb_5fps.h
@@ -1,3 +1,13 @@
+/*
+SPDX-License-Identifier: Beerware
+
+"THE BEER-WARE LICENSE" (Revision 42):
+<owen.lyke@sparkfun.com> added this file. As long as you retain this notice you 
+can do whatever you want with this stuff. If we meet some day, and you think 
+this stuff is worth it, you can buy me a beer in return
+
+Owen Lyke
+*/
 
 #include "HM01B0.h"
 
