866ec4281c8fef15519f691a80fc28a0bf8888ef
Martin Kojtal <martin.kojtal@arm.com>
2020-09-29 09:07:37 +0100

    CMake components: add list of all components

--- tools/cmake/components.cmake
+++ tools/cmake/components.cmake
@@ -1,13 +1,16 @@
 # Copyright (c) 2020 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-set(_MBED_COMPONENTS_ENABLED GLOBAL PROPERTY)
+# Enabled components in Mbed OS
+set(MBED_COMPONENTS_ENABLED GLOBAL PROPERTY)
+# All available components in Mbed OS
+set(MBED_COMPONENTS_AVAILABLE GLOBAL PROPERTY)
 
 # These 2 link functions are provided due to CMake behavior with object libraries.
 # See https://gitlab.kitware.com/cmake/cmake/-/issues/18090 for details
 
 function(mbed_link_component component_target)
-    get_property(enabled_components GLOBAL PROPERTY _MBED_COMPONENTS_ENABLED)
+    get_property(enabled_components GLOBAL PROPERTY MBED_COMPONENTS_ENABLED)
     # No magic here, we just store what is being enabled. We later use this list
     # to link with mbed-os and an application
     foreach(component IN LISTS ARGN)
@@ -19,15 +22,17 @@ function(mbed_link_component component_target)
             list(APPEND enabled_components ${component})
         endif()
     endforeach()
-    set_property(GLOBAL PROPERTY _MBED_COMPONENTS_ENABLED ${enabled_components})
+    set_property(GLOBAL PROPERTY MBED_COMPONENTS_ENABLED ${enabled_components})
 
     message(DEBUG "Mbed OS component: " "${component_target}" " enabling components: " "${ARGN}")
     # link with all arguments provided
+    set_property(GLOBAL APPEND PROPERTY MBED_COMPONENTS_AVAILABLE ${component_target})
+
     target_link_libraries(${component_target} ${ARGN})
 endfunction()
 
 function(mbed_link_application application_target)
-    get_property(enabled_components GLOBAL PROPERTY _MBED_COMPONENTS_ENABLED)
+    get_property(enabled_components GLOBAL PROPERTY MBED_COMPONENTS_ENABLED)
     message(DEBUG "Linking Mbed OS components: " "${enabled_components}")
     target_link_libraries(${application_target} ${enabled_components} ${ARGN} mbed-os)
 endfunction()
