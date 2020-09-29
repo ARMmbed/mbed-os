e4add7f06656abca35a3d84fe13883d040e47587
Martin Kojtal <martin.kojtal@arm.com>
2020-09-29 14:08:23 +0100

    mbed.h: fix symbols

--- mbed.h
+++ mbed.h
@@ -18,20 +18,20 @@
 
 #include "platform/mbed_version.h"
 
-#ifdef MBED_COMPONENT_RTOS_ENABLED
+#ifdef MBED_CONF_RTOS_PRESENT
 #include "rtos/rtos.h"
 #endif
 
-#ifdef MBED_COMPONENT_NETSOCKET_ENABLED
+#ifdef MBED_CONF_NETSOCKET_PRESENT
 #include "netsocket/nsapi.h"
 #include "netsocket/nsapi_ppp.h"
 #endif
 
-#ifdef MBED_COMPONENT_EVENTS_ENABLED
+#ifdef MBED_CONF_EVENTS_PRESENT
 #include "events/mbed_events.h"
 #endif
 
-#ifdef MBED_COMPONENT_MBED_OS_FILESYSTEM_ENABLED
+#ifdef MBED_CONF_FILESYSTEM_PRESENT
 #include "filesystem/mbed_filesystem.h"
 #endif
 
