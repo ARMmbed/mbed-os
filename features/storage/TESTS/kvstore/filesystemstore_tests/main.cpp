53fe4cf6f985042f5101e4751fecd6586acdb33c
Qinghao Shi <Qinghao.shi@arm.com>
2020-10-01 11:51:53 +0100

    TEST: Extend the kvstore test time out to 120s

--- features/storage/TESTS/kvstore/filesystemstore_tests/main.cpp
+++ features/storage/TESTS/kvstore/filesystemstore_tests/main.cpp
@@ -504,7 +504,7 @@ utest::v1::status_t greentea_failure_handler(const Case *const source, const fai
 // Test setup
 utest::v1::status_t test_setup(const size_t number_of_cases)
 {
-    GREENTEA_SETUP(60, "default_auto");
+    GREENTEA_SETUP(120, "default_auto");
     return verbose_test_setup_handler(number_of_cases);
 }
 
