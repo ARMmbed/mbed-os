5753ed91e2dc8bd79598e739804041177a97413d
Harrison Mutai <harrison.mutai@arm.com>
2020-10-08 09:49:20 +0100

    Add unit test for script that checks copyright and license notice
    Add unit test and dependent stubs for testing of scancode-evaluate.py.
    license_check takes a JSON as argument and checks for source missing
    copyright or license notices.
    
    Each JSON represents a separate test case, and stubs are added for test
    case 3 and 4 where license_check looks for an spdx notice in the source file.

--- tools/test/travis-ci/scancode_evaluate_test.py
+++ tools/test/travis-ci/scancode_evaluate_test.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import importlib
 import os
-import sys
+import json
 from unittest import TestCase
 
 # TODO: fix scancode to match python naming conventROOTi
@@ -12,7 +12,7 @@ SCANCODE_EVALUATE = importlib.import_module("scancode-evaluate")
 license_check = SCANCODE_EVALUATE.license_check
 
 ROOT = os.path.abspath(
-	os.path.join(os.path.dirname(__file__))
+    os.path.join(os.path.dirname(__file__))
 )
 
 # path to stub files
@@ -43,64 +43,50 @@ invalid_header_2 = "/* mbed Microcontroller Library\
 
 # implement test class
 class TestScancodeEvaluate(TestCase):
-	""" Test scancode evaluation script """
+    """ Test scancode evaluation script """
 
-	def test_scancode_case_1(self):
-		""" Test Case 1 -- faulty json file
-			@inputs scancode_test_1.json
-			@outputs -1 if any error in file licenses found
-		"""
-		expected_result = -1
-		test_json = ROOT + "/scancode_test/scancode_test_1.json"
+    def test_scancode_case_1(self):
+        """ Test Case 1 -- faulty json file
+            @inputs scancode_test_1.json
+            @outputs -1 if any error in file licenses found
+        """
+        # generate scancode_test_1.json
+        with open("scancode_test/scancode_test_1.json", 'w') as f:
+            json.dump({"headers": {"tool name": "scancode test fail"}}, f)
 
-		# pass json path to test function
-		result = license_check(ROOT, test_json)
+        self.assertEqual(-1, license_check(ROOT, ROOT + "/scancode_test/scancode_test_1.json"))
+        os.remove("scancode_test/scancode_test_1.json")
 
-		self.assertEqual(expected_result, result)
+    def test_scancode_case_2(self):
+        """ Test Case 2 -- no errors in license headers, try multiple types i.e Apache-2.0, BSD3
+            @inputs scancode_test_2.json [4 Apache-2.0, 4 BSD-3.0]
+            @outputs 0
+        """
+        self.assertEqual(0, license_check(ROOT, ROOT + "/scancode_test/scancode_test_2.json"), "False Negative(s)")
 
-	def test_scancode_case_2(self):
-		""" Test Case 2 -- no errors in license headers, try multiple types i.e Apache-2.0, BSD3
-			@inputs scancode_test_2.json [4 Apache-2.0, 4 BSD-3.0]
-			@outputs 0
-		"""
-		expected_result = 0
-		test_json = ROOT + "/scancode_test/scancode_test_2.json"
+    def test_scancode_case_3(self):
+        """ Test Case 3 -- all files containing errors [2 files without a header, 2 files non-permissive + missing SPDX,
+             1 missing SPDX]
+            @inputs scancode_test_3.json
+            @output 7
+        """
+        # create stub files with a non-permissive license and missing spdx
+        for i in range(3, 6):
+            with open(stub_path + "test" + str(i) + ".h", "w") as file:
+                if i == 5:
+                    file.write(invalid_header_2)
+                else:
+                    file.write(invalid_header_1)
 
-		result = license_check(ROOT, test_json)
-		self.assertEqual(expected_result, result, "False Negative(s)")
+        self.assertEqual(7, license_check(ROOT, ROOT + "/scancode_test/scancode_test_3.json"), "False Positive(s)")
+        # delete stub files
+        os.remove(stub_path + "test3.h")
+        os.remove(stub_path + "test4.h")
+        os.remove(stub_path + "test5.h")
 
-	def test_scancode_case_3(self):
-		""" Test Case 3 -- all files containing errors
-			@inputs scancode_test_3.json [2 no header, 2 non-permissive + spdx, 1 missing SPDX]
-			@output 5
-		"""
-		# create stub files with a non-permissive license and missing spdx
-		for i in range(3, 6):
-			with open(stub_path + "test" + str(i) + ".h", "w") as file:
-				if i == 5:
-					file.write(invalid_header_2)
-				else:
-					file.write(invalid_header_1)
-
-		expected_result = 7
-		test_json = ROOT + "/scancode_test/scancode_test_3.json"
-
-		result = license_check(ROOT, test_json)
-
-		self.assertEqual(expected_result, result, "False Positive(s)")
-		# delete stub files
-		os.remove(stub_path + "test3.h")
-		os.remove(stub_path + "test4.h")
-		os.remove(stub_path + "test5.h")
-
-	def test_scancode_case_4(self):
-		""" Test Case 4 -- license header permissive and non-permissive 'license' [FP]
-			@inputs scancode_test_4.json
-			@outputs 0
-		"""
-		expected_result = 0
-		test_json = ROOT + "/scancode_test/scancode_test_4.json"
-
-		result = license_check(ROOT, test_json)
-
-		self.assertEqual(expected_result, result, "Non-Permissive Header False Positive")
+    def test_scancode_case_4(self):
+        """ Test Case 4 -- license header permissive and "non-permissive license" false positive
+            @inputs scancode_test_4.json
+            @outputs 0
+        """
+        self.assertEqual(0, license_check(ROOT, ROOT + "/scancode_test/scancode_test_4.json"), "Non-Permissive Header False Positive")
