b0503398426d845aa54348f527d03043e427acc2
zhizhe.zhu <zhizhe.zhu@samsung.com>
2020-10-20 14:44:41 +0800

    S5JS100: Fix RTC range.(gcc time_t is signed) Fix RTC does not stop counting after a software reset.
    Signed-off-by: zhizhe.zhu <zhizhe.zhu@samsung.com>

--- hal/tests/TESTS/mbed_hal/rtc/main.cpp
+++ hal/tests/TESTS/mbed_hal/rtc/main.cpp
@@ -161,7 +161,7 @@ void rtc_range_test()
 {
     static const RealTimeClock::time_point starts[] = {
         RealTimeClock::time_point{0x00000000s},
-        RealTimeClock::time_point{0xEFFFFFFFs},
+        RealTimeClock::time_point{0x7ffffffas},   //0xEFFFFFFF had already overflow, the max is 0x7FFFFFFF, time_t is 32bit and signed
         RealTimeClock::time_point{0x00001000s},
         RealTimeClock::time_point{0x00010000s},
     };
@@ -184,7 +184,6 @@ void rtc_accuracy_test()
     const auto start = RealTimeClock::time_point(100s);
     RealTimeClock::init();
     RealTimeClock::write(start);
-
     timer1.start();
     while (RealTimeClock::now() < (start + 10s)) {
         /* Just wait. */
