19726cd87ed7e012c74b0c457d8f8efb763b7868
Hugues Kamba <hugues.kamba@arm.com>
2020-10-28 16:53:46 +0000

    CMake: Fix third party toolchain file suppport
    Compile defintions are always included. However, functions that set compile options
    are only called if a third party toolchain file is not used

--- tools/cmake/app.cmake
+++ tools/cmake/app.cmake
@@ -9,11 +9,19 @@ endif()
 
 include(${MBED_CONFIG_PATH}/mbed_config.cmake)
 
+set(MBED_TOOLCHAIN_FILE_USED FALSE)
+
 # Set default toolchain file
 if(NOT CMAKE_TOOLCHAIN_FILE)
+    set(MBED_TOOLCHAIN_FILE_USED TRUE)
+
     set(CMAKE_TOOLCHAIN_FILE "${MBED_PATH}/tools/cmake/toolchain.cmake" CACHE INTERNAL "")
+
+    # Specify locations for toolchains and generic options
+    include(${MBED_PATH}/tools/cmake/toolchains/${MBED_TOOLCHAIN}.cmake)
+
+    # Specify available build profiles and add options for the selected build profile
+    include(${MBED_PATH}/tools/cmake/profile.cmake)
 endif()
 
-# Toolchain setup
-include(${MBED_PATH}/tools/cmake/toolchains/${MBED_TOOLCHAIN}.cmake)
 enable_language(C CXX ASM)
