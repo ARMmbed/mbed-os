fe99303b2d93d771ff1f000beb1cc40035768544
George Psimenos <georgios.psimenos@arm.com>
2021-01-20 09:29:18 +0000

    Fix CI in feature-std-pin-name

--- hal/source/mbed_pinmap_default.cpp
+++ hal/source/mbed_pinmap_default.cpp
@@ -43,7 +43,7 @@ static const PinList ff_arduino_uno_list = {
     ff_arduino_uno_pins
 };
 
-static_assert(sizeof(ff_arduino_pins) / sizeof(ff_arduino_pins[0]) == sizeof(ff_arduino_names) / sizeof(ff_arduino_names[0]),
+static_assert(sizeof(ff_arduino_uno_pins) / sizeof(ff_arduino_uno_pins[0]) == sizeof(ff_arduino_uno_names) / sizeof(ff_arduino_uno_names[0]),
               "Arrays must have the same length");
 
 const PinList *pinmap_ff_arduino_uno_pins()
