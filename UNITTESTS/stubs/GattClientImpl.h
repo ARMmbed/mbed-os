84e2d838df243618008a2ec30591043e15d5b777
Paul Szczepanek <paul.szczepanek@arm.com>
2021-01-25 17:45:54 +0000

    add stubs

--- UNITTESTS/stubs/GattClientImpl.h
+++ UNITTESTS/stubs/GattClientImpl.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License") { };
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -19,16 +19,114 @@
 #define BLE_GATTCLIENTSTUB_H
 
 #include "gmock/gmock.h"
+#include "events/EventQueue.h"
+#include "events/mbed_events.h"
 
 namespace ble {
 namespace impl {
 
 class GattClient {
 public:
+    using EventHandler = ble::GattClient::EventHandler;
+    using WriteOp_t = ble::GattClient::WriteOp_t;
+    using HVXCallback_t  = ble::GattClient::HVXCallback_t ;
+    using GattClientShutdownCallback_t  = ble::GattClient::GattClientShutdownCallback_t ;
+    using GattClientShutdownCallbackChain_t  = ble::GattClient::GattClientShutdownCallbackChain_t ;
+    using HVXCallbackChain_t  = ble::GattClient::HVXCallbackChain_t ;
+    using ReadCallbackChain_t  = ble::GattClient::ReadCallbackChain_t ;
+    using WriteCallbackChain_t  = ble::GattClient::WriteCallbackChain_t ;
+
     GattClient() {};
     GattClient(const GattClient&) = delete;
     GattClient& operator=(const GattClient&) = delete;
     virtual ~GattClient() {};
+
+    virtual void setEventHandler(EventHandler *handler) { };
+
+    virtual ble_error_t launchServiceDiscovery(
+            ble::connection_handle_t connectionHandle,
+            ServiceDiscovery::ServiceCallback_t sc = nullptr,
+            ServiceDiscovery::CharacteristicCallback_t cc = nullptr,
+            const UUID &matchingServiceUUID = UUID::ShortUUIDBytes_t(BLE_UUID_UNKNOWN),
+            const UUID &matchingCharacteristicUUIDIn = UUID::ShortUUIDBytes_t(BLE_UUID_UNKNOWN)
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t discoverServices(
+            ble::connection_handle_t connectionHandle,
+            ServiceDiscovery::ServiceCallback_t callback,
+            const UUID &matchingServiceUUID = UUID::ShortUUIDBytes_t(BLE_UUID_UNKNOWN)
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t discoverServices(
+            ble::connection_handle_t connectionHandle,
+            ServiceDiscovery::ServiceCallback_t callback,
+            GattAttribute::Handle_t startHandle,
+            GattAttribute::Handle_t endHandle
+    ) { return BLE_ERROR_NONE; };
+
+    virtual bool isServiceDiscoveryActive(void) const { return true; };
+
+    virtual void terminateServiceDiscovery(void) { };
+
+    virtual ble_error_t read(
+            ble::connection_handle_t connHandle,
+            GattAttribute::Handle_t attributeHandle,
+            uint16_t offset
+    ) const { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t write(
+            GattClient::WriteOp_t cmd,
+            ble::connection_handle_t connHandle,
+            GattAttribute::Handle_t attributeHandle,
+            size_t length,
+            const uint8_t *value
+    ) const { return BLE_ERROR_NONE; };
+
+    /* Event callback handlers. */
+
+    virtual void onDataRead(ReadCallback_t callback) { };
+
+    virtual ReadCallbackChain_t &onDataRead() { static ReadCallbackChain_t chain; return chain; };
+
+    virtual void onDataWritten(WriteCallback_t callback) { };
+
+    virtual WriteCallbackChain_t &onDataWritten() { static WriteCallbackChain_t chain; return chain; };
+
+    virtual void onServiceDiscoveryTermination(
+            ServiceDiscovery::TerminationCallback_t callback
+    ) { };
+
+    virtual ble_error_t discoverCharacteristicDescriptors(
+            const DiscoveredCharacteristic &characteristic,
+            const CharacteristicDescriptorDiscovery::DiscoveryCallback_t &discoveryCallback,
+            const CharacteristicDescriptorDiscovery::TerminationCallback_t &terminationCallback
+    ) { return BLE_ERROR_NONE; };
+
+    virtual bool isCharacteristicDescriptorDiscoveryActive(
+            const DiscoveredCharacteristic &characteristic
+    ) const { return true; };
+
+    virtual void terminateCharacteristicDescriptorDiscovery(
+            const DiscoveredCharacteristic &characteristic
+    ) { };
+
+    virtual ble_error_t negotiateAttMtu(ble::connection_handle_t connection) { return BLE_ERROR_NONE; };
+
+    virtual void onHVX(HVXCallback_t callback) { };
+
+    virtual void onShutdown(const GattClientShutdownCallback_t &callback) { };
+
+    virtual GattClientShutdownCallbackChain_t &onShutdown() { static GattClientShutdownCallbackChain_t chain; return chain; };
+
+    virtual HVXCallbackChain_t &onHVX() { static HVXCallbackChain_t chain; return chain; };
+
+    virtual ble_error_t reset(void) { return BLE_ERROR_NONE; };
+
+    virtual void processReadResponse(const GattReadCallbackParams *params) { };
+
+    virtual void processWriteResponse(const GattWriteCallbackParams *params) { };
+
+    virtual void processHVXEvent(const GattHVXCallbackParams *params) { };
 };
 
 }
--- UNITTESTS/stubs/GattServerImpl.h
+++ UNITTESTS/stubs/GattServerImpl.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2020 ARM Limited
  * SPDX-License-Identifier: Apache-2.0
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * Licensed under the Apache License, Version 2.0 (the "License") { return BLE_ERROR_NONE; };
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -20,6 +20,7 @@
 
 #include "gmock/gmock.h"
 #include "ble/GattServer.h"
+#include "source/generic/GattServerEvents.h"
 
 namespace ble {
 namespace impl {
@@ -31,7 +32,103 @@ public:
     GattServer& operator=(const GattServer&) = delete;
     virtual ~GattServer() {};
 
-    virtual ble_error_t addService(GattService &service) { return ble_error_t::BLE_ERROR_NONE; };
+    using EventHandler = ble::GattServer::EventHandler;
+    using DataSentCallback_t = ble::GattServer::DataSentCallback_t ;
+    using DataSentCallbackChain_t = ble::GattServer::DataSentCallbackChain_t ;
+    using DataWrittenCallback_t = ble::GattServer::DataWrittenCallback_t ;
+    using DataWrittenCallbackChain_t = ble::GattServer::DataWrittenCallbackChain_t ;
+    using DataReadCallback_t = ble::GattServer::DataReadCallback_t;
+    using DataReadCallbackChain_t = ble::GattServer::DataReadCallbackChain_t;
+    using GattServerShutdownCallback_t = ble::GattServer::GattServerShutdownCallback_t;
+    using GattServerShutdownCallbackChain_t = ble::GattServer::GattServerShutdownCallbackChain_t;
+    using EventCallback_t = ble::GattServer::EventCallback_t;
+
+    virtual void setEventHandler(EventHandler *handler) { };
+
+    virtual ble_error_t reset(ble::GattServer* server) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t addService(GattService &service) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t read(
+        GattAttribute::Handle_t attributeHandle,
+        uint8_t buffer[],
+        uint16_t *lengthP
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t read(
+        ble::connection_handle_t connectionHandle,
+        GattAttribute::Handle_t attributeHandle,
+        uint8_t *buffer,
+        uint16_t *lengthP
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t write(
+        GattAttribute::Handle_t attributeHandle,
+        const uint8_t *value,
+        uint16_t size,
+        bool localOnly = false
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t write(
+        ble::connection_handle_t connectionHandle,
+        GattAttribute::Handle_t attributeHandle,
+        const uint8_t *value,
+        uint16_t size,
+        bool localOnly = false
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t areUpdatesEnabled(
+        const GattCharacteristic &characteristic,
+        bool *enabledP
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble_error_t areUpdatesEnabled(
+        ble::connection_handle_t connectionHandle,
+        const GattCharacteristic &characteristic,
+        bool *enabledP
+    ) { return BLE_ERROR_NONE; };
+
+    virtual ble::Gap::PreferredConnectionParams_t getPreferredConnectionParams() {
+        ble::Gap::PreferredConnectionParams_t params = {0}; return params;
+    };
+
+    virtual void setPreferredConnectionParams(const ble::Gap::PreferredConnectionParams_t &params) { };
+
+    virtual bool isOnDataReadAvailable() const { return true; };
+
+    virtual void onDataSent(const DataSentCallback_t &callback) { };
+
+    virtual DataSentCallbackChain_t &onDataSent() { static DataSentCallbackChain_t chain; return chain; };
+
+    virtual void onDataWritten(const DataWrittenCallback_t &callback) { };
+
+    virtual DataWrittenCallbackChain_t &onDataWritten() { static DataWrittenCallbackChain_t chain; return chain; };
+
+    virtual ble_error_t onDataRead(const DataReadCallback_t &callback) { return BLE_ERROR_NONE; };
+
+    virtual DataReadCallbackChain_t &onDataRead() { static DataReadCallbackChain_t chain; return chain; };
+
+    virtual void onShutdown(const GattServerShutdownCallback_t &callback) { };
+
+    virtual GattServerShutdownCallbackChain_t &onShutdown() { static GattServerShutdownCallbackChain_t chain; return chain; };
+
+    virtual void onUpdatesEnabled(EventCallback_t callback) { };
+
+    virtual void onUpdatesDisabled(EventCallback_t callback) { };
+
+    virtual void onConfirmationReceived(EventCallback_t callback) { };
+
+    virtual void handleDataWrittenEvent(const GattWriteCallbackParams *params) { };
+
+    virtual void handleDataReadEvent(const GattReadCallbackParams *params) { };
+
+    virtual void handleEvent(
+        GattServerEvents::gattEvent_e type,
+        ble::connection_handle_t connHandle,
+        GattAttribute::Handle_t attributeHandle
+    ) { };
+
+    virtual void handleDataSentEvent(unsigned count) { };
 };
 
 }
