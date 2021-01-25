c8f79282f6c6bf50374e1dd49e0eeb9a637ee54b
Vikas Katariya <Vikas.katariya@arm.com>
2021-01-25 15:36:42 +0000

    Add missing license header and SPDX identifier
    This files are imported and part of:
    https://github.com/mcu-tools/mcuboot/commit/a8e12dae381080e898cea0c6f7408009b0163f9f
    
    Issue has been raised https://github.com/mcu-tools/mcuboot/issues/930
    
    As these scripts are going to be part of release its important to have
    licensing information.

--- tools/psa/tfm/bin_utils/imgtool/boot_record.py
+++ tools/psa/tfm/bin_utils/imgtool/boot_record.py
@@ -1,6 +1,8 @@
 # Copyright (c) 2019, Arm Limited.
 # Copyright (c) 2020, Linaro Limited
 #
+# SPDX-License-Identifier: Apache-2.0
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
