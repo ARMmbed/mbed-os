deeaa7970f995b4aa7f3a7535b0435db64f50302
Hugues Kamba <hugues.kamba@arm.com>
2021-01-26 13:52:29 +0000

    CMake: Add post build operation support
    A CMake custom target, mbed-post-build, is added as a dependency of the
    application CMake target if a Mbed target adds a CMake custom target
    named mbed-post-build-bin. mbed-post-build-bin is added as a dependency
    of mbed-post-build. mbed-post-build-bin depends on the application binary.
    This is done so a CMake custom command that executes post-build can be added.
    
    The Python scripts that implement the operations have been modified to add
    CLI entry points so they can be called from CMake. Dependency on the old
    tool has been removed on those scripts by passing them exactly what they
    require instead of passing old tool Python objects. A consequence of that
    was to slightly amend how the old tool calls some of those Python modules.
    
    Support has only been added for Mbed targets that currently have a requirement
    for post build operations. This includes: LPC1114, LPC1768, ARCH_PRO, LPC54114,
    LPC546XX, FF_LPC546XX, CY8CKIT064B0S2_4343W, CYTFM_064B0S2_4343W, CYSBSYSKIT_01
    
    The following targets are not supported as TFM support is not yet included:
    ARM_MUSCA_B1, ARM_MUSCA_B1_NS, ARM_MUSCA_S1, ARM_MUSCA_S1_NS.

---
+++ targets/TARGET_Cypress/scripts/mbed_set_post_build_cypress.cmake
@@ -0,0 +1,68 @@
+# Copyright (c) 2021 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+include(${MBED_PATH}/tools/cmake/mbed_set_post_build.cmake)
+
+#
+# Merge Cortex-M4 HEX and a Cortex-M0 HEX.
+#
+function(mbed_post_build_psoc6_merge_hex)
+    find_package(Python3)
+
+    set (extra_macro_args ${ARGN})
+
+    list(LENGTH cortex_m0_hex num_extra_args)
+
+    if(${num_extra_args} GREATER 0)
+        list(GET extra_macro_args 0 cortex_m0_hex)
+        set(post_build_command
+            COMMAND ${Python3_EXECUTABLE} ${MBED_PATH}/targets/TARGET_Cypress/scripts/PSOC6.py
+                merge
+                --elf $<TARGET_FILE:${APP_TARGET}>
+                --m4hex ${CMAKE_BINARY_DIR}/${APP_TARGET}.hex
+                --m0hex ${cortex_m0_hex}
+        )
+    else()
+        set(post_build_command
+            COMMAND ${Python3_EXECUTABLE} ${MBED_PATH}/targets/TARGET_Cypress/scripts/PSOC6.py
+                merge
+                --elf $<TARGET_FILE:${APP_TARGET}>
+                --m4hex ${CMAKE_BINARY_DIR}/${APP_TARGET}.hex
+        )
+    endif()
+
+    mbed_set_post_build_operation()
+endfunction()
+
+
+#
+# Sign a Cortex-M4 HEX with Cortex-M0 HEX.
+#
+function(mbed_post_build_psoc6_sign_image
+    m0hex_filename
+    mbed_target_name
+    policy_file_name
+    boot_scheme
+    cm0_img_id
+    cm4_img_id
+    cortex_m0_hex
+)
+    find_package(Python3)
+
+    set(post_build_command
+        COMMAND ${Python3_EXECUTABLE} ${MBED_PATH}/targets/TARGET_Cypress/scripts/PSOC6.py
+            sign
+            --build-dir ${CMAKE_BINARY_DIR}
+            --m0hex-filename ${m0hex_filename}
+            --target-name ${mbed_target_name}
+            --policy-file-name ${policy_file_name}
+            --boot-scheme ${boot_scheme}
+            --cm0-img-id ${cm0_img_id}
+            --cm4-img-id ${cm4_img_id}
+            --elf $<TARGET_FILE:${APP_TARGET}>
+            --m4hex ${CMAKE_BINARY_DIR}/${APP_TARGET}.hex
+            --m0hex ${cortex_m0_hex}
+    )
+
+    mbed_set_post_build_operation()
+endfunction()
---
+++ targets/TARGET_NXP/scripts/mbed_set_post_build_nxp.cmake
@@ -0,0 +1,18 @@
+# Copyright (c) 2021 ARM Limited. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+include(${MBED_PATH}/tools/cmake/mbed_set_post_build.cmake)
+
+#
+# Patch an LPC target vector table in the binary file.
+#
+function(mbed_post_build_lpc_patch_vtable)
+    find_package(Python3)
+
+    set(post_build_command
+        COMMAND ${Python3_EXECUTABLE} ${MBED_PATH}/targets/TARGET_NXP/scripts/LPC.py
+        ${CMAKE_BINARY_DIR}/${APP_TARGET}.bin
+    )
+
+    mbed_set_post_build_operation()
+endfunction()
