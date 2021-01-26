91e7676cc442e4d1e0425a9ae0b658e298434d0f
reme <jean-marc.reme@st.com>
2021-01-26 11:16:17 +0100

    STM32WL : ADDING STM32WL SUPPORT
    Add code concerning all STM32WL platforms
    - STM32 Cube Firmware
    
    Update CMakeLists.txt due to mbedtools evolution

---
+++ targets/TARGET_STM/TARGET_STM32WL/spi_device.h
@@ -0,0 +1,24 @@
+/* mbed Microcontroller Library
+ * SPDX-License-Identifier: BSD-3-Clause
+ ******************************************************************************
+ *
+ * Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
+
+#ifndef MBED_SPI_DEVICE_H
+#define MBED_SPI_DEVICE_H
+
+#include "stm32wlxx_ll_spi.h"
+
+// Defines the word legnth capability of the device where Nth bit allows for N window size
+#define STM32_SPI_CAPABILITY_WORD_LENGTH (0x0000FFF8)
+
+#endif
