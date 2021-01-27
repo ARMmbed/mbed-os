8e2c9575be27cffd2bbabce3854844256de33323
Martin Kojtal <martin.kojtal@arm.com>
2021-01-27 12:20:48 +0000

    CMake cores: remove dsp as it is on by default

--- tools/cmake/cores/Cortex-M33FE-NS.cmake
+++ tools/cmake/cores/Cortex-M33FE-NS.cmake
@@ -7,7 +7,7 @@ if(${MBED_TOOLCHAIN} STREQUAL "GCC_ARM")
         "-mthumb"
         "-mfpu=fpv5-sp-d16"
         "-mfloat-abi=softfp"
-        "-mcpu=cortex-m33+dsp"
+        "-mcpu=cortex-m33"
     )
 elseif(${MBED_TOOLCHAIN} STREQUAL "ARM")
     list(APPEND common_options
