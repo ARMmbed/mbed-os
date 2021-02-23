5ef82f59ccd98a1b66eb6670c5eae7205742c86a
Lingkai Dong <lingkai.dong@arm.com>
2021-02-23 17:34:23 +0000

    Fix imgtool import for CYTFM_064B0S2_4343W signing
    Fixes: #14153
    
    The target CYTFM_064B0S2_4343W is signed using `cysecuretools` which
    depends on `imgtool` managed by `pip`. However, Arm Musca targets
    requires a modified/wrapped version of the image tool copied from
    trusted-firmware-m + MCUboot.
    
    To avoid conflicts in the version of `imgtool` used, we should only
    add the copied version of `imgtool` to python's system path for
    Musca targets instead of doing it globally for all targets.

--- tools/targets/__init__.py
+++ tools/targets/__init__.py
@@ -26,16 +26,12 @@ import sys
 from collections import namedtuple
 from copy import copy
 from future.utils import raise_from
+from os.path import dirname, abspath, join
 from tools.resources import FileType
 from tools.settings import ROOT
 from tools.targets.LPC import patch
 from tools.paths import TOOLS_BOOTLOADERS
 from tools.utils import json_file_to_dict, NotSupportedException
-
-# Add PSA TF-M binary utility scripts in system path
-from os.path import dirname, abspath, join
-TFM_SCRIPTS = abspath(join(dirname(__file__), '..', 'psa', 'tfm', 'bin_utils'))
-sys.path.insert(0, TFM_SCRIPTS)
 
 
 __all__ = ["target", "TARGETS", "TARGET_MAP", "TARGET_NAMES", "CORE_LABELS",
