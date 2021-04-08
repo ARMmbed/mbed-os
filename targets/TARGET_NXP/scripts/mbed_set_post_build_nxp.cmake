dfc4010e240669ba732cb3c79da90d9560c3cb5c
Martin Kojtal <martin.kojtal@arm.com>
2021-04-08 13:35:17 +0100

    CMake targets: remove MBED_PATH, use stadard variables
    - list files included via module path
    - <project-name>_SOURCE_DIR for sources that are out of the current processed CMake
    - CMAKE_CURRENT_LIST_DIR for listfiles

--- targets/TARGET_NXP/scripts/mbed_set_post_build_nxp.cmake
+++ targets/TARGET_NXP/scripts/mbed_set_post_build_nxp.cmake
@@ -1,7 +1,7 @@
 # Copyright (c) 2021 ARM Limited. All rights reserved.
 # SPDX-License-Identifier: Apache-2.0
 
-include(${MBED_PATH}/tools/cmake/mbed_set_post_build.cmake)
+include(mbed_set_post_build)
 
 #
 # Patch an LPC target vector table in the binary file.
@@ -10,7 +10,7 @@ function(mbed_post_build_lpc_patch_vtable mbed_target_name)
     find_package(Python3)
 
     set(post_build_command
-        COMMAND ${Python3_EXECUTABLE} ${MBED_PATH}/targets/TARGET_NXP/scripts/LPC.py
+        COMMAND ${Python3_EXECUTABLE} ${CMAKE_CURRENT_LIST_DIR}/LPC.py
         ${CMAKE_BINARY_DIR}/$<TARGET_PROPERTY:mbed-post-build-bin-${mbed_target_name},application>.bin
     )
 
