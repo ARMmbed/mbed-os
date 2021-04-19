d974b4743939c8ba62483842c35f720180cf5120
Affrin Pinhero <affrin.pinhero@hcl.com>
2021-04-19 16:57:27 +0530

    driver/i2c: Added I2C timing calculation function.
    This commit adds I2C timing value automatic calculation algorithm
    for all supported families added. This patch improves I2C timing calculation
    according to I2C input clock and I2C bus speed.
    This commit also allows user to change the system clock and I2C input clock.
    
    Related issue: #12907
    
    Pull request type:
    [x] Patch update (Bug fix / Target update / Docs update / Test update / Refactor)
    [] Feature update (New feature / Functionality change / New API)
    [] Major update (Breaking change E.g. Return code change / API behaviour change)
    
    Test results:
    [] No Tests required for this change (E.g docs only update)
    [x] Covered by existing mbed-os tests (Greentea or Unittest)
    [] Tests / results supplied as part of this PR
    
    Signed-off-by: Affrin Pinhero <affrin.pinhero@hcl.com>

--- targets/TARGET_STM/TARGET_STM32G4/i2c_device.c
+++ targets/TARGET_STM/TARGET_STM32G4/i2c_device.c
@@ -16,7 +16,7 @@
 #include "i2c_device.h"
 #include "mbed_assert.h"
 #include "mbed_error.h"
-#include "stm32h7xx_ll_rcc.h"
+#include "stm32g4xx_ll_rcc.h"
 
 /*  Define I2C Device */
 #if DEVICE_I2C
@@ -30,49 +30,72 @@ uint32_t i2c_get_pclk(I2CName i2c)
 {
     uint32_t clocksource;
     uint32_t pclk = 0;
-    PLL3_ClocksTypeDef pll3_clocks;
-    if ((i2c == I2C_1) || (i2c == I2C_2) || (i2c == I2C_3)) {
-        clocksource = __HAL_RCC_GET_I2C123_SOURCE();
+    if (i2c == I2C_1) {
+        clocksource = __HAL_RCC_GET_I2C1_SOURCE();
         switch (clocksource) {
-            case RCC_I2C123CLKSOURCE_D2PCLK1:
+            case RCC_I2C1CLKSOURCE_PCLK1:
                 pclk = HAL_RCC_GetPCLK1Freq();
                 break;
-            case RCC_I2C123CLKSOURCE_PLL3:
-                HAL_RCCEx_GetPLL3ClockFreq(&pll3_clocks);
-                pclk = pll3_clocks.PLL3_R_Frequency;
+
+            case RCC_I2C1CLKSOURCE_SYSCLK:
+                pclk = HAL_RCC_GetSysClockFreq();
                 break;
-            case RCC_I2C123CLKSOURCE_HSI:
+
+            case RCC_I2C1CLKSOURCE_HSI:
                 pclk = HSI_VALUE;
-                break;
-            case RCC_I2C123CLKSOURCE_CSI:
-                pclk = CSI_VALUE;
-                break;
-            default:
-                // should not happend
-                error("I2C123: Invalid clock source");
                 break;
         }
     }
+#if defined I2C2_BASE
+    else if (i2c == I2C_2) {
+        clocksource = __HAL_RCC_GET_I2C2_SOURCE();
+        switch (clocksource) {
+            case RCC_I2C2CLKSOURCE_PCLK1:
+                pclk = HAL_RCC_GetPCLK1Freq();
+                break;
+
+            case RCC_I2C2CLKSOURCE_SYSCLK:
+                pclk = HAL_RCC_GetSysClockFreq();
+                break;
+
+            case RCC_I2C2CLKSOURCE_HSI:
+                pclk = HSI_VALUE;
+                break;
+        }
+    }
+#endif
+#if defined I2C3_BASE
+    else if (i2c == I2C_3) {
+        clocksource = __HAL_RCC_GET_I2C3_SOURCE();
+        switch (clocksource) {
+            case RCC_I2C3CLKSOURCE_PCLK1:
+                pclk = HAL_RCC_GetPCLK1Freq();
+                break;
+
+            case RCC_I2C3CLKSOURCE_SYSCLK:
+                pclk = HAL_RCC_GetSysClockFreq();
+                break;
+
+            case RCC_I2C3CLKSOURCE_HSI:
+                pclk = HSI_VALUE;
+                break;
+        }
+    }
+#endif
 #if defined I2C4_BASE
     else if (i2c == I2C_4) {
         clocksource = __HAL_RCC_GET_I2C4_SOURCE();
         switch (clocksource) {
-            case RCC_I2C4CLKSOURCE_D3PCLK1:
-                pclk = HAL_RCCEx_GetD3PCLK1Freq();
+            case RCC_I2C4CLKSOURCE_PCLK1:
+                pclk = HAL_RCC_GetPCLK1Freq();
                 break;
-            case RCC_I2C4CLKSOURCE_PLL3:
-                HAL_RCCEx_GetPLL3ClockFreq(&pll3_clocks);
-                pclk = pll3_clocks.PLL3_R_Frequency;
+
+            case RCC_I2C4CLKSOURCE_SYSCLK:
+                pclk = HAL_RCC_GetSysClockFreq();
                 break;
+
             case RCC_I2C4CLKSOURCE_HSI:
                 pclk = HSI_VALUE;
-                break;
-            case RCC_I2C4CLKSOURCE_CSI:
-                pclk = CSI_VALUE;
-                break;
-            default:
-                // should not happend
-                error("I2C4: Invalid clock source");
                 break;
         }
     }
@@ -83,13 +106,7 @@ uint32_t i2c_get_pclk(I2CName i2c)
     }
     return pclk;
 }
-/**
-  * @}
-  */
 
-/** @defgroup I2C_DEVICE_Exported_Functions I2C_DEVICE Exported Functions
-  * @{
-  */
 /**
  * @brief  Provide the suitable timing depending on requested frequency
  * @param  hz Required I2C clock in Hz.
@@ -123,5 +140,4 @@ uint32_t i2c_get_timing(I2CName i2c, int hz)
 /**
   * @}
   */
-
 #endif // DEVICE_I2C
