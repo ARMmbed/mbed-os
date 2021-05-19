ARMmbed/mbed-os

commit 957486e0ebb57522301a82ed73ec885892e08b74

BLE: Move traces out of header file to avoid collisions. This change required the creation of the implementation files of SecurityDb classes.

connectivity/FEATURE_BLE/source/generic/KVStoreSecurityDb.cpp
@@ -62,6 +62,14 @@ KVStoreSecurityDb::~KVStoreSecurityDb()
 {
 }
 
+KVStoreSecurityDb::entry_t *KVStoreSecurityDb::as_entry(entry_handle_t db_handle) {
+    entry_t* entry = reinterpret_cast<entry_t*>(db_handle);
+    if (!entry) {
+        tr_error("Invalid security DB handle used");
+    }
+    return entry;
+}
+
 bool KVStoreSecurityDb::open_db()
 {
     uint8_t version = 0;

connectivity/FEATURE_BLE/source/generic/KVStoreSecurityDb.h
@@ -25,12 +25,9 @@
 #include "mbed_error.h"
 
 #include "SecurityDb.h"
-#include "mbed-trace/mbed_trace.h"
 
 #define STR_EXPAND(tok) #tok
 #define STR(tok) STR_EXPAND(tok)
-
-#define TRACE_GROUP "BLDB"
 
 namespace ble {
 
@@ -65,14 +62,6 @@ private:
 
     static constexpr char DB_VERSION[DB_KEY_SIZE] = { 'v','e','r' };
     static constexpr char DB_RESTORE[DB_KEY_SIZE] = { 'r','e','s' };
-
-    static entry_t* as_entry(entry_handle_t db_handle) {
-        entry_t* entry = reinterpret_cast<entry_t*>(db_handle);
-        if (!entry) {
-            tr_error("Invalid security DB handle used");
-        }
-        return entry;
-    }
 
     template<class T>
     static void db_read(T *value, const char* key) {

connectivity/FEATURE_BLE/source/cordio/source/PalAttClientImpl.cpp
@@ -27,6 +27,8 @@
 #include "att_api.h"
 #include "att_defs.h"
 
+#include "mbed-trace/mbed_trace.h"
+
 #define TRACE_GROUP "BLAT"
 
 namespace ble {

connectivity/FEATURE_BLE/source/generic/FileSecurityDb.cpp
@@ -90,6 +90,14 @@ FileSecurityDb::~FileSecurityDb()
     fclose(_db_file);
 }
 
+FileSecurityDb::entry_t* FileSecurityDb::as_entry(entry_handle_t db_handle) {
+    entry_t* entry = reinterpret_cast<entry_t*>(db_handle);
+    if (!entry) {
+        tr_error("Invalid security DB handle used");
+    }
+    return entry;
+}
+
 FILE* FileSecurityDb::open_db_file(const char *db_path)
 {
     if (!db_path) {

connectivity/FEATURE_BLE/source/generic/FileSecurityDb.h
@@ -24,9 +24,6 @@
 #include <stdio.h>
 
 #include "SecurityDb.h"
-#include "mbed-trace/mbed_trace.h"
-
-#define TRACE_GROUP "BLDB"
 
 namespace ble {
 
@@ -40,13 +37,7 @@ private:
         size_t file_offset;
     };
 
-    static entry_t* as_entry(entry_handle_t db_handle) {
-        entry_t* entry = reinterpret_cast<entry_t*>(db_handle);
-        if (!entry) {
-            tr_error("Invalid security DB handle used");
-        }
-        return entry;
-    }
+    static entry_t* as_entry(entry_handle_t db_handle);
 
     template<class T>
     void db_read(T *value, long int offset) {

connectivity/FEATURE_BLE/source/generic/MemorySecurityDb.cpp
@@ -1,0 +1,196 @@
+/* mbed Microcontroller Library
+ * Copyright (c) 2021 ARM Limited
+ * SPDX-License-Identifier: Apache-2.0
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "MemorySecurityDb.h"
+
+#include "mbed-trace/mbed_trace.h"
+#include "common/ble_trace_helpers.h"
+
+#define TRACE_GROUP "BLDB"
+
+#define _STR(x) #x
+#define STR(x) _STR(x)
+
+
+namespace ble {
+
+MemorySecurityDb::entry_t *MemorySecurityDb::as_entry(entry_handle_t db_handle)
+{
+    entry_t* entry = reinterpret_cast<entry_t*>(db_handle);
+    if (!entry) {
+        tr_error("Invalid security DB handle used");
+    }
+    return entry;
+}
+
+
+MemorySecurityDb::MemorySecurityDb() : SecurityDb()
+{
+    tr_info("Using memory security DB (capacity " STR(BLE_SECURITY_DATABASE_MAX_ENTRIES) " entries) - no persistence across reset");
+}
+
+
+SecurityDistributionFlags_t *MemorySecurityDb::get_distribution_flags(
+    entry_handle_t db_handle
+) {
+    return reinterpret_cast<SecurityDistributionFlags_t*>(db_handle);
+}
+
+/* local keys */
+
+/* set */
+void MemorySecurityDb::set_entry_local_ltk(
+    entry_handle_t db_handle,
+    const ltk_t &ltk
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: local ltk %s", get_index(db_handle), to_string(ltk));
+        entry->flags.ltk_sent = true;
+        entry->local_keys.ltk = ltk;
+    }
+}
+
+void MemorySecurityDb::set_entry_local_ediv_rand(
+    entry_handle_t db_handle,
+    const ediv_t &ediv,
+    const rand_t &rand
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: local ediv %s rand %s", get_index(db_handle), to_string(ediv), to_string(rand));
+        entry->local_keys.ediv = ediv;
+        entry->local_keys.rand = rand;
+    }
+}
+
+void MemorySecurityDb::set_entry_peer_ltk(
+    entry_handle_t db_handle,
+    const ltk_t &ltk
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: peer ltk %s", get_index(db_handle), to_string(ltk));
+        entry->peer_keys.ltk = ltk;
+        entry->flags.ltk_stored = true;
+    }
+}
+
+void MemorySecurityDb::set_entry_peer_ediv_rand(
+    entry_handle_t db_handle,
+    const ediv_t &ediv,
+    const rand_t &rand
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: peer ediv %s rand %s", get_index(db_handle), to_string(ediv), to_string(rand));
+        entry->peer_keys.ediv = ediv;
+        entry->peer_keys.rand = rand;
+    }
+}
+
+void MemorySecurityDb::set_entry_peer_irk(
+    entry_handle_t db_handle,
+    const irk_t &irk
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: peer irk %s", get_index(db_handle), to_string(irk));
+        entry->peer_identity.irk = irk;
+        entry->flags.irk_stored = true;
+    }
+}
+
+void MemorySecurityDb::set_entry_peer_bdaddr(
+    entry_handle_t db_handle,
+    bool address_is_public,
+    const address_t &peer_address
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: %s peer address %s", get_index(db_handle), address_is_public? "public" : "private", to_string(peer_address));
+        entry->peer_identity.identity_address = peer_address;
+        entry->peer_identity.identity_address_is_public = address_is_public;
+    }
+}
+
+void MemorySecurityDb::set_entry_peer_csrk(
+    entry_handle_t db_handle,
+    const csrk_t &csrk
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: peer csrk %s", get_index(db_handle), to_string(csrk));
+        entry->flags.csrk_stored = true;
+        entry->peer_signing.csrk = csrk;
+    }
+}
+
+void MemorySecurityDb::set_entry_peer_sign_counter(
+    entry_handle_t db_handle,
+    sign_count_t sign_counter
+) {
+    entry_t *entry = as_entry(db_handle);
+    if (entry) {
+        tr_info("Write DB entry %d: sign counter %lu", get_index(db_handle), sign_counter);
+        entry->peer_signing.counter = sign_counter;
+    }
+}
+
+uint8_t MemorySecurityDb::get_entry_count() {
+    return BLE_SECURITY_DATABASE_MAX_ENTRIES;
+}
+
+SecurityDistributionFlags_t *MemorySecurityDb::get_entry_handle_by_index(uint8_t index) {
+    if (index < BLE_SECURITY_DATABASE_MAX_ENTRIES) {
+        return &_entries[index].flags;
+    } else {
+        return nullptr;
+    }
+}
+
+void MemorySecurityDb::reset_entry(entry_handle_t db_entry) {
+    auto *entry = reinterpret_cast<entry_t*>(db_entry);
+    *entry = entry_t();
+}
+
+SecurityEntryIdentity_t *MemorySecurityDb::read_in_entry_peer_identity(entry_handle_t db_entry) {
+    auto *entry = reinterpret_cast<entry_t*>(db_entry);
+    return &entry->peer_identity;
+}
+
+SecurityEntryKeys_t *MemorySecurityDb::read_in_entry_peer_keys(entry_handle_t db_entry) {
+    auto *entry = reinterpret_cast<entry_t*>(db_entry);
+    return &entry->peer_keys;
+}
+
+SecurityEntryKeys_t *MemorySecurityDb::read_in_entry_local_keys(entry_handle_t db_entry) {
+    auto *entry = reinterpret_cast<entry_t*>(db_entry);
+    return &entry->local_keys;
+}
+
+SecurityEntrySigning_t *MemorySecurityDb::read_in_entry_peer_signing(entry_handle_t db_entry) {
+    auto *entry = reinterpret_cast<entry_t*>(db_entry);
+    return &entry->peer_signing;
+}
+
+uint8_t MemorySecurityDb::get_index(const entry_handle_t db_handle) const
+{
+    return reinterpret_cast<entry_t*>(db_handle) - _entries;
+}
+
+} /* namespace ble */

connectivity/FEATURE_BLE/source/generic/MemorySecurityDb.h
@@ -21,15 +21,6 @@
 
 #include "SecurityDb.h"
 
-#include "mbed-trace/mbed_trace.h"
-#include "common/ble_trace_helpers.h"
-
-#define TRACE_GROUP "BLDB"
-
-#define _STR(x) #x
-#define STR(x) _STR(x)
-
-
 namespace ble {
 
 /** Naive memory implementation for verification. */
@@ -44,28 +35,16 @@ private:
         SecurityEntrySigning_t peer_signing;
     };
 
-    static entry_t* as_entry(entry_handle_t db_handle)
-    {
-        entry_t* entry = reinterpret_cast<entry_t*>(db_handle);
-        if (!entry) {
-            tr_error("Invalid security DB handle used");
-        }
-        return entry;
-    }
+    static entry_t *as_entry(entry_handle_t db_handle);
 
 public:
-    MemorySecurityDb() : SecurityDb()
-    {
-        tr_info("Using memory security DB (capacity " STR(BLE_SECURITY_DATABASE_MAX_ENTRIES) " entries) - no persistence across reset");
-    }
+    MemorySecurityDb();
 
     ~MemorySecurityDb() override = default;
 
-    SecurityDistributionFlags_t* get_distribution_flags(
+    SecurityDistributionFlags_t *get_distribution_flags(
         entry_handle_t db_handle
-    ) override {
-        return reinterpret_cast<SecurityDistributionFlags_t*>(db_handle);
-    }
+    ) override;
 
     /* local keys */
 
@@ -73,27 +52,13 @@ public:
     void set_entry_local_ltk(
         entry_handle_t db_handle,
         const ltk_t &ltk
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: local ltk %s", get_index(db_handle), to_string(ltk));
-            entry->flags.ltk_sent = true;
-            entry->local_keys.ltk = ltk;
-        }
-    }
+    ) override;
 
     void set_entry_local_ediv_rand(
         entry_handle_t db_handle,
         const ediv_t &ediv,
         const rand_t &rand
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: local ediv %s rand %s", get_index(db_handle), to_string(ediv), to_string(rand));
-            entry->local_keys.ediv = ediv;
-            entry->local_keys.rand = rand;
-        }
-    }
+    ) override;
 
     /* peer's keys */
 
@@ -102,118 +67,51 @@ public:
     void set_entry_peer_ltk(
         entry_handle_t db_handle,
         const ltk_t &ltk
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: peer ltk %s", get_index(db_handle), to_string(ltk));
-            entry->peer_keys.ltk = ltk;
-            entry->flags.ltk_stored = true;
-        }
-    }
+    ) override;
 
     void set_entry_peer_ediv_rand(
         entry_handle_t db_handle,
         const ediv_t &ediv,
         const rand_t &rand
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: peer ediv %s rand %s", get_index(db_handle), to_string(ediv), to_string(rand));
-            entry->peer_keys.ediv = ediv;
-            entry->peer_keys.rand = rand;
-        }
-    }
+    ) override;
 
     void set_entry_peer_irk(
         entry_handle_t db_handle,
         const irk_t &irk
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: peer irk %s", get_index(db_handle), to_string(irk));
-            entry->peer_identity.irk = irk;
-            entry->flags.irk_stored = true;
-        }
-    }
+    ) override;
 
     void set_entry_peer_bdaddr(
         entry_handle_t db_handle,
         bool address_is_public,
         const address_t &peer_address
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: %s peer address %s", get_index(db_handle), address_is_public? "public" : "private", to_string(peer_address));
-            entry->peer_identity.identity_address = peer_address;
-            entry->peer_identity.identity_address_is_public = address_is_public;
-        }
-    }
+    ) override;
 
     void set_entry_peer_csrk(
         entry_handle_t db_handle,
         const csrk_t &csrk
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: peer csrk %s", get_index(db_handle), to_string(csrk));
-            entry->flags.csrk_stored = true;
-            entry->peer_signing.csrk = csrk;
-        }
-    }
+    ) override;
 
     void set_entry_peer_sign_counter(
         entry_handle_t db_handle,
         sign_count_t sign_counter
-    ) override {
-        entry_t *entry = as_entry(db_handle);
-        if (entry) {
-            tr_info("Write DB entry %d: sign counter %lu", get_index(db_handle), sign_counter);
-            entry->peer_signing.counter = sign_counter;
-        }
-    }
+    ) override;
 
 private:
-    uint8_t get_entry_count() override {
-        return BLE_SECURITY_DATABASE_MAX_ENTRIES;
-    }
+    uint8_t get_entry_count() override;
 
-    SecurityDistributionFlags_t* get_entry_handle_by_index(uint8_t index) override {
-        if (index < BLE_SECURITY_DATABASE_MAX_ENTRIES) {
-            return &_entries[index].flags;
-        } else {
-            return nullptr;
-        }
-    }
+    SecurityDistributionFlags_t *get_entry_handle_by_index(uint8_t index) override;
 
-    void reset_entry(entry_handle_t db_entry) override{
-        auto *entry = reinterpret_cast<entry_t*>(db_entry);
-        *entry = entry_t();
-    }
+    void reset_entry(entry_handle_t db_entry) override;
 
-    SecurityEntryIdentity_t* read_in_entry_peer_identity(entry_handle_t db_entry) override {
-        auto *entry = reinterpret_cast<entry_t*>(db_entry);
-        return &entry->peer_identity;
-    };
+    SecurityEntryIdentity_t *read_in_entry_peer_identity(entry_handle_t db_entry) override;
 
-    SecurityEntryKeys_t* read_in_entry_peer_keys(entry_handle_t db_entry) override {
-        auto *entry = reinterpret_cast<entry_t*>(db_entry);
-        return &entry->peer_keys;
-    };
+    SecurityEntryKeys_t *read_in_entry_peer_keys(entry_handle_t db_entry) override;
 
-    SecurityEntryKeys_t* read_in_entry_local_keys(entry_handle_t db_entry) override {
-        auto *entry = reinterpret_cast<entry_t*>(db_entry);
-        return &entry->local_keys;
-    };
+    SecurityEntryKeys_t *read_in_entry_local_keys(entry_handle_t db_entry) override;
 
-    SecurityEntrySigning_t* read_in_entry_peer_signing(entry_handle_t db_entry) override {
-        auto *entry = reinterpret_cast<entry_t*>(db_entry);
-        return &entry->peer_signing;
-    };
+    SecurityEntrySigning_t *read_in_entry_peer_signing(entry_handle_t db_entry) override;
 
-    uint8_t get_index(const entry_handle_t db_handle) const
-    {
-        return reinterpret_cast<entry_t*>(db_handle) - _entries;
-    }
+    uint8_t get_index(const entry_handle_t db_handle) const;
 
 private:
     entry_t _entries[BLE_SECURITY_DATABASE_MAX_ENTRIES];

commit 2578c5356b4c233243aa344aa3ae35c0e971e146

Fix missing `as_entry` method in KVStoreSecurityDb
This commit introduces changes to fix a missing method, `as_entry`, and traces that were not updated properly to work with this new method.

Prior to this fix, it was not possible to use KVStoreSecurityDb as the build would fail since the `as_entry` method was not declared in the class header file.

This bug was introduced with commit 957486e0ebb57522301a82ed73ec885892e08b74 in PR ARMmbed/mbed-os#14198

connectivity/FEATURE_BLE/source/generic/KVStoreSecurityDb.cpp
@@ -158,7 +158,7 @@ void KVStoreSecurityDb::set_entry_local_ltk(
     SecurityEntryKeys_t* current_entry = read_in_entry_local_keys(db_handle);
     current_entry->ltk = ltk;
 
-    tr_info("Write DB entry %d: local ltk %s", get_index(db_handle), to_string(ltk));
+    tr_info("Write DB entry %d: local ltk %s", get_index(entry), to_string(ltk));
     db_write_entry(current_entry, DB_ENTRY_LOCAL_KEYS, get_index(entry));
 }
 
@@ -177,7 +177,7 @@ void KVStoreSecurityDb::set_entry_local_ediv_rand(
     current_entry->ediv = ediv;
     current_entry->rand = rand;
 
-    tr_info("Write DB entry %d: local ediv %s rand %s", get_index(db_handle), to_string(ediv), to_string(rand));
+    tr_info("Write DB entry %d: local ediv %s rand %s", get_index(entry), to_string(ediv), to_string(rand));
     db_write_entry(current_entry, DB_ENTRY_LOCAL_KEYS, get_index(entry));
 }
 
@@ -200,7 +200,7 @@ void KVStoreSecurityDb::set_entry_peer_ltk(
     SecurityEntryKeys_t* current_entry = read_in_entry_peer_keys(db_handle);
     current_entry->ltk = ltk;
 
-    tr_info("Write DB entry %d: peer ltk %s", get_index(db_handle), to_string(ltk));
+    tr_info("Write DB entry %d: peer ltk %s", get_index(entry), to_string(ltk));
     db_write_entry(current_entry, DB_ENTRY_PEER_KEYS, get_index(entry));
 }
 
@@ -219,7 +219,7 @@ void KVStoreSecurityDb::set_entry_peer_ediv_rand(
     current_entry->ediv = ediv;
     current_entry->rand = rand;
 
-    tr_info("Write DB entry %d: peer ediv %s rand %s", get_index(db_handle), to_string(ediv), to_string(rand));
+    tr_info("Write DB entry %d: peer ediv %s rand %s", get_index(entry), to_string(ediv), to_string(rand));
     db_write_entry(current_entry, DB_ENTRY_PEER_KEYS, get_index(entry));
 }
 
@@ -238,7 +238,7 @@ void KVStoreSecurityDb::set_entry_peer_irk(
     SecurityEntryIdentity_t* current_entry = read_in_entry_peer_identity(db_handle);
     current_entry->irk = irk;
 
-    tr_info("Write DB entry %d: peer irk %s", get_index(db_handle), to_string(irk));
+    tr_info("Write DB entry %d: peer irk %s", get_index(entry), to_string(irk));
     db_write_entry(current_entry, DB_ENTRY_PEER_IDENTITY, get_index(entry));
 }
 
@@ -257,7 +257,7 @@ void KVStoreSecurityDb::set_entry_peer_bdaddr(
     current_entry->identity_address = peer_address;
     current_entry->identity_address_is_public = address_is_public;
 
-    tr_info("Write DB entry %d: %s peer address %s", get_index(db_handle), address_is_public? "public" : "private", to_string(peer_address));
+    tr_info("Write DB entry %d: %s peer address %s", get_index(entry), address_is_public? "public" : "private", to_string(peer_address));
     db_write_entry(current_entry, DB_ENTRY_PEER_IDENTITY, get_index(entry));
 }
 
@@ -276,7 +276,7 @@ void KVStoreSecurityDb::set_entry_peer_csrk(
     SecurityEntrySigning_t* current_entry = read_in_entry_peer_signing(db_handle);
     current_entry->csrk = csrk;
 
-    tr_info("Write DB entry %d: peer csrk %s", get_index(db_handle), to_string(csrk));
+    tr_info("Write DB entry %d: peer csrk %s", get_index(entry), to_string(csrk));
     db_write_entry(current_entry, DB_ENTRY_PEER_SIGNING, get_index(entry));
 }
 

connectivity/FEATURE_BLE/source/generic/KVStoreSecurityDb.h
@@ -40,6 +40,8 @@ private:
         sign_count_t peer_sign_counter;
     };
 
+    static entry_t *as_entry(entry_handle_t db_handle);
+
     static constexpr uint8_t KVSTORESECURITYDB_VERSION = 1;
 
     static constexpr size_t DB_PREFIX_SIZE = 7 + sizeof (STR(MBED_CONF_STORAGE_DEFAULT_KV)) - 1;