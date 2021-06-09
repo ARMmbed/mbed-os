3d16954a12190dab6bff589088a18899d7d7a2da
Rajkumar Kanagaraj <rajkumar.kanagaraj@arm.com>
2021-06-09 02:50:43 -0700

    Refactor Mbed rtos host-tests to use standalone htrun greentea
    - Updated rtos host-tests to import from htrun

--- rtos/tests/TESTS/host_tests/timing_drift_auto.py
+++ rtos/tests/TESTS/host_tests/timing_drift_auto.py
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from mbed_host_tests import BaseHostTest
+from htrun import BaseHostTest
 import time
 
 
