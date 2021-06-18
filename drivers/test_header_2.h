51c5ff37ced8ca42c4f157888a4d87f9b196867e
Martin Kojtal <martin.kojtal@arm.com>
2021-06-18 14:59:00 +0100

    WIP: Testing 2 headers with 2 license text

---
+++ drivers/test_header_2.h
@@ -0,0 +1,22 @@
+/*
+ * \copyright
+ * Copyright (c) 2019, Cypress Semiconductor Corporation, All Rights Reserved
+ * SPDX-License-Identifier: LicenseRef-PBL
+ *
+ * This file and the related binary are licensed under the
+ * Permissive Binary License, Version 1.0 (the "License")
+ * you may not use these files except in compliance with the License.
+ *
+ * You may obtain a copy of the License here:
+ * LICENSE-permissive-binary-license-1.0.txt and at
+ * https://www.mbed.com/licenses/PBL-1.0
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef TEST_FILE_2_H
+#define TEST_FILE_2_H
+
+#endif
