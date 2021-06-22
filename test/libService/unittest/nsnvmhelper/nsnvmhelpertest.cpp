7d51801c764efdcd155d7f3f0ca4213a9946704b
Arto Kinnunen <arto.kinnunen@arm.com>
2021-06-22 12:29:14 +0300

    Squashed 'features/frameworks/nanostack-libservice/' changes from 260d80f42d..c343e4e2cc
    c343e4e2cc Merge pull request #99 from PelionIoT/remove_ns_trace
    70c411b0f8 Remove ns_trace.h
    60e99d2c02 Update copyright (#100)
    
    git-subtree-dir: features/frameworks/nanostack-libservice
    git-subtree-split: c343e4e2ccb1517881ac85140974f3eb8cd85d46

--- test/libService/unittest/nsnvmhelper/nsnvmhelpertest.cpp
+++ test/libService/unittest/nsnvmhelper/nsnvmhelpertest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016 ARM Limited. All rights reserved.
+ * Copyright (c) 2016, 2020, Pelion and affiliates.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
