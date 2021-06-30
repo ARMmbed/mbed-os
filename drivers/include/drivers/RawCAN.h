debfda67f269e0aa3703457a8adb028d690af178
Mohammed Mubeen <abdulmubeen.mohammed@hcl.com>
2021-06-30 17:40:32 +0530

    Updated the Licenses for RawCAN and aligned indentation

--- drivers/include/drivers/RawCAN.h
+++ drivers/include/drivers/RawCAN.h
@@ -1,18 +1,18 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2019 ARM Limited
- * SPDX-License-Identifier: Apache-2.0
+/*
+ *  Copyright (C) 2021, STMicroelectronics, All Rights Reserved
+ *  SPDX-License-Identifier: Apache-2.0
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may
+ *  not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 #ifndef RAWCAN_H
@@ -30,7 +30,7 @@
 
 namespace mbed {
 class RawCAN: public CAN {
-    public:
+public:
     RawCAN(PinName rd, PinName td);
 
     /** Initialize CAN interface and set the frequency
@@ -61,8 +61,8 @@ class RawCAN: public CAN {
      */
     int read(CANMessage &msg, int handle = 0);
     
-    };
-}    //namespace mbed
+};
+}
 
 #endif
 #endif //RAWCAN_H
