38ca4bdce40a61541b6863799874f5b38b821267
Martin Kojtal <martin.kojtal@arm.com>
2021-07-12 16:13:25 +0100

    semihosting: add compat header where is required
    ARMCC provides __semihost via compat header. As CMSIS 5.8.0 removed this compat header,
    we need to explicitly include it to fix definition missing error.

--- platform/source/LocalFileSystem.cpp
+++ platform/source/LocalFileSystem.cpp
@@ -14,6 +14,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#if defined(__ARMCC_VERSION)
+#include <arm_compat.h>
+#endif
+
 #include "platform/LocalFileSystem.h"
 
 #if DEVICE_LOCALFILESYSTEM
