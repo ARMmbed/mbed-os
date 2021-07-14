d147abc3e556c58e5e343d34b729bc2192e18bd3
mbedmain <mbedmain@mbedos-ci-vm.server.eu02.arm.com>
2021-07-14 11:11:35 +0100

    Update Mbed version block

--- platform/include/platform/mbed_version.h
+++ platform/include/platform/mbed_version.h
@@ -38,7 +38,7 @@
   *
   * @note 99 is default value for development version (master branch)
   */
-#define MBED_MINOR_VERSION 12
+#define MBED_MINOR_VERSION 13
 
 /** MBED_PATCH_VERSION
   * Mbed OS patch version
