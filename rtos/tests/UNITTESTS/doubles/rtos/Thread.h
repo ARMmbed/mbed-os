5fe0f8916b0d13aaea4ba82eb3ecf3dc340c308a
Lingkai Dong <lingkai.dong@arm.com>
2021-07-19 10:01:34 +0100

    Unit tests: Fix style in rtos Thread.h stubs

--- rtos/tests/UNITTESTS/doubles/rtos/Thread.h
+++ rtos/tests/UNITTESTS/doubles/rtos/Thread.h
@@ -48,11 +48,23 @@ public:
 
     osStatus start(mbed::Callback<void()> task);
 
-    osStatus join() {return 0;};
+    osStatus join()
+    {
+        return 0;
+    };
     osStatus terminate();
-    osStatus set_priority(osPriority priority){return 0;};
-    osPriority get_priority() const{return osPriorityNormal;};
-    uint32_t flags_set(uint32_t flags){return 0;};
+    osStatus set_priority(osPriority priority)
+    {
+        return 0;
+    };
+    osPriority get_priority() const
+    {
+        return osPriorityNormal;
+    };
+    uint32_t flags_set(uint32_t flags)
+    {
+        return 0;
+    };
 
     /** State of the Thread */
     enum State {
@@ -77,26 +89,33 @@ public:
         Deleted,            /**< The task has been deleted or not started */
     };
 
-    State get_state() const {
-      return Ready;
+    State get_state() const
+    {
+        return Ready;
     };
-    uint32_t stack_size() const {
-      return 0;
+    uint32_t stack_size() const
+    {
+        return 0;
     };
-    uint32_t free_stack() const {
-      return 0;
+    uint32_t free_stack() const
+    {
+        return 0;
     };
-    uint32_t used_stack() const {
-      return 0;
+    uint32_t used_stack() const
+    {
+        return 0;
     };
-    uint32_t max_stack() const {
-      return 0;
+    uint32_t max_stack() const
+    {
+        return 0;
     };
-    const char *get_name() const {
-      return "";
+    const char *get_name() const
+    {
+        return "";
     };
-    osThreadId_t get_id() const {
-      return 0;
+    osThreadId_t get_id() const
+    {
+        return 0;
     };
     virtual ~Thread();
 private:
