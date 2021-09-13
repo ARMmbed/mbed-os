77960d351b109e47d24a9ed9f8a48c5901384c71
Rajkumar Kanagaraj <rajkumar.kanagaraj@arm.com>
2021-09-13 13:22:44 +0100

    Greentea: Fix flash overflown issue in callback_big test
    The "Callback_big" greentea test has six test cases that require
    a minimum above 36kb ROM to build all test cases. LPC1114 target
    has only 32KB ROM memory, so the limit 6th test case to include
    or exclude based on the target minimum ROM size.
    
    Note:
    In LPC1114 target, callback_big test has ROM overflow build issue
    for both Mbed CLI1 and CLI2.

--- platform/tests/TESTS/mbed_functional/callback/main.cpp
+++ platform/tests/TESTS/mbed_functional/callback/main.cpp
@@ -20,6 +20,8 @@
 #include "utest.h"
 #include <mstd_functional>
 
+#define TEST_MIN_REQ_ROM_SIZE (36 * 1024)
+
 using namespace utest::v1;
 
 template <typename T>
@@ -884,8 +886,7 @@ Case cases[] = {
     Case("Testing callbacks with 2 uint64s", test_dispatch2<uint64_t>),
     Case("Testing callbacks with 3 uint64s", test_dispatch3<uint64_t>),
     Case("Testing callbacks with 4 uint64s", test_dispatch4<uint64_t>),
-// IAR currently crashes at link time with this test - skip it as it's well beyond anything needed by real code
-#ifndef __ICCARM__
+#if !defined(__ICCARM__) && (!defined(MBED_ROM_SIZE) || (MBED_ROM_SIZE >= TEST_MIN_REQ_ROM_SIZE))
     Case("Testing callbacks with 5 uint64s", test_dispatch5<uint64_t>),
 #endif
 #elif DO_SMALL_TEST
