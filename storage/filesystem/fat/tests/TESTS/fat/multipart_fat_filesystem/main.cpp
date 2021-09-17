5b4e80c73cf77f805bcf1709bde09ce8a5b7702f
Hari Limaye <hari.limaye@arm.com>
2021-09-17 23:34:08 +0100

    Greentea: Refactor FAT Filesystem test cases to be independent
    In the Greentea test suite `multipart_fat_filesystem`, all the test
    cases are dependent on setup that occurs in the first case. This case
    formats (with FAT) a HeapBlockDevice that is shared between cases.
    Alongside this, the first case checks that sufficient heap memory is
    available for the dynamic memory allocation required for the FAT format
    process. This check is performed by testing if we can successfully
    allocate the required number of bytes, and skipping the case if not. The
    check is then propagated to the other cases by skipping those if the
    HeapBlockDevice pointer is falsy, which it will be if the first test is
    skipped.
    
    We refactor the cases to be independent of one another. Now each test
    case checks that sufficient memory can be allocated for FAT formatting,
    performing said formatting or skipping as indicated by this check.
    
    In doing so we replace remove some raw pointers, and replace some C
    style arrays with std::array, in order to make memory management less
    error prone.

--- storage/filesystem/fat/tests/TESTS/fat/multipart_fat_filesystem/main.cpp
+++ storage/filesystem/fat/tests/TESTS/fat/multipart_fat_filesystem/main.cpp
@@ -18,6 +18,7 @@
 #include "greentea-client/test_env.h"
 #include "unity.h"
 #include "utest.h"
+#include <memory>
 
 #include "HeapBlockDevice.h"
 #include "FATFileSystem.h"
@@ -32,36 +33,58 @@ using namespace utest::v1;
 #error [NOT_SUPPORTED] Filesystem tests not supported by default
 #else
 
-static const int mem_alloc_threshold = 32 * 1024;
+static constexpr int mem_alloc_threshold = 32 * 1024;
+static constexpr int BLOCK_SIZE = 512;
+static constexpr int BLOCK_COUNT = 512;
+static constexpr uint8_t LINUX_FS_PARTITION_ID = 0x83;
 
-// Test block device
-#define BLOCK_SIZE 512
-#define BLOCK_COUNT 512
-HeapBlockDevice *bd = 0;
+bool check_heap_allocation(size_t bytes)
+{
+    unique_ptr<uint8_t[]> dummy {new (std::nothrow) uint8_t[bytes]};
+    return dummy.get();
+}
+
+void format_partitions(HeapBlockDevice &bd)
+{
+    // Create two partitions splitting device in ~half
+    TEST_ASSERT_EQUAL(0, MBRBlockDevice::partition(&bd, 1, LINUX_FS_PARTITION_ID, 0, (BLOCK_COUNT / 2) * BLOCK_SIZE));
+    TEST_ASSERT_EQUAL(0, MBRBlockDevice::partition(&bd, 2, LINUX_FS_PARTITION_ID, -(BLOCK_COUNT / 2) * BLOCK_SIZE));
+
+    // Load both partitions
+    MBRBlockDevice part1(&bd, 1);
+    MBRBlockDevice part2(&bd, 2);
+    TEST_ASSERT_EQUAL(0, part1.init());
+    TEST_ASSERT_EQUAL(0, part2.init());
+
+    // Format both partitions
+    TEST_ASSERT_EQUAL(0, FATFileSystem::format(&part1));
+    TEST_ASSERT_EQUAL(0, FATFileSystem::format(&part2));
+
+    // Unload the partitions
+    TEST_ASSERT_EQUAL(0, part1.deinit());
+    TEST_ASSERT_EQUAL(0, part2.deinit());
+}
 
 // Test formatting and partitioning
 void test_format()
 {
-    uint8_t *dummy = new (std::nothrow) uint8_t[mem_alloc_threshold];
-    TEST_SKIP_UNLESS_MESSAGE(dummy, "Not enough heap memory to run test. Test skipped.");
-    delete[] dummy;
+    TEST_SKIP_UNLESS_MESSAGE(check_heap_allocation(mem_alloc_threshold), "Not enough heap memory to run test. Test skipped.");
 
-    bd = new (std::nothrow) HeapBlockDevice(BLOCK_COUNT * BLOCK_SIZE, BLOCK_SIZE);
-    TEST_SKIP_UNLESS_MESSAGE(bd, "Not enough heap memory to run test. Test skipped.");
+    HeapBlockDevice bd {BLOCK_COUNT * BLOCK_SIZE, BLOCK_SIZE};
 
     // Create two partitions splitting device in ~half
-    int err = MBRBlockDevice::partition(bd, 1, 0x83, 0, (BLOCK_COUNT / 2) * BLOCK_SIZE);
+    int err = MBRBlockDevice::partition(&bd, 1, LINUX_FS_PARTITION_ID, 0, (BLOCK_COUNT / 2) * BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
 
-    err = MBRBlockDevice::partition(bd, 2, 0x83, -(BLOCK_COUNT / 2) * BLOCK_SIZE);
+    err = MBRBlockDevice::partition(&bd, 2, LINUX_FS_PARTITION_ID, -(BLOCK_COUNT / 2) * BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
 
     // Load both partitions
-    MBRBlockDevice part1(bd, 1);
+    MBRBlockDevice part1(&bd, 1);
     err = part1.init();
     TEST_ASSERT_EQUAL(0, err);
 
-    MBRBlockDevice part2(bd, 2);
+    MBRBlockDevice part2(&bd, 2);
     err = part2.init();
     TEST_ASSERT_EQUAL(0, err);
 
@@ -85,14 +108,18 @@ void test_format()
 template <ssize_t TEST_SIZE>
 void test_read_write()
 {
-    TEST_SKIP_UNLESS_MESSAGE(bd, "Not enough heap memory to run test. Test skipped.");
+    TEST_SKIP_UNLESS_MESSAGE(check_heap_allocation(mem_alloc_threshold), "Not enough heap memory to run test. Test skipped.");
+
+    HeapBlockDevice bd {BLOCK_COUNT * BLOCK_SIZE, BLOCK_SIZE};
+
+    format_partitions(bd);
 
     // Load both partitions
-    MBRBlockDevice part1(bd, 1);
+    MBRBlockDevice part1(&bd, 1);
     int err = part1.init();
     TEST_ASSERT_EQUAL(0, err);
 
-    MBRBlockDevice part2(bd, 2);
+    MBRBlockDevice part2(&bd, 2);
     err = part2.init();
     TEST_ASSERT_EQUAL(0, err);
 
@@ -106,11 +133,8 @@ void test_read_write()
     err = fs2.mount(&part2);
     TEST_ASSERT_EQUAL(0, err);
 
-    uint8_t *buffer1 = new (std::nothrow) uint8_t[TEST_SIZE];
-    TEST_SKIP_UNLESS_MESSAGE(buffer1, "Not enough heap memory to run test. Test skipped.");
-
-    uint8_t *buffer2 = new (std::nothrow) uint8_t[TEST_SIZE];
-    TEST_SKIP_UNLESS_MESSAGE(buffer2, "Not enough heap memory to run test. Test skipped.");
+    std::array<uint8_t, TEST_SIZE> buffer1 { };
+    std::array<uint8_t, TEST_SIZE> buffer2 { };
 
     // Fill with random sequence
     srand(1);
@@ -127,28 +151,28 @@ void test_read_write()
     File file;
     err = file.open(&fs1, "test_read_write.dat", O_WRONLY | O_CREAT);
     TEST_ASSERT_EQUAL(0, err);
-    ssize_t size = file.write(buffer1, TEST_SIZE);
+    ssize_t size = file.write(buffer1.data(), TEST_SIZE);
     TEST_ASSERT_EQUAL(TEST_SIZE, size);
     err = file.close();
     TEST_ASSERT_EQUAL(0, err);
 
     err = file.open(&fs2, "test_read_write.dat", O_WRONLY | O_CREAT);
     TEST_ASSERT_EQUAL(0, err);
-    size = file.write(buffer2, TEST_SIZE);
+    size = file.write(buffer2.data(), TEST_SIZE);
     TEST_ASSERT_EQUAL(TEST_SIZE, size);
     err = file.close();
     TEST_ASSERT_EQUAL(0, err);
 
     err = file.open(&fs1, "test_read_write.dat", O_RDONLY);
     TEST_ASSERT_EQUAL(0, err);
-    size = file.read(buffer1, TEST_SIZE);
+    size = file.read(buffer1.data(), TEST_SIZE);
     TEST_ASSERT_EQUAL(TEST_SIZE, size);
     err = file.close();
     TEST_ASSERT_EQUAL(0, err);
 
     err = file.open(&fs2, "test_read_write.dat", O_RDONLY);
     TEST_ASSERT_EQUAL(0, err);
-    size = file.read(buffer2, TEST_SIZE);
+    size = file.read(buffer2.data(), TEST_SIZE);
     TEST_ASSERT_EQUAL(TEST_SIZE, size);
     err = file.close();
     TEST_ASSERT_EQUAL(0, err);
@@ -175,59 +199,55 @@ void test_read_write()
 
     err = part2.deinit();
     TEST_ASSERT_EQUAL(0, err);
-
-    delete[] buffer1;
-    delete[] buffer2;
 }
 
 void test_single_mbr()
 {
-    TEST_SKIP_UNLESS_MESSAGE(bd, "Not enough heap memory to run test. Test skipped.");
+    TEST_SKIP_UNLESS_MESSAGE(check_heap_allocation(mem_alloc_threshold), "Not enough heap memory to run test. Test skipped.");
 
-    int err = bd->init();
+    HeapBlockDevice bd {BLOCK_COUNT * BLOCK_SIZE, BLOCK_SIZE};
+
+    format_partitions(bd);
+
+    int err = bd.init();
     TEST_ASSERT_EQUAL(0, err);
 
     const bd_addr_t MBR_OFFSET = 0;
     const bd_addr_t FAT1_OFFSET = 1;
     const bd_addr_t FAT2_OFFSET = BLOCK_COUNT / 2;
 
-    uint8_t *buffer = new (std::nothrow) uint8_t[BLOCK_SIZE];
-    TEST_SKIP_UNLESS_MESSAGE(buffer, "Not enough heap memory to run test. Test skipped.");
+    std::array<uint8_t, BLOCK_SIZE> buffer { };
 
     // Check that all three header blocks have the 0x55aa signature
-    err = bd->read(buffer, MBR_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
+    err = bd.read(buffer.data(), MBR_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT(memcmp(&buffer[BLOCK_SIZE - 2], "\x55\xaa", 2) == 0);
 
-    err = bd->read(buffer, FAT1_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
+    err = bd.read(buffer.data(), FAT1_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT(memcmp(&buffer[BLOCK_SIZE - 2], "\x55\xaa", 2) == 0);
 
-    err = bd->read(buffer, FAT2_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
+    err = bd.read(buffer.data(), FAT2_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT(memcmp(&buffer[BLOCK_SIZE - 2], "\x55\xaa", 2) == 0);
 
     // Check that the headers for both filesystems contain a jump code
     // indicating they are actual FAT superblocks and not an extra MBR
-    err = bd->read(buffer, FAT1_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
+    err = bd.read(buffer.data(), FAT1_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT(buffer[0] == 0xe9 || buffer[0] == 0xeb || buffer[0] == 0xe8);
 
-    err = bd->read(buffer, FAT2_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
+    err = bd.read(buffer.data(), FAT2_OFFSET * BLOCK_SIZE, BLOCK_SIZE);
     TEST_ASSERT_EQUAL(0, err);
     TEST_ASSERT(buffer[0] == 0xe9 || buffer[0] == 0xeb || buffer[0] == 0xe8);
 
-    delete[] buffer;
-
-    bd->deinit();
+    bd.deinit();
     TEST_ASSERT_EQUAL(0, err);
-
-    delete bd;
 }
 
 void test_with_other_fs()
 {
-    TEST_SKIP_UNLESS_MESSAGE(bd, "Not enough heap memory to run test. Test skipped.");
+    TEST_SKIP_UNLESS_MESSAGE(check_heap_allocation(mem_alloc_threshold), "Not enough heap memory to run test. Test skipped.");
 
     // Stage 0 - LittleFS
     // Stage 1 - FatFS with MBR
@@ -235,9 +255,9 @@ void test_with_other_fs()
     // Make sure that at no stage we are able to mount the current file system after using the
     // previous one
 
-    // start from scratch in this test
-    bd = new (std::nothrow) HeapBlockDevice(BLOCK_COUNT * BLOCK_SIZE, BLOCK_SIZE);
-    TEST_SKIP_UNLESS_MESSAGE(bd, "Not enough heap memory to run test. Test skipped.");
+    HeapBlockDevice bd {BLOCK_COUNT * BLOCK_SIZE, BLOCK_SIZE};
+
+    format_partitions(bd);
 
     int err;
 
@@ -248,10 +268,10 @@ void test_with_other_fs()
 
         if (stage == 1) {
             printf("Stage %d: FAT FS\n", stage + 1);
-            err = MBRBlockDevice::partition(bd, 1, 0x83, 0, BLOCK_COUNT * BLOCK_SIZE);
+            err = MBRBlockDevice::partition(&bd, 1, LINUX_FS_PARTITION_ID, 0, BLOCK_COUNT * BLOCK_SIZE);
             TEST_ASSERT_EQUAL(0, err);
 
-            part = new (std::nothrow) MBRBlockDevice(bd, 1);
+            part = new (std::nothrow) MBRBlockDevice(&bd, 1);
             TEST_SKIP_UNLESS_MESSAGE(part, "Not enough heap memory to run test. Test skipped.");
 
             err = part->init();
@@ -260,7 +280,7 @@ void test_with_other_fs()
             fs = new FATFileSystem("fat");
         } else {
             printf("Stage %d: Little FS\n", stage + 1);
-            part = bd;
+            part = &bd;
             fs = new LittleFileSystem("lfs");
         }
         TEST_SKIP_UNLESS_MESSAGE(fs, "Not enough heap memory to run test. Test skipped.");
@@ -279,9 +299,6 @@ void test_with_other_fs()
             delete part;
         }
     }
-
-    delete bd;
-    bd = 0;
 }
 
 // Test setup
