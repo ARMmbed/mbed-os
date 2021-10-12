7f15a2802e482256b2555b2a130d496d585c29b5
Jerome Coutant <jerome.coutant@st.com>
2021-10-12 17:46:00 +0200

    STM32F2 remove non-UTF characters

--- targets/TARGET_STM/TARGET_STM32F2/STM32Cube_FW/STM32F2xx_HAL_Driver/stm32f2xx_hal_wwdg.c
+++ targets/TARGET_STM/TARGET_STM32F2/STM32Cube_FW/STM32F2xx_HAL_Driver/stm32f2xx_hal_wwdg.c
@@ -40,7 +40,7 @@
         (++) max time (mS) = 1000 * (Counter - 0x40) / WWDG clock
     (+) Typical values:
         (++) Counter min (T[5;0] = 0x00) at 30MHz (PCLK1) with zero prescaler:
-             max timeout before reset: approximately 136.53µs
+             max timeout before reset: approximately 136.53us
         (++) Counter max (T[5;0] = 0x3F) at 30MHz (PCLK1) with prescaler
              dividing by 8:
              max timeout before reset: approximately 69.91ms
