94833a0ba6097307226f74aa3e91e54efc1f9486
wally0258 <68685645+wally0258@users.noreply.github.com>
2022-01-25 09:05:31 +0800

    Update can_api.c
    Update Copyright

--- targets/TARGET_NUVOTON/TARGET_M261/can_api.c
+++ targets/TARGET_NUVOTON/TARGET_M261/can_api.c
@@ -1,5 +1,5 @@
 /* mbed Microcontroller Library
- * Copyright (c) 2015-2016 Nuvoton
+ * Copyright (c) 2015-2022 Nuvoton
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
