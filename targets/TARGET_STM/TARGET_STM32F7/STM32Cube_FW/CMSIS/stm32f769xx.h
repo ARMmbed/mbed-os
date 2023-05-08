30ef52bfe6f2a6ba9c2ccff3ff47c7a36872902b
Jookia <contact@jookia.org>
2023-05-08 10:29:32 +1000

    STM32F7: Enable QSPI workaround for STM32F769xx
    On the STM32769NI at least this patch is required for stable QSPI use.
    Enable it uncondtionally in case other boards need it too.
    
    Further discussions:
    
    https://github.com/ARMmbed/mbed-os/issues/10049
    https://github.com/ARMmbed/mbed-os/issues/15108
    
    https://github.com/STMicroelectronics/STM32CubeF7/issues/52
    https://github.com/STMicroelectronics/STM32CubeF7/issues/82

--- targets/TARGET_STM/TARGET_STM32F7/STM32Cube_FW/CMSIS/stm32f769xx.h
+++ targets/TARGET_STM/TARGET_STM32F7/STM32Cube_FW/CMSIS/stm32f769xx.h
@@ -10615,6 +10615,8 @@ typedef struct
 /*                                    QUADSPI                                 */
 /*                                                                            */
 /******************************************************************************/
+/* QUADSPI IP version */
+#define QSPI1_V1_0
 /*****************  Bit definition for QUADSPI_CR register  *******************/
 #define QUADSPI_CR_EN_Pos                (0U)
 #define QUADSPI_CR_EN_Msk                (0x1UL << QUADSPI_CR_EN_Pos)           /*!< 0x00000001 */
