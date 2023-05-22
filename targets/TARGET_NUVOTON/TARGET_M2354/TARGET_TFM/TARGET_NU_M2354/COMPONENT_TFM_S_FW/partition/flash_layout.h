8b69a94cef395ca8b8ba2a9c0ff2b274250ee5e7
Chun-Chieh Li <ccli8@nuvoton.com>
2023-05-22 16:48:03 +0800

    M2354: Fix debug failure in Mbed Studio
    In Mbed Studio, debugging, based on pyOCD, requires Mbed OS application code starting on the sector boundary.
    
    Modification list:
    1.  Update TF-M import assets with MCUboot header padding to sector aligned
    2.  Following above, change header size argument (-H) in wrapper.py command line
    3.  Following below, fix min-write-size (--align) to 4 (per flash_area_align()) in wrapper.py command line
        https://docs.mcuboot.com/design.html#image-trailer
    
    Related issue:
    https://github.com/ARMmbed/mbed-os/issues/15417

--- targets/TARGET_NUVOTON/TARGET_M2354/TARGET_TFM/TARGET_NU_M2354/COMPONENT_TFM_S_FW/partition/flash_layout.h
+++ targets/TARGET_NUVOTON/TARGET_M2354/TARGET_TFM/TARGET_NU_M2354/COMPONENT_TFM_S_FW/partition/flash_layout.h
@@ -2,6 +2,8 @@
  * Copyright (c) 2017-2020 Arm Limited. All rights reserved.
  * Copyright (c) 2020 Nuvoton Technology Corp. All rights reserved.
  *
+ * SPDX-License-Identifier: Apache-2.0
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
