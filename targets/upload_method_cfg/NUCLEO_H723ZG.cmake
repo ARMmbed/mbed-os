7edab2d1ce877a3a8b96a2a924dd68de003df982
Jamie Smith <smit109@usc.edu>
2024-01-17 01:22:09 -0800

    Add upload methods for NUCLEO_H723ZG (#209)
    * Add upload methods for NUCLEO_H723ZG
    
    * Oops fix comment
    
    * Also fix another comment

--- targets/upload_method_cfg/NUCLEO_H723ZG.cmake
+++ targets/upload_method_cfg/NUCLEO_H723ZG.cmake
@@ -1,28 +1,27 @@
-# Mbed OS upload method configuration file for target WIO_H725AE
+# Mbed OS upload method configuration file for target NUCLEO_H723ZG.
 # To change any of these parameters from their default values, set them in your build script between where you
 # include app.cmake and where you add mbed os as a subdirectory.
 
 # Notes:
-# 1. Keep in mind the WIO_H725AE does not contain any on-board debugger, so all upload methods counts with external debugger, usually ST-Link.
-# 2. Using the JLINK upload method with your dev board requires converting its ST-LINK (versions lower than 3) into a J-Link.  See here for details: https://www.segger.com/products/debug-probes/j-link/models/other-j-links/st-link-on-board/
-# 3. PyOCD reports error "No cores were discovered!" 
-# 4. OpenOCD tested with ST-Link V2-1
+# 1. To use this target with PyOCD, you need to install a pack: `pyocd pack install STM32H723ZGTx`.
+#    You might also need to run `pyocd pack update` first.
+# 2. Latest pyocd did not work with this target for me.  Got 'Error while running debug sequence 'DebugDeviceUnlock''.
 
 # General config parameters
 # -------------------------------------------------------------
-set(UPLOAD_METHOD_DEFAULT STM32CUBE)
+set(UPLOAD_METHOD_DEFAULT MBED)
 
 # Config options for MBED
 # -------------------------------------------------------------
 
-set(MBED_UPLOAD_ENABLED FALSE)
+set(MBED_UPLOAD_ENABLED TRUE)
 set(MBED_RESET_BAUDRATE 115200)
 
 # Config options for JLINK
 # -------------------------------------------------------------
 
-set(JLINK_UPLOAD_ENABLED TRUE)
-set(JLINK_CPU_NAME STM32H725AE)
+set(JLINK_UPLOAD_ENABLED FALSE)
+set(JLINK_CPU_NAME STM32H723ZG)
 set(JLINK_CLOCK_SPEED 4000)
 set(JLINK_UPLOAD_INTERFACE SWD)
 
@@ -30,7 +29,7 @@ set(JLINK_UPLOAD_INTERFACE SWD)
 # -------------------------------------------------------------
 
 set(PYOCD_UPLOAD_ENABLED FALSE)
-set(PYOCD_TARGET_NAME STM32H725AEIx)
+set(PYOCD_TARGET_NAME STM32H723ZGTx)
 set(PYOCD_CLOCK_SPEED 4000k)
 
 # Config options for OPENOCD
@@ -38,7 +37,7 @@ set(PYOCD_CLOCK_SPEED 4000k)
 
 set(OPENOCD_UPLOAD_ENABLED TRUE)
 set(OPENOCD_CHIP_CONFIG_COMMANDS
-    -f ${CMAKE_CURRENT_LIST_DIR}/openocd_cfgs/stm32h72x.cfg)
+    -f ${CMAKE_CURRENT_LIST_DIR}/openocd_cfgs/nucleo_h72x.cfg)
 
 # Config options for STM32Cube
 # -------------------------------------------------------------
